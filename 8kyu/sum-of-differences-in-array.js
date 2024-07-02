d94afa697d0011da3a997f40be83b052df5e3fb0
Irene Volodko <ice7aye@gmail.com>
2024-07-02 22:45:50 +0300

    add Sum of differences in array

---
+++ 8kyu/sum-of-differences-in-array.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+//
+// EXAMPLE:
+// Input = [2, 1, 10] Output = 9
+//
+// SOLUTION:
+function sumOfDifferences(arr) {
+    let reversed = arr.sort((a, b) => b - a)
+    let result = []
+    for (let i = 0; i < reversed.length - 1; i++) {
+        result.push(reversed[i] - reversed[i + 1])
+    }
+    return arr.length > 1 ? result.reduce((sum, current) => sum + current) : 0
+}
