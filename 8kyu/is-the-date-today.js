44f357818b8d10b02941d18b767403217cf20bed
Irene Volodko <ice7aye@gmail.com>
2024-07-02 22:30:43 +0300

    add Is the date today

--- 8kyu/is-the-date-today.js
+++ 8kyu/is-the-date-today.js
@@ -1,9 +1,9 @@
-// DESCRIPTION:
+// ❓ DESCRIPTION:
 // Write a simple function that takes as a parameter a date object and returns a boolean value representing
 // whether the date is today or not.
 // Make sure that your function does not return a false positive by only checking the day.
 
-// SOLUTION:
+// ✅ SOLUTION:
 function isToday(date) {
     let today = new Date()
     return today.toDateString() === date.toDateString()
