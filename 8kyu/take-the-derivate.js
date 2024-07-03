97f5c618afe7e77c36e4dfdb34954ff107fdb138
Irene Volodko <ice7aye@gmail.com>
2024-07-03 20:42:50 +0300

    add take the derivate

---
+++ 8kyu/take-the-derivate.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// This function takes two numbers as parameters,
+// the first number being the coefficient, and the second number being the exponent.
+// Your function should multiply the two numbers, and then subtract 1 from the exponent.
+// Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+// The output of this function should be a string
+//
+// EXAMPLE:
+// Input: (7, 8) --> Output: "56x^7"
+// Input: (5, 9) --> Output: "45x^8"
+
+function derive(coefficient,exponent) {
+    return `${coefficient * exponent}x^${exponent - 1}`
+}
+
+console.log('Output: ' ,derive(7,8))
+console.log('Output: ' ,derive(5, 9))
