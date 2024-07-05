1946714118f2c5da0ad80acc6f3706412b138804
Irene Volodko <ice7aye@gmail.com>
2024-07-05 20:48:43 +0300

    maximum multiple - test

--- 7kyu/maximum-multiple.js
+++ 7kyu/maximum-multiple.js
@@ -1,5 +1,8 @@
+
 function maxMultiple(divisor, bound){
     for (let i = bound; i <= bound; i--) {
         if ( i % divisor === 0 ) { return i}
     }
 }
+console.log('Output: ' , maxMultiple(2,7))
+console.log('Output: ' , maxMultiple(3,10))
