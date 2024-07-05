36280b7a4cb1e08486b2d704b075c2277565280d
Irene Volodko <ice7aye@gmail.com>
2024-07-05 22:03:57 +0300

    fix string case

--- 7kyu/fix-string-case.js
+++ 7kyu/fix-string-case.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// you will be given a string that may have mixed uppercase and lowercase letters
+// and your task is to convert that string to either lowercase only or uppercase only based on.
+// if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+//
+// ✅ SOLUTION:
 function solve(s){
     let lower = 0
     let upper = 0
