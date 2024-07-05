37d665bc9b99d39abd6bfde841f6a4c947636908
Irene Volodko <ice7aye@gmail.com>
2024-07-05 22:19:10 +0300

    add maximum length difference

---
+++ 7kyu/maximum-length-difference.js
@@ -0,0 +1,10 @@
+function mxdiflg(a1, a2) {
+    if (a1.length === 0 || a2.length === 0) return -1
+    let result = []
+    for (let i = 0; i < a1.length; i++) {
+        for (let j = 0; j < a2.length; j++) {
+            result.push(Math.abs(a1[i].length - a2[j].length))
+        }
+    }
+    return Math.max(...result, 0)
+}
