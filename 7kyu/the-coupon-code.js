e0de90bb275b8ea6ccbf0f19d66d0b76ff4b2025
Irene Volodko <ice7aye@gmail.com>
2024-07-05 21:25:14 +0300

    the coupon code

--- 7kyu/the-coupon-code.js
+++ 7kyu/the-coupon-code.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+// A coupon is no more valid on the day AFTER the expiration date.
+// All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+//
+// ✅ SOLUTION:
+
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
     return Date.parse(expirationDate) >= Date.parse(currentDate) && enteredCode === correctCode
 }
