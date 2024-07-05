185614d4f6cc274352a2fd508034521df79fae74
Irene Volodko <ice7aye@gmail.com>
2024-07-05 22:21:03 +0300

    maximum length difference

--- 7kyu/maximum-length-difference.js
+++ 7kyu/maximum-length-difference.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z.
+// Let x be any string in the first array and y be any string in the second array.
+// Find max(abs(length(x) − length(y)))
+// If a1 and/or a2 are empty return -1.
+//
+// ✅ SOLUTION:
 function mxdiflg( a1 , a2 ) {
     if (a1.length === 0 || a2.length === 0) return -1
     let result = []
