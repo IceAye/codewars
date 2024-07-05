5335ac0c0999c2825b85e3d5fabdb69572aaf684
Irene Volodko <ice7aye@gmail.com>
2024-07-05 21:24:02 +0300

    test the coupon code

--- 7kyu/the-coupon-code.js
+++ 7kyu/the-coupon-code.js
@@ -1,3 +1,5 @@
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
     return Date.parse(expirationDate) >= Date.parse(currentDate) && enteredCode === correctCode
 }
+console.log('Output: ' , checkCoupon('123','123','September 5, 2014','October 1, 2014'))
+console.log('Output: ' , checkCoupon('123a','123','September 5, 2014','October 1, 2014'))
