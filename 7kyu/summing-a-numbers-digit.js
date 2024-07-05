f2e144374f4d519bd6b43bc47c96bafca6f9bcf9
Irene Volodko <ice7aye@gmail.com>
2024-07-05 20:35:53 +0300

    summing a number's digits - test

--- 7kyu/summing-a-numbers-digit.js
+++ 7kyu/summing-a-numbers-digit.js
@@ -1,3 +1,6 @@
 function sumDigits(number) {
     return Math.abs(number).toString().split('').reduce((sum, current) => +sum + +current, 0)
 }
+console.log('Output: ' , sumDigits(0))
+console.log('Output: ' , sumDigits(99))
+console.log('Output: ' , sumDigits(-32))
