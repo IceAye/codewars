23b367904c832d0cdb78fd80fdb8446d42a4a217
Irene Volodko <ice7aye@gmail.com>
2024-07-06 22:49:40 +0300

    regex count lowercase letter

--- 8kyu/regex-count-lowercase-letters.js
+++ 8kyu/regex-count-lowercase-letters.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Your task is simply to count the total number of lowercase letters in a string.
+//
+// ✅ SOLUTION:
 function lowercaseCount(str){
     return (str.match(/[a-z]/g) || []).length
 }
