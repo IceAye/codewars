f14d4140029d0a4c72c3175653d20dfc1f6cdd0a
Irene Volodko <ice7aye@gmail.com>
2024-07-06 22:19:41 +0300

    fix days

--- 8kyu/return-the-day.js
+++ 8kyu/return-the-day.js
@@ -1,11 +1,11 @@
 const days = {
-    "1": "Sunday",
-    "2": "Monday",
-    "3": "Tuesday",
-    "4": "Wednesday",
-    "5": "Thursday",
-    "6": "Friday",
-    "7": "Saturday"
+    1: "Sunday",
+    2: "Monday",
+    3: "Tuesday",
+    4: "Wednesday",
+    5: "Thursday",
+    6: "Friday",
+    7: "Saturday"
 }
 function whatday( num ) {
     return days[num] || "Wrong, please enter a number between 1 and 7"
