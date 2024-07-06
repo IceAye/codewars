fae72b6deafaed70c46259cde7a6a0fe62a2421a
Irene Volodko <ice7aye@gmail.com>
2024-07-06 22:48:44 +0300

    test regex count lowercase letter

--- 8kyu/regex-count-lowercase-letters.js
+++ 8kyu/regex-count-lowercase-letters.js
@@ -1,3 +1,7 @@
 function lowercaseCount(str){
     return (str.match(/[a-z]/g) || []).length
 }
+
+console.log('Output: ' , lowercaseCount("abcABC123"))
+console.log('Output: ' , lowercaseCount(""))
+console.log('Output: ' , lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"))
