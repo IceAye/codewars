6d8c07e34f57e2091387b37244a0d6d6a2193135
Irene Volodko <ice7aye@gmail.com>
2024-07-07 23:14:02 +0300

    test multiply the number

--- 8kyu/multiply-the-number.js
+++ 8kyu/multiply-the-number.js
@@ -1,3 +1,7 @@
 function multiply(number){
     return Math.pow(5, String(Math.abs(number)).length) * number
 }
+
+console.log('Output: ' , multiply(10))
+console.log('Output: ' , multiply(5))
+console.log('Output: ' , multiply(-3))
