068abf4f1cd0b81e715037cac84b35408695f30a
Irene Volodko <ice7aye@gmail.com>
2024-07-07 22:42:58 +0300

    test and refactor vowel count

--- 7kyu/vowel-count.js
+++ 7kyu/vowel-count.js
@@ -1,9 +1,17 @@
-function getCount(str) {
-    let sum = [];
+// DESCRIPTION:
+// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount( str ) {
+    let sum = 0
     for (let i = 0; i < str.length; i++) {
-        if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
-            sum.push(i);
+        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+            sum++
         }
     }
-    return sum.length;
+    return sum
 }
+
+console.log(getCount('abracadabra'))
+console.log(getCount('o a kak ushakov lil vo kashu kakao'))
