5119e8c6eaeff07ebb9e47beaad64f046b15ad32
Irene Volodko <ice7aye@gmail.com>
2024-07-07 23:02:58 +0300

    test formatting decimal places

--- 8kyu/formatting-decimal-places.js
+++ 8kyu/formatting-decimal-places.js
@@ -1,3 +1,6 @@
 function twoDecimalPlaces(n) {
     return +n.toFixed(2)
 }
+
+console.log('Output: ' , twoDecimalPlaces(4.659725356))
+console.log('Output: ' , twoDecimalPlaces(173735326.3783732637948948))
