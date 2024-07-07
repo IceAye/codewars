4d18aabd06cdfdc19a8e22b1ef4a8559e14222d8
Irene Volodko <ice7aye@gmail.com>
2024-07-07 22:58:44 +0300

    description USD to CNY

--- 8kyu/usd-to-cny.js
+++ 8kyu/usd-to-cny.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// Create a function that converts US dollars (USD) to Chinese Yuan (CNY) .
+// The input is the amount of USD as an integer, and the output should be a string
+// that states the amount of Yuan followed by 'Chinese Yuan'
+//
+// The conversion rate you should use is 6.75 CNY for every 1 USD.
+// All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
+//
+// 📌 EXAMPLES:
+// 15  -> '101.25 Chinese Yuan'
+// 465 -> '3138.75 Chinese Yuan'
+//
+// ✅ SOLUTION:
 function usdcny( usd ) {
     return String((usd * 6.75).toFixed(2)) + " Chinese Yuan"
 }
