e2f4b9cac39c0a001cf75eda69c72e3e85b4a66a
Irene Volodko <ice7aye@gmail.com>
2024-07-08 22:32:05 +0300

    test find the odd int

--- 6kyu/find-the-odd-int.js
+++ 6kyu/find-the-odd-int.js
@@ -5,3 +5,6 @@ function findOdd(A) {
     }
     return +Object.keys(count).filter((item) => count[item] % 2 !==0);
 }
+
+console.log(findOdd([0,1,0,1,0]))
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]))
