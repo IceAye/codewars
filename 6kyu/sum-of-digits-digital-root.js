dfe144637b8d96c35fc658f5762fecfcabb15230
Irene Volodko <ice7aye@gmail.com>
2024-07-08 23:05:00 +0300

    description sum of digits

--- 6kyu/sum-of-digits-digital-root.js
+++ 6kyu/sum-of-digits-digital-root.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Digital root is the recursive sum of all the digits in a number.
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+//
+// 📌 EXAMPLES:
+// 16  -->  1 + 6 = 7
+// 942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+//
+// ✅ SOLUTION:
 function digitalRoot(n) {
     if (n < 10) {
         return n;
