70c84b188662a02d7adf185bc08f4534c151a43c
Irene Volodko <ice7aye@gmail.com>
2024-07-08 22:07:16 +0300

    test who likes it

--- 6kyu/who-likes-it.js
+++ 6kyu/who-likes-it.js
@@ -5,3 +5,7 @@ function likes( names ) {
                 (names.length === 3) ? `${names[0]}, ${names[1]} and ${names[2]} like this` :
                     `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
 }
+
+console.log(likes([]))
+console.log(likes(['Peter']))
+console.log(likes(['Max', 'John', 'Mark']))
