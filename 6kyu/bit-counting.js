45ed7fc78e90044de3b44877e3a3d4790a1bc973
Irene Volodko <ice7aye@gmail.com>
2024-07-08 23:09:45 +0300

    test bit counting

--- 6kyu/bit-counting.js
+++ 6kyu/bit-counting.js
@@ -1,3 +1,7 @@
 let countBits = function(n) {
     return n.toString(2).split('').reduce((sum, current) => +sum + +current, 0)
 }
+
+console.log(countBits(4))
+console.log(countBits(26))
+console.log(countBits(77231418))
