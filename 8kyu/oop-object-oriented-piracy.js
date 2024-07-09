2a40d02b343a4ab47e37d35c716a99ffcc79f827
Irene Volodko <ice7aye@gmail.com>
2024-07-09 22:32:16 +0300

    test oop object oriented piracy

--- 8kyu/oop-object-oriented-piracy.js
+++ 8kyu/oop-object-oriented-piracy.js
@@ -4,6 +4,15 @@ class Ship {
         this.crew = crew
     }
     isWorthIt() {
-        return this.draft - this.crew * 1.5 > 20
+        return (this.draft - this.crew * 1.5) > 20
     }
 }
+
+const emptyShip = new Ship(0,0)
+const aWorthyShip = new Ship(100,20)
+const biggerBoat = new Ship(35,20)
+
+
+console.log('Output: ' , emptyShip.isWorthIt())
+console.log('Output: ' , biggerBoat.isWorthIt())
+console.log('Output: ' , aWorthyShip.isWorthIt())
