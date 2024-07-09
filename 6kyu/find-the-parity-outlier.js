62639e69cf5b4aa664edf11e8d9766daa28be781
Irene Volodko <ice7aye@gmail.com>
2024-07-09 23:14:50 +0300

    test find the parity outlier

--- 6kyu/find-the-parity-outlier.js
+++ 6kyu/find-the-parity-outlier.js
@@ -7,11 +7,15 @@ function findOutlier( integers ) {
     }
 
     let numberOdd = [];
-    for  (let i = 0; i < integers.length; i++) {
-        if (integers[i] % 2 === 0 ) {
+    for (let i = 0; i < integers.length; i++) {
+        if (integers[i] % 2 === 0) {
             numberOdd.push(integers[i]);
         }
     }
 
-    return numberOdd.length !==1 ? +numberEven : +numberOdd;
+    return numberOdd.length !== 1 ? +numberEven : +numberOdd;
 }
+
+console.log(findOutlier([0 , 1 , 2]))
+console.log(findOutlier([2 , 6 , 8 , 10 , 3]))
+console.log(findOutlier([2 , 6 , 8 , 200 , 700 , 1 , 84 , 10 , 4]))
