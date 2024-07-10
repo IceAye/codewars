58c1a5dd21e7a0a2aae0899ef5ed329c2a247fda
Irene Volodko <ice7aye@gmail.com>
2024-07-10 22:26:59 +0300

    test name on billboard

--- 8kyu/name-on-billboard.js
+++ 8kyu/name-on-billboard.js
@@ -1,3 +1,6 @@
 function billboard(name, price = 30){
     return name.length / (1 / price)
 }
+console.log('Output: ' , billboard("Jeong-Ho Aristotelis"))
+console.log('Output: ' , billboard("Abishai Charalampos"))
+console.log('Output: ' , billboard("Idwal Augustin"))
