237392c51a9825ddd1bb139bb17e838ba66265dd
Irene Volodko <ice7aye@gmail.com>
2024-07-10 22:12:51 +0300

    test color ghost

--- 8kyu/color-ghost.js
+++ 8kyu/color-ghost.js
@@ -2,3 +2,6 @@ let Ghost = function() {
     let colors = ['white', 'yellow', 'purple', 'red']
     this.color = colors[Math.floor(Math.random() * (colors.length))]
 }
+
+ghost = new Ghost()
+console.log('Output: ' , ghost.color)
