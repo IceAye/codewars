ed020a6b43b2b13ba0e9df2d6cbc3a0708a7f9f8
Irene Volodko <ice7aye@gmail.com>
2024-07-10 23:27:53 +0300

    description fixme get fullname

--- 7kyu/fixme-get-full-name.js
+++ 7kyu/fixme-get-full-name.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// The code provided is supposed return a person's Full Name given their first and last names.
+// But it's not working properly.
+// The first and/or last names are never null, but may be empty.
+//
+// ✅ SOLUTION:
 class Dinglemouse {
     constructor( firstName, lastName ){
         this.firstName = firstName;
