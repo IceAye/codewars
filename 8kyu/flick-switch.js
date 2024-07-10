b3aa43e44e3b6538fe3fc5521924bd0ff4351f97
Irene Volodko <ice7aye@gmail.com>
2024-07-10 22:04:08 +0300

    test flick switch

--- 8kyu/flick-switch.js
+++ 8kyu/flick-switch.js
@@ -2,3 +2,6 @@ function flickSwitch(arr){
     let flick = true
     return arr.map(item => (item === 'flick') ? flick = !flick : flick)
 }
+console.log('Output: ' , flickSwitch(["codewars", "flick", "code", "wars"]))
+console.log('Output: ' , flickSwitch(["flick", "chocolate", "adventure", "sunshine"]))
+console.log('Output: ' , flickSwitch(["bicycle", "jarmony", "flick", "sheep", "flick"]))
