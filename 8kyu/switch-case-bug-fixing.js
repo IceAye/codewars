db516e5f6356c40864eb284b4b517488dbc2a58e
Irene Volodko <ice7aye@gmail.com>
2024-07-10 23:42:44 +0300

    description switch case bug fixing

--- 8kyu/switch-case-bug-fixing.js
+++ 8kyu/switch-case-bug-fixing.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Oh no! Timmy's evalObject function doesn't work.
+// He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+//
+// ✅ SOLUTION:
 function evalObject(value){
     switch(value.operation){
         case ('+'):
