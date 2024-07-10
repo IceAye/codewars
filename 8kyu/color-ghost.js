701b4b2c623d34bb3d36cd9d7e252011ac15147d
Irene Volodko <ice7aye@gmail.com>
2024-07-10 22:14:43 +0300

    description color ghost

--- 8kyu/color-ghost.js
+++ 8kyu/color-ghost.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Create a class Ghost
+// Ghost objects are instantiated without any arguments.
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+//
+// ✅ SOLUTION:
 let Ghost = function() {
     let colors = ['white', 'yellow', 'purple', 'red']
     this.color = colors[Math.floor(Math.random() * (colors.length))]
