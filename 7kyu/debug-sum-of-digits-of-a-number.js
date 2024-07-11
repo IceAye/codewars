cf1c19939106ca35133ce7b09fcb07d06133a4ba
Irene Volodko <ice7aye@gmail.com>
2024-07-11 19:17:34 +0300

    description debug sum of digits

--- 7kyu/debug-sum-of-digits-of-a-number.js
+++ 7kyu/debug-sum-of-digits-of-a-number.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Debug   function getSumOfDigits that takes positive integer to calculate sum of its digits.
+// Assume that argument is an integer.
+// ✅ SOLUTION:
 function getSumOfDigits(integer) {
     let sum = 0
     let digits =  integer.toString();
