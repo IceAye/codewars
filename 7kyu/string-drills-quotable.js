05c5386dab6c3245ad778886c4d10990410f76f9
Irene Volodko <ice7aye@gmail.com>
2024-07-11 19:24:19 +0300

    add string drills quotable

---
+++ 7kyu/string-drills-quotable.js
@@ -0,0 +1,5 @@
+function quotable(name, quote){
+    return `${name} said: "${quote}"`
+}
+console.log('Output: ' , quotable('Grae', 'Practice makes perfect'))
+console.log('Output: ' , quotable('Dan', 'Get back to work, Grae'))
