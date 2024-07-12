c20b7f97e004b473fa1899fa0f437cce8e2b5f25
Irene Volodko <ice7aye@gmail.com>
2024-07-12 12:00:49 +0300

    fix the hashtag generator

--- 5kyu/the-hashtag-generator.js
+++ 5kyu/the-hashtag-generator.js
@@ -1,10 +1,16 @@
-function generateHashtag (str) {
-    if (str === '') return false
+function generateHashtag( str ) {
     let array = str.split(' ')
     for (let i = 0; i < array.length; i++) {
-        array[i] = String(array[i].at(0)).toUpperCase() + array[i].slice(1)
+        if (array[i] === '') {
+        } else {
+            array[i] = String(array[i].at(0)).toUpperCase() + array[i].slice(1)
+        }
     }
     let result = "#" + array.join('')
-
-    return (result.length > 140) ? false : result
+    return (result.length === 1 || result.length > 140) ? false : result
 }
+console.log('Output: ' , generateHashtag (""))
+console.log('Output: ' , generateHashtag ("Do We have A Hashtag"))
+console.log('Output: ' , generateHashtag (" ".repeat(200)))
+console.log('Output: ' , generateHashtag ("Codewars"))
+console.log('Output: ' , generateHashtag ("code" + " ".repeat(140) + "wars"))
