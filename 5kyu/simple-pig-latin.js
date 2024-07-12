0d18e0d6019bbbaa0b127e0f78df25ff49987762
Irene Volodko <ice7aye@gmail.com>
2024-07-12 10:07:45 +0300

    test simple pig latin

--- 5kyu/simple-pig-latin.js
+++ 5kyu/simple-pig-latin.js
@@ -1,3 +1,6 @@
 function pigIt(str){
     return str.replace(/\w+/g, (match) => match.slice(1) + match.at(0) + "ay")
 }
+
+console.log('Output: ' , pigIt('Pig latin is cool'))
+console.log('Output: ' , pigIt('Hello world !'))
