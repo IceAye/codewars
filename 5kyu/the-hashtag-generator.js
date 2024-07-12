64295fb5a51bd89d1f3b3854bb3f45638e048f8d
Irene Volodko <ice7aye@gmail.com>
2024-07-12 12:07:36 +0300

    refactor the hashtag generator

--- 5kyu/the-hashtag-generator.js
+++ 5kyu/the-hashtag-generator.js
@@ -1,14 +1,13 @@
-function generateHashtag( str ) {
-    let array = str.split(' ')
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === '') {
-        } else {
-            array[i] = String(array[i].at(0)).toUpperCase() + array[i].slice(1)
-        }
-    }
-    let result = "#" + array.join('')
-    return (result.length === 1 || result.length > 140) ? false : result
+function generateHashtag (str) {
+    if (!str.trim()) return false
+
+    let result = "#" +
+        str.split(' ')
+            .map(item => item.charAt(0).toUpperCase() + item.slice(1))
+            .join('')
+    return result.length > 140 ? false : result
 }
+
 console.log('Output: ' , generateHashtag (""))
 console.log('Output: ' , generateHashtag ("Do We have A Hashtag"))
 console.log('Output: ' , generateHashtag (" ".repeat(200)))
