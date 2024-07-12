df7132b1b6d7a37d55791713807a951d30f24106
Irene Volodko <ice7aye@gmail.com>
2024-07-12 10:08:52 +0300

    description simple pig latin

--- 5kyu/simple-pig-latin.js
+++ 5kyu/simple-pig-latin.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+// Leave punctuation marks untouched.
+//
+// ✅ SOLUTION:
 function pigIt(str){
     return str.replace(/\w+/g, (match) => match.slice(1) + match.at(0) + "ay")
 }
