c85d82a17ad3c2854e018e56da31df51059abfff
Irene Volodko <ice7aye@gmail.com>
2024-07-12 17:39:47 +0300

    test rot13

--- 5kyu/rot-13.js
+++ 5kyu/rot-13.js
@@ -4,3 +4,5 @@ function rot13(message) {
 
     return message.replace(/[a-z]/gi, char => encoded[input.indexOf(char)])
 }
+console.log('Output: ' , rot13("1TEst"))
+console.log('Output: ' , rot13("N[E59};s"))
