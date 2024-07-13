10b540e2702ba7352bf538f3e8767b344209325b
Irene Volodko <ice7aye@gmail.com>
2024-07-14 01:00:12 +0300

    test break camel case

--- 6kyu/break-camel-case.js
+++ 6kyu/break-camel-case.js
@@ -1,3 +1,6 @@
 function solution(string) {
     return string.replace(/([A-Z])/g, ' $1')
 }
+
+console.log('Output: ' , solution("camelCasing"))
+console.log('Output: ' , solution("camelCasingTest"))
