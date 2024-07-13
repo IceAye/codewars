33f469fb1bc8486e9c92e36ab216fa4429720e2d
Irene Volodko <ice7aye@gmail.com>
2024-07-13 23:40:56 +0300

    description rgb to hex conversion

--- 5kyu/rgb-to-hex-conversion.js
+++ 5kyu/rgb-to-hex-conversion.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// The rgb function is incomplete.
+// Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned.
+// Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+//
+// 📌 Examples (input --> output):
+// 255, 255, 255 --> "FFFFFF"
+// 255, 255, 300 --> "FFFFFF"
+//
+// ✅ SOLUTION:
+
 function hex( x ) {
     if (x > 255) {
         x = 255
