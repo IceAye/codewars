d793e95d7f4ca5551f54068614765c75a0a7a08f
Irene Volodko <ice7aye@gmail.com>
2024-07-13 23:44:23 +0300

    test take a ten minutes walk

--- 6kyu/take-a-ten-minutes-walk.js
+++ 6kyu/take-a-ten-minutes-walk.js
@@ -3,3 +3,6 @@ function isValidWalk(walk) {
         walk.filter(item => item.match(/w/)).length === walk.filter(item => item.match(/e/)).length &&
         walk.length === 10
 }
+
+console.log('Output: ' , isValidWalk(['n','s','n','s','n','s','n','s','n','s']))
+console.log('Output: ' , isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']))
