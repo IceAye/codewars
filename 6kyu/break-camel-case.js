62309d70248120b0aa5cb2117b1dd525e814a8bd
Irene Volodko <ice7aye@gmail.com>
2024-07-14 01:01:11 +0300

    description break camel case

--- 6kyu/break-camel-case.js
+++ 6kyu/break-camel-case.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Complete the solution so that the function will break up camel casing, using a space between words.
+//
+// 📌 Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+//
+// ✅ SOLUTION:
 function solution(string) {
     return string.replace(/([A-Z])/g, ' $1')
 }
