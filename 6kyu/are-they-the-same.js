84d2d882a3ebab141becb7443e29e92d8b7c5a18
Irene Volodko <ice7aye@gmail.com>
2024-07-14 19:57:02 +0300

    description are they the same

--- 6kyu/are-they-the-same.js
+++ 6kyu/are-they-the-same.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given two arrays a and b write a function comp(a, b) (or compSame(a, b)) that checks whether the two arrays
+// have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears).
+// "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+//
+// ✅ SOLUTION:
+
 let a1 = [121, 144, 19, 161, 19, 144, 19, 11]
 let a2 = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
 function comp(array1, array2){
