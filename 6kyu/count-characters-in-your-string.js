86de5bb776f6ca087201389e5c770de61fb62dea
Irene Volodko <ice7aye@gmail.com>
2024-07-14 23:10:35 +0300

    add count character in your string

---
+++ 6kyu/count-characters-in-your-string.js
@@ -0,0 +1,12 @@
+function count( string ) {
+    let counting = {}
+    let array = string.split('')
+    for (let i = 0; i < array.length; i++) {
+        if (!Object.hasOwn(counting , array[i])) {
+            counting[array[i]] = 1
+        } else {
+            counting[array[i]] += 1
+        }
+    }
+    return counting
+}
