733ac7c06b872976a2567beb5a7a4ed48229b9ca
Irene Volodko <ice7aye@gmail.com>
2024-07-15 21:20:23 +0300

    description extract the domain from url

--- 5kyu/extract-the-domain-from-url.js
+++ 5kyu/extract-the-domain-from-url.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string.
+//
+// ✅ SOLUTION:
 function domainName(url){
     return url.replace(/.+\/\/|www.|\..+/g, '')
 }
