cc6e9b3776585280404102c3a1743bad7b583bb8
Irene Volodko <ice7aye@gmail.com>
2024-07-17 01:05:35 +0300

    description pythagorean triple

--- 8kyu/pythagorean-triple.js
+++ 8kyu/pythagorean-triple.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given an unsorted array of 3 positive integers [ n1, n2, n3 ],
+// determine if it is possible to form a Pythagorean Triple using those 3 integers.
+//
+// ✅ SOLUTION:
 function isPythagoreanTriple(integers) {
     const sorted = integers.sort((a, b) => a - b)
     return (sorted[0]**2 + sorted[1]**2) === sorted[2]**2
