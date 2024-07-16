a8829892ac27543b89c1b6ed6d6113085e338a69
Irene Volodko <ice7aye@gmail.com>
2024-07-16 23:58:01 +0300

    test contamination 1 string

--- 8kyu/contamination-1-string.js
+++ 8kyu/contamination-1-string.js
@@ -1,3 +1,7 @@
 function contamination(text, char){
     return  text.replace(/./g, char)
 }
+
+console.log(contamination("abc","z"))
+console.log(contamination("_3ebzgh4","&"))
+console.log(contamination("//case"," "))
