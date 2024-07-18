da51b1aa2635f04b66a276534be19b2d215de5e9
Irene Volodko <ice7aye@gmail.com>
2024-07-19 01:32:56 +0300

    add flatten and sort an array

---
+++ 7kyu/flatten-and-sort-an-array.js
@@ -0,0 +1,6 @@
+function flattenAndSort(array) {
+    return array.flat().sort((a, b) => a - b)
+}
+
+console.log(flattenAndSort([[], [1]]))
+console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
