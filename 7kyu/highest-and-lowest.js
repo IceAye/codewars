38c207b81049bed2743ba5a94376572aab35f98d
Irene Volodko <ice7aye@gmail.com>
2024-07-18 23:43:01 +0300

    test highest and lowest

--- 7kyu/highest-and-lowest.js
+++ 7kyu/highest-and-lowest.js
@@ -4,3 +4,6 @@ function highAndLow(numbers){
     let minNum = Math.min.apply(null, num);
     return  String(`${maxNum}` + " " + `${minNum}`);
 }
+
+console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"))
+console.log(highAndLow("10 2 -1 -20"))
