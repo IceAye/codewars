8d96d9370dd7849eb09d0253828a96042d0b07f3
Irene Volodko <ice7aye@gmail.com>
2024-07-21 01:11:38 +0300

    description count the smiley faces

--- 6kyu/count-the-smiley-faces.js
+++ 6kyu/count-the-smiley-faces.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+//
+// Rules for a smiling face:
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+//
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+//
+// ✅ SOLUTION:
 function countSmileys(arr) {
     let sum = 0
     for (const smile of arr) {
