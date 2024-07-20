76ac9aafb7a7541bb8d478bf6d4a1c5a5a005ef3
Irene Volodko <ice7aye@gmail.com>
2024-07-21 01:20:30 +0300

    add remove duplicate words

---
+++ 7kyu/remove-duplicate-words.js
@@ -0,0 +1,5 @@
+function removeDuplicateWords (s) {
+    return [...new Set(s.split(' '))].join(' ')
+}
+
+console.log(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
