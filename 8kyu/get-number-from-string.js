fd7a93acc81cd7453400e6ec7a00ae2ed4b504e0
Irene Volodko <ice7aye@gmail.com>
2024-07-22 00:05:33 +0300

    description get number from string

--- 8kyu/get-number-from-string.js
+++ 8kyu/get-number-from-string.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Write a function which removes from string all non-digit characters and parse the remaining to number.
+//
+// 📌 EXAMPLE:
+// "hell5o wor6ld" -> 56
+//
+// ✅ SOLUTION:
+
 function getNumberFromString(s) {
     return +s.replace(/\D/g, "")
 }
