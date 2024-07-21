be82344f0cdb696ae5b48858525957c5f41a600b
Irene Volodko <ice7aye@gmail.com>
2024-07-22 00:46:06 +0300

    description classic hello world

--- 8kyu/classic-hello-world.js
+++ 8kyu/classic-hello-world.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You are given a method called main, make it print the line Hello World!,
+// (yes, that includes a new line character at the end) and don't return anything
+//
+// Note that for some languages, the function main is the entry point of the program.
+//
+// ✅ SOLUTION:
 class Solution {
     static main() {
         console.log("Hello World!")
