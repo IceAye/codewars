3cac5fc60d1c33cf7358e454f79020292f258d02
Irene Volodko <ice7aye@gmail.com>
2024-07-23 23:54:26 +0300

    description united states of react

--- 7kyu/united-states-of-react.js
+++ 7kyu/united-states-of-react.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// Some say The United States has never been so divided.
+// This Kata is a chance for you to unite a once great nation and MAKE AMERICA CODE AGAIN!
+// You will be given a component called States.
+// You will need to establish an initial state for the component called united and for it to be set to false.
+//
+// Then write a unite function that changes this state to true.
+// Then conditionally render a bespoke message in the component.
+// If the States are united then display "Code for everyone"
+// If the States aren't united then display "Make America code again"
+//
+// ✅ SOLUTION:
+
 import React from 'react';
 
 export class States extends React.Component {
