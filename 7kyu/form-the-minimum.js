d53a902377159acc80c0f64fca905997239c5652
Irene Volodko <ice7aye@gmail.com>
2024-07-25 23:13:35 +0300

    test form the minimum

--- 7kyu/form-the-minimum.js
+++ 7kyu/form-the-minimum.js
@@ -1,3 +1,8 @@
-function minValue(values){
-    return +[...new Set(values)].sort().join('')
+function minValue(values) {
+    return +[...new Set(values)].sort().join("");
 }
+
+console.log(minValue([1, 3, 1]));
+console.log(minValue([4, 7, 5, 7]));
+console.log(minValue([4, 8, 1, 4]));
+console.log(minValue([5, 6, 9, 9, 7, 6, 4]));
