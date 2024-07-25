503e604a2fab3bcd02990817dfbc2f75c1e2d7cb
Irene Volodko <ice7aye@gmail.com>
2024-07-25 23:14:44 +0300

    description form the minimum

--- 7kyu/form-the-minimum.js
+++ 7kyu/form-the-minimum.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+//
+// Notes:
+// Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+//
+// ✅ SOLUTION:
+
 function minValue(values) {
     return +[...new Set(values)].sort().join("");
 }
