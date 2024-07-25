be2b31c781c9f4999dc21419b890464dd92c0470
Irene Volodko <ice7aye@gmail.com>
2024-07-25 22:24:55 +0300

    test compare with margin

--- 8kyu/compare-with-margin.js
+++ 8kyu/compare-with-margin.js
@@ -1,3 +1,9 @@
 function closeCompare(a, b, margin = 0) {
     return margin >= Math.abs(a - b) ? 0 : a > b ? 1 : -1;
 }
+
+console.log(closeCompare(4, 5));
+console.log(closeCompare(5, 5));
+console.log(closeCompare(2, 5, 3));
+console.log(closeCompare(8.1, 5, 3));
+console.log(closeCompare(1.99, 5, 3));
