d06b5b4288182323566a2c20c07049dc81761531
Irene Volodko <ice7aye@gmail.com>
2024-07-26 23:23:17 +0300

    refactor loop statement for

--- 8kyu/loop-statement-for.js
+++ 8kyu/loop-statement-for.js
@@ -1,7 +1,7 @@
 function pickIt(arr){
     let odd=[],even=[];
     for (let i = 0; i < arr.length; i++) {
-        arr[i] % 2 === 0 ? even.push(arr[i]) : odd.push(arr[i])
+        (arr[i] % 2 === 0 ? even : odd).push(arr[i])
     }
     return [odd,even];
 }
