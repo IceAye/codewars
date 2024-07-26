b4d753df4741f7d41af095d51f25c855f5e53312
Irene Volodko <ice7aye@gmail.com>
2024-07-26 23:16:26 +0300

    test delete occurrences of an element if it occurs more than n times

--- 6kyu/delete-occurrences-of-an-element-if-more-than-n-times.js
+++ 6kyu/delete-occurrences-of-an-element-if-more-than-n-times.js
@@ -6,3 +6,7 @@ function deleteNth(arr, n) {
     }
     return arr;
 }
+
+console.log(deleteNth([20, 37, 20, 21], 1));
+console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3));
+console.log(deleteNth([12, 39, 19, 39, 39, 19, 12], 1));
