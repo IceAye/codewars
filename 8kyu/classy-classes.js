84e9d8dad461f4b1659dc665201399b1cb0e91ed
Irene Volodko <ice7aye@gmail.com>
2024-07-28 23:56:11 +0300

    description classy classes

--- 8kyu/classy-classes.js
+++ 8kyu/classy-classes.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Your task is to complete this Class, the Person class has been created.
+// You must fill in the Constructor method to accept a name as string and an age as number,
+// complete the get Info property and getInfo method/Info getter which should return johns age is 34
+//
+// ✅ SOLUTION:
+
 class Person {
     constructor(name, age) {
         this.name = name;
