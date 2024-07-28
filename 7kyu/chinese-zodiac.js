978c20bf321bcd747f8d4ed43072fd0fadcb351e
Irene Volodko <ice7aye@gmail.com>
2024-07-28 23:03:56 +0300

    description chinese zodiac

--- 7kyu/chinese-zodiac.js
+++ 7kyu/chinese-zodiac.js
@@ -1,3 +1,17 @@
+// ❓ DESCRIPTION:
+// The Chinese zodiac is a repeating cycle of 12 years, with each year being represented by an animal and its reputed attributes.
+// The lunar calendar is divided into cycles of 60 years each, and each year has a combination of an animal and an element.
+// There are 12 animals and 5 elements; the animals change each year, and the elements change every 2 years.
+// The current cycle was initiated in the year of 1984 which was the year of the Wood Rat.
+//
+// Since the current calendar is Gregorian, I will only be using years from the epoch 1924.
+// For simplicity I am counting the year as a whole year and not from January/February to the end of the year.
+//
+// ##Task
+// Given a year, return the element and animal that year represents ("Element Animal").
+//
+// ✅ SOLUTION:
+
 const animals = [
     "Rat",
     "Ox",
@@ -29,3 +43,4 @@ console.log(chineseZodiac(1998));
 console.log(chineseZodiac(2016));
 console.log(chineseZodiac(1924));
 console.log(chineseZodiac(1968));
+console.log(chineseZodiac(1986));
