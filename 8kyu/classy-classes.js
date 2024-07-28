427462c23f11c725db7da21bfd97621017428c12
Irene Volodko <ice7aye@gmail.com>
2024-07-28 23:55:13 +0300

    add classy classes

---
+++ 8kyu/classy-classes.js
@@ -0,0 +1,18 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    get info() {
+        return this.getInfo();
+    }
+
+    getInfo() {
+        return `${this.name}s age is ${this.age}`;
+    }
+}
+
+let john = new Person("john", 34);
+
+console.log(john.getInfo());
