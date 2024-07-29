fdd3909ae5ae4575e1a3fa10be4b1bf33a1623ca
Irene Volodko <ice7aye@gmail.com>
2024-07-29 23:17:32 +0300

    description distance betwrrn two points 2d

--- 8kyu/geometry-basics-distance-between-two-points-2d.js
+++ 8kyu/geometry-basics-distance-between-two-points-2d.js
@@ -1,6 +1,13 @@
-function distanceBetweenPoints(a, b) {
-    return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+// ❓ DESCRIPTION:
+// This series of katas will introduce you to basics of doing geometry with computers.
+// Point objects have attributes x and y.
+// Write a function calculating distance between Point a and Point b.
+//
+// ✅ SOLUTION:
+
+function distanceBetweenPoints( a , b ) {
+    return Math.sqrt(Math.pow(a.x - b.x , 2) + Math.pow(a.y - b.y , 2));
 }
 
-console.log(distanceBetweenPoints({ x: 3, y: 3 }, { x: 3, y: 3 }));
-console.log(distanceBetweenPoints({ x: 1, y: 6 }, { x: 4, y: 2 }));
+console.log(distanceBetweenPoints({x: 3 , y: 3} , {x: 3 , y: 3}));
+console.log(distanceBetweenPoints({x: 1 , y: 6} , {x: 4 , y: 2}));
