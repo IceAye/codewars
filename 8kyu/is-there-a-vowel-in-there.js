6da5e3fc6b627c1c9ca3e27bbc24104ae9e4cba7
Irene Volodko <ice7aye@gmail.com>
2024-07-29 23:49:14 +0300

    refactor is there a vowel in there

--- 8kyu/is-there-a-vowel-in-there.js
+++ 8kyu/is-there-a-vowel-in-there.js
@@ -1,11 +1,12 @@
-function isVow(a) {
-    return a.map(item => item === 97 ? "a" : item === 101 ? "e" : item === 105 ? "i" : item === 111 ? "o" : item === 117 ? "u" : item);
+function isVow( a ) {
+    const vowels = {
+        97: 'a' ,
+        101: 'e' ,
+        105: 'i' ,
+        111: 'o' ,
+        117: 'u' ,
+    }
+    return a.map(item => vowels[item] ? vowels[item] : item);
 }
 
-console.log(
-    isVow([
-        118, 117, 120, 121, 117, 98, 122, 97, 120, 106, 104, 116, 113, 114, 113,
-        120, 106,
-    ]),
-);
-console.log(isVow([101, 121, 110, 113, 113, 103, 121, 121, 101, 107, 103]));
+console.log(isVow([101 , 121 , 110 , 113 , 113 , 103 , 121 , 121 , 101 , 107 , 103]));
