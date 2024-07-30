37eb5efba34a1781ce30b3df56b924f34398613b
Irene Volodko <ice7aye@gmail.com>
2024-07-30 23:21:02 +0300

    description collatz conjecture

--- 8kyu/collatz-conjecture.js
+++ 8kyu/collatz-conjecture.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// The Collatz conjecture (also known as 3n+1 conjecture) is a conjecture that applying the following algorithm to any
+// number we will always eventually reach one:  [This is writen in pseudocode] if(number is even) number = number / 2
+// if(number is odd) number = 3*number + 1 #Task  Your task is to make a function hotpo that takes a positive n as
+// input and returns the number of times you need to perform this algorithm to get n = 1.
+//
+// ✅ SOLUTION:
 let hotpo = function ( n ) {
     let result = 0;
 
