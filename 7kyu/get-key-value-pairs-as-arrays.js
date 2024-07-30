dae4acff39c179dadf9127603b2fa66b65bf3ed2
Irene Volodko <ice7aye@gmail.com>
2024-07-31 01:17:52 +0300

    add get key/value pairs as arrays

---
+++ 7kyu/get-key-value-pairs-as-arrays.js
@@ -0,0 +1,6 @@
+function keysAndValues(data) {
+    return [Object.keys(data), Object.values(data)];
+}
+
+console.log(keysAndValues({ a: 1, b: 2, c: 3 }));
+console.log(keysAndValues({ 1: "a", 2: "b", 3: "c" }));
