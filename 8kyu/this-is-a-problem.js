ebac2ccef2a4b2f2ccfdcceb191fbf960ab9baf8
Irene Volodko <ice7aye@gmail.com>
2024-07-31 23:34:34 +0300

    refactor this is a problem

--- 8kyu/this-is-a-problem.js
+++ 8kyu/this-is-a-problem.js
@@ -1,11 +1,7 @@
 function NameMe(first, last) {
     this.firstName = first;
     this.lastName = last;
-    return {
-        name: this.firstName + " " + this.lastName,
-        firstName: this.firstName,
-        lastName: this.lastName,
-    };
+    this.name = this.firstName + " " + this.lastName;
 }
 
 let n = new NameMe("John", "Doe");
