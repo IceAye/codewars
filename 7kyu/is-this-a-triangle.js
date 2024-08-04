19febe337c32325ca0c5d021738739580b97cc69
Irene Volodko <ice7aye@gmail.com>
2024-08-04 23:51:56 +0300

    description is this a triangle

--- 7kyu/is-this-a-triangle.js
+++ 7kyu/is-this-a-triangle.js
@@ -1,7 +1,13 @@
+// ❓ DESCRIPTION:
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be
+// built with the sides of given length and false in any other case.  (In this case, all triangles must have surface
+// greater than 0 to be accepted).
+//
+// ✅ SOLUTION:
 function isTriangle( a , b , c ) {
     return a + b > c && a + c > b && b + c > a;
 }
 
-console.log(isTriangle(1,2,2))
-console.log(isTriangle(1,2,3))
-console.log(isTriangle(0,2,3))
+console.log(isTriangle(1 , 2 , 2))
+console.log(isTriangle(1 , 2 , 3))
+console.log(isTriangle(0 , 2 , 3))
