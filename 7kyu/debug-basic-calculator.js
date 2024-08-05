66ca10bf73df8c50825b9724ae0125cb93722692
Irene Volodko <ice7aye@gmail.com>
2024-08-05 23:42:04 +0300

    refactor debug basic calculator

--- 7kyu/debug-basic-calculator.js
+++ 7kyu/debug-basic-calculator.js
@@ -1,18 +1,14 @@
-let calculate = function calculate(a, o, b) {
-    if (o === "+") {
-        return a + b;
-    } else if (o === "-") {
-        return a - b;
-    } else if (o === "/" && b !== 0) {
-        return a / b;
-    } else if (o === "*") {
-        return a * b;
-    } else return null;
+let calculate = function calculate( a , o , b ) {
+    if (o === "+") return a + b;
+    if (o === "-") return a - b;
+    if (o === "/" && b !== 0) return a / b;
+    if (o === "*") return a * b;
+    return null;
 };
 
-console.log(calculate(4, "+", 2));
-console.log(calculate(4, "-", 2));
-console.log(calculate(2, "*", 2));
-console.log(calculate(2, "/", 2));
-console.log(calculate(8, "m", 2));
-console.log(calculate(4, "/", 0));
+console.log(calculate(4 , "+" , 2));
+console.log(calculate(4 , "-" , 2));
+console.log(calculate(2 , "*" , 2));
+console.log(calculate(2 , "/" , 2));
+console.log(calculate(8 , "m" , 2));
+console.log(calculate(4 , "/" , 0));
