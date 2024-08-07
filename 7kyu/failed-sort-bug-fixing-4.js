f9bd9192723fa27ad59973dce4033da1536d083f
Irene Volodko <ice7aye@gmail.com>
2024-08-07 23:02:22 +0300

    add failed sort bug fixing 4

---
+++ 7kyu/failed-sort-bug-fixing-4.js
@@ -0,0 +1,10 @@
+let sortArray = function (value) {
+    return value
+        .split("")
+        .sort((c, p) => c - p)
+        .join("");
+};
+
+console.log(sortArray("12345"));
+console.log(sortArray("54321"));
+console.log(sortArray("34251"));
