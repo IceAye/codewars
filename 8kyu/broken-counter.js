ab05d90f99c0c8483a5a610a5897f179b092f43f
Irene Volodko <ice7aye@gmail.com>
2024-08-07 22:56:35 +0300

    add broken counter

---
+++ 8kyu/broken-counter.js
@@ -0,0 +1,25 @@
+function Counter() {
+    return (this.value = 0);
+}
+
+Counter.prototype.increase = function () {
+    return this.value++;
+};
+
+Counter.prototype.getValue = function () {
+    return this.value;
+};
+
+Counter.prototype.reset = function () {
+    this.value = 0;
+};
+
+let counter = new Counter();
+
+console.log(counter.getValue());
+
+counter.increase();
+console.log(counter.getValue());
+
+counter.reset();
+console.log(counter.getValue());
