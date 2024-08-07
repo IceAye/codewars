7e524afc7baef126702c73004daa882db48173d2
Irene Volodko <ice7aye@gmail.com>
2024-08-07 23:03:41 +0300

    description failed sort bug fixing 4

--- 7kyu/failed-sort-bug-fixing-4.js
+++ 7kyu/failed-sort-bug-fixing-4.js
@@ -1,7 +1,12 @@
-let sortArray = function (value) {
+// ❓ DESCRIPTION:
+// Oh no, Timmy's Sort doesn't seem to be working? Your task is to fix the sortArray function to sort all numbers in
+// ascending order.
+//
+// ✅ SOLUTION:
+let sortArray = function ( value ) {
     return value
         .split("")
-        .sort((c, p) => c - p)
+        .sort(( c , p ) => c - p)
         .join("");
 };
 
