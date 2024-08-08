bc9131ef54fafbb972214a3a0faf874b78cce3a6
Irene Volodko <ice7aye@gmail.com>
2024-08-08 23:37:38 +0300

    description double char

--- 8kyu/double-char.js
+++ 8kyu/double-char.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+//
+// ✅ SOLUTION:
 const doubleChar = (str) => str.split("").map(item => item + item).join("");
 
 console.log(doubleChar("abcd"))
