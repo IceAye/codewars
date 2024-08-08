3461a0d5413992658d7f4667c3fdb7b3c026240f
Irene Volodko <ice7aye@gmail.com>
2024-08-08 23:44:46 +0300

    description short long short

--- 8kyu/short-long-short.js
+++ 8kyu/short-long-short.js
@@ -1,7 +1,12 @@
-function solution(a, b){
+// ❓ DESCRIPTION:
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and
+// the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+//
+// ✅ SOLUTION:
+function solution( a , b ) {
     return a.length > b.length ? b + a + b : a + b + a;
 }
 
-console.log(solution('45', '1'))
-console.log(solution('13', '200'))
-console.log(solution('Soon', 'Me'))
+console.log(solution('45' , '1'))
+console.log(solution('13' , '200'))
+console.log(solution('Soon' , 'Me'))
