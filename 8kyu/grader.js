6d1edb4bed35981d6e4d08eeb34fa63979f9c749
Irene Volodko <ice7aye@gmail.com>
2024-08-12 20:34:29 +0300

    add grader

---
+++ 8kyu/grader.js
@@ -0,0 +1,9 @@
+function grader( score ) {
+    return score < 0.6 || score > 1 ? "F" : score < 0.7 ? "D" : score < 0.8 ? "C" : score < 0.9 ? "B" : "A"
+}
+
+console.log(grader(0.7));
+console.log(grader(0.9));
+console.log(grader(0.6));
+console.log(grader(1));
+console.log(grader(0.5));
