3d5131a9f64ab618be38eed6b84a29cea0b5b120
Irene Volodko <ice7aye@gmail.com>
2024-08-14 17:33:32 +0300

    description area of square

--- 8kyu/area-of-square.js
+++ 8kyu/area-of-square.js
@@ -1,5 +1,10 @@
-function squareArea(A) {
-    return +Math.pow(2 * A / Math.PI, 2).toFixed(2)
+// ❓ DESCRIPTION:
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as
+// the input. Return the result rounded to two decimals.
+//
+// ✅ SOLUTION:
+function squareArea( A ) {
+    return +Math.pow(2 * A / Math.PI , 2).toFixed(2)
 }
 
 console.log(squareArea(2));
