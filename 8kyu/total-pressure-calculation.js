7fb5c36601933b392b9ee724290f70a08b1c32bf
Irene Volodko <ice7aye@gmail.com>
2024-08-14 19:19:25 +0300

    description total pressure calculation

--- 8kyu/total-pressure-calculation.js
+++ 8kyu/total-pressure-calculation.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given the molecular mass of two molecules, their masses in a vessel of volume at a specific temperature,
+// find the total pressure, exerted by the molecules.
+//
+// ✅ SOLUTION:
 const solution = (M1, M2, m1, m2, v, t) => {
     return ((m1 / M1 + m2 / M2) * 0.082 * (273.15 + t)) / v;
 };
