4d5c939cbf28a7a58b092eded2d3ee5ccee54adb
Irene Volodko <ice7aye@gmail.com>
2024-08-17 01:28:51 +0300

    description ASCII total

--- 8kyu/ascii-total.js
+++ 8kyu/ascii-total.js
@@ -1,5 +1,14 @@
-function uniTotal(string) {
-    return [...string].reduce((sum, item) => sum + item.charCodeAt(0), 0);
+// ❓ DESCRIPTION:
+// You'll be given a string, and have to return the sum of all characters as an int. The function should be able to
+// handle all printable ASCII characters.
+//
+// 📌 Examples:
+// uniTotal("a") == 97
+// uniTotal("aaa") == 291
+//
+// ✅ SOLUTION:
+function uniTotal( string ) {
+    return [...string].reduce(( sum , item ) => sum + item.charCodeAt(0) , 0);
 }
 
 console.log(uniTotal(""));
