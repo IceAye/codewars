154fa4927c078536f303f0713259046b4718aa38
Irene Volodko <ice7aye@gmail.com>
2024-08-16 23:57:29 +0300

    description largest 5 digit number in a series

--- 7kyu/largest-5-digit-number-in-a-series.js
+++ 7kyu/largest-5-digit-number-in-a-series.js
@@ -1,8 +1,17 @@
-function solution(digits) {
+// ❓ DESCRIPTION:
+// In the following 6 digit number: 283910 91 is the greatest sequence of 2 consecutive digits.
+// In the following 10 digit number: 1234567890 67890 is the greatest sequence of 5 consecutive digits.
+//
+// Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number
+// given. The number will be passed in as a string of only digits. It should return a five digit integer. The number
+// passed may be as large as 1000 digits.
+//
+// ✅ SOLUTION:
+function solution( digits ) {
     let max = 0;
 
     for (let i = 0; i < digits.length; i++) {
-        let number = digits.slice(i, i + 5);
+        let number = digits.slice(i , i + 5);
         if (+number > max) {
             max = +number;
         }
