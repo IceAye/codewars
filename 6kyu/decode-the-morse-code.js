d579bde56aef722b42c5b1c4fd7d55988caed110
Irene Volodko <ice7aye@gmail.com>
2024-08-18 16:07:39 +0300

    description decode Morse code

--- 6kyu/decode-the-morse-code.js
+++ 6kyu/decode-the-morse-code.js
@@ -1,4 +1,10 @@
-//use preloaded MORSE_CODE[morse]
+// ❓ DESCRIPTION:
+// Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+// The Morse code table is preloaded for you as a dictionary, feel free to use it:
+// Coffeescript/C++/Go/JavaScript/Julia/PHP/Python/Ruby/TypeScript: MORSE_CODE['.--']
+// All the test strings would contain valid Morse code, so you may skip checking for errors and exceptions.
+//
+// ✅ SOLUTION:
 decodeMorse = function(morseCode){
     return morseCode
         .trim()
