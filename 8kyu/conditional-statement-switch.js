f97c33b5c212eb204c143df1dd2dac0703293ea5
Irene Volodko <ice7aye@gmail.com>
2024-08-18 14:24:13 +0300

    description conditional statement

--- 8kyu/conditional-statement-switch.js
+++ 8kyu/conditional-statement-switch.js
@@ -1,4 +1,10 @@
-function howManydays(month) {
+// ❓ DESCRIPTION:
+// Complete the function howManydays. It accepts 1 parameter month, which means the month of the year. Different months
+// have a different number of days as shown in the table below. Return the number of days that are in month. There is
+// no need for input validation: month will always be greater than 0 and less than or equal to 12.
+//
+// ✅ SOLUTION:
+function howManydays( month ) {
     let days;
     switch (month) {
         case 2:
