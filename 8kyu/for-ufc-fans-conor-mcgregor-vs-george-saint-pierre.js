549c052e1cec4114ab80ed421b6b865eefa3cf27
Irene Volodko <ice7aye@gmail.com>
2024-08-19 22:56:20 +0300

    add for UFC fans Conor McGregor vs George Saint Pierre

---
+++ 8kyu/for-ufc-fans-conor-mcgregor-vs-george-saint-pierre.js
@@ -0,0 +1,8 @@
+let quote = function (fighter) {
+    return fighter.toLowerCase() === "george saint pierre"
+        ? "I am not impressed by your performance."
+        : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
+
+console.log(quote("george saint pierre"));
+console.log(quote('Conor McGregor'));
