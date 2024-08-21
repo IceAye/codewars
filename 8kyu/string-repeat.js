2899f731ac10242f9cfeef80b669da6a8c9ee021
Irene Volodko <ice7aye@gmail.com>
2024-08-21 23:46:31 +0300

    description string repeat

--- 8kyu/string-repeat.js
+++ 8kyu/string-repeat.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+//
+// ✅ SOLUTION:
 function repeatStr (n, s) {
     return s.repeat(n);
 }
