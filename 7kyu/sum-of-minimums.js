f18019f3f000ff30af6b3b9e010a026fcfd1781e
Irene Volodko <ice7aye@gmail.com>
2024-08-22 01:41:23 +0300

    add sum of minimums

---
+++ 7kyu/sum-of-minimums.js
@@ -0,0 +1,25 @@
+function sumOfMinimums(arr) {
+    let sum = 0;
+
+    for (const row of arr) {
+        sum += Math.min(...row);
+    }
+
+    return sum;
+}
+
+console.log(
+    sumOfMinimums([
+        [7, 9, 8, 6, 2],
+        [6, 3, 5, 4, 3],
+        [5, 8, 7, 4, 5],
+    ]),
+);
+console.log(
+    sumOfMinimums([
+        [11, 12, 14, 54],
+        [67, 89, 90, 56],
+        [7, 9, 4, 3],
+        [9, 8, 6, 7],
+    ]),
+);
