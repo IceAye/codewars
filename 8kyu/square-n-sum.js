036d19d60b9da22624972da0108aafab0665b891
Irene Volodko <ice7aye@gmail.com>
2024-08-21 23:49:48 +0300

    description square(n) sum

--- 8kyu/square-n-sum.js
+++ 8kyu/square-n-sum.js
@@ -1,3 +1,7 @@
+// ❓ ESCRIPTION:
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//
+// ✅ SOLUTION:
 function squareSum(numbers){
     return numbers.reduce((sum, number) => sum + Math.pow(number, 2),0);
 }
