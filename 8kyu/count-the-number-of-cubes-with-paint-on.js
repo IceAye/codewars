bc14d19d6a3c48edadd1950f331f04e2cc960737
Irene Volodko <ice7aye@gmail.com>
2024-08-22 01:11:21 +0300

    description count the number of cubes with paint on

--- 8kyu/count-the-number-of-cubes-with-paint-on.js
+++ 8kyu/count-the-number-of-cubes-with-paint-on.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Upon arriving at an interview, you are presented with a solid blue cube. The cube is then dipped in red paint, coating the entire surface of the cube. The interviewer then proceeds to cut through the cube in all three dimensions a certain number of times.
+//
+// Your function takes as parameter the number of times the cube has been cut. You must return the number of smaller cubes created by the cuts that have at least one red face.
+//
+// ✅ SOLUTION:
 function countSquares(cuts) {
     return cuts === 0 ? 1 : 6 * Math.pow(cuts, 2) + 2;
 }
