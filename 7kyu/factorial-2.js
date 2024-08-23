79a5fb592d8381b3bfe4771a57c57883e5a58b65
Irene Volodko <ice7aye@gmail.com>
2024-08-23 17:07:51 +0300

    add factorial

---
+++ 7kyu/factorial-2.js
@@ -0,0 +1,13 @@
+function factorial(n) {
+    let sum = 1;
+
+    while (n > 0) {
+        sum *= n;
+        n--;
+    }
+    return sum;
+}
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(4));
