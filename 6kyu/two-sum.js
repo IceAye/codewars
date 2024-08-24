2910fa37cc99c253d8f9d4220c8bd58f89a24fb0
Irene Volodko <ice7aye@gmail.com>
2024-08-24 18:09:59 +0300

    description solution for two sum

--- 6kyu/two-sum.js
+++ 6kyu/two-sum.js
@@ -1,4 +1,11 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+//
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+//
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+//
+// ✅ SOLUTION 1:
 function twoSum(numbers, target) {
     for (let i = 0; i < numbers.length - 1; i++) {
         for (let j = 1; j < numbers.length; j++) {
@@ -7,7 +14,7 @@ function twoSum(numbers, target) {
     }
 }
 
-// SOLUTION 2 (without nested loop)
+// ✅ SOLUTION 2 (without nested loop)
 function twoSum(numbers, target) {
     for (let i = 0; i < numbers.length - 1; i++) {
         let x = target - numbers[i];
