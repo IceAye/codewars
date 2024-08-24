898b084b27094c6e0ef24e49c4f7bb936e972012
Irene Volodko <ice7aye@gmail.com>
2024-08-24 17:01:59 +0300

    add enumerable magic 1 true for all

---
+++ 8kyu/enumerable-magic-1-true-for-all.js
@@ -0,0 +1,14 @@
+function all(arr, fun) {
+    return arr.every(fun);
+}
+
+console.log(
+    all([1, 2, 3, 4, 5], function (v) {
+        return v < 9;
+    }),
+);
+console.log(
+    all([1, 2, 3, 4, 5], function (v) {
+        return v > 9;
+    }),
+);
