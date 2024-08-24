65dd8fe3f00f795ee12e5fb62f47d4a75868505a
Irene Volodko <ice7aye@gmail.com>
2024-08-24 18:09:07 +0300

    add solution 2 for two sum

--- 6kyu/two-sum.js
+++ 6kyu/two-sum.js
@@ -1,8 +1,18 @@
+// SOLUTION 1:
 function twoSum(numbers, target) {
     for (let i = 0; i < numbers.length - 1; i++) {
         for (let j = 1; j < numbers.length; j++) {
             if (numbers[i] + numbers[j] === target) return [i, j];
         }
+    }
+}
+
+// SOLUTION 2 (without nested loop)
+function twoSum(numbers, target) {
+    for (let i = 0; i < numbers.length - 1; i++) {
+        let x = target - numbers[i];
+        if (numbers.includes(x) && numbers.indexOf(x) !== i)
+            return [i, numbers.indexOf(x)]
     }
 }
 
