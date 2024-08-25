8399cd652d0286db00e88c0d3e5a260f3c823319
Irene Volodko <ice7aye@gmail.com>
2024-08-25 18:31:12 +0300

    add solution 2 power of two

--- 7kyu/power-of-two.js
+++ 7kyu/power-of-two.js
@@ -1,8 +1,14 @@
+// SOLUTION 1:
 function isPowerOfTwo(n) {
     if (n === 1) return true;
     if (n < 1) return false;
 
     return isPowerOfTwo(n / 2);
+}
+
+// SOLUTION 2:
+function isPowerOfTwo(n) {
+    return Number.isInteger(Math.log2(n));
 }
 
 console.log(isPowerOfTwo(2));
