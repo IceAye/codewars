584c8e430e9c69485849641c243818a37860c49f
Irene Volodko <ice7aye@gmail.com>
2024-08-26 18:53:31 +0300

    add solution 2 which are in

--- 6kyu/which-are-in.js
+++ 6kyu/which-are-in.js
@@ -1,3 +1,4 @@
+// SOLUTION 1:
 function inArray(array1,array2){
     let result = [];
 
@@ -7,7 +8,28 @@ function inArray(array1,array2){
     }
     return result.sort();
 }
+// SOLUTION 2:
+function inArray(array1, array2) {
+    return array1
+        .filter((string1) => array2.find((string2) => string2.match(string1)))
+        .sort();
+}
 
-console.log(inArray(["xyz", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]));
-console.log(inArray(["live", "strong", "arp"], ["lively", "alive", "harp", "sharp", "armstrong"]));
-console.log(inArray(["tarp", "mice", "bull"], ["lively", "alive", "harp", "sharp", "armstrong"]));
+console.log(
+    inArray(
+        ["xyz", "live", "strong"],
+        ["lively", "alive", "harp", "sharp", "armstrong"],
+    ),
+);
+console.log(
+    inArray(
+        ["live", "strong", "arp"],
+        ["lively", "alive", "harp", "sharp", "armstrong"],
+    ),
+);
+console.log(
+    inArray(
+        ["tarp", "mice", "bull"],
+        ["lively", "alive", "harp", "sharp", "armstrong"],
+    ),
+);
