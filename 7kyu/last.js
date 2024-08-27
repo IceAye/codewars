19b95d2fbab38a2795283e48129fcc2a7372536b
Irene Volodko <ice7aye@gmail.com>
2024-08-27 23:54:28 +0300

    description last

--- 7kyu/last.js
+++ 7kyu/last.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Find the last element of the given argument(s). If a single argument is passed and is a list/array or a string, return its last element. It is guaranteed that there will be at least one argument and that single-argument arrays/lists/strings will not be empty.
+//
+// ✅ SOLUTION:
 function last(start, ...rest) {
     if (rest.length) return rest.pop();
     if (start.length) return [...start].pop();
