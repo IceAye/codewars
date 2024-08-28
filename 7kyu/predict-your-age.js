76de5f4bd27305a70fd70e6acb5ae75feaf3b1ba
Irene Volodko <ice7aye@gmail.com>
2024-08-28 13:21:35 +0300

    description predict your age

--- 7kyu/predict-your-age.js
+++ 7kyu/predict-your-age.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+//
+// In honor of my grandfather's memory we will write a function using his formula!
+//
+// Take a list of ages when each of your great-grandparent died.
+// Multiply each number by itself.
+// Add them all together.
+// Take the square root of the result.
+// Divide by two.
+//
+// ✅ SOLUTION:
 function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
     return Math.floor(
         Math.sqrt(
