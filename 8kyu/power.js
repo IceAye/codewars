4c13a8b892944e6e015bb79a005509536d588a5c
Irene Volodko <ice7aye@gmail.com>
2024-08-28 13:06:45 +0300

    description solution 2 power

--- 8kyu/power.js
+++ 8kyu/power.js
@@ -1,4 +1,9 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// The goal is to create a function of two inputs number and power, that "raises" the number up to power (ie multiplies number by itself power times).
+//
+// Note: Math.pow and some other Math functions like eval() and ** are disabled.
+//
+// ✅ SOLUTION 1:
 function numberToPower(number, power){
     let result = 1;
     for (let i = 0; i < power; i++) {
@@ -7,7 +12,7 @@ function numberToPower(number, power){
     return result;
 }
 
-// SOLUTION 2 (recursion):
+// ✅ SOLUTION 2 (recursion):
 function numberToPower(number, power){
     return power > 0 ? number * numberToPower(number, power - 1) : 1;
 }
