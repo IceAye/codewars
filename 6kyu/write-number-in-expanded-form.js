3b55103b3550819d7afd098dd612d7104e4e5567
Irene Volodko <ice7aye@gmail.com>
2024-08-28 13:45:49 +0300

    description write number in expanded form

--- 6kyu/write-number-in-expanded-form.js
+++ 6kyu/write-number-in-expanded-form.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You will be given a number and you will need to return it as a string in Expanded Form.
+//
+// NOTE: All numbers will be whole numbers greater than 0.
+//
+// ✅ SOLUTION:
 function expandedForm(num) {
     let answer = [];
     let i = 10;
@@ -12,7 +18,6 @@ function expandedForm(num) {
 }
 
 console.log(expandedForm(12));
-console.log(expandedForm(22));
 console.log(expandedForm(125));
 console.log(expandedForm(1257));
 console.log(expandedForm(70304));
