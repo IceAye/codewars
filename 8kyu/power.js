7cc445825cd77f4eeed6e5081a69a07feeddb0a0
Irene Volodko <ice7aye@gmail.com>
2024-08-28 13:05:45 +0300

    add solution 2 power

--- 8kyu/power.js
+++ 8kyu/power.js
@@ -1,9 +1,15 @@
+// SOLUTION 1:
 function numberToPower(number, power){
     let result = 1;
     for (let i = 0; i < power; i++) {
         result *= number;
     }
     return result;
+}
+
+// SOLUTION 2 (recursion):
+function numberToPower(number, power){
+    return power > 0 ? number * numberToPower(number, power - 1) : 1;
 }
 
 console.log(numberToPower(3, 2))
