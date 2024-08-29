330fbf14aa6aa9aa7ac53b9b5e3e4211a50c3783
Irene Volodko <ice7aye@gmail.com>
2024-08-30 00:54:24 +0300

    description greet me

--- 7kyu/greet-me.js
+++ 7kyu/greet-me.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+//
+// ✅ SOLUTION:
 let greet = function (name) {
     return `Hello ${name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()}!`;
 };
