56f9bc35301bb94cefa1ac6c5cdfbd753618ec37
Irene Volodko <ice7aye@gmail.com>
2024-08-29 23:28:29 +0300

    description barking mad

--- 8kyu/barking-mad.js
+++ 8kyu/barking-mad.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+//
+// snoopy.bark(); // return "Woof"
+// scoobydoo.bark(); // undefined
+//
+// Use method prototypes to enable all Dogs to bark.
+//
+// ✅ SOLUTION:
 function Dog (breed) {
     this.breed = breed;
 }
