6331dcfbf66aeee6ff03f218b4b5ee899284d697
Irene Volodko <ice7aye@gmail.com>
2024-08-29 23:55:27 +0300

    description sum arrays

--- 8kyu/sum-arrays.js
+++ 8kyu/sum-arrays.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+//
+// ✅ SOLUTION:
 function sum (numbers) {
     "use strict";
     return numbers.isNaN ? 0 : (numbers.reduce((result, current) => result + current, 0));
