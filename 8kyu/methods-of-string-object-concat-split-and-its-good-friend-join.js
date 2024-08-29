12cc36de06c64a8d201021eecadfbfa0ba47b791
Irene Volodko <ice7aye@gmail.com>
2024-08-30 00:42:23 +0300

    description methods of string object concat split and its good friend join

--- 8kyu/methods-of-string-object-concat-split-and-its-good-friend-join.js
+++ 8kyu/methods-of-string-object-concat-split-and-its-good-friend-join.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Implement a function which accepts 2 arguments: string and separator.
+//
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+//
+// ✅ SOLUTION:
 function splitAndMerge(string, separator) {
     return string
         .split(" ")
