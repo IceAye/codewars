6cca889aeeffeeda617ae99a3bc46b4debf75195
Irene Volodko <ice7aye@gmail.com>
2024-08-30 22:28:18 +0300

    description maximum subarray sum

--- 5kyu/maximum-subarray-sum.js
+++ 5kyu/maximum-subarray-sum.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers.
+// Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+//
+// Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+//
+// ✅ SOLUTION:
 let maxSequence = function (arr) {
     let maxSum = 0;
     let currentSum = 0;
