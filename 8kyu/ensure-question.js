52672eaa4cfe1729fcf1600ea9448561f52b66ec
Irene Volodko <ice7aye@gmail.com>
2024-08-30 13:04:07 +0300

    description ensure question

--- 8kyu/ensure-question.js
+++ 8kyu/ensure-question.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+//
+// ✅ SOLUTION:
 function ensureQuestion(s) {
     return s.endsWith("?") ? s : s + "?";
 }
