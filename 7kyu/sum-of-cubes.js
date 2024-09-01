a464916c5310e2388d5f2816d0f478e215c01a6d
Irene Volodko <ice7aye@gmail.com>
2024-09-01 13:48:39 +0300

    add solution 2 sum of cubes

--- 7kyu/sum-of-cubes.js
+++ 7kyu/sum-of-cubes.js
@@ -1,7 +1,14 @@
+// SOLUTION 1:
 function sumCubes(n) {
     return Array.from({ length: n }, (_, index) => index + 1)
         .map((item) => item ** 3)
         .reduce((sum, current) => sum + current);
+}
+
+// SOLUTION 2 (recursion):
+function sumCubes(n) {
+    if (n === 1) return 1;
+    return n**3 + sumCubes(n - 1)
 }
 
 console.log(sumCubes(2));
