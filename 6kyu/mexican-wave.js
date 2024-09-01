397d00990cd265347bc2bb6212b1b6f6e9a235ec
Irene Volodko <ice7aye@gmail.com>
2024-09-01 14:18:13 +0300

    add mexican wave

---
+++ 6kyu/mexican-wave.js
@@ -0,0 +1,11 @@
+function wave(str) {
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+        if (/\w/.test(str[i]))
+            result.push(str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1));
+    }
+    return result;
+}
+
+console.log(wave("hello"));
+console.log(wave("two words"));
