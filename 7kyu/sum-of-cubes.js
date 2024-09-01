bf658ff380af54542d8e271ed6f8cb0374783aa8
Irene Volodko <ice7aye@gmail.com>
2024-09-01 13:49:35 +0300

    description sum of cubes

--- 7kyu/sum-of-cubes.js
+++ 7kyu/sum-of-cubes.js
@@ -1,11 +1,16 @@
-// SOLUTION 1:
+// ❓ DECRIPTION:
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
+//
+// Assume that the input n will always be a positive integer.
+//
+// ✅ SOLUTION 1:
 function sumCubes(n) {
     return Array.from({ length: n }, (_, index) => index + 1)
         .map((item) => item ** 3)
         .reduce((sum, current) => sum + current);
 }
 
-// SOLUTION 2 (recursion):
+// ✅ SOLUTION 2 (recursion):
 function sumCubes(n) {
     if (n === 1) return 1;
     return n**3 + sumCubes(n - 1)
