8ed3bd039b6637f38d470ad2251dccaa51c020f0
Irene Volodko <ice7aye@gmail.com>
2024-09-02 23:25:04 +0300

    description find the scope

--- 8kyu/find-the-scope.js
+++ 8kyu/find-the-scope.js
@@ -1,7 +1,14 @@
+// ❓ DESCRIPTION:
+// Given an array of 4 integers
+// [a,b,c,d] representing two points (a, b) and (c, d), return a string representation of the slope of the line joining these two points.
+//
+// For an undefined slope (division by 0), return undefined . Note that the "undefined" is case-sensitive.
+//
+// ✅ SOLUTION:
 function slope(points) {
     return points[2] === points[0]
         ? undefined
-        : String(parseInt(points[3] - points[1]) / (points[2] - points[0]));
+        : String((points[3] - points[1]) / (points[2] - points[0]));
 }
 
 console.log(slope([19, 3, 20, 3]));
