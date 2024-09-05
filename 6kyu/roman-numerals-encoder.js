acb4670589f1fcaa637201ca986f995598bb612b
Irene Volodko <ice7aye@gmail.com>
2024-09-05 05:37:43 +0300

    description roman numerals encoder

--- 6kyu/roman-numerals-encoder.js
+++ 6kyu/roman-numerals-encoder.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Create a function taking a positive integer between 1 and 3999 (both included) as its parameter and returning a string containing the Roman Numeral representation of that integer.
+//
+// Modern Roman numerals are written by expressing each digit separately starting with the leftmost digit and skipping any digit with a value of zero. There cannot be more than 3 identical symbols in a row.
+//
+// ✅ SOLUTION:
 function solution(number) {
     let x = {
         0: "M",
