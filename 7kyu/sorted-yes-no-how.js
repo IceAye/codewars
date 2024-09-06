e10f79d6d0ddf337ff336818ee478e6c18fa8ec0
Irene Volodko <ice7aye@gmail.com>
2024-09-07 01:39:57 +0300

    description sorted yes no how

--- 7kyu/sorted-yes-no-how.js
+++ 7kyu/sorted-yes-no-how.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Complete the method which accepts an array of integers, and returns one of the following:
+//
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// You can assume the array will always be valid, and there will always be one correct answer.
+//
+// ✅ SOLUTION:
 function isSortedAndHow(array) {
     if (array.length === 2) {
         if (array[0] < array[1]) return "yes, ascending";
