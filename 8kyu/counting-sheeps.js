a4bc468e1fd94a49428a53af7de4861bf26a67c7
Irene Volodko <ice7aye@gmail.com>
2024-09-06 23:54:46 +0300

    description counting ships

--- 8kyu/counting-sheeps.js
+++ 8kyu/counting-sheeps.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+//
+// ✅ SOLUTION:
 function countSheeps(sheep) {
     return sheep.reduce((sum, current) => sum + (current == true ? 1 : 0), 0);
 }
+
+console.log(countSheeps([undefined,null,false,true]))
+console.log(countSheeps([undefined]))
+console.log(countSheeps([true]))
