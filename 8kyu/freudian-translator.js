c9a024fe9c74b12aa8e6d18a156911a52af3d4eb
Irene Volodko <ice7aye@gmail.com>
2024-09-06 23:49:40 +0300

    add solution 2 freudian translator

--- 8kyu/freudian-translator.js
+++ 8kyu/freudian-translator.js
@@ -1,5 +1,11 @@
+// SOLUTION 1:
 function toFreud(string) {
     return string !== "" ? string.split(" ").map(item => "sex").join(" ") : ""
+}
+
+// SOLUTION 2 (regular expression)
+function toFreud(string) {
+    return string.replace(/[^\s]+/g, "sex")
 }
 
 console.log(toFreud(""));
