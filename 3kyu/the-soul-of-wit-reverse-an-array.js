0ed0bca8f4aaa924c6eb34ad5e7d91da996c9894
Irene Volodko <ice7aye@gmail.com>
2024-09-07 18:13:44 +0300

    description the soul of wit reverse an array

--- 3kyu/the-soul-of-wit-reverse-an-array.js
+++ 3kyu/the-soul-of-wit-reverse-an-array.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// No time for stories. Reverse an array, return the result. Do whatever you want with the original array. Don't use Array.prototype.reverse.
+// You have 30 bytes to spare.
+// require isn't allowed as well.
+//
+// ✅SOLUTION:
+
 reverse=a=>a.map(a.pop,[...a])
 
 console.log(reverse([1, 2, 3]));
