e5d3b3705ec20c93429a022f089bf6b12c9d59ee
Irene Volodko <ice7aye@gmail.com>
2024-09-07 18:53:48 +0300

    description a snail enters a bar

--- 7kyu/a-snail-enters-a-bar.js
+++ 7kyu/a-snail-enters-a-bar.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// A snail is crawling along a rubber band that has an initial length of x units. The snail moves at a constant speed of y units per minute. As the snail crawls from the left end of the rubber band to the right end, the rubber band increases in length from the right side every minute, adding z units to its length.
+//
+// The question is: Will the snail be able to reach the right end of the rubber band within 1 year?
+//
+// ✅ SOLUTION:
 function canSnailReachEnd(length, speed, lengthIncreases) {
     if (speed < lengthIncreases) return false;
     let limit = 365 * 24 * 60;
