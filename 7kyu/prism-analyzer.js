6de7eb1cbabe5ea3852900d0cf79209dc089d9c7
Irene Volodko <ice7aye@gmail.com>
2024-09-07 20:40:26 +0300

    description prism analyzer

--- 7kyu/prism-analyzer.js
+++ 7kyu/prism-analyzer.js
@@ -1,9 +1,21 @@
+// ❓ DESCRIPTION:
+// In this kata, you are given the sum of the number of vertices, edges, and faces of an
+// N-sided prism, which is equal to s. Your task is to determine and return a tuple containing four values: the number of vertices, edges,
+// faces, and the number of sides of the base of the prism.
+// A prism is a polyhedron with two parallel, congruent bases and rectangular lateral faces connecting the corresponding sides of the bases.
+// For an N-sided prism:
+// Vertices (V): The total number of vertices.
+// Edges (E): The total number of edges.
+// Faces (F): The total number of faces.
+// Base sides (B): The number of sides of the base of the prism.
+//
+// ✅ SOLUTION:
 function prismProperties(s) {
-    let n = (s - 2) / 6
-    let v = 2 * n;
-    let e = 3 * n;
-    let f = n + 2;
-    return [v, e, f, n];
+    let b = (s - 2) / 6
+    let v = 2 * b;
+    let e = 3 * b;
+    let f = b + 2;
+    return [v, e, f, b];
 }
 
 console.log(prismProperties(26));
