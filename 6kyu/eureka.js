2bcd5a07147bdfcfa3f07a429717f96d6d4ba0b2
Irene Volodko <ice7aye@gmail.com>
2024-09-07 03:10:06 +0300

    description eureka

--- 6kyu/eureka.js
+++ 6kyu/eureka.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// The number 8989 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number: 89=81+9289=81+92
+// The next number in having this property is 135135:
+// See this property again: 135=11+32+53135=11+32+53
+// Task
+// We need a function to collect these numbers, that may receive two integers aa, bb that defines the range [a,b][a,b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+//
+// ✅ SOLUTION:
 function sumDigPow(a, b) {
     let result = [];
     for (let i = a; i <= b; i++) {
