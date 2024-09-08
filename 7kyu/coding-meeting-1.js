544eca6715371aee75f978b3e79686217226c2bc
Irene Volodko <ice7aye@gmail.com>
2024-09-09 02:09:31 +0300

    description coding meeting 1

--- 7kyu/coding-meeting-1.js
+++ 7kyu/coding-meeting-1.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+//
+// Your task is to return the number of JavaScript developers coming from Europe.
+// If, there are no JavaScript developers from Europe then your function should return 0.
+//
+// ✅ SOLUTION:
 function countDevelopers(list) {
     let count = 0;
     for (const item of list) {
