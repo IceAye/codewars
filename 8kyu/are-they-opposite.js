0a24ce02aec70eff2f6cc21074ea91817364253c
Irene Volodko <ice7aye@gmail.com>
2024-09-09 01:53:19 +0300

    description are they opposite

--- 8kyu/are-they-opposite.js
+++ 8kyu/are-they-opposite.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: The result should be a boolean value, instead of a string.
+//
+// The opposite means: All letters of the two strings are the same, but the case is opposite. you can assume that the string only contains letters or it's a empty string. Also take note of the edge case - if both strings are empty then you should return false/False.
+//
+// ✅ SOLUTION:
+
 function isOpposite(s1, s2) {
     if (s1.length !== s2.length || s1.length === 0 || s2.length === 0) return false;
     for (let i = 0; i < s1.length; i++) {
