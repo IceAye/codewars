00f86d202fddfabee4624612a5ee7412d9d26249
Irene Volodko <ice7aye@gmail.com>
2024-09-08 23:54:54 +0300

    description abbreviate a two word name

--- 8kyu/abbreviate-a-two-word-name.js
+++ 8kyu/abbreviate-a-two-word-name.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+//
+// The output should be two capital letters with a dot separating them.
+//
+// ✅ SOLUTION:
 function abbrevName( name ) {
     let fullName = name.toUpperCase().split(' ');
     return fullName[0][0] + '.' + fullName[1][0];
