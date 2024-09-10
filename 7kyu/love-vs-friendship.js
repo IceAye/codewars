ca4044ff97ae3ed27e9d76ed143951411df9806b
Irene Volodko <ice7aye@gmail.com>
2024-09-10 23:02:33 +0300

    add solution 2 love vs friendship

--- 7kyu/love-vs-friendship.js
+++ 7kyu/love-vs-friendship.js
@@ -1,9 +1,15 @@
+// SOLUTION 1:
 function wordsToMarks(string) {
     let sum = 0;
     for (let i = 0; i < string.length; i++) {
         sum += string.charCodeAt(i) - 96;
     }
     return sum;
+}
+
+// SOLUTION 2 (with reduce):
+function wordsToMarks(string) {
+    return [...string].reduce((sum, currentChar) => sum + currentChar.charCodeAt() - 96, 0);
 }
 
 console.log(wordsToMarks("attitude"));
