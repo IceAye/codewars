53ca9999e15683a9f6fd0d18d56445e7cba8f587
Irene Volodko <ice7aye@gmail.com>
2024-09-11 00:08:22 +0300

    add largest pair sum in array

---
+++ 7kyu/largest-pair-sum-in-array.js
@@ -0,0 +1,8 @@
+function largestPairSum(numbers) {
+    numbers.sort((a, b) => b - a);
+    return numbers[0] + numbers[1];
+}
+
+console.log(largestPairSum([10, 14, 2, 23, 19]));
+console.log(largestPairSum([-100, -29, -24, -19, 19]));
+console.log(largestPairSum([1, 2, 3, 4, 6, -1, 2]));
