8efe4c800027d01fa2daeae7c73a1c70a5b0f798
Irene Volodko <ice7aye@gmail.com>
2024-09-10 22:44:45 +0300

    description solution 2 evil or odious

--- 8kyu/evil-or-odious.js
+++ 8kyu/evil-or-odious.js
@@ -1,4 +1,13 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// The number n is Evil if it has an even number of 1's in its binary representation.
+// The first few Evil numbers: 3, 5, 6, 9, 10, 12, 15, 17, 18, 20
+//
+// The number n is Odious if it has an odd number of 1's in its binary representation.
+// The first few Odious numbers: 1, 2, 4, 7, 8, 11, 13, 14, 16, 19
+//
+// You have to write a function that determine if a number is Evil of Odious, function should return "It's Evil!" in case of evil number and "It's Odious!" in case of odious number.
+//
+// ✅ SOLUTION 1:
 function evil(n) {
     let binaryNumber = n.toString(2);
     let count = 0;
@@ -9,7 +18,7 @@ function evil(n) {
     return count % 2 === 0 ? "It's Evil!" : "It's Odious!";
 }
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 function evil(n) {
     return n.toString(2).match(/1/g).length % 2 === 0 ? "It's Evil!" : "It's Odious!";
 }
