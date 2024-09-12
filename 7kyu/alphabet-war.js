30d5a1d64f4563ecf7ce648424c2ce6dea42feb4
Irene Volodko <ice7aye@gmail.com>
2024-09-12 23:32:34 +0300

    add solution 2 alphabet war

--- 7kyu/alphabet-war.js
+++ 7kyu/alphabet-war.js
@@ -1,3 +1,5 @@
+
+// SOLUTION 1:
 const leftPower = {
     w: 4,
     p: 3,
@@ -20,6 +22,22 @@ function alphabetWar(fight) {
         if (RightPower.hasOwnProperty(char)) right += RightPower[char];
     }
     return left > right ? "Left side wins!" : right > left ? "Right side wins!" : "Let's fight again!";
+}
+
+// SOLUTION 2:
+const power = {
+    w: -4,
+    p: -3,
+    b: -2,
+    s: -1,
+    m: 4,
+    q: 3,
+    d: 2,
+    z: 1,
+}
+function alphabetWar(fight) {
+    let result = [...fight].reduce((sum, current) => sum + (power[current] || 0), 0);
+    return  result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";
 }
 
 console.log(alphabetWar("zdqmwpbs"));
