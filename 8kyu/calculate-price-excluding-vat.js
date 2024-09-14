8464b34127616b5ff6fb82ce5694f9c39e4d9e6e
Irene Volodko <ice7aye@gmail.com>
2024-09-14 23:39:52 +0300

    description calculate price excluding vat

--- 8kyu/calculate-price-excluding-vat.js
+++ 8kyu/calculate-price-excluding-vat.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Write a function that calculates the original product price, without VAT.
+//
+// Notes:
+// VAT is always 15% for the purposes of this Kata.
+// Round the result to 2 decimal places.
+// If null value given then return -1.
+//
+// ✅ SOLUTION:
 const excludingVatPrice = (price) => +(price / 1.15).toFixed(2);
 
 console.log(excludingVatPrice(230));
