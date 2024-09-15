eb35337b9df3daf9f337181f269f88db1e40d352
Irene Volodko <ice7aye@gmail.com>
2024-09-15 23:08:22 +0300

    description return two highest values in list

--- 8kyu/return-two-highest-values-in-list.js
+++ 8kyu/return-two-highest-values-in-list.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+//
+// The result should also be ordered from highest to lowest.
+//
+// ✅ SOLUTION:
 function twoHighest(arr) {
     let set = new Set(arr)
     return [...set].sort((a, b) => b - a).slice(0,2)
