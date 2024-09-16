38457316ef9b4129c35ec795b4e1ce381a14397d
Irene Volodko <ice7aye@gmail.com>
2024-09-16 23:33:22 +0300

    description methods of string object

--- 8kyu/methods-of-string-object-indexof-lastindexof-search.js
+++ 8kyu/methods-of-string-object-indexof-lastindexof-search.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Coding in function firstToLast, function accept 2 parameters:str and c. str is a string. c is a char.
+// Please return the gap between the first position of c and the last position of c.
+// If there are a lot of c in the str, should return a positive integer; If there is only one c in str, should return 0; If there is no c in the str, should return -1. Retrieval should not ignored the case.
+//
+// ✅ SOLUTION:
 function firstToLast(str,c){
     return str.indexOf(c) === -1 ? -1 : str.lastIndexOf(c) - str.indexOf(c);
 }
