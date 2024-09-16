ab0c660fc0c999a576519083885efe791429f61c
Irene Volodko <ice7aye@gmail.com>
2024-09-17 01:11:24 +0300

    description for twins types

--- 8kyu/for-twins-types.js
+++ 8kyu/for-twins-types.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+//
+// ✅ SOLUTION:
 function typeValidation(variable, type) {
     return typeof variable === type;
 }
