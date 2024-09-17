265f087e95569eab600aa21b81a5384769238c53
Irene Volodko <ice7aye@gmail.com>
2024-09-17 22:58:17 +0300

    description count of positives sum of negatives

--- 8kyu/count-of-positives-sum-of-negatives.js
+++ 8kyu/count-of-positives-sum-of-negatives.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Given an array of integers.
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// If the input is an empty array or is null, return an empty array.
+//
+// 📌 EXAMPLE:
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+//
+// ✅ SOLUTION:
 function countPositivesSumNegatives(input) {
     let positiveCount = 0;
     let negativeSum = 0;
