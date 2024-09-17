59fedae1f17228caacb5e9217bd8b2a093f2ea33
Irene Volodko <ice7aye@gmail.com>
2024-09-17 23:09:03 +0300

    description is he gonna survive

--- 8kyu/is-he-gonna-survive.js
+++ 8kyu/is-he-gonna-survive.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+// Return true if yes, false otherwise :)
+//
+// 📌 EXAMPLE:
+// hero(10, 5), true
+// hero(7, 4), false
+// hero(4, 5), false
+//
+// ✅ SOLUTION:
 function hero(bullets, dragons){
     return bullets >= dragons * 2
 }
