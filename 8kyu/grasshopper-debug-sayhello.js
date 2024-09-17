2a79caf7309414d5d2af2ae826c4cbe50398168d
Irene Volodko <ice7aye@gmail.com>
2024-09-17 23:01:25 +0300

    description grasshopper debug say hello

--- 8kyu/grasshopper-debug-sayhello.js
+++ 8kyu/grasshopper-debug-sayhello.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+//
+// 📌 EXAMPLE:
+// Hello, Mr. Spock
+//
+// ✅ SOLUTION:
 function sayHello(name) {
     return 'Hello, ' + name;
 }
