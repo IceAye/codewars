01b1ad043d63f7326a285011589861cc5cd6b920
Irene Volodko <ice7aye@gmail.com>
2024-09-17 23:03:51 +0300

    description reversed sequence

--- 8kyu/reversed-sequence.js
+++ 8kyu/reversed-sequence.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+//
+// 📌 EXAMPLE:
+// n=5 --> [5,4,3,2,1]
+//
+// ✅ SOLUTION:
 const reverseSeq = n => {
     let arr = [];
     while (n > 0) {
