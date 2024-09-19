c7e084519aff51e51e4084e9a38231066516fb41
Irene Volodko <ice7aye@gmail.com>
2024-09-19 15:48:03 +0300

    description age range compatibility equation

--- 8kyu/age-range-compatibility-equation.js
+++ 8kyu/age-range-compatibility-equation.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Everybody knows the classic "half your age plus seven" dating rule that a lot of people follow (including myself). It's the 'recommended' age range in which to date someone.
+// Given an integer (1 <= n <= 100) representing a person's age, return their minimum and maximum age range.
+// You should floor all your answers so that an integer is given instead of a float (which doesn't represent age). Return your answer in the form [min]-[max]
+//
+// ✅ SOLUTION:
+
 function datingRange(age) {
     let min, max;
     if (age <= 14) {
