33caa2e354a35b166f7bfba7fabd2dc60fe11f41
Irene Volodko <ice7aye@gmail.com>
2024-09-19 16:23:55 +0300

    add solution 2 maximum product

--- 7kyu/maximum-product.js
+++ 7kyu/maximum-product.js
@@ -1,5 +1,15 @@
+// SOLUTION 1:
 function adjacentElementsProduct(array) {
     return Math.max(...(array.map((item, index, array) => item * array[index + 1])).slice(0, -1))
+}
+
+// SOLUTION 2:
+function adjacentElementsProduct(array) {
+    let arrayMult = [];
+    for (let i = 0; i < array.length - 1; i++) {
+        arrayMult.push(array[i] * array[i + 1]);
+    }
+    return Math.max(...arrayMult);
 }
 
 console.log(adjacentElementsProduct([5, 8]));
