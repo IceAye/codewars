105996a4e8b477cb7c455354c329a3989408142d
Irene Volodko <ice7aye@gmail.com>
2024-09-19 16:58:30 +0300

    description camelcase method

--- 6kyu/camel-case-method.js
+++ 6kyu/camel-case-method.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// (actually this is PascalCase method!)
+// Write a method (or function, depending on the language) that converts a string to camelCase, that is, all words must have their first letter capitalized and spaces must be removed.
+//
+// ✅ SOLUTION:
 String.prototype.camelCase = function () {
     return this.replace(/( |^)\w/g, c => c.toUpperCase()).replace(/\s/g, "")
 };
