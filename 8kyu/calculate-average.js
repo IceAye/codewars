fe570afb501dd20e51fb66e2b593de5d7dcfe159
Irene Volodko <ice7aye@gmail.com>
2024-09-21 23:46:03 +0300

    description calculate average

--- 8kyu/calculate-average.js
+++ 8kyu/calculate-average.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Write a function which calculates the average of the numbers in a given list.
+//
+// Note: Empty arrays should return 0.
+//
+// ✅ SOLUTION:
 function findAverage(array) {
     return array.length > 0 ? array.reduce((sum, current) => sum + current, 0)/array.length : 0;
 }
