0d684e44030b0853a812a1fa68c979b94152a80a
Irene Volodko <ice7aye@gmail.com>
2024-09-22 23:06:53 +0300

    description jumping dutch act

--- 8kyu/jumping-dutch-act.js
+++ 8kyu/jumping-dutch-act.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Mr. despair wants to jump off Dutch act, So he came to the top of a building.
+// Scientific research shows that a man jumped from the top of the roof, when the floor more than 6, the person will often die in an instant; When the floor is less than or equal to 6, the person will not immediately die, he would scream. (without proof)
+// Input: floor, The height of the building (floor)
+// Output: a string, The voice of despair(When jumping Dutch act)
+//
+// 📌 EXAMPLE:
+// sc(2) should return "Aa~ Pa! Aa!"
+//
+// ✅ SOLUTION:
 function sc(floor) {
     return floor < 2
         ? ""
