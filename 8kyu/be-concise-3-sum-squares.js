6468ab126e0c1e77873a3a70cdd8b2ad2c54624b
Irene Volodko <ice7aye@gmail.com>
2024-09-23 23:51:24 +0300

    description sum squares

--- 8kyu/be-concise-3-sum-squares.js
+++ 8kyu/be-concise-3-sum-squares.js
@@ -1,5 +1,11 @@
+// ❓ DESCRIPTION:
+// You are given a program sumSquares that takes an array as input and returns the sum of the squares of each item in an array.
+// Shorten the code such that it meets the requirements.
+//
+// ✅ SOLUTION:
 const sumSquares = (array) => array.reduce((sum, current) => sum + current**2, 0)
 
+// 📌 TESTCASE:
 console.log(sumSquares([1,2,3,4,5]))
 console.log(sumSquares([7,3,9,6,5]))
 console.log(sumSquares([11,13,15,18,2]))
