ec8d911349fcaabd3ef86d050969d3eee54ea154
Irene Volodko <ice7aye@gmail.com>
2024-09-23 23:37:38 +0300

    description true for any

--- 8kyu/enumerable-magic-2-true-for-any.js
+++ 8kyu/enumerable-magic-2-true-for-any.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
+//
+// ✅ SOLUTION:
 function any(arr, fun){
     return arr.some(fun)
 }
 
+// 📌 TESTCASE:
 console.log(any([1,2,3,4], function(v, i){return v > 3}))
 console.log(any([1,2,3,4], function(v, i){return v > 4}))
 console.log(any([], function(v, i){return v > 4}))
