a7a613855fd09352ba34052505ebfcc8aa636174
Irene Volodko <ice7aye@gmail.com>
2024-09-23 23:33:51 +0300

    description a bugs trilogy episode 1

--- 8kyu/a-bugs-trilogy-1-let-math-random-decide-your-future.js
+++ 8kyu/a-bugs-trilogy-1-let-math-random-decide-your-future.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// This code is a mess! Would you help Pac to fix the code in time.
+//
+// ✅ SOLUTION:
 function yourFutureCareer() {
     let career = Math.random();
     return career <= 0.32
@@ -7,6 +11,7 @@ function yourFutureCareer() {
             : "Full-Stack Developer";
 }
 
+// 📌 TESTCASE:
 console.log(yourFutureCareer())
 console.log(yourFutureCareer())
 console.log(yourFutureCareer())
