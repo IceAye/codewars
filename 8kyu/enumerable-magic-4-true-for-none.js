b3ac3f1399af0f4da39cde53a9bf21c2ee5607a9
Irene Volodko <ice7aye@gmail.com>
2024-09-23 23:26:02 +0300

    description true for none

--- 8kyu/enumerable-magic-4-true-for-none.js
+++ 8kyu/enumerable-magic-4-true-for-none.js
@@ -1,7 +1,13 @@
+// ❓ DESCRIPTION:
+// Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+//
+// ✅ SOLUTION:
+
 function none(arr, fun){
     return !arr.some(fun)
 }
 
+// 📌 TESTCASE:
 console.log(none([1,2,3,4,5],function(item){ return item > 5 }));
 console.log(none([1,2,3,4,5],function(item){ return item > 4 }));
 console.log(none([],function(item){ return item > 4 }));
