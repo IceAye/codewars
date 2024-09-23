406e2691f9c25d42fe6bb838ef2c1f5fa797b2a3
Irene Volodko <ice7aye@gmail.com>
2024-09-23 23:24:58 +0300

    add true for none

---
+++ 8kyu/enumerable-magic-4-true-for-none.js
@@ -0,0 +1,7 @@
+function none(arr, fun){
+    return !arr.some(fun)
+}
+
+console.log(none([1,2,3,4,5],function(item){ return item > 5 }));
+console.log(none([1,2,3,4,5],function(item){ return item > 4 }));
+console.log(none([],function(item){ return item > 4 }));
