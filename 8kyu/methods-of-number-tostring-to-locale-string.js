533fd9589ca0ec5994417ff4c15f25ea4d2b27df
Irene Volodko <ice7aye@gmail.com>
2024-09-25 23:44:11 +0300

    description methods toString and toLocaleString

--- 8kyu/methods-of-number-tostring-to-locale-string.js
+++ 8kyu/methods-of-number-tostring-to-locale-string.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Coding in function colorOf. function accept 3 parameter:r g b. It means value of color red green and blue. the value range is 0-255.
+// Use toString(16) Convert numbers r g b to hex string form. at last, combine them to a web color code and return it.
+// the color code should starting with "#". and then use 2 characters per color.
+//
+// ✅ SOLUTION:
 function colorOf(r, g, b) {
     function convertToHex(x) {
         return x.toString(16).length === 1 ? "0" + x.toString(16) : x.toString(16);
@@ -5,6 +11,7 @@ function colorOf(r, g, b) {
     return "#" + convertToHex(r) + convertToHex(g) + convertToHex(b);
 }
 
+// 📌 TESTCASE:
 console.log(colorOf(255, 0, 0));
 console.log(colorOf(0, 111, 0));
 console.log(colorOf(1, 2, 3));
