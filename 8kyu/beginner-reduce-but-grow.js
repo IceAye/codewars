50a204bd41a917c080dd56d2f5e6fe47a2dc8524
Irene Volodko <ice7aye@gmail.com>
2024-09-25 23:54:58 +0300

    description reduce but grow

--- 8kyu/beginner-reduce-but-grow.js
+++ 8kyu/beginner-reduce-but-grow.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given a non-empty array of integers, return the result of multiplying the values together in order.
+//
+// ✅ SOLUTION:
 function grow(x){
     return x.reduce((product, factor) => product * factor);
 }
