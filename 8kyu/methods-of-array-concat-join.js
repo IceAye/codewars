448312cd96b333fc9fca30021900850b9e2ddb15
Irene Volodko <ice7aye@gmail.com>
2024-09-26 21:58:22 +0300

    description methods concat and join

--- 8kyu/methods-of-array-concat-join.js
+++ 8kyu/methods-of-array-concat-join.js
@@ -1,8 +1,17 @@
+// ❓ DESCRIPTION:
+// Coding in function bigToSmall. function accept 1 parameter arr(2D number array).
+// Your task is: First to all, refer to the example above, flat output arr to a one-dimensional array.
+// And then sort array in descending order.
+// Finally, use the separator ">" to connect the elements into a string.
+// Don't complain about the situation like 1>1 is not reasonable, it is just a separator.
+//
+// ✅ SOLUTION:
 function bigToSmall(arr){
     let result = [].concat(...arr)
     return result.sort((a, b) => b - a).join(">")
 }
 
+// 📌 TESTCASE:
 console.log(bigToSmall([[1,2],[3,4],[5,6]]))
 console.log(bigToSmall([[1,3,5],[2,4,6]]))
 console.log(bigToSmall([[1,1],[1],[1,1]]))
