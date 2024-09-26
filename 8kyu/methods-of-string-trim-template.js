9cdfb7e25ae06846847e7717720a456229570ab9
Irene Volodko <ice7aye@gmail.com>
2024-09-26 23:19:17 +0300

    description methods of string

--- 8kyu/methods-of-string-trim-template.js
+++ 8kyu/methods-of-string-trim-template.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Coding in function fiveLine, function accept 1 parameter:s. s is a string.
+// Please return a string of 5 lines(newline symbol is \n). The first line has one s; Second line have two s; and so on..Fifth line have five s;
+// Note1: The two sides of the parameter s may contain some whitespace, please clear them before using s.
+// Note2: Using a string template can make your job easier.
+//
+// ✅ SOLUTION:
 function fiveLine(s){
     let string = s.trim()
     let result = string;
@@ -9,6 +16,7 @@ function fiveLine(s){
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(fiveLine("  a"));
 console.log(fiveLine("\txy \n"));
 console.log(fiveLine("           Ok               "));
