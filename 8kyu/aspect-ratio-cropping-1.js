a6ec36a81f59d97f296ddd6f85338b4ce6d98692
Irene Volodko <ice7aye@gmail.com>
2024-09-28 03:38:40 +0300

    description aspect ratio part 1

--- 8kyu/aspect-ratio-cropping-1.js
+++ 8kyu/aspect-ratio-cropping-1.js
@@ -1,6 +1,11 @@
+// ❓ DESCRIPTION:
+// Write a function that accepts arbitrary X and Y resolutions and converts them into resolutions with a 16:9 aspect ratio that maintain equal height. Round your answers up to the nearest integer.
+//
+// ✅ SOLUTION:
 function aspectRatio(x, y) {
     return [Math.ceil(y * 16 / 9), y];
 }
 
+// 📌 TESTCASE:
 console.log(aspectRatio(640, 480));
 console.log(aspectRatio(960, 720));
