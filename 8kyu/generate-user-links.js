9c8fb968d3df736f1b607857f220ebfd56055123
Irene Volodko <ice7aye@gmail.com>
2024-09-28 03:00:40 +0300

    description generate user links

--- 8kyu/generate-user-links.js
+++ 8kyu/generate-user-links.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Your task is to create userlinks for the url, you will be given a username and must return a valid link.
+//
+// ✅ SOLUTION:
 function generateLink(user) {
     return `http://www.codewars.com/users/${encodeURIComponent(user)}`;
 }
 
+// 📌 TESTCASE:
 console.log(generateLink('matt c'));
 console.log(generateLink('g964'));
 console.log(generateLink('colbydauph'));
