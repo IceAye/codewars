ecc48a2bbb554377b60fe4a67a625693cdfb1e85
Irene Volodko <ice7aye@gmail.com>
2024-09-29 22:50:27 +0300

    description improving math round

--- 8kyu/improving-math-round.js
+++ 8kyu/improving-math-round.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Your task is to create a method, Math.roundTo(number, precision) which rounds a given number to precision decimal places.
+//
+// ✅ SOLUTION:
 Math.roundTo = function (number, precision) {
     return +number.toFixed(precision)
 }
 
+// 📌 TESTCASE:
 console.log(Math.roundTo(3.1415926535, 4));
 console.log(Math.roundTo(4, 5));
 console.log(Math.roundTo(4.1235343424, 6));
