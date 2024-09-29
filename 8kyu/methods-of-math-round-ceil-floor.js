36803ec7a5ee1e3585637397c7b1a7cac22132ea
Irene Volodko <ice7aye@gmail.com>
2024-09-29 21:01:55 +0300

    description methods of math

--- 8kyu/methods-of-math-round-ceil-floor.js
+++ 8kyu/methods-of-math-round-ceil-floor.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Coding in function roundIt. function accept 1 parameter n. It's a number with a decimal point. Please use different methods based on the location of the decimal point, turn the number into an integer.
+// If the decimal point is on the left side of the number (that is, the count of digits on the left of the decimal point is less than that on the right), Using ceil() method.
+// If the decimal point is on the right side of the number (that is, the count of digits on the left of the decimal point is more than that on the right), Using floor() method.
+// If the decimal point is on the middle of the number (that is, the count of digits on the left of the decimal point is equals that on the right), Using round() method.
+//
+// ✅ SOLUTION:
 function roundIt(n) {
     let [a, b] = n.toString().split(".");
     return a.length < b.length
@@ -7,6 +14,7 @@ function roundIt(n) {
             : Math.round(n);
 }
 
+// 📌 TESTCASE:
 console.log(roundIt(3.45));
 console.log(roundIt(34.5));
 console.log(roundIt(34.56));
