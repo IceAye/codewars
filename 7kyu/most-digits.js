e9f6026dbee9e4b5887b5c6dd67f931d5b671d39
Irene Volodko <ice7aye@gmail.com>
2024-09-29 23:19:28 +0300

    description most digits

--- 7kyu/most-digits.js
+++ 7kyu/most-digits.js
@@ -1,7 +1,13 @@
+// ❓ DESCRIPTION:
+// Find the number with the most digits.
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+//
+// ✅ SOLUTION:
 function findLongest(array){
     return array.reduce((prev, curr) => prev.toString().length >= curr.toString().length ? prev : curr )
 }
 
+// 📌 TESTCASE:
 console.log(findLongest([1, 10, 100]));
 console.log(findLongest([9000, 8, 800]));
 console.log(findLongest([8, 900, 500]));
