bc0e671419259b5215a3e26b7630190133fb1fd1
Irene Volodko <ice7aye@gmail.com>
2024-09-29 23:09:26 +0300

    description find the vowel

--- 8kyu/find-the-vowels.js
+++ 8kyu/find-the-vowels.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+// So given a string "super", we should return a list of [2, 4].
+// Vowels in this context refers to: a e i o u y (including upper case)
+// This is indexed from [1..n] (not zero indexed!)
+//
+// ✅ SOLUTION:
 function vowelIndices(word) {
     let array = [];
     for (let i = 0; i < word.length; i++) {
@@ -6,6 +13,7 @@ function vowelIndices(word) {
     return array;
 }
 
+// 📌 TESTCASE:
 console.log(vowelIndices("mmm"));
 console.log(vowelIndices("apple"));
 console.log(vowelIndices("orange"));
