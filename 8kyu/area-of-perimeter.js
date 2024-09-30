b29e3a4f1150424c3670c15ae3a2cd3382034e2b
Irene Volodko <ice7aye@gmail.com>
2024-09-30 23:32:48 +0300

    description area of perimeter

--- 8kyu/area-of-perimeter.js
+++ 8kyu/area-of-perimeter.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+//
+// ✅ SOLUTION:
 const areaOrPerimeter = function(l , w) {
     return ( l === w) ? l**2 : (l + w) * 2;
 };
