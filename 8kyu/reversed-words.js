7ca00cb2b78bc24ec5335eb6372e29eb9e096c5d
Irene Volodko <ice7aye@gmail.com>
2024-09-30 23:23:17 +0300

    description reversed words

--- 8kyu/reversed-words.js
+++ 8kyu/reversed-words.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Complete the solution so that it reverses all of the words within the string passed in.
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+//
+// ✅ SOLUTION:
 function reverseWords(str){
     return str.split(' ').reverse().join(' ');
 }
