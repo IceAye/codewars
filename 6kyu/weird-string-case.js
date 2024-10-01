ce6c37671b3576ac13ef422e0a03674fcb7ae6c8
Irene Volodko <ice7aye@gmail.com>
2024-10-01 14:28:07 +0300

    description weird string case

--- 6kyu/weird-string-case.js
+++ 6kyu/weird-string-case.js
@@ -1,4 +1,8 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased and you need to start over for each word.
+// The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+//
+// ✅ SOLUTION 1:
 function toWeirdCase(string) {
     let array = string.split(" ");
     let result = [];
@@ -12,7 +16,7 @@ function toWeirdCase(string) {
     return result.join(" ");
 }
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 function toWeirdCase(string) {
     return string
         .split(" ")
@@ -27,6 +31,7 @@ function toWeirdCase(string) {
         .join(" ");
 }
 
+// 📌 TESTCASE:
 console.log(toWeirdCase('This is a test'))
 console.log(toWeirdCase('UPPER CASE'))
 console.log(toWeirdCase('lower case'))
