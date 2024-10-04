2fffa563c074e854c9466300d48a1431d4f4657d
Irene Volodko <ice7aye@gmail.com>
2024-10-05 02:42:41 +0300

    add solution 2 parts of a list

--- 7kyu/parts-of-a-list.js
+++ 7kyu/parts-of-a-list.js
@@ -1,9 +1,15 @@
+// SOLUTION 1:
 function partlist(arr) {
     let result = [];
     for (let i = 1; i < arr.length; i++) {
         result.push([arr.slice(0, i).join(" "), arr.slice(i).join(" ")]);
     }
     return result;
+}
+
+// SOLUTION 2:
+function partlist(arr) {
+    return arr.map((item, index) => [arr.slice(0, index).join(" "), arr.slice(index).join(" ")]).slice(1);
 }
 
 console.log(partlist(["I", "wish", "I", "hadn't", "come"]));
