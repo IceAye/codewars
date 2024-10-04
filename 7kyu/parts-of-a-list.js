fbf9df261ff92d99220a35ed043b550751e2d8d2
Irene Volodko <ice7aye@gmail.com>
2024-10-05 02:43:56 +0300

    description solution 2 parts of a list

--- 7kyu/parts-of-a-list.js
+++ 7kyu/parts-of-a-list.js
@@ -1,4 +1,10 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+// Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+// Each part will be in a string
+// Elements of a pair must be in the same order as in the original array.
+//
+// ✅ SOLUTION 1:
 function partlist(arr) {
     let result = [];
     for (let i = 1; i < arr.length; i++) {
@@ -7,11 +13,12 @@ function partlist(arr) {
     return result;
 }
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 function partlist(arr) {
     return arr.map((item, index) => [arr.slice(0, index).join(" "), arr.slice(index).join(" ")]).slice(1);
 }
 
+// 📌 TESTCASE:
 console.log(partlist(["I", "wish", "I", "hadn't", "come"]));
 console.log(partlist(["cdIw", "tzIy", "xDu", "rThG"]));
 console.log(partlist(["dOXj", "nMlK", "QGT", "LSt", "BHNR"]));
