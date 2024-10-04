5b39d9f614220164a8c735bce8712fec297cff86
Irene Volodko <ice7aye@gmail.com>
2024-10-04 22:49:41 +0300

    description reverse a number

--- 7kyu/reverse-a-number.js
+++ 7kyu/reverse-a-number.js
@@ -1,8 +1,14 @@
+// ❓ DESCRIPTION:
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+//
+// ✅ SOLUTION:
 function reverseNumber(n) {
     if (n >= 0 && n < 10) return n;
     return Math.sign(n) * +[...Math.abs(n).toString()].reverse().join("");
 }
 
+// 📌 TESTCASE:
 console.log(reverseNumber(5));
 console.log(reverseNumber(123));
 console.log(reverseNumber(-123));
