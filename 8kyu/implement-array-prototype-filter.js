6af47fb735b9b44b343fc198ee93e8a59fd68d11
Irene Volodko <ice7aye@gmail.com>
2024-10-05 06:39:21 +0300

    description implement array prototype filter

--- 8kyu/implement-array-prototype-filter.js
+++ 8kyu/implement-array-prototype-filter.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// What we want to implement is Array.prototype.filter() function, just like the existing Array.prototype.filter(). Another similar function is _.filter() in underscore.js and lodash.js.
+//
+// ✅ SOLUTION:
 Array.prototype.filter = function (func) {
     let array = [];
     for (let i = 0; i < this.length; i++) {
@@ -6,4 +10,5 @@ Array.prototype.filter = function (func) {
     return array;
 }
 
+// 📌 TESTCASE:
 console.log([1,2,3,4,5].filter((num)=>{ return num > 3}));
