7e053feb8d5fc8bd1437278c3060fe9c0e653c7b
Irene Volodko <ice7aye@gmail.com>
2024-10-05 06:06:55 +0300

    description arguments to binary array

--- 8kyu/arguments-to-binary-addition.js
+++ 8kyu/arguments-to-binary-addition.js
@@ -1,9 +1,15 @@
+// ❓ DESCRIPTION:
+// Given an array add all the number elements and return the binary equivalent of that sum; to make the code bullet proof anything else than a number should be added as 0 since it can't be added.
+// If your language can handle float binaries assume the array won't contain float or doubles.
+//
+// ✅ SOLUTION:
 function arr2bin(arr) {
     return arr
         .reduce((acc, curr) => (typeof curr === "number" ? acc + curr : acc), 0)
         .toString(2);
 }
 
+// 📌 TESTCASE:
 console.log(arr2bin([1,2,3,4,5]));
 console.log(arr2bin([1,10,100,1000]));
 console.log(arr2bin([null]));
