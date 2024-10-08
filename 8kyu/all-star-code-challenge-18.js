3cce486576e01f53bd72b601324cbfa3db46b592
Irene Volodko <ice7aye@gmail.com>
2024-10-08 23:57:58 +0300

    description all star code challenge

--- 8kyu/all-star-code-challenge-18.js
+++ 8kyu/all-star-code-challenge-18.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+// If no occurrences can be found, a count of 0 should be returned.
+//
+// ✅ SOLUTION:
 function strCount(str , letter) {
     let counter = str.split('').filter(item => item === letter);
     return counter.length;
