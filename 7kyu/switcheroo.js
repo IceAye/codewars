91dbf4ff4a943efe370de034eaba270e6f0ff662
Irene Volodko <ice7aye@gmail.com>
2024-10-10 02:43:50 +0300

    description switcheroo

--- 7kyu/switcheroo.js
+++ 7kyu/switcheroo.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+//
+// ✅ SOLUTION:
 function switcheroo(x){
     return x.replaceAll(/[ab]/g, c => c === "a" ? "b" : "a");
 }
 
+// 📌 TESTCASE:
 console.log(switcheroo('abc'))
 console.log(switcheroo('aaabcccbaaa'))
 console.log(switcheroo('ccccc'))
