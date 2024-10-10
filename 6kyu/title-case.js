8f2cc46b9a59507dc17b35dfc87fa9e9579b563a
Irene Volodko <ice7aye@gmail.com>
2024-10-10 05:03:12 +0300

    description title case

--- 6kyu/title-case.js
+++ 6kyu/title-case.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+//
+// Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+//
+// Arguments
+// First argument (required): the original string to be converted.
+// Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+//
+// ✅ SOLUTION:
 function titleCase(title, minorWords) {
     if (title.length === 0) return "";
 
@@ -20,6 +30,7 @@ function titleCase(title, minorWords) {
     }
 }
 
+// 📌 TESTCASE:
 console.log(titleCase(""));
 console.log(titleCase("a clash of KINGS", "a an the of"));
 console.log(titleCase("THE WIND IN THE WILLOWS", "The In"));
