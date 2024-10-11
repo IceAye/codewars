471ba230b32d8175e1014eb73dd506c4e50de835
Irene Volodko <ice7aye@gmail.com>
2024-10-12 02:21:48 +0300

    description pole vault starting marks

--- 8kyu/pole-vault-starting-marks.js
+++ 8kyu/pole-vault-starting-marks.js
@@ -14,6 +14,7 @@ function startingMark(bodyHeight){
     return +((bodyHeight - 1.52) * (10.67 - 9.45) / (1.83 - 1.52) + 9.45).toFixed(2)
 }
 
+// 📌 TESTCASE:
 console.log(startingMark(1.22));
 console.log(startingMark(2.13));
 console.log(startingMark(1.52));
