c435febac4ea3655d72714c916cb630c7833a32f
Irene Volodko <ice7aye@gmail.com>
2024-10-11 23:53:09 +0300

    description alternating case

--- 8kyu/alternating-case.js
+++ 8kyu/alternating-case.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
+//
+// ✅ SOLUTION:
 String.prototype.toAlternatingCase = function () {
     let word = [];
     for (let i = 0; i < this.length; i++) {
