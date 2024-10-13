3243254846e80481137c921e07481dc08f52fe39
Irene Volodko <ice7aye@gmail.com>
2024-10-13 22:27:46 +0300

    description sum of triangular numbers

--- 7kyu/sum-of-triangular-numbers.js
+++ 7kyu/sum-of-triangular-numbers.js
@@ -1,4 +1,9 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+// Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.) obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc."
+// Triangular Numbers cannot be negative so return 0 if a negative number is given.
+//
+// ✅ SOLUTION 1:
 function sumTriangularNumbers(n) {
     if (n <= 0) return 0;
     let result = 0;
@@ -16,7 +21,7 @@ function sumTriangularNumbers(n) {
     return result;
 }
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 function sumTriangularNumbers(n) {
     if (n <= 0) return 0;
     let result = 0;
@@ -26,6 +31,7 @@ function sumTriangularNumbers(n) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(sumTriangularNumbers(6))
 console.log(sumTriangularNumbers(34))
 console.log(sumTriangularNumbers(-291))
