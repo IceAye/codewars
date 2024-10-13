0a97b5c20051358bac85426bfd83db5af070c0cb
Irene Volodko <ice7aye@gmail.com>
2024-10-13 23:49:51 +0300

    description solution 3 filter the number

--- 7kyu/filter-the-number.js
+++ 7kyu/filter-the-number.js
@@ -1,18 +1,27 @@
-//SOLUTION 1:
+// ❓ DESCRIPTION:
+// Filter the number
+// Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+// Task
+// Your task is to return a number from a string.
+// Details
+// You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+//
+// ✅ SOLUTION 1:
 let filterString = function(value) {
     return +value.replace(/[^\d]/gi, "");
 }
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 let filterString = function(value) {
     return +value.match(/\d/g).join("");
 }
 
-// SOLUTION 3:
+// ✅ SOLUTION 3:
 let filterString = function(value) {
     return +[...value].filter(item => Number.isInteger(+item)).join("");
 }
 
+// 📌 TESTCASE:
 console.log(filterString("123"));
 console.log(filterString("a1b2c3"));
 console.log(filterString("aa1bb2cc3dd"));
