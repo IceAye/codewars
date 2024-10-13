c824b83b7316142c2abd8db1f2f2bde708873cde
Irene Volodko <ice7aye@gmail.com>
2024-10-13 23:47:56 +0300

    add solution 2 filter the number

--- 7kyu/filter-the-number.js
+++ 7kyu/filter-the-number.js
@@ -3,6 +3,11 @@ let filterString = function(value) {
     return +value.replace(/[^\d]/gi, "");
 }
 
+// SOLUTION 2:
+let filterString = function(value) {
+    return +value.match(/\d/g).join("");
+}
+
 console.log(filterString("123"));
 console.log(filterString("a1b2c3"));
 console.log(filterString("aa1bb2cc3dd"));
