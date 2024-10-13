989a0580c526637b44de491b6d923900f87e27ae
Irene Volodko <ice7aye@gmail.com>
2024-10-13 22:26:32 +0300

    add solution 2 sum of triangular numbers

--- 7kyu/sum-of-triangular-numbers.js
+++ 7kyu/sum-of-triangular-numbers.js
@@ -1,3 +1,4 @@
+// SOLUTION 1:
 function sumTriangularNumbers(n) {
     if (n <= 0) return 0;
     let result = 0;
@@ -11,6 +12,16 @@ function sumTriangularNumbers(n) {
     while (n > 0) {
         result += triangleNums(n);
         n--;
+    }
+    return result;
+}
+
+// SOLUTION 2:
+function sumTriangularNumbers(n) {
+    if (n <= 0) return 0;
+    let result = 0;
+    for (let i = 1; i <= n; i++) {
+        result += (i * (i + 1))/2
     }
     return result;
 }
