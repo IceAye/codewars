2df98a86a5e33ddc255d012468d830b6b66a9302
Irene Volodko <ice7aye@gmail.com>
2024-10-13 23:32:25 +0300

    description moves in squared strings

--- 7kyu/moves-in-squared-strings.js
+++ 7kyu/moves-in-squared-strings.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Write two functions (vert_mirror, hor_mirror) and high-order function oper(fct, s) where fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+//
+// ✅ SOLUTION:
 function vertMirror(strng) {
     return strng.split("\n").map(item => [...item].reverse().join("")).join("\n");
 }
@@ -8,6 +12,7 @@ function oper(fct, s) {
     return fct(s);
 }
 
+// 📌 TESTCASE:
 console.log(vertMirror("hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"));
 console.log(horMirror("lVHt\nJVhv\nCSbg\nyeCt"));
 console.log(oper(vertMirror, "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"));
