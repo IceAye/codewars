5732860b699ffcf73663487731e3d407ede8c524
Irene Volodko <ice7aye@gmail.com>
2024-10-13 23:48:24 +0300

    add solution 3 filter the number

--- 7kyu/filter-the-number.js
+++ 7kyu/filter-the-number.js
@@ -8,6 +8,11 @@ let filterString = function(value) {
     return +value.match(/\d/g).join("");
 }
 
+// SOLUTION 3:
+let filterString = function(value) {
+    return +[...value].filter(item => Number.isInteger(+item)).join("");
+}
+
 console.log(filterString("123"));
 console.log(filterString("a1b2c3"));
 console.log(filterString("aa1bb2cc3dd"));
