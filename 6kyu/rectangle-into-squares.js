7f9f87cf5a144979a13a2acd38a53696178355f0
Irene Volodko <ice7aye@gmail.com>
2024-10-14 06:17:03 +0300

    description rectangle into squares

--- 6kyu/rectangle-into-squares.js
+++ 6kyu/rectangle-into-squares.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// The drawing below gives an idea of how to cut a given "true" rectangle into squares ("true" rectangle meaning that the two dimensions are different).
+// You will be given two dimensions
+// a positive integer length
+// a positive integer width
+// You will return a collection or a string (depending on the language; Shell bash, PowerShell, Pascal and Fortran return a string) with the size of each of the squares.
+//
+// ✅ SOLUTION:
 function sqInRect(lng, wdth) {
     if (lng === wdth) return null;
     let result = [];
@@ -8,6 +16,7 @@ function sqInRect(lng, wdth) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(sqInRect(5, 5));
 console.log(sqInRect(5, 3));
 console.log(sqInRect(3, 5));
