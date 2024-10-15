95fabd65bafbbcaf68de051ad0892382e1c0816b
Irene Volodko <ice7aye@gmail.com>
2024-10-15 23:28:11 +0300

    description greet developers

--- 7kyu/coding-meetup-2-greet-developers.js
+++ 7kyu/coding-meetup-2-greet-developers.js
@@ -1,8 +1,14 @@
-
+// ❓ DESCRIPTION:
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+// Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+// Hi < firstName here >, what do you like the most about < language here >?
+//
+// ✅ SOLUTION:
 function greetDevelopers(list) {
     return list.map(item => ({...item, greeting: `Hi ${item.firstName}, what do you like the most about ${item.language}?`}))
 }
 
+// 📌 TESTCASE:
 let list1 = [
     { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
     { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
