7bdfeec74c1f3b101942ef6dfdeb89bc588bb562
Irene Volodko <ice7aye@gmail.com>
2024-10-15 23:35:41 +0300

    description all unique

--- 7kyu/all-unique.js
+++ 7kyu/all-unique.js
@@ -1,7 +1,13 @@
+// ❓ DESCRIPTION:
+// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+//
+// ✅ SOLUTION:
 function hasUniqueChars(str){
     return new Set(str).size === str.length
 }
 
+// 📌 TESTCASE:
 console.log(hasUniqueChars("  nAa"));
 console.log(hasUniqueChars("abcdef"));
 console.log(hasUniqueChars("aA"));
