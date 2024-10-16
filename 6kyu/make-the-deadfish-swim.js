6efa613eeebe303660007f79dc290abfee48fb8f
Irene Volodko <ice7aye@gmail.com>
2024-10-16 05:25:48 +0300

    description make the deadfish swim

--- 6kyu/make-the-deadfish-swim.js
+++ 6kyu/make-the-deadfish-swim.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Write a simple parser that will parse and run Deadfish.
+// Deadfish has 4 commands, each 1 character long:
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+//
+// ✅ SOLUTION:
 function parse(data) {
     let result = [];
     data.split("").reduce((acc, curr) => {
@@ -10,5 +20,6 @@ function parse(data) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(parse("iiisdoso"));
 console.log(parse("iiisxxxdoso"));
