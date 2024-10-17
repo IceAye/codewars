f9f13a4037dc65717d679f148f411732afa09c4c
Irene Volodko <ice7aye@gmail.com>
2024-10-17 14:28:30 +0300

    description odd even string sort

--- 7kyu/odd-even-string-sort.js
+++ 7kyu/odd-even-string-sort.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given a string s, your task is to return another string such that even-indexed and odd-indexed characters of s are grouped and the groups are space-separated. Even-indexed group comes as first, followed by a space, and then by the odd-indexed part.
+// Notes
+// Tested strings are at least 8 characters long.
+//
+// ✅ SOLUTION:
 function sortMyString(S) {
     return [...S].reduce((acc, curr, currentIndex) => {
         acc[currentIndex % 2] += curr;
@@ -5,5 +11,6 @@ function sortMyString(S) {
     }, ["", ""]).join(" ")
 }
 
+// 📌 TESTCASE:
 console.log(sortMyString("CodeWars"));
 console.log(sortMyString("YCOLUE'VREER"));
