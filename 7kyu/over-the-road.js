eefff48157db5dff1dce816cf874fd90b920445f
Irene Volodko <ice7aye@gmail.com>
2024-10-19 21:58:48 +0300

    add over the road

---
+++ 7kyu/over-the-road.js
@@ -0,0 +1,9 @@
+function overTheRoad(address, n){
+    return n * 2 - address + 1;
+}
+console.log(overTheRoad(1,3));
+console.log(overTheRoad(3,3));
+console.log(overTheRoad(2,3));
+console.log(overTheRoad(3,5));
+console.log(overTheRoad(7,11));
+console.log(overTheRoad(23633656673,310027696726));
