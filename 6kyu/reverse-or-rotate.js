86b8369bf958e847efa3edc40ee175b739d676e7
Irene Volodko <ice7aye@gmail.com>
2024-10-19 23:40:44 +0300

    description reverse or rotate

--- 6kyu/reverse-or-rotate.js
+++ 6kyu/reverse-or-rotate.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+// If the sum of a chunk's digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+// If
+// sz is <= 0 or if str == "" return ""
+// sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+//
+// ✅ SOLUTION:
 function revrot(str, sz) {
     if (str === 0 || sz === 0 || str < sz) return "";
     let array = [];
@@ -12,6 +20,7 @@ function revrot(str, sz) {
     return array.join("")
 }
 
+// 📌 TESTCASE:
 console.log(revrot("1234", 0));
 console.log(revrot("1234", 5));
 console.log(revrot("733049910872815764", 5));
