9bba8fba3cc648fd46b1c187ea39da1d732957c6
Irene Volodko <ice7aye@gmail.com>
2024-10-19 22:36:39 +0300

    add sums of parts

---
+++ 6kyu/sums-of-parts.js
@@ -0,0 +1,15 @@
+function partsSums(ls) {
+    let sum = ls.reduce((acc, curr) => acc + curr, 0);
+    let result = [sum];
+    let acc = 0;
+    for (let i = 0; i < ls.length; i++) {
+        acc += ls[i];
+        result.push(sum - acc);
+    }
+    return result;
+}
+
+console.log(partsSums([]));
+console.log(partsSums([0, 1, 3, 6, 10]));
+console.log(partsSums([1, 2, 3, 4, 5, 6]));
+console.log(partsSums([744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358]));
