f520bd028ddbee554e3a9aab0b824a18dbdc9485
Irene Volodko <ice7aye@gmail.com>
2024-10-19 21:28:23 +0300

    description simple remove duplicates

--- 7kyu/simple-remove-duplicates.js
+++ 7kyu/simple-remove-duplicates.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+//
+// ✅ SOLUTION:
 function solve(arr) {
     return [...new Set(arr)].sort((a, b) => arr.lastIndexOf(a) - arr.lastIndexOf(b));
 }
 
+// 📌 TESTCASE:
 console.log(solve([3,4,4,3,6,3]));
 console.log(solve([1,2,1,2,1,2,3]));
 console.log(solve([1,2,3,4]));
