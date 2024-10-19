337ef390e914270ee65d1c251e67bec458bcb27f
Irene Volodko <ice7aye@gmail.com>
2024-10-19 22:38:26 +0300

    description solution 2 sums of parts

--- 6kyu/sums-of-parts.js
+++ 6kyu/sums-of-parts.js
@@ -1,4 +1,22 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// et us consider this example (array written in general format):
+//
+// ls = [0, 1, 3, 6, 10]
+//
+// Its following parts:
+//
+// ls = [0, 1, 3, 6, 10]
+// ls = [1, 3, 6, 10]
+// ls = [3, 6, 10]
+// ls = [6, 10]
+// ls = [10]
+// ls = []
+// The corresponding sums are (put together in a list): [20, 20, 19, 16, 10, 0]
+//
+// The function parts_sums (or its variants in other languages) will take as parameter a list ls and return a list of the sums of its parts as defined above.
+// Take a look at performance: some lists have thousands of elements.
+//
+// ✅ SOLUTION 1:
 function partsSums(ls) {
     let sum = ls.reduce((acc, curr) => acc + curr, 0);
     let result = [sum];
@@ -9,13 +27,14 @@ function partsSums(ls) {
     }
     return result;
 }
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 function partsSums(ls) {
     ls.unshift(0)
     let sum = ls.reduce((acc, curr) => acc + curr, 0);
     return ls.map(item => sum = sum - item);
 }
 
+// 📌 TESTCASE:
 console.log(partsSums([]));
 console.log(partsSums([0, 1, 3, 6, 10]));
 console.log(partsSums([1, 2, 3, 4, 5, 6]));
