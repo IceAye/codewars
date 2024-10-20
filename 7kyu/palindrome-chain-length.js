82a803e12e4a4599060fd2c868b9b1326acfd8f7
Irene Volodko <ice7aye@gmail.com>
2024-10-20 22:05:34 +0300

    description palindrome chain length

--- 7kyu/palindrome-chain-length.js
+++ 7kyu/palindrome-chain-length.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Number is a palindrome if it is equal to the number with digits in reversed order. For example, 5, 44, 171, 4884 are palindromes, and 43, 194, 4773 are not.
+// Write a function which takes a positive integer and returns the number of special steps needed to obtain a palindrome. The special step is: "reverse the digits, and add to the original number". If the resulting number is not a palindrome, repeat the procedure with the sum until the resulting number is a palindrome.
+// If the input number is already a palindrome, the number of steps is 0.
+//
+// ✅ SOLUTION:
 
 let palindromeChainLength = function(n) {
     let count = 0;
@@ -10,6 +16,7 @@ let palindromeChainLength = function(n) {
     return count;
 };
 
+// 📌 TESTCASE:
 console.log(palindromeChainLength(88))
 console.log(palindromeChainLength(87))
 console.log(palindromeChainLength(89))
