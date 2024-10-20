4e4e65a76be7eb6ad7f6888f39d42ce2448f886c
Irene Volodko <ice7aye@gmail.com>
2024-10-20 03:30:30 +0300

    description string incrementer

--- 5kyu/string-incrementer.js
+++ 5kyu/string-incrementer.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Your job is to write a function which increments a string, to create a new string.
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+// Attention: If the number has leading zeros the amount of digits should be considered.
+//
+// ✅ SOLUTION:
 function incrementString (strng) {
     let result = "";
     strng.replace(/\d+$/, c => result = c);
@@ -5,7 +12,7 @@ function incrementString (strng) {
     return strng.replace(/(\d+)?$/, c => c ? number : 1);
 }
 
-
+// 📌 TESTCASE:
 console.log(incrementString("foobar000"));
 console.log(incrementString("foobar999"));
 console.log(incrementString("foobar00999"));
