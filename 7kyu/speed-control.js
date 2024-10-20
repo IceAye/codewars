7872bf7730f2b7e3cec2c2769153877b6289851e
Irene Volodko <ice7aye@gmail.com>
2024-10-20 22:30:37 +0300

    description speed control

--- 7kyu/speed-control.js
+++ 7kyu/speed-control.js
@@ -1,3 +1,17 @@
+// ❓ DESCRIPTION:
+// In John's car the GPS records every s seconds the distance travelled from an origin (distances are measured in an arbitrary but consistent unit). For example, below is part of a record with s = 15:
+// x = [0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25]
+// The sections are:
+// 0.0-0.19, 0.19-0.5, 0.5-0.75, 0.75-1.0, 1.0-1.25, 1.25-1.50, 1.5-1.75, 1.75-2.0, 2.0-2.25
+// We can calculate John's average hourly speed on every section and we get:
+// [45.6, 74.4, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0]
+// Given s and x the task is to return as an integer the *floor* of the maximum average speed per hour obtained on the sections of x. If x length is less than or equal to 1 return 0 since the car didn't move.
+// Note
+// With floats it can happen that results depends on the operations order. To calculate hourly speed you can use:
+//  (3600 * delta_distance) / s.
+// Happy coding!
+//
+// ✅ SOLUTION:
 function gps(s, x) {
     if (x.length <= 1) return 0;
     let result = [];
@@ -7,7 +21,7 @@ function gps(s, x) {
     return Math.max(...result);
 }
 
-
+// 📌 TESTCASE:
 console.log(gps(15, [0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25]))
 console.log(gps(20, [0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61]))
 console.log(gps(12, [0.0, 0.11, 0.22, 0.33, 0.44, 0.65, 1.08, 1.26, 1.68, 1.89, 2.1, 2.31, 2.52, 3.25]))
