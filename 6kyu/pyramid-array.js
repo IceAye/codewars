016a9413516519cb180b860d3ae35807afb09eb2
Irene Volodko <ice7aye@gmail.com>
2024-10-21 04:06:11 +0300

    description pyramid array

--- 6kyu/pyramid-array.js
+++ 6kyu/pyramid-array.js
@@ -1,7 +1,18 @@
+// ❓ DESCRIPTION:
+// Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+//
+// pyramid(0) => [ ]
+// pyramid(1) => [ [1] ]
+// pyramid(2) => [ [1], [1, 1] ]
+// pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+// Note: the subarrays should be filled with 1s
+//
+// ✅ SOLUTION:
 function pyramid(n) {
     return new Array(n).fill().map((item, index) => new Array(index + 1).fill(1))
 }
 
+// 📌 TESTCASE:
 console.log(pyramid(0))
 console.log(pyramid(1))
 console.log(pyramid(2))
