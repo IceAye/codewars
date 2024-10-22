c2cd3c1fcf5f3e5dad74c3f42bf14e07e98c646f
Irene Volodko <ice7aye@gmail.com>
2024-10-23 00:04:28 +0300

    description sum of odd numbers

--- 7kyu/sum-of-odd-cubed-numbers.js
+++ 7kyu/sum-of-odd-cubed-numbers.js
@@ -1,13 +1,18 @@
+// ❓ DESCRIPTION:
+// Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
+//
+// ✅ SOLUTION:
 function cubeOdd(arr) {
-    arr.includes()
-    return arr.reduce((acc, curr) => {
+    let number = arr.reduce((acc, curr) => {
         if (typeof(curr) === 'number') {
             acc += Math.abs(curr ** 3) % 2 === 1 ? curr ** 3 : 0
             return acc;
         } else return undefined
     }, 0)
+    return Number.isNaN(number) ? undefined : number
 }
 
+// 📌 TESTCASE:
 console.log(cubeOdd([1, 2, 3, 4])); // 28
 console.log(cubeOdd([-3, -2, 2, 3])); // 0
 console.log(cubeOdd(["a", 12, 9, "z", 42])); // undefined
