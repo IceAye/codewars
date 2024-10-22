586c107b17f5a1f0170a9f08cfba6368ff16573e
Irene Volodko <ice7aye@gmail.com>
2024-10-22 13:44:58 +0300

    description the supermarket queue

--- 6kyu/the-supermarket-queue.js
+++ 6kyu/the-supermarket-queue.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+//
+// input
+// customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+// n: a positive integer, the number of checkout tills.
+// output
+// The function should return an integer, the total time required.
+//
+// ✅ SOLUTION:
 function queueTime(customers, n) {
     let queue = customers.slice(0,n)
     for (let i = n ; i < customers.length; i++) {
@@ -7,7 +17,7 @@ function queueTime(customers, n) {
     return queue.length ? Math.max(...queue) : 0
 }
 
-
+// 📌 TESTCASE:
 console.log(queueTime([], 1)) // 0
 console.log(queueTime([5,3,4], 1)) // 12
 console.log(queueTime([10,2,3,3], 2)) // 10
