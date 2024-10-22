f3dd97c69e6e021c8662264a3f00bf223f0c5537
Irene Volodko <ice7aye@gmail.com>
2024-10-22 11:49:09 +0300

    description esrever

--- 7kyu/esrever.js
+++ 7kyu/esrever.js
@@ -1,6 +1,12 @@
+// ❓ DESCRIPTION:
+// Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+// (the dedicated builtin(s) functionalities are deactivated)
+//
+// ✅ SOLUTION:
 reverse = function(array) {
     return array.map((item, index) => array[array.length - 1 - index])
 }
 
+// 📌 TESTCASE:
 console.log(reverse([1,2,3]))
 console.log(reverse([1,null,14,"two"]))
