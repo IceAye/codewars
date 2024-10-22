1aa1b2569a2c03087316e9a96f4864221fb400d6
Irene Volodko <ice7aye@gmail.com>
2024-10-22 15:35:42 +0300

    description alternating split

--- 6kyu/simple-encryption-1-alternating-split.js
+++ 6kyu/simple-encryption-1-alternating-split.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed characters of S with all the even-indexed characters of S, this process should be repeated N times.
+// Together with the encryption function, you should also implement a decryption function which reverses the process.
+// If the string S is an empty value or the integer N is not positive, return the first argument without changes.
+//
+// ✅ SOLUTION:
 function encrypt(text, n) {
     function reverse(text) {
         let [even, odd] = [...text].reduce(
@@ -31,15 +37,16 @@ function decrypt(encryptedText, n) {
     return decrypt(dereverse(encryptedText), n - 1)
 }
 
+// 📌 TESTCASE:
 console.log(encrypt("This is a test!", 1)); // "hsi  etTi sats!"
 console.log(encrypt("This is a test!", 2)); // "s eT ashi tist!"
 console.log(encrypt("This is a test!", 3)); // " Tah itse sits!"
 console.log(encrypt("This is a test!", -1)); // "This is a test!"
 console.log(encrypt("This is a test!", 4)); // "This is a test!"
-console.log(encrypt(null, 0)); // "This is a test!"
+console.log(encrypt(null, 0)); // NULL
 console.log(decrypt("hsi  etTi sats!", 1)); // "This is a test!"
 console.log(decrypt("s eT ashi tist!", 2)); // "This is a test!"
 console.log(decrypt(" Tah itse sits!", 3)); // "This is a test!"
 console.log(decrypt("This is a test!", -1)); // "This is a test!"
 console.log(decrypt("hskt svr neetn!Ti aai eyitrsig", 1)); // "This kata is very interesting!"
-console.log(decrypt(null, 0)); // "This kata is very interesting!"
+console.log(decrypt(null, 0)); // NULL
