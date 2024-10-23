7e1a8361acee03d6ee13846db58f8d3e40e850c3
Irene Volodko <ice7aye@gmail.com>
2024-10-23 23:41:34 +0300

    add description find the nth digit of a number

--- 7kyu/find-the-nth-digit-of-a-number.js
+++ 7kyu/find-the-nth-digit-of-a-number.js
@@ -1,9 +1,19 @@
+// ❓ DESCRIPTION:
+// Complete the function that takes two numbers as input, num and nth and return the nth digit of num (counting from right to left).
+//
+// Note
+// If num is negative, ignore its sign and treat it as a positive value
+// If nth is not positive, return -1
+// Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0
+//
+// ✅ SOLUTION:
 function findDigit(num, nth) {
     if (nth <= 0) return -1;
     let number = Array.from(Math.abs(num).toString(), Number).reverse()[nth - 1];
     return Array.from(Math.abs(num).toString(), Number).reverse()[nth - 1] || 0;
 }
 
+// 📌 TESTCASE:
 console.log(findDigit(5673, 4)) // 5
 console.log(findDigit(129, 2)) // 2
 console.log(findDigit(-2825, 3)) // 8
