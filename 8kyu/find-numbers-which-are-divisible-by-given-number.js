0b024ec9ec79da46b9c69e08f5b89971d6a16f5a
Irene Volodko <ice7aye@gmail.com>
2024-10-23 23:47:57 +0300

    description find numbers which are divisible by given number

--- 8kyu/find-numbers-which-are-divisible-by-given-number.js
+++ 8kyu/find-numbers-which-are-divisible-by-given-number.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+//
+// 📌 EXAMPLE:
+// [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+//
+// ✅ SOLUTION:
 function divisibleBy(numbers, divisor){
     let arr = [];
     for (let i = 0; i < numbers.length; i++) {
