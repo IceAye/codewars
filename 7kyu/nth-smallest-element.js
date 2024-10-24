8fd7262bebcbed5a5d8988eadb3e83eeb5d2cba9
Irene Volodko <ice7aye@gmail.com>
2024-10-24 22:32:43 +0300

    description nth smallest element

--- 7kyu/nth-smallest-element.js
+++ 7kyu/nth-smallest-element.js
@@ -1,7 +1,15 @@
+// ❓ DESCRIPTION:
+// Given an array/list of integers, find the Nth smallest element in the array.
+// Array/list size is at least 3.
+// Array/list's numbers could be a mixture of positives , negatives and zeros.
+// Repetition in array/list's numbers could occur, so don't remove duplications.
+//
+// ✅ SOLUTION:
 function nthSmallest(arr, pos) {
     return arr.sort((a, b) => a - b)[pos - 1];
 }
 
+// 📌 TESTCASE:
 console.log(nthSmallest([3, 1, 2], 2)); //  2
 console.log(nthSmallest([15, 20, 7, 10, 4, 3], 3)); //  7
 console.log(nthSmallest([-5, -1, -6, -18], 4)); // -1
