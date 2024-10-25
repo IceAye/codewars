1f0eb35f5bc0b9bd22239023f5f24d6259bec4e1
Irene Volodko <ice7aye@gmail.com>
2024-10-25 20:38:06 +0300

    description highest rank in an array

--- 6kyu/highest-rank-number-in-an-array.js
+++ 6kyu/highest-rank-number-in-an-array.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+// Note: no empty arrays will be given.
+//
+// ✅ SOLUTION:
 function highestRank(arr) {
     let map = new Map();
     for (const item of arr) {
@@ -7,6 +12,7 @@ function highestRank(arr) {
     return [...map].filter(item => item[1] === maxValue).sort((a, b) => b[0] - a[0])[0][0];
 }
 
+// 📌 TESTCASE:
 console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12])); // 12
 console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10])); // 12
 console.log(highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10])); // 3
