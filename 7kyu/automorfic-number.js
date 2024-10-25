686868e05433bd9008ccaca12ff82fd6a413e379
Irene Volodko <ice7aye@gmail.com>
2024-10-25 14:11:07 +0300

    add automorfic number

---
+++ 7kyu/automorfic-number.js
@@ -0,0 +1,14 @@
+function automorphic(n){
+    return  String(n**2).endsWith(String(n)) ? "Automorphic" : "Not!!";
+}
+
+console.log(automorphic(1)) // "Automorphic"
+console.log(automorphic(3)) // "Not!!"
+console.log(automorphic(6)) // "Automorphic"
+console.log(automorphic(9)) // "Not!!"
+console.log(automorphic(25)) // "Automorphic"
+console.log(automorphic(53)) // "Not!!"
+console.log(automorphic(76)) // "Automorphic"
+console.log(automorphic(95)) // "Not!!"
+console.log(automorphic(625)) // "Automorphic"
+console.log(automorphic(225)) // "Not!!"
