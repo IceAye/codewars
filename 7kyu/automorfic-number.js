808a841d825c87a6290959d000169c7672977a2d
Irene Volodko <ice7aye@gmail.com>
2024-10-25 14:17:32 +0300

    description automorfic number

--- 7kyu/automorfic-number.js
+++ 7kyu/automorfic-number.js
@@ -1,7 +1,13 @@
+// ❓ DESCRIPTION:
+// A number is called Automorphic number if and only if its square ends in the same digits as the number itself.
+// Given a number determine if it Automorphic or not.
+//
+// ✅ SOLUTION:
 function automorphic(n){
     return  String(n**2).endsWith(String(n)) ? "Automorphic" : "Not!!";
 }
 
+// 📌 TESTCASE:
 console.log(automorphic(1)) // "Automorphic"
 console.log(automorphic(3)) // "Not!!"
 console.log(automorphic(6)) // "Automorphic"
