1f03f62ed2ea5348d63e854e691c2d6ec3ef0b09
Irene Volodko <ice7aye@gmail.com>
2024-10-25 21:41:15 +0300

    description integers recreation one

--- 5kyu/integers-recreation-one.js
+++ 5kyu/integers-recreation-one.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Find all integers between m and n (m and n integers with 1 <= m <= n) such that the sum of their squared divisors is itself a square.
+// We will return an array of subarrays or of tuples (in C an array of Pair) or a string. The subarrays (or tuples or Pairs) will have two elements: first the number the squared divisors of which is a square and then the sum of the squared divisors.
+//
+// ✅ SOLUTION:
 function listSquared(m, n) {
     let result = [];
     for (let i = m; i <= n; i++) {
@@ -13,6 +18,7 @@ function listSquared(m, n) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(listSquared(1, 250)) // [[1, 1], [42, 2500], [246, 84100]])
 console.log(listSquared(42, 250)) // [[42, 2500], [246, 84100]])
 console.log(listSquared(250, 500)) // [[287, 84100]])
