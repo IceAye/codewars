0d30c020c5c3c06f106c7055adbcf53bdb2ac572
Irene Volodko <ice7aye@gmail.com>
2024-10-28 23:33:30 +0300

    add solution 2 vaporcode

--- 7kyu/vapocode.js
+++ 7kyu/vapocode.js
@@ -1,7 +1,12 @@
+// SOLUTION 1:
 function vaporcode(string) {
     return string.toUpperCase().split(" ").join("").split("").join("  ");
 }
 
+// SOLUTION 2:
+function vaporcode(string) {
+    return Array.from(string.replace(/\s+/g, "").toUpperCase()).join("  ");
+}
 
 console.log(vaporcode("Lets go to the movies")) // "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
 console.log(vaporcode("Why isn't my code working")) // "W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G"
