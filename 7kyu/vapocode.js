a5fc66ff1e2e113b23a229ae0375c043bfe8c82e
Irene Volodko <ice7aye@gmail.com>
2024-10-28 23:32:53 +0300

    add vaporcode

---
+++ 7kyu/vapocode.js
@@ -0,0 +1,7 @@
+function vaporcode(string) {
+    return string.toUpperCase().split(" ").join("").split("").join("  ");
+}
+
+
+console.log(vaporcode("Lets go to the movies")) // "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+console.log(vaporcode("Why isn't my code working")) // "W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G"
