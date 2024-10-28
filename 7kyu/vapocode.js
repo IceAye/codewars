c86b79a6879e59a71d9ca035a4fa953336523e43
Irene Volodko <ice7aye@gmail.com>
2024-10-28 23:34:31 +0300

    description vaporcode

--- 7kyu/vapocode.js
+++ 7kyu/vapocode.js
@@ -1,12 +1,17 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+// Note that spaces should be ignored in this case.
+//
+// ✅ SOLUTION 1:
 function vaporcode(string) {
     return string.toUpperCase().split(" ").join("").split("").join("  ");
 }
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 function vaporcode(string) {
     return Array.from(string.replace(/\s+/g, "").toUpperCase()).join("  ");
 }
 
+// 📌 TESTCASE:
 console.log(vaporcode("Lets go to the movies")) // "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
 console.log(vaporcode("Why isn't my code working")) // "W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G"
