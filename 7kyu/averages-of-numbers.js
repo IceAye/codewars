fd68648666a84299b7ceb44d36e67708bc640959
Irene Volodko <ice7aye@gmail.com>
2024-10-29 23:26:49 +0300

    description averages of numbers

--- 7kyu/averages-of-numbers.js
+++ 7kyu/averages-of-numbers.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// #Get the averages of these numbers
+// Write a method, that gets an array of integer-numbers and return an array of the averages of each integer-number and his follower, if there is one.
+// If the array has 0 or 1 values or is null, your method should return an empty array.
+//
+// ✅ SOLUTION:
 function averages(numbers) {
     if (!Array.isArray(numbers) || numbers.length < 2) return [];
     let result = [];
@@ -7,6 +13,7 @@ function averages(numbers) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(averages([2, 2, 2, 2, 2])); // [2, 2, 2, 2]
 console.log(averages([2, -2, 2, -2, 2])); // [0, 0, 0, 0]
 console.log(averages([1, 3, 5, 1, -10])); // [2, 4, 3, -4.5]
