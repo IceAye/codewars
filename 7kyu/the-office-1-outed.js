14aaf8c7c28f9dba4cc0ad4b7508d77214cc537b
Irene Volodko <ice7aye@gmail.com>
2024-10-30 01:25:03 +0300

    add office outed

---
+++ 7kyu/the-office-1-outed.js
@@ -0,0 +1,11 @@
+function outed(meet, boss) {
+    let score = 0;
+    for (const person in meet) {
+        person === boss ? score += meet[person] * 2 : score += meet[person];
+    }
+    return score / Object.keys(meet).length  <= 5 ? 'Get Out Now!' : 'Nice Work Champ!'
+}
+console.log(outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura')) // 'Get Out Now!'
+console.log(outed({'tim':1, 'jim':3, 'randy':9, 'sandy':6, 'andy':7, 'katie':6, 'laura':9, 'saajid':9, 'alex':9, 'john':9, 'mr':8}, 'katie')) // 'Nice Work Champ!'
+console.log(outed({'tim':2, 'jim':4, 'randy':0, 'sandy':5, 'andy':8, 'katie':6, 'laura':2, 'saajid':2, 'alex':3, 'john':2, 'mr':8}, 'john')) // 'Get Out Now!'
+
