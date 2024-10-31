fe875b39b8ac0df0b410b9a1afb67c44ac9048f1
Irene Volodko <ice7aye@gmail.com>
2024-10-31 22:34:03 +0300

    description multiply all elements in an array

--- 7kyu/currying-functions-multiply-all-elements-in-an-array.js
+++ 7kyu/currying-functions-multiply-all-elements-in-an-array.js
@@ -1,9 +1,16 @@
+// ❓ DESCRIPTION:
+// To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+// You must not mutate the original array.
+//
+// ✅ SOLUTION:
 function multiplyAll(array) {
     return function (int) {
         return array.map((item) => item * int);
     };
 }
 
+// 📌 TESTCASE:
 console.log(multiplyAll([1, 2, 3])(2)); // [2,4,6]
 console.log(multiplyAll([1, 2, 3])(1)); // [1,2,3]
 console.log(multiplyAll([1, 2, 3])(0)); // [0,0,0]
