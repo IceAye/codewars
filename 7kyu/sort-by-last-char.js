a70c20de410a9a602ceac0130cc8c25ce10525ae
Irene Volodko <ice7aye@gmail.com>
2024-11-01 23:18:15 +0300

    add sort by last char

---
+++ 7kyu/sort-by-last-char.js
@@ -0,0 +1,9 @@
+function last(x){
+    return x.split(" ").sort((a, b) => {
+        return (a.at(-1) > b.at(-1)) - (a.at(-1) < b.at(-1))
+    })
+}
+
+console.log(last('man i need a taxi up to ubud')) // ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+console.log(last('what time are we climbing up the volcano')) // ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']
+console.log(last('take me to semynak')) // ['take', 'me', 'semynak', 'to']
