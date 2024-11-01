6e72bce4fa090068d1fb97e5538b0435d38158ec
Irene Volodko <ice7aye@gmail.com>
2024-11-01 22:35:42 +0300

    add boredom score

---
+++ 7kyu/the-office-2-boredom-score.js
@@ -0,0 +1,35 @@
+const score = {
+    accounts: 1,
+    finance: 2,
+    canteen: 10,
+    regulation: 3,
+    trading: 6,
+    change: 6,
+    IS: 8,
+    retail: 5,
+    cleaning: 4,
+    "pissing about": 25
+}
+
+function boredom(staff){
+    let result = 0;
+    for (const name in staff) {
+        result += score[staff[name]];
+    }
+    return result <= 80 ? "kill me now" : result < 100 ? "i can handle this" : "party time!!";
+}
+
+console.log(boredom({tim: 'change', jim: 'accounts',
+    randy: 'canteen', sandy: 'change', andy: 'change', katie: 'IS',
+    laura: 'change', saajid: 'IS', alex: 'trading', john: 'accounts',
+    mr: 'finance' })) // 'kill me now'
+
+console.log(boredom({ tim: 'IS', jim: 'finance',
+    randy: 'pissing about', sandy: 'cleaning', andy: 'cleaning',
+    katie: 'cleaning', laura: 'pissing about', saajid: 'regulation',
+    alex: 'regulation', john: 'accounts', mr: 'canteen' })) // 'i can handle this'
+
+console.log(boredom({ tim: 'accounts', jim: 'accounts',
+    randy: 'pissing about', sandy: 'finance', andy: 'change',
+    katie: 'IS', laura: 'IS', saajid: 'canteen', alex: 'pissing about',
+    john: 'retail', mr: 'pissing about' })) // 'party time!!'
