4be9806b9784cba3509eb8b6772418fabfb2689b
Irene Volodko <ice7aye@gmail.com>
2024-11-02 12:28:41 +0300

    add coding meetup 6

---
+++ 7kyu/coding-meetup-6-can-they-code-in-the-same-language.js
@@ -0,0 +1,19 @@
+function isSameLanguage(list) {
+    return list.every(item => item.language === list[0].language);
+}
+
+
+let list1 = [
+    { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+
+let list2 = [
+    { firstName: 'Mariami', lastName: 'G.', country: 'Georgia', continent: 'Europe', age: 29, language: 'Python' },
+    { firstName: 'Mia', lastName: 'H.', country: 'Germany', continent: 'Europe', age: 39, language: 'Ruby' },
+    { firstName: 'Maria', lastName: 'I.', country: 'Greece', continent: 'Europe', age: 32, language: 'C' },
+];
+
+console.log(isSameLanguage(list1)) // true
+console.log(isSameLanguage(list2)) // false
