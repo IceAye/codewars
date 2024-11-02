fd2ae5268f9c56575ce8cfc0d4bf085c0b7edbdc
Irene Volodko <ice7aye@gmail.com>
2024-11-02 12:23:19 +0300

    description solution 2 coding meetup 5

--- 7kyu/coding-meetup-5-prepare-the-count-of-languages.js
+++ 7kyu/coding-meetup-5-prepare-the-count-of-languages.js
@@ -1,11 +1,15 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// You will be given an array of objects (associative arrays in PHP, table in COBOL, dictionaries in Python) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+// Your task is to return an object (associative array in PHP, table in COBOL, dictionary in Python) which includes the count of each coding language represented at the meetup.
+//
+// ✅ SOLUTION 1:
 function countLanguages(list) {
     let map = new Map();
     list.map(item => map.set(item.language, map.get(item.language) + 1 || 1));
     return Object.fromEntries(map);
 }
 
-// SOLUTION 2 (with reducer):
+// ✅ SOLUTION 2 (with reducer):
 function countLanguages(list) {
     return list.reduce((acc, curr) => {
         acc.hasOwnProperty(curr.language) ? acc[curr.language] += 1 : acc[curr.language] = 1;
@@ -13,6 +17,7 @@ function countLanguages(list) {
     }, {})
 }
 
+// 📌 TESTCASE:
 let list1 = [
     { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
     { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
