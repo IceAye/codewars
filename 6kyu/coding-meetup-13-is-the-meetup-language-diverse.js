77b9a0a3c8cdb2441bfc2074a61fec09db79a0c4
Irene Volodko <ice7aye@gmail.com>
2024-11-04 21:18:15 +0300

    description is the meetup language diverse

--- 6kyu/coding-meetup-13-is-the-meetup-language-diverse.js
+++ 6kyu/coding-meetup-13-is-the-meetup-language-diverse.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// You will be given an array of objects representing data about developers who have signed up to attend the next web development meetup that you are organising. Three programming languages will be represented: Python, Ruby and JavaScript.
+// Your task is to return either:
+// true if the number of meetup participants representing any of the three programming languages is ** at most 2 times higher than the number of developers representing any of the remaining programming languages**; or
+// false otherwise.
+// Notes:
+// The strings representing all three programming languages will always be formatted in the same way (e.g. 'JavaScript' will always be formatted with upper-case 'J' and 'S'.
+// The input array will always be valid and formatted as in the example above.
+// Each of the 3 programming languages will always be represented.
+//
+// ✅ SOLUTION:
 function isLanguageDiverse(list) {
     let sorted = list.reduce((acc, curr) => {
         if (curr.language === "JavaScript") acc[0]++;
@@ -8,6 +19,7 @@ function isLanguageDiverse(list) {
     return sorted[2] / sorted[0] <= 2;
 }
 
+// 📌 TESTCASE:
 let list1 = [
     { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'Python' },
     { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'Ruby' },
