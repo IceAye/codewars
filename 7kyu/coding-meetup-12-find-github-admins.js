18cdf54622549245c29d38be63b6f997c6495662
Irene Volodko <ice7aye@gmail.com>
2024-11-04 20:48:11 +0300

    description find github admins

--- 7kyu/coding-meetup-12-find-github-admins.js
+++ 7kyu/coding-meetup-12-find-github-admins.js
@@ -1,7 +1,19 @@
+// ❓ DESCRIPTION:
+// You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+// Given the following input array write a function that when executed as findAdmin(list1, 'JavaScript') returns only the JavaScript developers who are GitHub admins.
+// Notes:
+// The original order should be preserved.
+// If there are no GitHub admin developers in a given language then return an empty array [].
+// The input array will always be valid and formatted as in the example above.
+// The strings representing whether someone is a GitHub admin will always be formatted as 'yes' and 'no' (all lower-case).
+// The strings representing a given language will always be formatted in the same way (e.g. 'JavaScript' will always be formatted with upper-case 'J' and 'S'.
+//
+// ✅ SOLUTION:
 function findAdmin(list, lang) {
     return list.filter(person => person.language === lang && person.githubAdmin === "yes");
 }
 
+// TESTCASE:
 let list1 = [
     { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
     { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
@@ -9,6 +21,7 @@ let list1 = [
     { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
 ];
 
+// 📌 TESTCASE:
 console.log(findAdmin(list1, 'JavaScript')) // [{ firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },{ firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' }]
 console.log(findAdmin(list1, 'Ruby')) // []
 console.log(findAdmin(list1, 'Python')) // []
