3a9aa6732e6475114b789634f87fe51d441dc360
Irene Volodko <ice7aye@gmail.com>
2024-11-04 20:31:32 +0300

    add find the average

---
+++ 7kyu/coding-meetup-11-find-the-average.js
@@ -0,0 +1,16 @@
+function getAverageAge(list) {
+    return Math.round(list.reduce((acc, curr) => acc + curr.age, 0) / list.length);
+}
+
+let list1 = [
+    { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+    { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+];
+
+let list2 = [
+    { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 20, language: 'Ruby' },
+    { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 21, language: 'C' },
+];
+
+console.log(getAverageAge(list1)) // 50
+console.log(getAverageAge(list2)) // 21
