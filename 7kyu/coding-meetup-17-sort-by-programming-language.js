dd292946d2d7a7e98657272c2ba88490eb5af2e4
Irene Volodko <ice7aye@gmail.com>
2024-11-06 00:23:13 +0300

    description sort by programming language

--- 7kyu/coding-meetup-17-sort-by-programming-language.js
+++ 7kyu/coding-meetup-17-sort-by-programming-language.js
@@ -1,7 +1,16 @@
+// ❓ DESCRIPTION:
+// You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+// Given the following input array WRite a function that returns the array sorted alphabetically by the programming language. In case there are some developers that code in the same language, sort them alphabetically by the first name:
+// Notes:
+// The input array will always be valid and formatted as in the example above.
+// The array does not include developers coding in the same language and sharing the same name.
+//
+// ✅ SOLUTION:
 function sortByLanguage(list) {
     return list.sort((a, b) => a.language.localeCompare(b.language) || a.firstName.localeCompare(b.firstName));
 }
 
+// 📌 TESTCASE:
 let list1 = [
     { firstName: 'Nikau', lastName: 'R.', country: 'New Zealand', continent: 'Oceania', age: 39, language: 'Ruby' },
     { firstName: 'Precious', lastName: 'G.', country: 'South Africa', continent: 'Africa', age: 22, language: 'JavaScript' },
