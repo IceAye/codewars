3421ce72c1424496528af1961d07d730e6cac61b
Irene Volodko <ice7aye@gmail.com>
2024-11-06 23:34:25 +0300

    description is the string uppercase

--- 8kyu/is-the-string-uppercase.js
+++ 8kyu/is-the-string-uppercase.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Create a method to see whether the string is ALL CAPS.
+//
+// ✅ SOLUTION:
 String.prototype.isUpperCase = function() {
     return this.toString() === this.toUpperCase();
 }
