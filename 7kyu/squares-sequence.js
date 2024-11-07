b484f72eaacd1941174294ebc4517e68e8a85aee
Irene Volodko <ice7aye@gmail.com>
2024-11-07 23:24:24 +0300

    description squares sequence

--- 7kyu/squares-sequence.js
+++ 7kyu/squares-sequence.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+//
+// ✅ SOLUTION:
 function squares(x, n) {
     if (n <= 0) return [];
     let result = [x];
@@ -10,6 +14,7 @@ function squares(x, n) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(squares(2,5)) // [2,4,16,256,65536]
 console.log(squares(3,3)) // [3,9,81]
 console.log(squares(5,3)) // [5,25,625]
