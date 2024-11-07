ddbe56900d1d52beb23e0f48c811f804d2f969c3
Irene Volodko <ice7aye@gmail.com>
2024-11-07 23:13:31 +0300

    add find count of most frequent item in an array

---
+++ 7kyu/find-count-of-most-frequent-item-in-an-array.js
@@ -0,0 +1,9 @@
+function mostFrequentItemCount(collection) {
+    let hashMap = new Map();
+    collection.map(item => hashMap.set(item, hashMap.get(item) + 1 || 1));
+    return Math.max(...hashMap.values()) | 0;
+}
+
+console.log(mostFrequentItemCount([3, -1, -1])) // 2
+console.log(mostFrequentItemCount([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3])) // 5
+console.log(mostFrequentItemCount([])) // 0
