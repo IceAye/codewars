dc2bd80c2165c31ef066e65915c3bf63aae1a69e
Irene Volodko <ice7aye@gmail.com>
2024-11-08 16:32:24 +0300

    description char code calculation

--- 7kyu/char-code-calculation.js
+++ 7kyu/char-code-calculation.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1.
+// Then replace any incidence of the number 7 with the number 1, and call this number 'total2'.
+// Then return the difference between the sum of the digits in total1 and total2.
+//
+// ✅ SOLUTION:
+
 function calc(x) {
     let number = [...x].map((item) => item.charCodeAt(0)).join("")
     let total1 = Array.from(number, Number).reduce((acc, curr) => acc + curr, 0);
@@ -5,6 +12,7 @@ function calc(x) {
     return total1 - total2;
 }
 
+// 📌 TESTCASE:
 console.log(calc('abcdef')) // 6
 console.log(calc('ifkhchlhfd')) // 6
 console.log(calc('aaaaaddddr')) // 30
