733e38c2211bdae035142f3522e5368782ac9bd5
Irene Volodko <ice7aye@gmail.com>
2024-11-08 16:39:14 +0300

    description functional addition

--- 7kyu/functional-addition.js
+++ 7kyu/functional-addition.js
@@ -1,8 +1,13 @@
+// ❓ DESCRIPTION:
+// Create a function add(n)/Add(n) which returns a function that always adds n to any number
+// Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+//
+// ✅ SOLUTION:
 function add(n) {
     return (x) => {
         return n + x;
     }
 }
 
-
+// 📌 TESTCASE:
 console.log(add(1)(3)) // 4
