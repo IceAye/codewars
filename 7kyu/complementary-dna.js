7496132b1e32e83ac5713be5a419a69e63961985
Irene Volodko <ice7aye@gmail.com>
2024-11-09 22:03:25 +0300

    add solution 2 complementary dna

--- 7kyu/complementary-dna.js
+++ 7kyu/complementary-dna.js
@@ -2,7 +2,7 @@
 // Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
 // In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 //
-// ✅ SOLUTION:
+// ✅ SOLUTION 1:
 function dnaStrand(dna){
     let strand = '';
     for (let letter of dna) {
@@ -12,6 +12,13 @@ function dnaStrand(dna){
         if (letter ==="G") {strand += "C"}
     }
     return strand;
+}
+
+// ✅ SOLUTION 2:
+let pairs = {'A':'T','T':'A','C':'G','G':'C'};
+
+function DNAStrand(dna){
+    return dna.split('').map(function(v){ return pairs[v] }).join('');
 }
 
 // 📌 TESTCASE:
