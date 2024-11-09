6a530592263ee0dd82c45e8ced68fc31c7065a3f
Irene Volodko <ice7aye@gmail.com>
2024-11-09 22:05:03 +0300

    add credit card mask

---
+++ 7kyu/credit card-mask.js
@@ -0,0 +1,7 @@
+function maskify(cc) {
+    return cc.slice(0, -4).replaceAll(/[\w-]/gi, '#') + cc.slice(-4)
+}
+
+console.log(maskify('4556364607935616')) // '############5616'
+console.log(maskify('1')) // '1'
+console.log(maskify('11111')) // '#1111'
