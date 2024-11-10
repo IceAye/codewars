1a9a6d45e60e363b10c89ae5322e9f80f97224e1
Irene Volodko <ice7aye@gmail.com>
2024-11-10 21:20:40 +0300

    add array element parity

---
+++ 7kyu/array-element-parity.js
@@ -0,0 +1,9 @@
+function solve(arr){
+    return arr.find(item => !arr.includes(-item))
+}
+
+console.log(solve([1,-1,2,-2,3])) // 3
+console.log(solve([-3,1,2,3,-1,-4,-2])) // -4
+console.log(solve([1,-1,2,-2,3,3])) // 3
+console.log(solve([-110,110,-38,-38,-62,62,-38,-38,-38])) // -38
+console.log(solve([ -9,-105,-9,-9,-9,-9,105])) // -9
