3c3321c9ec898d3aaa1916837240c1bb87cc250a
Irene Volodko <ice7aye@gmail.com>
2024-11-10 20:43:02 +0300

    add greatest common divisor

---
+++ 7kyu/greatest-common-divisor.js
@@ -0,0 +1,9 @@
+function mygcd(x,y){
+    return y === 0 ? x : mygcd(y, x % y)
+}
+
+
+console.log(mygcd(30, 12)) // 6
+console.log(mygcd(36, 12)) // 12
+console.log(mygcd( 8,  9)) //  1
+console.log(mygcd( 1,  1)) //  1
