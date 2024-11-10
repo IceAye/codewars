0373bf93c2a036bf8b842e69a6c4ae3370074428
Irene Volodko <ice7aye@gmail.com>
2024-11-10 20:44:05 +0300

    description greatest common divisor

--- 7kyu/greatest-common-divisor.js
+++ 7kyu/greatest-common-divisor.js
@@ -1,8 +1,14 @@
+// ❓ DESCRIPTION:
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+//
+// ✅ SOLUTION:
+
 function mygcd(x,y){
     return y === 0 ? x : mygcd(y, x % y)
 }
 
-
+// 📌 TESTCASE:
 console.log(mygcd(30, 12)) // 6
 console.log(mygcd(36, 12)) // 12
 console.log(mygcd( 8,  9)) //  1
