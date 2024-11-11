69addd3967f37d80ad658d155bfcfb088889fe04
Irene Volodko <ice7aye@gmail.com>
2024-11-11 19:27:37 +0300

    description reverse every other word in the string

--- 6kyu/reverse-everey-other-word-in-the-string.js
+++ 6kyu/reverse-everey-other-word-in-the-string.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+//
+// ✅ SOLUTION:
+
 function reverse(str) {
     return str
         .trim()
@@ -6,6 +11,7 @@ function reverse(str) {
         .join(" ");
 }
 
+// 📌 TESTCASE:
 console.log(reverse("Reverse this string, please!")) // "Reverse siht string, !esaelp"
 console.log(reverse("I really don't like reversing strings!")) // "I yllaer don't ekil reversing !sgnirts"
 console.log(reverse("  many    spaces   here  ")) // many secaps here
