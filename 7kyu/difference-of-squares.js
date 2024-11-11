bf8dc96e4476af039b4cf0e8290567d4d3d061d2
Irene Volodko <ice7aye@gmail.com>
2024-11-11 18:39:52 +0300

    description difference of squares

--- 7kyu/difference-of-squares.js
+++ 7kyu/difference-of-squares.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Recreation of Project Euler problem #6
+// Find the difference between the square of the sum of the first n natural numbers (1 <= n <= 100) and the sum of their squares.
+//
+// ✅ SOLUTION:
 function differenceOfSquares(n) {
     let resultForSquared = 0;
     let resultOfSquares = 0;
@@ -8,6 +13,7 @@ function differenceOfSquares(n) {
     return Math.pow(resultForSquared, 2) - resultOfSquares;
 }
 
+// 📌 TESTCASE:
 console.log(differenceOfSquares(5)) // 170
 console.log(differenceOfSquares(10)) // 2640
 console.log(differenceOfSquares(100)) // 25164150
