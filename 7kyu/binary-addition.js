662b28f9a1a933f727158564a125cd4195578f55
Irene Volodko <ice7aye@gmail.com>
2024-11-12 23:57:55 +0300

    description binary addition

--- 7kyu/binary-addition.js
+++ 7kyu/binary-addition.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+// The binary number returned should be a string.
+//
+// ✅ EXAMPLES:
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+//
+// 📌 SOLUTION:
+
 function addBinary(a,b) {
     return (a + b).toString(2)
 }
