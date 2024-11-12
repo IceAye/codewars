de3fa0cd3866229866dfbbf88c3d3b3af9106522
Irene Volodko <ice7aye@gmail.com>
2024-11-12 23:49:28 +0300

    description two to one

--- 7kyu/two-to-one.js
+++ 7kyu/two-to-one.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+//
+// ✅ SOLUTION:
 function longest(s1, s2) {
     let arr = [...s1, ...s2]
     return Array.from(new Set(arr)).sort().join('');
