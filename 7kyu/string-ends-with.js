6befa140f82dd9e79c2ff40fdbf5d895764efb19
Irene Volodko <ice7aye@gmail.com>
2024-11-12 23:52:16 +0300

    description string ends with

--- 7kyu/string-ends-with.js
+++ 7kyu/string-ends-with.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+//
+// ✅ SOLUTION:
 function solution(str, ending){
     return str.endsWith(`${ending}`);
 }
