a7552217b233d995a260bbe62be5928229937f28
Irene Volodko <ice7aye@gmail.com>
2024-11-13 23:33:57 +0300

    description reverse words

--- 7kyu/reverse-words.js
+++ 7kyu/reverse-words.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+//
+// 📌 EXAMPLES
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+//
+// ✅ SOLUTION:
 function reverseWords(str) {
     let arr = str.split(' ')
     let arrReverse = []
