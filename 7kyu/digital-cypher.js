bd125e348c6ff564cf1ceff4de912ad6f36c1afa
Irene Volodko <ice7aye@gmail.com>
2024-11-14 19:44:29 +0300

    add digital cypher

---
+++ 7kyu/digital-cypher.js
@@ -0,0 +1,9 @@
+function encode(str, n) {
+    const abc = "0abcdefghijklmnopqrstuvwxyz";
+    let key = n.toString();
+    return [...str].map((item, index) => abc.indexOf(item) + +key[index % key.length]);
+}
+
+
+console.log(encode("scout",1939)) // [ 20, 12, 18, 30, 21]
+console.log(encode("masterpiece",1939)) // [ 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8]
