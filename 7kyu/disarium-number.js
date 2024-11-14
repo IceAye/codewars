6046a670f370dfbc8b7838e52414409c8e173861
Irene Volodko <ice7aye@gmail.com>
2024-11-14 18:50:04 +0300

    description disarium number

--- 7kyu/disarium-number.js
+++ 7kyu/disarium-number.js
@@ -1,9 +1,14 @@
+// ❓ DESCRIPTION:
+// Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
+// Given a number, Find if it is Disarium or not .
+//
+// ✅ SOLUTION:
 function disariumNumber(n){
     return Array.from(n.toString(), Number).reduce((acc, curr, idx) => acc + curr ** (idx + 1), 0) === n
         ? "Disarium !!" : "Not !!";
 }
 
-
+// 📌 TESTCASE:
 console.log(disariumNumber(89)) // "Disarium !!"
 console.log(disariumNumber(564)) // "Not !!"
 console.log(disariumNumber(1024)) // "Not !!"
