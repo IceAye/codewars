eb07b4143f1ae73317b1277b64407f0938c79c45
Irene Volodko <ice7aye@gmail.com>
2024-11-17 22:47:19 +0300

    add description

--- 7kyu/seven-ate-9.js
+++ 7kyu/seven-ate-9.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Write a function that removes every lone 9 that is inbetween 7s.
+//
+// ✅ SOLUTION:
 function sevenAte9(str) {
     return str.replace(/79(?=7)/g, "7");
 }
 
+// 📌 TESTCASE:
 console.log(sevenAte9('797')) //  '77'
 console.log(sevenAte9('7979797')) // '7777'
 console.log(sevenAte9('165561786121789797')) // '16556178612178977'
