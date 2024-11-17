00ecbf30977bfbb6215736b783c59ef0640b84d3
Irene Volodko <ice7aye@gmail.com>
2024-11-17 22:25:39 +0300

    add solution

---
+++ 7kyu/substituting-variables-into-strings-padded-numbers.js
@@ -0,0 +1,8 @@
+function solution(value){
+    return `Value is ${value.toString().padStart(5, 0)}`;
+}
+
+console.log(solution(5)) // "Value is 00005"
+console.log(solution(1204)) // "Value is 01204"
+console.log(solution(109)) // "Value is 00109"
+console.log(solution(0)) // "Value is 00000"
