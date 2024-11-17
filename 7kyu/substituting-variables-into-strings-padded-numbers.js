12ee2815b60b4aa2c08192f939ae767778949ae6
Irene Volodko <ice7aye@gmail.com>
2024-11-17 22:26:37 +0300

    add description

--- 7kyu/substituting-variables-into-strings-padded-numbers.js
+++ 7kyu/substituting-variables-into-strings-padded-numbers.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+//
+// ✅ SOLUTION:
 function solution(value){
     return `Value is ${value.toString().padStart(5, 0)}`;
 }
 
+// 📌 TESTCASE:
 console.log(solution(5)) // "Value is 00005"
 console.log(solution(1204)) // "Value is 01204"
 console.log(solution(109)) // "Value is 00109"
