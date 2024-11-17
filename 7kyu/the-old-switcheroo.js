7f8c1820175d6755039b1a4f0b4d41d7f8bfcc63
Irene Volodko <ice7aye@gmail.com>
2024-11-17 22:14:13 +0300

    add solution 1

---
+++ 7kyu/the-old-switcheroo.js
@@ -0,0 +1,12 @@
+function vowel2index(str) {
+    let newString = "";
+    for (let i = 0; i < str.length; i++) {
+        /[euioa]/i.test(str[i]) ? newString += i + 1 : newString += str[i];
+    }
+    return newString;
+}
+
+console.log(vowel2index('this is my string')) // 'th3s 6s my str15ng'
+console.log(vowel2index('Codewars is the best site in the world')) // 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+console.log(vowel2index('Tomorrow is going to be raining')) //  'T2m4rr7w 10s g1415ng t20 b23 r2627n29ng'
+console.log(vowel2index('')) //  ''
