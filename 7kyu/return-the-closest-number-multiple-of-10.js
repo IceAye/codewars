c8dc6c38a0f83985fbcad5cf81915bec79ba5d86
Irene Volodko <ice7aye@gmail.com>
2024-11-18 23:04:36 +0300

    add description

--- 7kyu/return-the-closest-number-multiple-of-10.js
+++ 7kyu/return-the-closest-number-multiple-of-10.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Given a number return the closest number to it that is divisible by 10.
+//
+// ✅ SOLUTION:
 const closestMultiple10 = num => {
     return Math.round(num / 10) * 10;
 };
 
+// 📌 TESTCASE:
 console.log(closestMultiple10(22)) // 20
 console.log(closestMultiple10(25)) // 30
 console.log(closestMultiple10(37)) // 40
