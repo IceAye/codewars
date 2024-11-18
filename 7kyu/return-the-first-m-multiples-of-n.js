a466502592a05faf77dccbc63ae3a356f9ff55f0
Irene Volodko <ice7aye@gmail.com>
2024-11-18 23:18:43 +0300

    add description

--- 7kyu/return-the-first-m-multiples-of-n.js
+++ 7kyu/return-the-first-m-multiples-of-n.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Implement a function that takes two numbers m and n and returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+//
+// ✅ SOLUTION:
 function multiples(m, n){
     let result = [];
     let i = 1;
@@ -8,4 +12,5 @@ function multiples(m, n){
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(multiples(3, 5)) // [5, 10, 15]
