aef4d58b61b4302ca38feb3f2142e2d2c75f1fa5
Irene Volodko <ice7aye@gmail.com>
2024-11-18 22:56:39 +0300

    add dedscription

--- 7kyu/array-series-5-product-array.js
+++ 7kyu/array-series-5-product-array.js
@@ -1,4 +1,7 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+//
+// ✅ SOLUTION 1:
 function productArray(numbers){
     let result = [];
     for (const number of numbers) {
@@ -8,11 +11,12 @@ function productArray(numbers){
     return result;
 }
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 function productArray(numbers){
     return numbers.map(number => numbers.reduce((acc,curr) => acc * curr) / number)
 }
 
+// 📌 TESTCASE:
 console.log(productArray([12,20])) // [20,12]
 console.log(productArray([3,27,4,2])) // [216,24,162,324]
 console.log(productArray([13,10,5,2,9])) // [900,1170,2340,5850,1300]
