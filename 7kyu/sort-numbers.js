8b0362e4838e6005541da382421533db9b6fffcf
Irene Volodko <ice7aye@gmail.com>
2024-11-19 23:29:13 +0300

    add description

--- 7kyu/sort-numbers.js
+++ 7kyu/sort-numbers.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+//
+// 📌 EXAMPLE:
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+//
+// ✅ SOLUTION:
 function solution(nums){
     return nums === null ? [] : nums.sort((a, b) => a - b)
 }
