9383d79e1e7a315213480110500ff9b0f482fa84
Irene Volodko <ice7aye@gmail.com>
2024-11-19 23:36:26 +0300

    add description

--- 7kyu/small-enough-beginner.js
+++ 7kyu/small-enough-beginner.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+// You can assume all values in the array are numbers.
+//
+// ✅ SOLUTION:
 function smallEnough(a, limit){
     return a.filter(item => item <= limit).toString() === a.toString()
 }
