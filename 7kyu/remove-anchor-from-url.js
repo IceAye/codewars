ad2ebec013b0d40544ef79fba37943e2f522d0df
Irene Volodko <ice7aye@gmail.com>
2024-11-19 23:33:32 +0300

    add solution 2

--- 7kyu/remove-anchor-from-url.js
+++ 7kyu/remove-anchor-from-url.js
@@ -1,3 +1,9 @@
+// SOLUTION 1:
 function removeUrlAnchor(url){
     return url.indexOf('#') === -1 ? url : url.slice(0, url.indexOf('#'))
+}
+
+// SOLUTION 2:
+function removeUrlAnchor(url){
+    return url.split('#')[0];
 }
