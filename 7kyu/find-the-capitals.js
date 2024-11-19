79d2f9ef3efc3267a8549003ea1386c6bea24ea2
Irene Volodko <ice7aye@gmail.com>
2024-11-19 23:39:31 +0300

    add description

--- 7kyu/find-the-capitals.js
+++ 7kyu/find-the-capitals.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+//
+// 📌 EXAMPLE
+// "CodEWaRs" --> [0,3,4,6]
+//
+// ✅ SOLUTION:
 let capitals = function ( word ) {
     let result = []
 
