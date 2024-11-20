8d340afe981544c55d01623f1c5cdaca15b9af6d
Irene Volodko <ice7aye@gmail.com>
2024-11-20 17:53:28 +0300

    add description

--- 7kyu/return-string-of-first-characters.js
+++ 7kyu/return-string-of-first-characters.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// In this exercise, a string is passed to a method and a new string has to be returned with the first character of each word in the string.
+// For example:
+// "This Is A Test" ==> "TIAT"
+// Strings will only contain letters and spaces, with exactly 1 space between words, and no leading/trailing spaces.
+//
+// ✅ SOLUTION:
 function makeString(s){
     return s.split(" ").reduce((acc, curr) => {
         acc += curr[0];
@@ -5,6 +12,7 @@ function makeString(s){
     }, "")
 }
 
+// 📌 TESTCASE:
 console.log(makeString("sees eyes xray yoat")) // "sexy"
 console.log(makeString("brown eyes are nice")) // "bean"
 console.log(makeString("cars are very nice")) // "cavn"
