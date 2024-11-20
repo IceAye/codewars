9ea905128e88998576891d726d9e7f238d693470
Irene Volodko <ice7aye@gmail.com>
2024-11-20 23:51:57 +0300

    add solution 1

---
+++ 7kyu/a-letters-best-friend.js
@@ -0,0 +1,17 @@
+function bestFriend(txt, a, b) {
+    let arr = txt.split('')
+    let arrIndex = []
+    let index = arr.indexOf(a)
+    while (index !== -1) {
+        arrIndex.push(index)
+        index = arr.indexOf(a, index + 1)
+    }
+
+    let arrB = 0
+
+    for (let i = 0; i < arrIndex.length; i++) {
+        if (arr[arrIndex[i] + 1] === b) { arrB += 1}
+    }
+
+    return arrB === arrIndex.length
+}
