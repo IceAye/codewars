038188113e575c633f11065a2cff5590bd753459
Irene Volodko <ice7aye@gmail.com>
2024-11-20 23:52:56 +0300

    add description

--- 7kyu/a-letters-best-friend.js
+++ 7kyu/a-letters-best-friend.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given a string, return if all occurrences of a given letter are always immediately followed by the other given letter.
+//
+// ✅ SOLUTION 1:
 function bestFriend(txt, a, b) {
     let arr = txt.split('')
     let arrIndex = []
@@ -16,6 +20,7 @@ function bestFriend(txt, a, b) {
     return arrB === arrIndex.length
 }
 
+// ✅ SOLUTION 2:
 function bestFriend(txt, a, b) {
     for(let i = 0; i < txt.length; i++){
         if(txt[i] === a && txt[i + 1] !== b) return false
