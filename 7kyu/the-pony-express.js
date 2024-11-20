f704cfca6370c7d107bdcfed32bdc4a0a1cb1218
Irene Volodko <ice7aye@gmail.com>
2024-11-20 23:56:38 +0300

    add description

--- 7kyu/the-pony-express.js
+++ 7kyu/the-pony-express.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// The Pony Express was a mail service operating in the US in 1859-60.
+// It reduced the time for messages to travel between the Atlantic and Pacific coasts to about 10 days, before it was made obsolete by the transcontinental telegraph.
+// How it worked
+// There were a number of stations, where:
+// The rider switched to a fresh horse and carried on, or
+// The mail bag was handed over to the next rider
+// Kata Task
+// stations is a list/array of distances (miles) from one station to the next along the Pony Express route.
+// Implement the riders method/function, to return how many riders are necessary to get the mail from one end to the other.
+// NOTE: Each rider travels as far as he can, but never more than 100 miles.
+//
+// ✅ SOLUTION 1:
 function riders(stations) {
 
     let totalRiders = 1
@@ -15,6 +28,7 @@ function riders(stations) {
     return totalRiders
 }
 
+// ✅ SOLUTION 2:
 function riders(stations, number = 100) {
     return stations.map( el => number + el > 100 ? [100, number = el][0] : [0, number += el]).filter(e => e >= 100).length
 }
