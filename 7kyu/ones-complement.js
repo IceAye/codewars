2d2e19b30a8c47ce6aa13c66177e8c099cb1a6e2
Irene Volodko <ice7aye@gmail.com>
2024-11-21 22:42:42 +0300

    add description

--- 7kyu/ones-complement.js
+++ 7kyu/ones-complement.js
@@ -1,7 +1,13 @@
+// ❓ DESCRIPTION:
+// The Ones' Complement of a binary number is the number obtained by swapping all the 0s for 1s and all the 1s for 0s.
+// For any given binary number,formatted as a string, return the Ones' Complement of that number.
+//
+// ✅ SOLUTION:
 function onesComplement(n) {
     return n.replace(/./g, c => c === "0" ? "1" : "0");
-};
+}
 
+// 📌 TESTCASE:
 console.log(onesComplement("0")) // "1"
 console.log(onesComplement("1")) // "0"
 console.log(onesComplement("01")) // "10"
