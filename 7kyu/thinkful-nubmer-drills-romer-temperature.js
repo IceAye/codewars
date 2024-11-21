fecc2173b15a13d2065546d01ce5c24f7e645ba8
Irene Volodko <ice7aye@gmail.com>
2024-11-21 13:03:35 +0300

    add solution

---
+++ 7kyu/thinkful-nubmer-drills-romer-temperature.js
@@ -0,0 +1,7 @@
+function celsiusToRomer(temp) {
+    return 21 / 40 * temp + 7.5
+}
+
+console.log(celsiusToRomer(24)) // 20.1
+console.log(celsiusToRomer(8)) // 11.7
+console.log(celsiusToRomer(29)) // 22.725
