6d494b49efc352c8d1403ce3c5b9c1274cfe3cc7
Irene Volodko <ice7aye@gmail.com>
2024-11-22 16:48:08 +0300

    add description

--- 7kyu/collision-detection.js
+++ 7kyu/collision-detection.js
@@ -1,9 +1,15 @@
+// ❓ DESCRIPTION:
+// Create a function to determine whether or not two circles are colliding. You will be given the position of both circles in addition to their radii:
+// If a collision is detected, return true. If not, return false.
+//
+// ✅ SOLUTION:
 function collision(x1, y1, radius1, x2, y2, radius2) {
     let distance = radius1 + radius2;
     let center = Math.hypot((x2 - x1), (y2 - y1));
     return center < distance;
 }
 
+// 📌 TESTCASE:
 console.log(collision(1, 1, 1, 1.1, 1.1, 0.1))  // true
 console.log(collision(-1, 1, 10, -10.1, 1.1, 1))  // true
 console.log(collision(-5, 5, 5.0001, 5, -5, 5*Math.sqrt(5)))  // true
