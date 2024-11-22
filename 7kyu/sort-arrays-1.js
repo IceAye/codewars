ec28596821365dc8adb05932842b977f9eda2f81
Irene Volodko <ice7aye@gmail.com>
2024-11-22 21:55:29 +0300

    add solution

---
+++ 7kyu/sort-arrays-1.js
@@ -0,0 +1,6 @@
+sortme = function( names ){
+    return names.sort();
+}
+
+console.log(sortme(['one', 'two', 'three' ])) // ["one", "three", "two"]
+console.log(sortme(["PVFv","bmsx","nciE","oEjR","sYDN","ueVjt"])) // ["PVFv","bmsx","nciE","oEjR","sYDN","ueVjt"]
