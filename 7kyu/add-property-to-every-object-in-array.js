e72378f9f9f3398afbea96dd836f6043464654df
Irene Volodko <ice7aye@gmail.com>
2024-11-22 22:23:57 +0300

    add description

--- 7kyu/add-property-to-every-object-in-array.js
+++ 7kyu/add-property-to-every-object-in-array.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+//
+// ✅ SOLUTION:
+
 let questions = [{
     question: "What's the currency of the USA?",
     choices: ["US dollar", "Ruble", "Horses", "Gold"],
