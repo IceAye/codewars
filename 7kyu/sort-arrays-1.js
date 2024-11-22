83397a0dcb00c25c6f6b386e3f1760ef1bdf120d
Irene Volodko <ice7aye@gmail.com>
2024-11-22 21:56:40 +0300

    add description

--- 7kyu/sort-arrays-1.js
+++ 7kyu/sort-arrays-1.js
@@ -1,6 +1,11 @@
+// ❓ DESCRIPTION:
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+//
+// ✅ SOLUTION:
 sortme = function( names ){
     return names.sort();
 }
 
+// 📌 TESTCASE:
 console.log(sortme(['one', 'two', 'three' ])) // ["one", "three", "two"]
 console.log(sortme(["PVFv","bmsx","nciE","oEjR","sYDN","ueVjt"])) // ["PVFv","bmsx","nciE","oEjR","sYDN","ueVjt"]
