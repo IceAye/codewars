c33afede45fff4133f691cc16e72d9970a158a44
Irene Volodko <ice7aye@gmail.com>
2024-11-24 00:17:29 +0300

    add description

--- 4kyu/most-frequently-used-words-in-a-text.js
+++ 4kyu/most-frequently-used-words-in-a-text.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// Write a function that, given a string of text (possibly with punctuation and line-breaks), returns an array of the top-3 most occurring words, in descending order of the number of occurrences.
+// Assumptions:
+// A word is a string of letters (A to Z) optionally containing one or more apostrophes (') in ASCII.
+// Apostrophes can appear at the start, middle or end of a word ('abc, abc', 'abc', ab'c are all valid)
+// Any other characters (e.g. #, \, / , . ...) are not part of a word and should be treated as whitespace.
+// Matches should be case-insensitive, and the words in the result should be lowercased.
+// Ties may be broken arbitrarily.
+// If a text contains fewer than three unique words, then either the top-2 or top-1 words should be returned, or an empty array if a text contains no words.
+//
+// ✅ SOLUTION:
 function topThreeWords(text) {
     let array = text.replace(/\s+/g, " ").replace(/[:/,.]/g, "").trim().split(" ");
     let map = new Map();
@@ -9,6 +20,7 @@ function topThreeWords(text) {
                              }, [])
 }
 
+// 📌 TESTCASE:
 console.log(topThreeWords("a a a  b  c c  d d d d  e e e e e"))  // ['e','d','a']
 console.log(topThreeWords("a a a c b b"))  // ['a','b','c']
 console.log(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"))  // ['e','ddd','aa']
