b9d7e12b9d99a93b9c3e810d734a4be3516fb003
Irene Volodko <ice7aye@gmail.com>
2024-11-23 23:20:48 +0300

    add description

--- 7kyu/nth-root-of-a-number.js
+++ 7kyu/nth-root-of-a-number.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Given two numbers x and n, calculate the (positive) nth root of x; this means that being r = result, r^n = x
+//
+// ✅ SOLUTION:
 function root(x, n) {
     return x**(1/n);
 }
 
+// 📌 TESTCASE:
 console.log(root(8, 3)) // 2
 console.log(root( 6.25, 2)) // 2.25
 console.log(root(256, 4)) // 4
