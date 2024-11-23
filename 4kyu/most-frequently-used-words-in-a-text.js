481e1bfede13e3cae98958b03ee5810772298ccc
Irene Volodko <ice7aye@gmail.com>
2024-11-24 00:16:22 +0300

    add solution

---
+++ 4kyu/most-frequently-used-words-in-a-text.js
@@ -0,0 +1,25 @@
+function topThreeWords(text) {
+    let array = text.replace(/\s+/g, " ").replace(/[:/,.]/g, "").trim().split(" ");
+    let map = new Map();
+    array.map(item => map.set(item.toLowerCase(), map.get(item.toLowerCase()) + 1 || 1));
+    return [...map.entries()].sort((a,b) => b[1] - a[1]).slice(0,3)
+                             .reduce((acc, curr) => {
+                                 if (/\w/g.test(curr[0])) acc.push(curr[0]);
+                                 return acc;
+                             }, [])
+}
+
+console.log(topThreeWords("a a a  b  c c  d d d d  e e e e e"))  // ['e','d','a']
+console.log(topThreeWords("a a a c b b"))  // ['a','b','c']
+console.log(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"))  // ['e','ddd','aa']
+console.log(topThreeWords("  //wont won't won't "))  // ["won't", "wont"]
+console.log(topThreeWords("  , e   .. "))  // ["e"]
+console.log(topThreeWords("  ...  "))  // []
+console.log(topThreeWords("  '  "))  // []
+console.log(topThreeWords(
+    `In a village of La Mancha, the name of which I have no desire to call to
+ mind, there lived not long since one of those gentlemen that keep a lance
+ in the lance-rack, an old buckler, a lean hack, and a greyhound for
+ coursing. An olla of rather more beef than mutton, a salad on most
+ nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+ on Sundays, made away with three-quarters of his income.`))  // ['a','of','on']
