3539d8266962873218e4a82f5fff346d4fef87a5
Irene Volodko <ice7aye@gmail.com>
2024-11-24 20:49:28 +0300

    add description

--- 7kyu/find-the-sum-of-the-roots-of-a-quadratic-equation.js
+++ 7kyu/find-the-sum-of-the-roots-of-a-quadratic-equation.js
@@ -1,9 +1,14 @@
+// ❓ DESCRIPTION:
+// Implement function which will return sum of roots of a quadratic equation rounded to 2 decimal places, if there are any possible roots, else return None/null/nil/nothing. If you use discriminant,when discriminant = 0, x1 = x2 = root => return sum of both roots. There will always be valid arguments.
+//
+// ✅ SOLUTION:
 function roots(a,b,c){
     let discriminant = b*b - 4 * a * c;
     if (discriminant < 0) return null;
     return +(-b/a).toFixed(2);
 }
 
+// 📌 TESTCASE:
 console.log(roots(1, -35, -23)) // 35
 console.log(roots(6, 0, -24)) // 0
 console.log(roots(-5, 21, 0)) // 4.2
