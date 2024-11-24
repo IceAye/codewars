f032f88f0236e41d5df8b9182fbba236acc8f1cd
Irene Volodko <ice7aye@gmail.com>
2024-11-24 20:02:28 +0300

    add solution

---
+++ 7kyu/digitize.js
@@ -0,0 +1,9 @@
+function digitize(n) {
+    return Array.from(n.toString(), Number);
+}
+
+console.log(digitize(123)) // [1,2,3]
+console.log(digitize(1)) // [1]
+console.log(digitize(0)) // [0]
+console.log(digitize(1230)) // [1,2,3, 0]
+console.log(digitize(8675309)) // [8,6,7,5,3,0,9]
