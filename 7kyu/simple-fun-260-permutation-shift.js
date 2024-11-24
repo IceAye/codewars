2fd45fc70049bb3b7c2fe28ada16471978cabae1
Irene Volodko <ice7aye@gmail.com>
2024-11-24 21:53:37 +0300

    add description

--- 7kyu/simple-fun-260-permutation-shift.js
+++ 7kyu/simple-fun-260-permutation-shift.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Define the permutation element shift as the difference between the element's value and its index. For example, for permutation [1, 0, 2, 3] of array [0, 1, 2, 3] shifts for the respective elements are [1, -1, 0, 0].
+// Define the permutation shift as the difference between the maximal and the minimal element shifts among all of the permutation elements.
+// Given a permutation of integers from 0 to n - 1 for some integer n, find its shift.
+//
+// ✅ SOLUTION:
 function permutationShift(permutation) {
     let [max, min] = permutation.reduce((acc, curr, index) => {
         acc[0] = Math.max(acc[0], curr - index);
@@ -7,6 +13,7 @@ function permutationShift(permutation) {
     return max - min;
 }
 
+// 📌 TESTCASE:
 console.log(permutationShift([1, 0, 2, 3])) // 2
 console.log(permutationShift([0, 1, 2, 3, 5, 4])) // 2
 console.log(permutationShift([5, 4, 3, 2, 1, 0])) // 10
