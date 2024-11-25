01a9316cc38344a3c927eba7510eaefdac05726b
Irene Volodko <ice7aye@gmail.com>
2024-11-25 20:15:34 +0300

    add description

--- 7kyu/disagreeable-ascii.js
+++ 7kyu/disagreeable-ascii.js
@@ -1,7 +1,14 @@
+// ❓ DESCRIPTION:
+// You would like to get the 'weight' of a name by getting the sum of the ascii values. However you believe that capital letters should be worth more than mere lowercase letters. Spaces, numbers, or any other character are worth 0.
+// To find who has the 'weightier' name you will switch all the values.
+// For example Joe will have a weight of 254, instead of 286 using normal ascii values.
+//
+// ✅ SOLUTION:
 function getWeight(name){
     return [...name].reduce((acc, curr)=> acc + (/[a-z]/.test(curr) ? curr.charCodeAt(0) - 32 : /[A-Z]/.test(curr) ? curr.charCodeAt(0) + 32 : 0), 0);
 }
 
+// 📌 TESTCASE:
 console.log(getWeight("Joe")) // 254
 console.log(getWeight("CJ")) // 205
 console.log(getWeight("cj")) // 141
