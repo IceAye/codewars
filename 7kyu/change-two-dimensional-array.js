8ec7bd358badf5ba2f398659c3ba3550bbec281f
Irene Volodko <ice7aye@gmail.com>
2024-11-26 23:33:12 +0300

    add description

--- 7kyu/change-two-dimensional-array.js
+++ 7kyu/change-two-dimensional-array.js
@@ -1,4 +1,8 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// Function receive a two-dimensional square array of random integers. On the main diagonal, all the negative integers must be changed to 0, while the others must be changed to 1 (Note: 0 is considered non-negative, here).
+// (You can mutate the input if you want, but it is a better practice to not mutate the input)
+//
+// ✅ SOLUTION 1:
 function matrix(array) {
     for (let i= 0; i < array.length; i++) {
         array[i][i] = array[i][i] < 0 ? 0 : 1;
@@ -6,11 +10,12 @@ function matrix(array) {
     return array;
 }
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 function matrix(array) {
     return array.map((row, index1) => row.map((number, index2) => index1 === index2 ? number < 0 ? 0 : 1 : number))
 }
 
+// 📌 TESTCASE:
 console.log(
     matrix([
         [-1, 4, -5, -9, 3],
