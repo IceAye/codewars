9ca5e6086d9042a7dff349193527015502583130
Irene Volodko <ice7aye@gmail.com>
2024-11-28 22:03:44 +0300

    add description

--- 7kyu/delta-bits.js
+++ 7kyu/delta-bits.js
@@ -1,5 +1,11 @@
+// ❓ DESCRIPTION:
+// Complete the function to determine the number of bits required to convert integer A to integer B (where A and B >= 0)
+// The upper limit for A and B is 216, int.MaxValue or similar.
+//
+// ✅ SOLUTION:
 function convertBits(a, b){
     return (a ^ b).toString(2).replace(/0/g, "").length;
 }
 
+// 📌 TESTCASE:
 console.log(convertBits(31,14)) // 2
