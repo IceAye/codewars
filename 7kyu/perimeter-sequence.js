faefb29830bda0e9f9c52ab5ddc950c68937d92a
Irene Volodko <ice7aye@gmail.com>
2024-11-28 23:02:29 +0300

    add description

--- 7kyu/perimeter-sequence.js
+++ 7kyu/perimeter-sequence.js
@@ -1,5 +1,11 @@
+// ❓ DESCRIPTION:
+// The blocksize is a by a and a ≥ 1.
+// What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+//
+// ✅ SOLUTION:
 function perimeterSequence(a,n) {
     return a * n * 4;
 }
 
+// 📌 TESTCASE:
 console.log(perimeterSequence(1,3)) // 12
