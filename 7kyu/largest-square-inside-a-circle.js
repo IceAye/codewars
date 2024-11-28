0552808f3b73ce1de58a6c9ed1eb7a6487e9b3d3
Irene Volodko <ice7aye@gmail.com>
2024-11-28 22:28:30 +0300

    add description

--- 7kyu/largest-square-inside-a-circle.js
+++ 7kyu/largest-square-inside-a-circle.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Determine the area of the largest square that can fit inside a circle with radius r.
+//
+// ✅ SOLUTION:
 function areaLargestSquare(r) {
     return (2 * r)**2 / 2;
 }
 
+// 📌 TESTCASE:
 console.log(areaLargestSquare(5)) // 50
 console.log(areaLargestSquare(7)) // 98
 console.log(areaLargestSquare(15)) // 450
