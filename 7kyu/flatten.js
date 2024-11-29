89ffa31eefb8dad9d1131fa44a7a06efc30703fc
Irene Volodko <ice7aye@gmail.com>
2024-11-29 22:17:33 +0300

    add solution 2

--- 7kyu/flatten.js
+++ 7kyu/flatten.js
@@ -2,6 +2,10 @@ const flatten = function (array){
     return array.flat();
 }
 
+const flatten = function (array){
+    return [].concat(...array);
+}
+
 console.log(flatten([])) // []
 console.log(flatten([1, 2, 3])) // [1, 2, 3]
 console.log(flatten([[1, 2, 3], ["a", "b", "c"], [1, 2, 3]])) // [1, 2, 3, "a", "b", "c", 1, 2, 3]
