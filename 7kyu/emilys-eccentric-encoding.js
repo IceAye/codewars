a91ed54cc6cdf4ba4ad64e33a867b5fb4ff624dd
Irene Volodko <ice7aye@gmail.com>
2024-11-29 21:42:16 +0300

    add solution

---
+++ 7kyu/emilys-eccentric-encoding.js
@@ -0,0 +1,6 @@
+function decode(str) {
+    const abc = '_abcdefghijklmnopqrstuvwxyz';
+    return [...str].map(word => word.replace(/[a-z]/g, c => abc.at(-abc.indexOf(c)))).join('');
+}
+
+console.log(decode("I dlfow orpv gl rmgviervd lm Mlmwzb nlimrmt!")) // "I would like to interview on Monday morning!"
