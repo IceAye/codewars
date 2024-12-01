b5bf466a80f0adaa483b0fda718e932220ae16b6
Irene Volodko <ice7aye@gmail.com>
2024-12-01 21:07:48 +0300

    add description

--- 7kyu/chain-me.js
+++ 7kyu/chain-me.js
@@ -1,7 +1,14 @@
+// ❓ DESCRIPTION:
+// Write a generic function chainer
+// Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+// The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+//
+// ✅ SOLUTION:
 function chain(input, fs) {
     return fs.reduce((input, curr) => curr(input), input);
 }
 
+// 📌 TESTCASE:
 function add(x) {
     return x + 10;
 }
