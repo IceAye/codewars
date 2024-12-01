ffa682132eae77a6262ef392333f5f69c93f6dd2
Irene Volodko <ice7aye@gmail.com>
2024-12-01 16:52:02 +0300

    add description

--- 7kyu/word-values.js
+++ 7kyu/word-values.js
@@ -1,7 +1,17 @@
+// ❓ DESCRIPTION:
+// Given a string "abc" and assuming that each letter in the string has a value equal to its position in the alphabet, our string will have a value of 1 + 2 + 3 = 6. This means that: a = 1, b = 2, c = 3 ... z = 26.
+// You will be given a list of strings and your task will be to return the values of the strings as explained above multiplied by the position of that string in the list. For our purpose, position begins with 1.
+// ["abc", "abc abc"] should return [6, 24] because of [ 6 * 1, 12 * 2 ]. Note how spaces are ignored.
+// "abc" has a value of 6, while "abc abc" has a value of 12. Now, the value at position 1 is multiplied by 1 while the value at position 2 is multiplied by 2.
+// Input will only contain lowercase characters and spaces.
+// Good luck!
+//
+// ✅ SOLUTION:
 function wordValue(a) {
     const abc = " abcdefghijklmnopqrstuvwxyz";
     return a.map( (item, index) => [...item].reduce((acc, curr) => acc + +abc.indexOf(curr), 0) * (index + 1))
 }
 
+// 📌 TESTCASE:
 console.log(wordValue(["codewars", "abc", "xyz"])); // [88,12,225]
 console.log(wordValue(["abc abc", "abc abc", "abc", "abc"])); // [12,24,18,24]
