a9035397cc91a2c77f935477ed2c6e2853b44966
Irene Volodko <ice7aye@gmail.com>
2024-12-01 20:53:51 +0300

    add description

--- 7kyu/complete-the-pattern-1.js
+++ 7kyu/complete-the-pattern-1.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+// Note:Returning the pattern is not the same as Printing the pattern.
+// Rules/Note:
+// If n < 1 then it should return "" i.e. empty string.
+// There are no whitespaces in the pattern.
+//
+// ✅ SOLUTION:
 function pattern(n){
     if (n < 0) return '';
     let output = '';
@@ -9,6 +17,7 @@ function pattern(n){
     return output + `${n}`.repeat(n);
 }
 
+// 📌 TESTCASE:
 console.log(pattern(1))  // "1"
 console.log(pattern(2))  // "1\n22"
 console.log(pattern(5))  // "1\n22\n333\n4444\n55555"
