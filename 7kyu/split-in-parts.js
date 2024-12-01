51654843d9dc7165126d98704831a6b7e487fa3a
Irene Volodko <ice7aye@gmail.com>
2024-12-01 21:59:34 +0300

    add description

--- 7kyu/split-in-parts.js
+++ 7kyu/split-in-parts.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
+//
+// ✅ SOLUTION:
 const splitInParts = function(s, partLength){
     let result = [];
     for (let i = 0; i < s.length; i += partLength) {
@@ -6,6 +10,7 @@ const splitInParts = function(s, partLength){
     return result.join(' ');
 }
 
+// 📌 TESTCASE:
 console.log(splitInParts("HelloKata", 1)) // "H e l l o K a t a"
 console.log(splitInParts("HelloKata", 9)) //  "HelloKata"
 console.log(splitInParts("supercalifragilisticexpialidocious", 3)) //  "sup erc ali fra gil ist ice xpi ali doc iou s"
