b1d2171c88d8a2853ed56565fe703c2c564e7497
Irene Volodko <ice7aye@gmail.com>
2024-12-02 20:44:37 +0300

    add solution 2

--- 6kyu/simple-frequency-sort.js
+++ 6kyu/simple-frequency-sort.js
@@ -14,6 +14,12 @@ function solve(arr) {
     return result;
 }
 
+function solve(arr) {
+    let map = new Map();
+    arr.map((item) => map.set(item , map.get(item) + 1 || 1));
+    return arr.sort((a,b) => map.get(b) - map.get(a) || a - b);
+}
+
 console.log(solve([2,3,5,3,7,9,5,3,7])) // [3,3,3,5,5,7,7,2,9]
 console.log(solve([1,2,3,0,5,0,1,6,8,8,6,9,1])) // [1,1,1,0,0,6,6,8,8,2,3,5,9]
 console.log(solve([5,9,6,9,6,5,9,9,4,4])) // [9,9,9,9,4,4,5,5,6,6]
