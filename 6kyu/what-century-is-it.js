242397b8d6c713aeaa6726bbbd4a47759ff2cc94
Irene Volodko <ice7aye@gmail.com>
2024-12-02 18:26:23 +0300

    add description

--- 6kyu/what-century-is-it.js
+++ 6kyu/what-century-is-it.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Return the century of the input year. The input will always be a 4 digit string, so there is no need for validation.
+//
+// ✅ SOLUTION 1:
 function whatCentury(year) {
     let century = year.endsWith("00") ? +year.slice(0,2) : +year.slice(0,2) + 1
     return century > 3 && century < 21 ? century + 'th'
@@ -7,11 +11,13 @@ function whatCentury(year) {
                     : century + 'th'
 }
 
+// ✅ SOLUTION 2:
 function whatCentury(year){
     let century = Math.ceil(year / 100);
     return century + (century < 20 ? 'th' : ['th', 'st', 'nd', 'rd'][century % 10] || 'th');
 }
 
+// 📌 TESTCASE:
 console.log(whatCentury("1999")) // "20th"
 console.log(whatCentury("2011")) // "21st"
 console.log(whatCentury("2154")) // "22nd"
