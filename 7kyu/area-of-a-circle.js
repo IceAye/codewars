db16d5f2dc848692966f77b70cdd65eabb7bb8cf
Irene Volodko <ice7aye@gmail.com>
2024-12-02 17:50:48 +0300

    add description

--- 7kyu/area-of-a-circle.js
+++ 7kyu/area-of-a-circle.js
@@ -1,8 +1,15 @@
+// ❓ DESCRIPTION:
+// Complete the function which will return the area of a circle with the given radius.
+// Returned value is expected to be accurate up to tolerance of 0.01.
+// If the radius is not positive, throw Error.
+//
+// ✅ SOLUTION:
 function circleArea(radius) {
     if (radius <= 0) throw new Error('error');
     return +(Math.PI * radius**2).toFixed(3);
 }
 
+// 📌 TESTCASE:
 console.log(circleArea(1)) // 3.141
 console.log(circleArea(43.2673)) //  5881.248
 console.log(circleArea(68     )) // 14526.724
