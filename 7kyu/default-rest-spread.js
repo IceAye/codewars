e6441ed6f8d401a327b419839d2c3c5e5bf5e6be
Irene Volodko <ice7aye@gmail.com>
2024-12-03 22:39:08 +0300

    add description

--- 7kyu/default-rest-spread.js
+++ 7kyu/default-rest-spread.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Your task is to Refactor these functions to pass the tests and to follow the new JS ES2015 examples below!
+// Default
+// In JavaScript, parameters of functions default to undefined. However, in some situations it might be useful to set a different default value. This is where default parameters can help.
+// In the past, the general strategy for setting defaults was to test parameter values in the body of the function and assign a value if they are undefined. If in the following example, no value is provided for b in the call, its value would be undefined when evaluating a*b and the call to multiple would have returned NaN.
+// With default parameters, the check in the function body is no longer necessary.
+// Rest
+// Rest parameters have been introduced to reduce the boilerplate code that was induced by the arguments ```javascript
+// Spread
+// Any argument in the argument list can use the spread syntax and it can be used multiple times.
+//
+// ✅ SOLUTION:
 function defaultExample(x, y = 7) {
     return x + y;
 }
@@ -10,6 +22,7 @@ function spreadExample(...x) {
     return x[0] + x[1] + x[2];
 }
 
+// 📌 TESTCASE:
 console.log(defaultExample(3, 7)) // 10
 console.log(defaultExample(3)) // 10
 console.log(defaultExample(5)) // 12
