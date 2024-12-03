2467550d60e069cd74490e635debcf84437e90ae
Irene Volodko <ice7aye@gmail.com>
2024-12-03 20:56:42 +0300

    add description

--- 5kyu/scramblies.js
+++ 5kyu/scramblies.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+// Notes:
+// Only lower case letters will be used (a-z). No punctuation or digits will be included.
+// Performance needs to be considered.
+//
+// ✅ SOLUTION:
 function scramble(str1, str2) {
     function toHashmap(word){
         let map = new Map();
@@ -12,7 +19,7 @@ function scramble(str1, str2) {
     return true;
 }
 
-
+// 📌 TESTCASE:
 console.log(scramble('rkqodlw', 'world')) // True
 console.log(scramble('cedewaraaossoqqyt', 'codewars')) // True
 console.log(scramble('katas', 'steak')) // False
