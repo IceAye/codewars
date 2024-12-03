9629b5c83134d18b737e9dce9771a5fb9d32752b
Irene Volodko <ice7aye@gmail.com>
2024-12-03 21:19:34 +0300

    add solution 2

--- 7kyu/bug-xcom-388-mass-spectrometer-crashes.js
+++ 7kyu/bug-xcom-388-mass-spectrometer-crashes.js
@@ -3,3 +3,9 @@ const Spectrometer = {
         return atomicMasses.length === 0 ? 0 : atomicMasses.sort((a, b) => b - a)[0]
     },
 };
+
+const Spectrometer = {
+    getHeaviest: function (atomicMasses) {
+        return atomicMasses.length === 0 ? 0 : Math.max(...new Set(atomicMasses))
+    },
+};
