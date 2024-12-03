4c91d50458d6935b04ce086ad6948e877d64a60b
Irene Volodko <ice7aye@gmail.com>
2024-12-03 22:21:19 +0300

    add solution

---
+++ 7kyu/one-line-task-square-a-number.js
@@ -0,0 +1,10 @@
+sq=x=>x/(1/x)-(-0.5)|0
+
+console.log(sq(0)) // 0
+console.log(sq(1)) // 1
+console.log(sq(2)) // 4
+console.log(sq(3)) // 9
+console.log(sq(4)) // 16
+console.log(sq(4.4)) // 19
+console.log(sq(-4.44)) // 20
+console.log(sq(32768)) // 1073741824
