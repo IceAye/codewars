a838a243f3c999c55b9e0e87ce9d59b8bf4bfc32
Irene Volodko <ice7aye@gmail.com>
2024-12-04 23:20:34 +0300

    add solution 2

--- 7kyu/vowel-one.js
+++ 7kyu/vowel-one.js
@@ -6,5 +6,9 @@ function vowelOne(s) {
     return result;
 }
 
+function vowelOne(s){
+    return s.replace(/[^aeiou]/gi, "0").replace(/[^\d]/g, "1");
+}
+
 console.log( vowelOne( "vowelOne" )) // "01010101"
 console.log( vowelOne( "123, arou" )) // "000001011"
