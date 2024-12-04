9e001501c902d569e591ec27ab450c61e44a06c2
Irene Volodko <ice7aye@gmail.com>
2024-12-04 22:52:42 +0300

    add solution

---
+++ 7kyu/sum-of-all-arguments.js
@@ -0,0 +1,9 @@
+function sum(...args) {
+    return args.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(sum(1)); // 1
+console.log(sum(1, 2)); // 3
+console.log(sum(5, 7, 9)); // 21
+console.log(sum(12, 1, 1, 1, 1)); // 16
+console.log(sum(12, 1, 1, 1, 1, 1, 1)); // 18
