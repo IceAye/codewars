4e5a93918363d5f8ba590f7eb76d6f274e76a6bb
Irene Volodko <ice7aye@gmail.com>
2024-12-04 23:10:41 +0300

    add description

--- 7kyu/refactored-greeting.js
+++ 7kyu/refactored-greeting.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// The following code could use a bit of object-oriented artistry. While it's a simple method and works just fine as it is, in a larger system it's best to organize methods into classes/objects. (Or, at least, something similar depending on your language)
+// Refactor the following code so that it belongs to a Person class/object. Each Person instance will have a greet method. The Person instance should be instantiated with a name so that it no longer has to be passed into each greet method call.
+//
+// ✅ SOLUTION:
 class Person {
     constructor(name) {
         this.name = name;
@@ -8,6 +13,7 @@ class Person {
     }
 }
 
+// 📌 TESTCASE:
 let jack = new Person('Jack');
 let jill = new Person('Jill');
 console.log(jack.name) // 'Jack'
