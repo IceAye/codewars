2fb48671838fbc0507eeebc049f00ac988196298
Irene Volodko <ice7aye@gmail.com>
2024-12-04 22:53:50 +0300

    add description

--- 7kyu/sum-of-all-arguments.js
+++ 7kyu/sum-of-all-arguments.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Write a function that finds the sum of all its arguments.
+//
+// ✅ SOLUTION:
 function sum(...args) {
     return args.reduce((acc, curr) => acc + curr, 0);
 }
 
+// 📌 TESTCASE:
 console.log(sum(1)); // 1
 console.log(sum(1, 2)); // 3
 console.log(sum(5, 7, 9)); // 21
