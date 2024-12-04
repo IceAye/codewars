43da30ddd2d762960df3b032beba537c64f0b764
Irene Volodko <ice7aye@gmail.com>
2024-12-04 23:31:54 +0300

    add description

--- 7kyu/javascript-class-like-objects.js
+++ 7kyu/javascript-class-like-objects.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// For this exercise you should create a JavaScript class like object called "Animal" that takes in "name" and "type" arguments. It should have a toString method that returns a human readable string indicating the argument information passed in. It should also allow the name property to be set.
+// The following is an example of how the final code would be used and what the expected return values should be:
+// var dog = new Animal('Max', 'dog');
+// dog.toString(); // should return 'Max is a dog'
+// dog.type; // should == 'dog'
+// dog.name; // should == 'Max'
+// dog.name = 'Lassie'; // should set name to 'Lassie'
+//
+// ✅ SOLUTION:
 class Animal {
     constructor(name, type) {
         this.name = name;
@@ -9,6 +19,7 @@ class Animal {
     }
 }
 
+// 📌 TESTCASE:
 let dog = new Animal('Max', 'dog');
 console.log(dog.toString()) // should return 'Max is a dog'
 console.log(dog.type) // should == 'dog'
