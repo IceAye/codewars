d0fb23e9c958eb66c6cf4c661fba091b94fbcbec
Irene Volodko <ice7aye@gmail.com>
2024-12-05 23:34:51 +0300

    add solution 2

--- 7kyu/find-the-duplicated-number-in-a-consecutive-unsorted-list.js
+++ 7kyu/find-the-duplicated-number-in-a-consecutive-unsorted-list.js
@@ -3,5 +3,9 @@ function findDup(arr) {
     return arr.reduce((acc, curr) => acc + curr) - [...set].reduce((acc, curr) => acc + curr);
 }
 
+function findDup(arr){
+    return arr.find(item => arr.indexOf(item) !== arr.lastIndexOf(item));
+}
+
 console.log(findDup([1,2,2,3])) // 2
 console.log(findDup([1,3,2,5,4,5,7,6])) // 5
