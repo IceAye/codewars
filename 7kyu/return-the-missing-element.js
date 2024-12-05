c470653de45c3c433511da21c29c629aabc2e7c9
Irene Volodko <ice7aye@gmail.com>
2024-12-05 22:42:20 +0300

    add solution 2

--- 7kyu/return-the-missing-element.js
+++ 7kyu/return-the-missing-element.js
@@ -6,5 +6,11 @@ function getMissingElement(superImportantArray) {
     return 9;
 }
 
+function getMissingElement(superImportantArray){
+    for (let i = 0; i <= 9; i++) {
+        if (superImportantArray.indexOf(i) === -1) return i;
+    }
+}
+
 console.log(getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4])); // 8
 console.log(getMissingElement([9, 2, 4, 5, 7, 0, 8, 6, 1])); // 3
