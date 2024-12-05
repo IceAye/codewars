def486d2e6961b25123704c5035656b02ce77f47
Irene Volodko <ice7aye@gmail.com>
2024-12-05 22:43:02 +0300

    add solution 3

--- 7kyu/return-the-missing-element.js
+++ 7kyu/return-the-missing-element.js
@@ -12,5 +12,9 @@ function getMissingElement(superImportantArray){
     }
 }
 
+function getMissingElement(superImportantArray) {
+    return superImportantArray.reduce((acc, curr) => acc - curr, 45);
+}
+
 console.log(getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4])); // 8
 console.log(getMissingElement([9, 2, 4, 5, 7, 0, 8, 6, 1])); // 3
