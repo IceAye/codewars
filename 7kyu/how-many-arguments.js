d7a2f34397daa712dc04775bcc3a9ee3bca41ad1
Irene Volodko <ice7aye@gmail.com>
2024-12-07 20:01:47 +0300

    add description

--- 7kyu/how-many-arguments.js
+++ 7kyu/how-many-arguments.js
@@ -1,12 +1,17 @@
+// ❓ DESCRIPTION:
+// Write a function that returns the number of arguments it received.
+//
+// ✅ SOLUTION 1:
 function args_count(...args) {
     return args.length;
 }
 
+// ✅ SOLUTION 2:
 function args_count() {
     return arguments.length;
 }
 
-
+// 📌 TESTCASE:
 console.log(args_count(1, 2)) // 2
 console.log(args_count()) // 0
 console.log(args_count('A', 'B', 'C')) // 3
