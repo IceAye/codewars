27b13a1ba1ee65ec1e34fad3c6619bcb5e4bfcd9
Irene Volodko <ice7aye@gmail.com>
2024-12-08 20:20:18 +0300

    add description

--- 7kyu/running-out-of-space.js
+++ 7kyu/running-out-of-space.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
+// For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
+//
+// ✅ SOLUTION 1:
 function spacey(array) {
     let result = [];
     let string = "";
@@ -8,11 +13,13 @@ function spacey(array) {
     return result;
 }
 
+// ✅ SOLUTION 2:
 function spacey(array) {
     let string = "";
     return array.map(item => string += item);
 }
 
+// 📌 TESTCASE:
 console.log(spacey(["kevin", "has", "no", "space"])); // ['kevin',  'kevinhas',  'kevinhasno',  'kevinhasnospace']
 console.log(spacey(["this", "cheese", "has", "no", "holes"])); // ['this',    'thischeese',    'thischeesehas',
 // 'thischeesehasno','thischeesehasnoholes']
