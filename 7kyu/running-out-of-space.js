251f2fa1dc26164bb925b0f3ff50d984475d9cea
Irene Volodko <ice7aye@gmail.com>
2024-12-08 20:19:11 +0300

    add solution 2

--- 7kyu/running-out-of-space.js
+++ 7kyu/running-out-of-space.js
@@ -8,6 +8,11 @@ function spacey(array) {
     return result;
 }
 
+function spacey(array) {
+    let string = "";
+    return array.map(item => string += item);
+}
+
 console.log(spacey(["kevin", "has", "no", "space"])); // ['kevin',  'kevinhas',  'kevinhasno',  'kevinhasnospace']
 console.log(spacey(["this", "cheese", "has", "no", "holes"])); // ['this',    'thischeese',    'thischeesehas',
 // 'thischeesehasno','thischeesehasnoholes']
