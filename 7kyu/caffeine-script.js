2194a0be73e11cddbabd0759d23cefc700b673f4
Irene Volodko <ice7aye@gmail.com>
2024-12-08 21:57:36 +0300

    add description

--- 7kyu/caffeine-script.js
+++ 7kyu/caffeine-script.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Complete the function which takes a non-zero integer as its argument.
+// If the integer is divisible by 3, return the string "Java".
+// If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+// If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+// If none of the condition is true, return the string "mocha_missing!"
+//
+// ✅ SOLUTION:
 function caffeineBuzz(n) {
     let string = "mocha_missing!";
     if (n % 3 === 0) string = "Java";
@@ -6,6 +14,7 @@ function caffeineBuzz(n) {
     return string;
 }
 
+// 📌 TESTCASE:
 console.log(caffeineBuzz(1)) // "mocha_missing!"
 console.log(caffeineBuzz(3)) // "Java"
 console.log(caffeineBuzz(6)) // "JavaScript"
