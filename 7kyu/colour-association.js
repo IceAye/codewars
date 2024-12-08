5d17488cc8440fc5e079bfb2b2402bcc6f2f651d
Irene Volodko <ice7aye@gmail.com>
2024-12-08 20:54:29 +0300

    add description

--- 7kyu/colour-association.js
+++ 7kyu/colour-association.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Colour plays an important role in our lifes. Most of us like this colour better then another. User experience specialists believe that certain colours have certain psychological meanings for us.
+// You are given a 2D array, composed of a colour and its 'common' association in each array element. The function you will write needs to return the colour as 'key' and association as its 'value'.
+//
+// ✅ SOLUTION 1:
 function colourAssociation(array) {
     let result = [];
     for (const [key, value] of array) {
@@ -8,10 +13,11 @@ function colourAssociation(array) {
     return result;
 }
 
+// ✅ SOLUTION 2:
 function colourAssociation(array) {
     return array.map(([key, value]) => ({ [key]: value }));
 }
 
-
+// 📌 TESTCASE:
 console.log(colourAssociation([["white", "goodness"], ["blue", "tranquility"]])) // [{white:"goodness"},{blue:"tranquility"}]
 console.log(colourAssociation([["red", "energy"],["yellow", "creativity"],["brown" , "friendly"],["green", "growth"]])) // [{red: "energy"},{yellow: "creativity"}, {brown: "friendly"},{green: "growth"}]
