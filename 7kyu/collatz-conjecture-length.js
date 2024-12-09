fe746c44253128b6ad96925355fc2ee8dae9691a
Irene Volodko <ice7aye@gmail.com>
2024-12-09 22:55:26 +0300

    add description

--- 7kyu/collatz-conjecture-length.js
+++ 7kyu/collatz-conjecture-length.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// The Collatz Conjecture states that for any positive natural number n, this process:
+// if n is even, divide it by 2
+// if n is odd, multiply it by 3 and add 1
+// repeat
+// will eventually reach n = 1.
+// For example, if n = 20, the resulting sequence will be:
+// [ 20, 10, 5, 16, 8, 4, 2, 1 ]
+// Write a program that will output the length of the Collatz Conjecture for any given n.
+//
+// ✅ SOLUTION:
 function collatz(n) {
     let count = 1;
     while (n > 1) {
@@ -7,5 +18,6 @@ function collatz(n) {
     return count;
 }
 
+// 📌 TESTCASE:
 console.log(collatz(20)) // 8
 console.log(collatz(15)) // 18
