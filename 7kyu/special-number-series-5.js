99a81ebff6323258c431c85fb46fe58f30d37d84
Irene Volodko <ice7aye@gmail.com>
2024-12-09 22:29:24 +0300

    add solution

---
+++ 7kyu/special-number-series-5.js
@@ -0,0 +1,14 @@
+function specialNumber(n){
+    return !/[^0-5]/gi.test(n) ? 'Special!!' : 'NOT!!';
+}
+
+console.log(specialNumber(2)) // "Special!!"
+console.log(specialNumber(3)) // "Special!!"
+console.log(specialNumber(6)) // "NOT!!"
+console.log(specialNumber(9)) // "NOT!!"
+console.log(specialNumber(11)) // "Special!!"
+console.log(specialNumber(55)) // "Special!!"
+console.log(specialNumber(26)) // "NOT!!"
+console.log(specialNumber(92)) // "NOT!!"
+console.log(specialNumber(25432)) // "Special!!"
+console.log(specialNumber(2783)) // "NOT!!"
