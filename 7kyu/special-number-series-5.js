9f4c44a38d87ef0d7187ea3cbec56f7fb5dceff4
Irene Volodko <ice7aye@gmail.com>
2024-12-09 22:32:22 +0300

    add description

--- 7kyu/special-number-series-5.js
+++ 7kyu/special-number-series-5.js
@@ -1,11 +1,18 @@
+// ❓ DESCRIPTION:
+// A number is a Special Number if it’s digits only consist 0, 1, 2, 3, 4 or 5
+// Given a number determine if it special number or not .
+//
+// ✅ SOLUTION 1:
 function specialNumber(n){
     return !/[^0-5]/gi.test(n) ? 'Special!!' : 'NOT!!';
 }
 
+// ✅ SOLUTION 2:
 function specialNumber(n){
     return /[6-9]/gi.test(n) ? 'NOT!!' : 'Special!!';
 }
 
+// 📌 TESTCASE:
 console.log(specialNumber(2)) // "Special!!"
 console.log(specialNumber(3)) // "Special!!"
 console.log(specialNumber(6)) // "NOT!!"
