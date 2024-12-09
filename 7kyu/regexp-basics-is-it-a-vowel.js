9e021ce2fb8ffc06258c79e73770ec0b1ca4c811
Irene Volodko <ice7aye@gmail.com>
2024-12-09 22:45:20 +0300

    add solution 2

--- 7kyu/regexp-basics-is-it-a-vowel.js
+++ 7kyu/regexp-basics-is-it-a-vowel.js
@@ -2,6 +2,10 @@ String.prototype.vowel = function() {
     return /[aeiuo]/i.test(this) && this.length === 1;
 };
 
+String.prototype.vowel = function() {
+    return /^[aeiuo]$/i.test(this);
+};
+
 console.log(''.vowel()) // false
 console.log('a'.vowel()) // true
 console.log('E'.vowel()) // true
