e970eadb2c38d48bdde1c28d8fec44b1dad19e6a
Irene Volodko <ice7aye@gmail.com>
2024-12-10 22:19:04 +0300

    add description

--- 7kyu/get-decimal-part-of-the-given-number.js
+++ 7kyu/get-decimal-part-of-the-given-number.js
@@ -1,12 +1,19 @@
+// ❓ DESCRIPTION:
+// Write a function that returns only the decimal part of the given number.
+// You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
+//
+// ✅ SOLUTION 1:
 function getDecimal(n){
     let result = '.' + n.toString().split('.')[1];
     return +result || 0;
 }
 
+// ✅ SOLUTION 2:
 function getDecimal(n){
     return Math.abs(n % 1);
 }
 
+// 📌 TESTCASE:
 console.log(getDecimal(10)) // 0
 console.log(getDecimal(-1.2)) // 0.2
 console.log(getDecimal(4.5)) // 0.5
