ba84d4a0ccb69648a833463ce03321651c297b13
Irene Volodko <ice7aye@gmail.com>
2024-12-10 21:29:32 +0300

    add solution 2

--- 7kyu/insert-dashes.js
+++ 7kyu/insert-dashes.js
@@ -10,6 +10,10 @@ function insertDash(num) {
     return result + array.at(-1);
 }
 
+function insertDash(num) {
+    return num.toString().replace(/[13579](?=[13579])/g, '$&-');
+}
+
 console.log(insertDash(454793)) // '4547-9-3'
 console.log(insertDash(123456)) // '123456'
 console.log(insertDash(1003567)) // '1003-567'
