320f107c42ebcdedb98514de8bc4b2beec9ac771
Irene Volodko <ice7aye@gmail.com>
2024-12-10 22:17:14 +0300

    add solution 2

--- 7kyu/get-decimal-part-of-the-given-number.js
+++ 7kyu/get-decimal-part-of-the-given-number.js
@@ -3,6 +3,10 @@ function getDecimal(n){
     return +result || 0;
 }
 
+function getDecimal(n){
+    return Math.abs(n % 1);
+}
+
 console.log(getDecimal(10)) // 0
 console.log(getDecimal(-1.2)) // 0.2
 console.log(getDecimal(4.5)) // 0.5
