1089227a6fa38e0eb5bd3bee321cddd5dd4d20de
Irene Volodko <ice7aye@gmail.com>
2024-12-11 20:44:06 +0300

    add description

--- 7kyu/valid-parenthesis.js
+++ 7kyu/valid-parenthesis.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+// All inputs will be strings, consisting only of characters ( and ).
+// Empty strings are considered balanced (and therefore valid), and will be tested.
+// For languages with mutable strings, the inputs should not be mutated.
+//
+// ✅ SOLUTION 1:
 function validParentheses(parenStr) {
     let stack = [];
     for (const parent of parenStr) {
@@ -12,6 +19,7 @@ function validParentheses(parenStr) {
     return stack.length === 0
 }
 
+// ✅ SOLUTION 2:
 function validParentheses(parenStr) {
     let sum = 0;
     for (const paren of parenStr) {
@@ -21,6 +29,7 @@ function validParentheses(parenStr) {
     return sum === 0;
 }
 
+// 📌 TESTCASE:
 console.log(validParentheses("()")); // true
 console.log(validParentheses("((()))")); //  true,
 console.log(validParentheses("()()()")); // true
