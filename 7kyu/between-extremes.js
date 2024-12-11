681a37b4ddb1fe236e40409fd9776ba1a3fa27fd
Irene Volodko <ice7aye@gmail.com>
2024-12-11 20:28:30 +0300

    add solution 3

--- 7kyu/between-extremes.js
+++ 7kyu/between-extremes.js
@@ -7,5 +7,20 @@ function betweenExtremes(numbers) {
     return numbers.at(-1) - numbers[0];
 }
 
+function betweenExtremes(numbers) {
+    let min = numbers[0];
+    let max = numbers[0];
+
+    for (let i = 1; i < numbers.length; i++) {
+        let currentNumber = numbers[i];
+        if (currentNumber >= max) {
+            max = currentNumber;
+        } else if (currentNumber <= min) {
+            min = currentNumber;
+        }
+    }
+    return max - min;
+}
+
 console.log(betweenExtremes([21, 34, 54, 43, 26, 12])) // 42
 console.log(betweenExtremes([-1, -41, -77, -100])) // 99
