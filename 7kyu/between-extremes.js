4dba0342bcdd73f40eb4d379915722c41643733e
Irene Volodko <ice7aye@gmail.com>
2024-12-11 20:25:49 +0300

    add solution 2

--- 7kyu/between-extremes.js
+++ 7kyu/between-extremes.js
@@ -2,5 +2,10 @@ function betweenExtremes(numbers) {
     return Math.max(...numbers) -  Math.min(...numbers);
 }
 
+function betweenExtremes(numbers) {
+    numbers.sort((a, b) => a - b);
+    return numbers.at(-1) - numbers[0];
+}
+
 console.log(betweenExtremes([21, 34, 54, 43, 26, 12])) // 42
 console.log(betweenExtremes([-1, -41, -77, -100])) // 99
