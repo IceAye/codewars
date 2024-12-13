70e94fb83601603360a640d87c220897940f1a6c
Irene Volodko <ice7aye@gmail.com>
2024-12-13 23:28:34 +0300

    add solution

---
+++ 7kyu/toleetspeak.js
@@ -0,0 +1,15 @@
+const leet = {
+    A: "@",  B: "8",  C: "(",  E: "3",  G: "6",  H: "#",
+    I: "!",  L: "1",  O: "0",  S: "$",  T: "7",  Z: "2",
+};
+
+function toLeetSpeak(str) {
+    return str.replace(/./g, (char) => leet[char] || char);
+}
+
+console.log(toLeetSpeak("LEET")); // "1337"
+console.log(toLeetSpeak("CODEWARS")); // "(0D3W@R$"
+console.log(toLeetSpeak("HELLO WORLD")); // "#3110 W0R1D"
+console.log(toLeetSpeak("LOREM IPSUM DOLOR SIT AMET")); // "10R3M !P$UM D010R $!7 @M37"
+console.log(toLeetSpeak("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG")); // "7#3 QU!(K 8R0WN F0X JUMP$ 0V3R 7#3 1@2Y
+// D06"
