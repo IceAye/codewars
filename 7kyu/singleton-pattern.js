dedce0a02aedb9b5dd445670e0e367a1c64f2298
Irene Volodko <ice7aye@gmail.com>
2024-12-13 23:40:01 +0300

    add description

--- 7kyu/singleton-pattern.js
+++ 7kyu/singleton-pattern.js
@@ -1,9 +1,16 @@
+// ❓ DESCRIPTION:
+// In software engineering, the singleton pattern is a design pattern that restricts the instantiation of a class to one object. This is useful when exactly one object is needed to coordinate actions across the system.
+// Create an Singleton pattern, so there is one object in system.
+//
+// ✅ SOLUTION:
 const Singleton = function () {
     if (Singleton.__instance) {
         return Singleton.__instance;
     }
     return (Singleton.__instance = this);
 };
+
+// 📌 TESTCASE:
 
 let obj1 = new Singleton();
 let obj2 = new Singleton();
