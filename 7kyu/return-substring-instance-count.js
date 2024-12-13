9aa140f3cd96a5481a971e82b6e02ca762c9c4b8
Irene Volodko <ice7aye@gmail.com>
2024-12-13 23:58:47 +0300

    add description

--- 7kyu/return-substring-instance-count.js
+++ 7kyu/return-substring-instance-count.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Complete the solution so that it returns the number of times the search_text is found within the full_text. Overlap is not permitted : "aaa" contains 1 instance of "aa", not 2.
+//
+// ✅ SOLUTION:
 function solution(fullText, searchText) {
     return (fullText.match(new RegExp(searchText, "g")) || []).length;
 }
 
+// 📌 TESTCASE:
 console.log(solution("abcdeb", "b")); // 2
 console.log(solution("abc", "b")); // 1
 console.log(solution("abbc", "bb")); // 1
