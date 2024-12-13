2375788646ca67e9aae245b7542f887e0221297d
Irene Volodko <ice7aye@gmail.com>
2024-12-13 23:29:58 +0300

    add description

--- 7kyu/toleetspeak.js
+++ 7kyu/toleetspeak.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
+// Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+//
+// ✅ SOLUTION:
 const leet = {
     A: "@",  B: "8",  C: "(",  E: "3",  G: "6",  H: "#",
     I: "!",  L: "1",  O: "0",  S: "$",  T: "7",  Z: "2",
@@ -7,6 +12,7 @@ function toLeetSpeak(str) {
     return str.replace(/./g, (char) => leet[char] || char);
 }
 
+// 📌 TESTCASE:
 console.log(toLeetSpeak("LEET")); // "1337"
 console.log(toLeetSpeak("CODEWARS")); // "(0D3W@R$"
 console.log(toLeetSpeak("HELLO WORLD")); // "#3110 W0R1D"
