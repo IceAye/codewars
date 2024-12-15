c9dfa4e2d2e469a65ff87d0c2c25314e52205751
Irene Volodko <ice7aye@gmail.com>
2024-12-15 20:08:52 +0300

    add description

--- 7kyu/is-n-divisible-by.js
+++ 7kyu/is-n-divisible-by.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Create a function that checks if the first argument n is divisible by all other arguments (return true if no other arguments).
+//
+// ✅ SOLUTION:
 function isDivisible(...args) {
     for (const arg of args) {
         if (args[0] % arg !== 0) return false;
@@ -5,6 +9,7 @@ function isDivisible(...args) {
     return true;
 }
 
+// 📌 TESTCASE:
 console.log(isDivisible(3,3,4)) // false
 console.log(isDivisible(12,3,4)) // true
 console.log(isDivisible(8,3,4,2,5)) // false
