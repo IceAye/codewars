9fad7cb3ad0e2b0a926901297d3ce36905d3960c
Irene Volodko <ice7aye@gmail.com>
2024-12-15 19:43:50 +0300

    add description

--- 7kyu/basic-js-calculating-averages.js
+++ 7kyu/basic-js-calculating-averages.js
@@ -1,9 +1,17 @@
+// ❓ DESCRIPTION:
+// Let's build a calculator that can calculate the average for an arbitrary number of arguments.
+// The test expects you to provide a Calculator object with an average method:
+// Calculator.average()
+// The test also expects that when you pass no arguments, it returns 0. The arguments are expected to be integers.
+//
+// ✅ SOLUTION:
 var Calculator = {
     average: function (...args) {
         return [...args].reduce((acc, curr) => acc + curr, 0) / args.length || 0;
     },
 };
 
+// 📌 TESTCASE:
 console.log(Calculator.average(3,4,5)) // 4
 console.log(Calculator.average(3,4,5,6,7,8,9,8,7,6,5,4,3)) // 5.769230769230769
 console.log(Calculator.average()) // 0
