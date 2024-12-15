0c1f74d2219f1dc2221c32f0be701cb74b100e0b
Irene Volodko <ice7aye@gmail.com>
2024-12-15 19:56:01 +0300

    add solution 2

--- 7kyu/count-all-the-sheep-on-farm-in-the-heights-of-new-zealand.js
+++ 7kyu/count-all-the-sheep-on-farm-in-the-heights-of-new-zealand.js
@@ -5,6 +5,9 @@ function lostSheep(friday, saturday, total) {
     return total - sum(friday) - sum(saturday);
 }
 
+function lostSheep(friday, saturday, total) {
+    return [...friday, ...saturday].reduce((acc, curr) => acc - curr, total);
+}
 
 console.log(lostSheep([1,2],[3,4],15))  // 5
 console.log(lostSheep([3,1,2],[4,5],21))  // 6
