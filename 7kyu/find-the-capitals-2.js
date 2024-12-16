2885e92362f24cf4b4ebe4b1dc4b80aa4d1562f4
Irene Volodko <ice7aye@gmail.com>
2024-12-16 22:57:08 +0300

    add description

--- 7kyu/find-the-capitals-2.js
+++ 7kyu/find-the-capitals-2.js
@@ -1,7 +1,13 @@
+// ❓ DESCRIPTION:
+// Complete the method that takes a sequence of objects with two keys each: country or state, and capital. Keys may be symbols or strings.
+// The method should return an array of sentences declaring the state or country and its capital.
+//
+// ✅ SOLUTION:
 function capital(capitals) {
     return capitals.map((item) => `The capital of ${item.state || item.country} is ${item.capital}`);
 }
 
+// 📌 TESTCASE:
 console.log(capital([{ state: "Maine", capital: "Augusta" }])) // "The capital of Maine is Augusta"
 console.log(capital([{ country: "Spain", capital: "Madrid" }])) // "The capital of Spain is Madrid"
 console.log(capital([{ state: "Maine", capital: "Augusta" },{ country: "Spain", capital: "Madrid" }]))
