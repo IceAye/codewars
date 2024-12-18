ffaef8737cb4863194a22de88dfe77c8ac194a5d
Irene Volodko <ice7aye@gmail.com>
2024-12-18 21:49:14 +0300

    add description

--- 7kyu/name-array-capping.js
+++ 7kyu/name-array-capping.js
@@ -1,6 +1,11 @@
+// ❓ DESCRIPTION:
+// Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+//
+// ✅ SOLUTION:
 function capMe(names) {
     return names.map(item => item[0].toUpperCase() + item.slice(1).toLowerCase());
 }
 
+// 📌 TESTCASE:
 console.log(capMe(['jo' , 'nelson' , 'jurie'])) // returns ['Jo', 'Nelson', 'Jurie']
 console.log(capMe(['KARLY' , 'DANIEL' , 'KELSEY'])) // returns ['Karly', 'Daniel', 'Kelsey']
