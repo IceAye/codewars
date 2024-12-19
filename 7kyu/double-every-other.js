35891313b0d9dc8320090cac2b25b61c248ab256
Irene Volodko <ice7aye@gmail.com>
2024-12-19 17:56:05 +0300

    add description

--- 7kyu/double-every-other.js
+++ 7kyu/double-every-other.js
@@ -1,5 +1,10 @@
+// ❓ DESCRIPTION:
+// Write a function that doubles every second integer in a list, starting from the left.
+//
+// ✅ SOLUTION:
 function doubleEveryOther(a) {
     return a.map((item, index) => (index % 2 === 1 ? item * 2 : item));
 }
 
+// 📌 TESTCASE:
 console.log(doubleEveryOther([1, 2, 3, 4])); // [1,4,3,8]
