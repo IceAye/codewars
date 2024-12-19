20e705e9b4f82716a92d25da05f4c9f255def415
Irene Volodko <ice7aye@gmail.com>
2024-12-19 16:48:48 +0300

    add description

--- 7kyu/discover-the-original-price.js
+++ 7kyu/discover-the-original-price.js
@@ -1,7 +1,18 @@
-function discoverOriginalPrice(discountedPrice, salePercentage) {
+// ❓ DESCRIPTION:
+// We need to write some code to return the original price of a product, the return type must be of type decimal and
+// the number must be rounded to two decimal places.
+// We will be given the sale price (discounted price), and the salepercentage, our job is to figure out the original price.
+// For example: Given an item at $75 sale price after
+// applying a 25% discount, the function should return the original price of that item before applying the sale
+// percentage, which is ($100.00) of course, rounded to two decimal places.  DiscoverOriginalPrice(75, 25) => 100.00M
+// where 75 is the sale price (discounted price), 25 is the sale percentage and 100 is the original price
+//
+// ✅ SOLUTION:
+function discoverOriginalPrice(discountedPrice , salePercentage) {
     return +(discountedPrice / (100 - salePercentage) * 100).toFixed(2)
 }
 
-console.log(discoverOriginalPrice(75, 25)); // 100
-console.log(discoverOriginalPrice(25, 75)); // 100
-console.log(discoverOriginalPrice(75.75, 25)); // 101
+// 📌 TESTCASE:
+console.log(discoverOriginalPrice(75 , 25)); // 100
+console.log(discoverOriginalPrice(25 , 75)); // 100
+console.log(discoverOriginalPrice(75.75 , 25)); // 101
