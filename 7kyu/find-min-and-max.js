96d29fcf51a90e5bf143cd813b4de53e0542a866
Irene Volodko <ice7aye@gmail.com>
2024-12-20 22:44:06 +0300

    add description

--- 7kyu/find-min-and-max.js
+++ 7kyu/find-min-and-max.js
@@ -1,9 +1,14 @@
+// ❓ DESCRIPTION:
+// Implement a function that returns the minimal and the maximal value of a list (in this order).
+//
+// ✅ SOLUTION:
 function getMinMax(arr){
     let max = Math.max(...arr);
     let min = Math.min(...arr);
     return [min, max];
-};
+}
 
+// 📌 TESTCASE:
 console.log(getMinMax([1])) // [1,1]
 console.log(getMinMax([1,2])) // [1,2]
 console.log(getMinMax([2,1])) // [1,2]
