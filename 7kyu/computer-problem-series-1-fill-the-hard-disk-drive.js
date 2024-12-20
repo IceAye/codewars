1a89c925174bc30425c168c9e97e3e0cd7bca8e5
Irene Volodko <ice7aye@gmail.com>
2024-12-20 23:19:37 +0300

    add description

--- 7kyu/computer-problem-series-1-fill-the-hard-disk-drive.js
+++ 7kyu/computer-problem-series-1-fill-the-hard-disk-drive.js
@@ -1,4 +1,12 @@
-function save(sizes, hd) {
+// ❓ DESCRIPTION:
+// Your task is to determine how many files of the copy queue you will be able to save into your Hard Disk Drive. The
+// files must be saved in the order they appear in the queue.
+// Zero size files can always be saved even HD full.
+// Input: Array of file sizes (0 <= s <= 100) Capacity of the HD (0 <= c <= 500) Output: Number of files that can be
+// fully saved in the HD.
+//
+// ✅ SOLUTION:
+function save(sizes , hd) {
     let count = 0;
     let sum = sizes[0];
 
@@ -10,11 +18,12 @@ function save(sizes, hd) {
     return count;
 }
 
-console.log(save([4, 4, 4, 3, 3], 12)) // 3
-console.log(save([4, 4, 4, 3, 3], 11)) // 2
-console.log(save([4, 8, 15, 16, 23, 42], 108)) // 6
-console.log(save([13], 13)) // 1
-console.log(save([1, 2, 3, 4], 250)) // 4
-console.log(save([100], 500)) // 1
-console.log(save([11, 13, 15, 17, 19], 8)) // 0
-console.log(save([45], 12)) // 0
+// 📌 TESTCASE:
+console.log(save([4 , 4 , 4 , 3 , 3] , 12)) // 3
+console.log(save([4 , 4 , 4 , 3 , 3] , 11)) // 2
+console.log(save([4 , 8 , 15 , 16 , 23 , 42] , 108)) // 6
+console.log(save([13] , 13)) // 1
+console.log(save([1 , 2 , 3 , 4] , 250)) // 4
+console.log(save([100] , 500)) // 1
+console.log(save([11 , 13 , 15 , 17 , 19] , 8)) // 0
+console.log(save([45] , 12)) // 0
