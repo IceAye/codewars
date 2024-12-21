63f885402fcc27ec707c13b7725f276625a90651
Irene Volodko <ice7aye@gmail.com>
2024-12-21 22:21:31 +0300

    add solution

---
+++ 7kyu/formatting-decimal-places-1.js
@@ -0,0 +1,9 @@
+function twoDecimalPlaces(number) {
+    return Math.trunc(number * 100) / 100
+}
+
+
+console.log(twoDecimalPlaces(10.1289767789)) // 10.12
+console.log(twoDecimalPlaces(-7488.83485834983)) // -7488.83
+console.log(twoDecimalPlaces(4.653725356)) // 4.65
+console.log(twoDecimalPlaces(10.1289767789)) // 10.12
