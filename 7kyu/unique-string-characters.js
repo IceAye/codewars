92103b65aaf4c0202108543450d9331fcd604f23
Irene Volodko <ice7aye@gmail.com>
2024-12-21 22:35:27 +0300

    add description

--- 7kyu/unique-string-characters.js
+++ 7kyu/unique-string-characters.js
@@ -1,9 +1,14 @@
+// ❓ DESCRIPTION:
+// In this Kata, you will be given two strings a and b and your task will be to return the characters that are not common in the two strings.
+//
+// ✅ SOLUTION:
 function solve(a, b) {
     let string1 = a.split('').filter(v => !b.split('').includes(v)).join('');
     let string2 = b.split('').filter(v => !a.split('').includes(v)).join('');
     return string1 + string2;
 }
 
+// 📌 TESTCASE:
 console.log(solve("xyab", "xzca")); // "ybzc"
 console.log(solve("xyabb", "xzca")); // "ybbzc"
 console.log(solve("abcd", "xyz")); // "abcdxyz"
