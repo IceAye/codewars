ebef79c44c84ac3f552fb00c0ee654cf8f8f3f4c
Irene Volodko <ice7aye@gmail.com>
2024-12-21 22:14:45 +0300

    add description

--- 7kyu/average-scores.js
+++ 7kyu/average-scores.js
@@ -1,7 +1,13 @@
+// ❓ DESCRIPTION:
+// Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number.
+// You are not allowed to use any loops (including for, for/in, while, and do/while loops).  The array will never be
+// empty.
+//
+// ✅ SOLUTION:
 function average(scores) {
-    return Math.round(scores.reduce((acc, curr) => acc + curr, 0) / scores.length);
+    return Math.round(scores.reduce((acc , curr) => acc + curr , 0) / scores.length);
 }
 
-
-console.log(average([49,3,5,300,7])) // 73
-console.log(average([90,98,89,100,100,86,94])) // 94
+// 📌 TESTCASE:
+console.log(average([49 , 3 , 5 , 300 , 7])) // 73
+console.log(average([90 , 98 , 89 , 100 , 100 , 86 , 94])) // 94
