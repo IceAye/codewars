3d09280faae6a8c4a13fcc3319b326d6d43075d5
Irene Volodko <ice7aye@gmail.com>
2024-12-22 17:59:57 +0300

    add description

--- 7kyu/numbers-in-strings.js
+++ 7kyu/numbers-in-strings.js
@@ -1,15 +1,22 @@
+// ❓ DESCRIPTION:
+// In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number
+// groupings and return the largest number. Numbers will not have leading zeros.  For example, solve("gh12cdy695m1") =
+// 695, because this is the largest of all number groupings.  Good luck!
+//
+// ✅ SOLUTION:
 function solve(s) {
     let max = 0;
     let array = s.split(/[a-z]/gi);
 
     for (const num of array) {
         if (num !== "") {
-            max = Math.max(max, +num);
+            max = Math.max(max , +num);
         }
     }
     return max;
 }
 
+// 📌 TESTCASE:
 console.log(solve("gh12cdy695m1")); // 695
 console.log(solve("2ti9iei7qhr5")); //   9
 console.log(solve("vih61w8oohj5")); //  61
