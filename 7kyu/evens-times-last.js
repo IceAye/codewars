1cc40d583cd1673f75bcdb659adea952dceea821
Irene Volodko <ice7aye@gmail.com>
2024-12-22 07:47:50 +0300

    add description

--- 7kyu/evens-times-last.js
+++ 7kyu/evens-times-last.js
@@ -1,8 +1,15 @@
+// ❓ DESCRIPTION:
+// Given a sequence of integers, return the sum of all the integers that have an even index (odd index in COBOL),
+// multiplied by the integer at the last index.  Indices in sequence start from 0.  If the sequence is empty, you
+// should return 0.
+//
+// ✅ SOLUTION:
 function evenLast(numbers) {
-    return numbers.reduce((acc, curr, ind) => {
+    return numbers.reduce((acc , curr , ind) => {
         if (ind % 2 === 0) acc += curr * numbers.at(-1);
         return acc;
-    }, 0)
+    } , 0)
 }
 
-console.log(evenLast([2, 3, 4, 5])) // 30
+// 📌 TESTCASE:
+console.log(evenLast([2 , 3 , 4 , 5])) // 30
