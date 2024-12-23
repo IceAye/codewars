7b6792963fc1ee02ef03f72e5c5c4b508a45d2b0
Irene Volodko <ice7aye@gmail.com>
2024-12-23 13:30:28 +0300

    add description

--- 7kyu/reverse-list.js
+++ 7kyu/reverse-list.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Write reverseList function that simply reverses lists.
+//
+// ✅ SOLUTION 1:
 function reverseList(arr) {
     return arr.reverse();
 }
 
+// ✅ SOLUTION 2:
 function reverseList(arr) {
     let array = [];
     for (let i = arr.length - 1; i >= 0; i--) {
