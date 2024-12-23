cd982267cad38d45e77fea797a02577dcb4bdd43
Irene Volodko <ice7aye@gmail.com>
2024-12-23 17:40:50 +0300

    add solution 2

--- 7kyu/soonerize-me.js
+++ 7kyu/soonerize-me.js
@@ -8,6 +8,13 @@ function spoonerize(words) {
     return change(word1, word2) + " " + change(word2, word1);
 }
 
+function spoonerize(words) {
+    let [word1, word2] = words.split(" ");
+    [word1, word2] = [word2[0] + word1.slice(1), word1[0] + word2.slice(1)];
+    return [word1, word2].join(" ")
+}
+
+
 console.log(spoonerize("nit picking")) // "pit nicking"
 console.log(spoonerize("wedding bells")) // "bedding wells"
 console.log(spoonerize("jelly beans")) // "belly jeans"
