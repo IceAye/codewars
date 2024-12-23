1b49ef46eb66eea05c2362f6a9e74dd31fbff2fd
Irene Volodko <ice7aye@gmail.com>
2024-12-23 13:28:57 +0300

    add solution 2

--- 7kyu/reverse-list.js
+++ 7kyu/reverse-list.js
@@ -1,3 +1,11 @@
 function reverseList(arr) {
     return arr.reverse();
 }
+
+function reverseList(arr) {
+    let array = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        array.push(arr[i]);
+    }
+    return array;
+}
