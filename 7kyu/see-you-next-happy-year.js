bac13ca2238878f07161fcfd3806a7c898bc348b
Irene Volodko <ice7aye@gmail.com>
2024-12-25 22:40:14 +0300

    add description

--- 7kyu/see-you-next-happy-year.js
+++ 7kyu/see-you-next-happy-year.js
@@ -1,9 +1,25 @@
-function nextHappyYear(year){
+// ❓ DESCRIPTION:
+// Scenario
+// You're saying good-bye your best friend , See you next happy year .
+// Happy Year is the year with only distinct digits , (e.g) 2018
+//
+// Task
+// Given a year, Find The next happy year or The closest year You'll see your best friend!alt!alt
+//
+// Notes
+// Year Of Course always Positive .
+// Have no fear , It is guaranteed that the answer exists .
+// It's not necessary that the year passed to the function is Happy one .
+//
+// ✅ SOLUTION:
+
+function nextHappyYear(year) {
     year += 1;
-    if (new Set(Array.from(year.toString(), Number)).size >= 4) return year;
+    if (new Set(Array.from(year.toString() , Number)).size >= 4) return year;
     return nextHappyYear(year);
 }
 
+// 📌 TESTCASE:
 console.log(nextHappyYear(1001)) // 1023
 console.log(nextHappyYear(1123)) // 1203
 console.log(nextHappyYear(2001)) // 2013
