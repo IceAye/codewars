e7c7b1f89a79f8502242d7a036aab7236eb544d2
Irene Volodko <ice7aye@gmail.com>
2024-12-25 23:02:45 +0300

    add description

--- 7kyu/is-it-a-letter.js
+++ 7kyu/is-it-a-letter.js
@@ -1,6 +1,12 @@
+// ❓ DESCRIPTION:
+// Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
+//
+// ✅ SOLUTION:
 function isItLetter(character) {
     return /[a-z]/gi.test(character);
 }
 
+
+// 📌 TESTCASE:
 console.log(isItLetter('a')) // true
 console.log(isItLetter('1')) // false
