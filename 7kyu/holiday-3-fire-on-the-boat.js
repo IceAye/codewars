65167133e487cf698fd579e1d1305f3c78a6a9db
Irene Volodko <ice7aye@gmail.com>
2024-12-26 04:57:46 +0300

    add description

--- 7kyu/holiday-3-fire-on-the-boat.js
+++ 7kyu/holiday-3-fire-on-the-boat.js
@@ -1,8 +1,15 @@
+// ❓ DESCRIPTION:
+// Enjoying your holiday, you head out on a scuba diving trip!
+// Disaster!! The boat has caught fire!!
+// You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring
+// into action. Change any instance of "Fire" into "~~". Then return the string.  Go to work!
+//
+// ✅ SOLUTION:
 function fireFight(s) {
-    return s.replaceAll("Fire", "~~");
+    return s.replaceAll("Fire" , "~~");
 }
 
-
+// 📌 TESTCASE:
 console.log(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"))
 // "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast"
 console.log(fireFight("Mast Deck Engine Water Fire")) // "Mast Deck Engine Water ~~"
