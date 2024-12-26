c57410ba254f20c15deea62beebcd8a8cac29fc8
Irene Volodko <ice7aye@gmail.com>
2024-12-26 05:17:57 +0300

    add solution 2

--- 7kyu/the-office-3-broken-photocopier.js
+++ 7kyu/the-office-3-broken-photocopier.js
@@ -2,6 +2,10 @@ function broken(x) {
     return x.replace(/./g, (char) => (char === "1" ? "0" : "1"));
 }
 
+function broken(x) {
+    return x.replace(/./g, (char) => char ^ 1);
+}
+
 console.log(broken("1")); // "0"
 console.log(broken("10000000101101111110011001000")); // "01111111010010000001100110111"
 console.log(broken("100010")); // "011101"
