c6103da61c319a5293a9cc3043060003b424b0b2
Irene Volodko <ice7aye@gmail.com>
2024-12-26 04:45:39 +0300

    add description

--- 7kyu/bubblesort-once.js
+++ 7kyu/bubblesort-once.js
@@ -1,11 +1,18 @@
+// ❓ DESCRIPTION:
+// Given an array of integers, your function bubblesortOnce/bubblesort_once/BubblesortOnce (or equivalent, depending on
+// your language's naming conventions) should return a new array equivalent to performing exactly 1 complete pass on
+// the original array. Your function should be pure, i.e. it should not mutate the input array.
+//
+// ✅ SOLUTION:
 function bubblesortOnce(a) {
     let sorted = [...a];
     for (let i = 0; i < sorted.length; i++) {
         if (sorted[i] > sorted[i + 1]) {
-            [sorted[i], sorted[i + 1]] = [sorted[i + 1], sorted[i]];
+            [sorted[i] , sorted[i + 1]] = [sorted[i + 1] , sorted[i]];
         }
     }
     return sorted;
 }
 
-console.log(bubblesortOnce([9, 7, 5, 3, 1, 2, 4, 6, 8])); // [7, 5, 3, 1, 2, 4, 6, 8, 9]
+// 📌 TESTCASE:
+console.log(bubblesortOnce([9 , 7 , 5 , 3 , 1 , 2 , 4 , 6 , 8])); // [7, 5, 3, 1, 2, 4, 6, 8, 9]
