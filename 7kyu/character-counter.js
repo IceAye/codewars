b2053227510d5a6b7421a4214953e0f64b7a16c1
Irene Volodko <ice7aye@gmail.com>
2024-12-27 17:22:28 +0300

    add description

--- 7kyu/character-counter.js
+++ 7kyu/character-counter.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You are going to be given a word. Your job will be to make sure that each character in that word has the exact same number of occurrences. You will return true if it is valid, or false if it is not.
+//
+// For this kata, capitals are considered the same as lowercase letters. Therefore: "A" == "a"
+//
+// The input is a string (with no spaces) containing [a-z],[A-Z],[0-9] and common symbols. The length will be 0 < length < 100.
+//
+// ✅ SOLUTION 1:
 function validateWord(s) {
     let map = new Map();
     for (const char of s) {
@@ -7,11 +15,13 @@ function validateWord(s) {
     return new Set(map.values()).size === 1;
 }
 
+// ✅ SOLUTION 2:
 function validateWord(s) {
     let c = s.toLowerCase()
     return c.length % new Set(c).size === 0
 }
 
+// 📌 TESTCASE:
 console.log(validateWord("abcabc")); // true
 console.log(validateWord("Abcabc")); // true
 console.log(validateWord("abc123")); // true
