b2046e55dc33317e46a1635732fceea8443835c5
Irene Volodko <ice7aye@gmail.com>
2024-12-28 07:34:23 +0300

    add solution 2

--- 7kyu/bingo-ot-not.js
+++ 7kyu/bingo-ot-not.js
@@ -12,5 +12,10 @@ function bingo(a) {
     return result.join("") === "bingo" ? "WIN" : "LOSE";
 }
 
+function bingo(a) {
+    return [2,9,14,7,15].every(x => a.includes(x)) ? "WIN" : "LOSE"
+}
+
+
 console.log(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // "LOSE"
 console.log(bingo([21, 13, 2, 7, 5, 14, 7, 15, 9, 10])); // "WIN"
