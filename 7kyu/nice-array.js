a90d5ee3d40452c9600963a050b90bfdd20ddcbf
Irene Volodko <ice7aye@gmail.com>
2024-12-28 06:53:33 +0300

    add description

--- 7kyu/nice-array.js
+++ 7kyu/nice-array.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// A Nice array is defined to be an array where for every value n in the array, there is also an element n - 1 or n + 1
+// in the array. Write a function named isNice/IsNice that returns true if its array argument is a Nice array, else
+// false. An empty array is not considered nice.
+//
+// ✅ SOLUTION:
 function isNice(arr) {
     if (arr.length === 0) return false;
     let count = 0;
@@ -7,6 +13,7 @@ function isNice(arr) {
     return count === arr.length;
 }
 
-console.log(isNice([2,10,9,3])) // true
-console.log(isNice([3,4,5,7])) // false
+// 📌 TESTCASE:
+console.log(isNice([2 , 10 , 9 , 3])) // true
+console.log(isNice([3 , 4 , 5 , 7])) // false
 console.log(isNice([])) // false
