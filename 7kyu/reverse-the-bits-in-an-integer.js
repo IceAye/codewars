af34184ce09c3ebff4eeae94b5e0e62e9840ae4f
Irene Volodko <ice7aye@gmail.com>
2024-12-29 06:25:44 +0300

    add solution

---
+++ 7kyu/reverse-the-bits-in-an-integer.js
@@ -0,0 +1,10 @@
+function reverseBits(n) {
+    return parseInt(n.toString(2).split("").reverse().join(""), 2);
+}
+
+console.log(reverseBits(417)); // 267
+console.log(reverseBits(267)); // 417
+console.log(reverseBits(0)); // 0
+console.log(reverseBits(2017)); // 1087
+console.log(reverseBits(1023)); // 1023
+console.log(reverseBits(1024)); // 1
