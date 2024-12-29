cf7be9b5d6df38bf873a57a3e1ec3793195cdbab
Irene Volodko <ice7aye@gmail.com>
2024-12-29 06:17:49 +0300

    add description

--- 7kyu/dot-calculator.js
+++ 7kyu/dot-calculator.js
@@ -1,4 +1,20 @@
-function dotCalculator (equation) {
+// ❓ DESCRIPTION:
+// You have to write a calculator that receives strings for input.
+// The dots will represent the number in the equation.
+// There will be dots on one side, an operator, and dots again after the operator.
+// The dots and the operator will be
+// separated by one space.
+// Here are the following valid operators :
+// + Addition
+// - Subtraction
+// * Multiplication
+// // Integer Division
+// Your Work (Task) You'll have to return a string that contains dots, as many the equation returns.
+// If the result is 0, return the empty string. When it comes to subtraction, the first number will always be greater
+// than or equal to the second number.
+//
+// ✅ SOLUTION:
+function dotCalculator(equation) {
     let array = equation.split(' ');
 
     let number1 = array[0].length;
@@ -23,6 +39,7 @@ function dotCalculator (equation) {
     return '.'.repeat(result);
 }
 
+// 📌 TESTCASE:
 console.log(dotCalculator("..... + ...............")) //  "...................."
 console.log(dotCalculator("..... - ...")) //  ".."
 console.log(dotCalculator("..... - .")) //  "...."
