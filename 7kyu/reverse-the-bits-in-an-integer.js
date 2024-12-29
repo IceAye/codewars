16ad30fe8ebabcf206908a3b067f3cddb3111e0d
Irene Volodko <ice7aye@gmail.com>
2024-12-29 06:26:46 +0300

    add solution

--- 7kyu/reverse-the-bits-in-an-integer.js
+++ 7kyu/reverse-the-bits-in-an-integer.js
@@ -1,7 +1,14 @@
+// ❓ DESCRIPTION:
+// Write a function that reverses the bits in an integer.
+// For example, the number 417 is 110100001 in binary. Reversing the binary is 100001011 which is 267.
+// You can assume that the number is not negative.
+//
+// ✅ SOLUTION:
 function reverseBits(n) {
-    return parseInt(n.toString(2).split("").reverse().join(""), 2);
+    return parseInt(n.toString(2).split("").reverse().join("") , 2);
 }
 
+// 📌 TESTCASE:
 console.log(reverseBits(417)); // 267
 console.log(reverseBits(267)); // 417
 console.log(reverseBits(0)); // 0
