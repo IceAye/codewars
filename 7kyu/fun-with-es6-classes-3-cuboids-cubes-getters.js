109ae4e1652028b21682b195b579bdb29970e6dc
Irene Volodko <ice7aye@gmail.com>
2024-12-29 06:50:09 +0300

    add description

--- 7kyu/fun-with-es6-classes-3-cuboids-cubes-getters.js
+++ 7kyu/fun-with-es6-classes-3-cuboids-cubes-getters.js
@@ -1,3 +1,18 @@
+// ❓ DESCRIPTION:
+// Define the following classes.
+//
+// I. Cuboid
+// The object constructor for the class Cuboid should receive exactly three arguments in the following order: length,
+// width, height and store these three values in this.length, this.width and this.height respectively.
+// The class Cuboid should then have a getter surfaceArea which returns the surface area of the cuboid and a getter
+// volume which returns the volume of the cuboid.
+//
+// II. Cube
+// class Cube is a subclass of class Cuboid. The constructor function of Cube should receive one argument only, its
+// length, and use that value passed in to set this.length, this.width and this.height.
+// Hint: Make a call to super, passing in the correct arguments, to make life easier ;)
+//
+// ✅ SOLUTION:
 class Cuboid {
     constructor(length, width, height) {
         this.length = length;
@@ -25,6 +40,7 @@ class Cube extends Cuboid {
     }
 }
 
+// 📌 TESTCASE:
 let cuboid = new Cuboid(1, 2, 3);
 console.log(cuboid.length); // 1
 console.log(cuboid.width); // 2
