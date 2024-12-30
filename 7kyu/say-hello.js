a49d0280bfa944825dc2530f4f5fa6e086b6f3a9
Irene Volodko <ice7aye@gmail.com>
2024-12-30 13:05:41 +0300

    add description

--- 7kyu/say-hello.js
+++ 7kyu/say-hello.js
@@ -1,6 +1,11 @@
+// ❓ DESCRIPTION:
+// Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
+//
+// ✅ SOLUTION:
 function greet(name) {
     return name ? `hello ${name}!` : null
 }
 
+// 📌 TESTCASE:
 console.log(greet("Niks")) // "hello Niks!"
 console.log(greet(null)) // null
