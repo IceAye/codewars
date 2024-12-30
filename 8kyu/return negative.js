95cd0f1583e3ff3c5f267115d24f9e4f0c18ea8d
Irene Volodko <ice7aye@gmail.com>
2024-12-30 23:49:30 +0300

    add description

--- 8kyu/return negative.js
+++ 8kyu/return negative.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already
+// negative?
+//
+// ✅ SOLUTION:
 function makeNegative(num) {
     return num > 0 ? -num : num;
 }
