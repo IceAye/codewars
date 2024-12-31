e977103076ad7f1379c28aaa4c093d149a0e9083
Irene Volodko <ice7aye@gmail.com>
2024-12-31 04:39:14 +0300

    add solution 2

--- 7kyu/number-pairs.js
+++ 7kyu/number-pairs.js
@@ -1,7 +1,4 @@
-// ❓ DESCRIPTION:
-// In this kata the aim is to compare each pair of integers from two arrays, and return a new array of large numbers.
-// Note: Both arrays have the same dimensions.
-//
+
 // ✅ SOLUTION 1:
 function getLargerNumbers(a , b) {
     let result = [];
