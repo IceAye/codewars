28eb8df33dac450f7348d98ffae9a05d03f5527e
Irene Volodko <ice7aye@gmail.com>
2024-12-31 04:15:03 +0300

    add description

--- 7kyu/filter-long-words.js
+++ 7kyu/filter-long-words.js
@@ -1,5 +1,11 @@
+// ❓ DESCRIPTION:
+// Write a function that takes a string and an an integer n as parameters and returns a list of all words that are
+// longer than n.
+//
+// ✅ SOLUTION:
 function filterLongWords(sentence , n) {
     return sentence.split(" ").filter(word => word.length > n);
 }
 
+// 📌 TESTCASE:
 console.log(filterLongWords("The quick brown fox jumps over the lazy dog" , 4)) // ['quick', 'brown', 'jumps']
