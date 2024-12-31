e860eee2a24c5b14d5dc92e2afd4261c97e460eb
Irene Volodko <ice7aye@gmail.com>
2024-12-31 03:52:29 +0300

    add description

--- 7kyu/make-backronym.js
+++ 7kyu/make-backronym.js
@@ -1,5 +1,10 @@
-//preload variable: dict
-
+// ❓ DESCRIPTION:
+// Complete the function to create backronyms. Transform the given string (without spaces) to a backronym, using the preloaded dictionary and return a string of words, separated with a single space (but no trailing spaces).
+//
+// The keys of the preloaded dictionary are uppercase letters A-Z and the values are predetermined words
+//
+// ✅ SOLUTION:
+// preload variable: dict
 const makeBackronym = function (string) {
     let result = [];
     for (const part of string.split('')) {
