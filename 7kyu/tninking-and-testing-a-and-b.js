43323f3878a71e1c31defc8d59b7209c876ae4b0
Irene Volodko <ice7aye@gmail.com>
2025-01-02 22:52:06 +0300

    add solution

---
+++ 7kyu/tninking-and-testing-a-and-b.js
@@ -0,0 +1,9 @@
+function testit(a,b){
+    return a | b
+}
+
+console.log(testit(0,1)) // 1
+console.log(testit(1,2)) // 3
+console.log(testit(10,20)) // 30
+console.log(testit(1,1)) // 1
+console.log(testit(1,3)) // 3
