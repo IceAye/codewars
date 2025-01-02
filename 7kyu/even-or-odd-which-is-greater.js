48967f6ff32ef5f98ef148bca448fd0fd652157b
Irene Volodko <ice7aye@gmail.com>
2025-01-02 23:09:01 +0300

    add description

--- 7kyu/even-or-odd-which-is-greater.js
+++ 7kyu/even-or-odd-which-is-greater.js
@@ -1,5 +1,13 @@
+// ❓ DESCRIPTION:
+// Given a string of digits confirm whether the sum of all the individual even digits are greater than the sum of all
+// the indiviudal odd digits. Always a string of numbers will be given.
+// If the sum of even numbers is greater than the odd numbers return: "Even is greater than Odd"
+// If the sum of odd numbers is greater than the sum of even numbers return: "Odd is greater than Even"
+// If the total of both even and odd numbers are identical return: "Even and Odd are the same"
+//
+// ✅ SOLUTION:
 function evenOrOdd(str) {
-    let numbers = Array.from(str, Number);
+    let numbers = Array.from(str , Number);
     let evens = 0;
     let odds = 0;
     for (const number of numbers) {
@@ -12,6 +20,7 @@ function evenOrOdd(str) {
             : "Even and Odd are the same";
 }
 
+// 📌 TESTCASE:
 console.log(evenOrOdd("12")); // 'Even is greater than Odd'
 console.log(evenOrOdd("123")); // 'Odd is greater than Even'
 console.log(evenOrOdd("112")); // 'Even and Odd are the same'
