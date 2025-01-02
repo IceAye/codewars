a0ad2b0224f361a586feb0bc1ebe62be43c3e90a
Irene Volodko <ice7aye@gmail.com>
2025-01-02 22:53:06 +0300

    add description

--- 7kyu/tninking-and-testing-a-and-b.js
+++ 7kyu/tninking-and-testing-a-and-b.js
@@ -1,7 +1,15 @@
+// ❓ DESCRIPTION:
+// No Story
+// No Description
+// Only by Thinking and Testing
+// Look at result of testcase, guess the code!
+//
+// ✅ SOLUTION:
 function testit(a,b){
     return a | b
 }
 
+// 📌 TESTCASE:
 console.log(testit(0,1)) // 1
 console.log(testit(1,2)) // 3
 console.log(testit(10,20)) // 30
