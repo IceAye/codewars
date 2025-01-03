c887ec672a8a84a95602a51408dabff3a700c5e0
Irene Volodko <ice7aye@gmail.com>
2025-01-03 23:05:04 +0300

    add description

--- 7kyu/simple-fun-172-count-number.js
+++ 7kyu/simple-fun-172-count-number.js
@@ -1,4 +1,10 @@
-function countNumber(n, x) {
+// ❓ DESCRIPTION:
+// Let's consider a table consisting of n rows and n columns. The cell located at the intersection of the i-th row and
+// the j-th column contains number i × j. The rows and columns are numbered starting from 1.  You are given a positive
+// integer x. Your task is to count the number of cells in a table that contain number x.
+//
+// ✅ SOLUTION:
+function countNumber(n , x) {
     let count = 0;
     let i = 1;
     while (i <= n) {
@@ -8,8 +14,9 @@ function countNumber(n, x) {
     return count;
 }
 
-console.log(countNumber(5,5)) // 2
-console.log(countNumber(10,5)) // 2
-console.log(countNumber(6,12)) // 4
-console.log(countNumber(100000,1000000000)) // 16
-console.log(countNumber(9,484)) // 0
+// 📌 TESTCASE:
+console.log(countNumber(5 , 5)) // 2
+console.log(countNumber(10 , 5)) // 2
+console.log(countNumber(6 , 12)) // 4
+console.log(countNumber(100000 , 1000000000)) // 16
+console.log(countNumber(9 , 484)) // 0
