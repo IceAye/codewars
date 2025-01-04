35e6aa9cc14d346651542c624e6de5873fe4d69f
Irene Volodko <ice7aye@gmail.com>
2025-01-04 23:42:24 +0300

    add solution 2

--- 7kyu/filter-unused-digits.js
+++ 7kyu/filter-unused-digits.js
@@ -5,5 +5,9 @@ function unusedDigits(...args) {
         .join("");
 }
 
+function unusedDigits(...args){
+    return "0123456789".replace(new RegExp('['+args.join('')+']','g'), '')
+}
+
 console.log(unusedDigits(12, 34, 56, 78)); // "09"
 console.log(unusedDigits(2015, 8, 26)); // "3479"
