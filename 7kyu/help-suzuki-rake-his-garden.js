66caad2d8c588ebd1d34f10f05cabeaffa861892
Irene Volodko <ice7aye@gmail.com>
2025-01-04 23:04:22 +0300

    add solution 1

---
+++ 7kyu/help-suzuki-rake-his-garden.js
@@ -0,0 +1,22 @@
+function rakeGarden(garden) {
+    return garden
+        .split(" ")
+        .map((item) => item === "rock" ? "rock" : "gravel")
+        .join(" ");
+}
+
+let garden1 =
+    "slug spider rock gravel gravel gravel gravel gravel gravel gravel";
+console.log(rakeGarden(garden1)); // 'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
+
+let garden2 =
+    "gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel";
+console.log(rakeGarden(garden2)); //  'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel
+// gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel
+// gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel
+// gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel
+// gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel
+// gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel
+// gravel gravel gravel gravel gravel gravel rock gravel gravel rock gravel gravel
+// gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel
+// gravel rock gravel gravel'
