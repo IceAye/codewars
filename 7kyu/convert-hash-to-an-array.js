add16b52e3046fcb4a4d67057480c11b6840c22e
Irene Volodko <ice7aye@gmail.com>
2025-01-04 22:56:16 +0300

    add description

--- 7kyu/convert-hash-to-an-array.js
+++ 7kyu/convert-hash-to-an-array.js
@@ -1,7 +1,19 @@
+// ❓ DESCRIPTION:
+// Convert a hash into an array. Nothing more, Nothing less.
+//
+// {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+// should be converted into
+//
+// [["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+// Good Luck!
+//
+// ✅ SOLUTION:
+
 function convertHashToArray(hash) {
     return Object.entries(hash);
 }
 
+// 📌 TESTCASE:
 console.log(convertHashToArray({ name: "Jeremy" })); // [["name", "Jeremy"]]
 console.log(convertHashToArray({ name: "Jeremy", age: 24 })); // [["age", 24], ["name", "Jeremy"]]
 
