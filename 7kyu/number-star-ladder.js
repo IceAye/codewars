9fe1c332341cfd0b1a35557c611ca8eddef2edee
Irene Volodko <ice7aye@gmail.com>
2025-01-05 04:44:14 +0300

    add description

--- 7kyu/number-star-ladder.js
+++ 7kyu/number-star-ladder.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Using n as a parameter in the function pattern, where n>0, complete the codes to get the pattern.
+// Note: There is no newline in the end (after the pattern ends)
+//
+// ✅ SOLUTION:
 function pattern(n) {
     let output = "1";
     let i = 1;
@@ -8,5 +13,6 @@ function pattern(n) {
     return output;
 }
 
+// 📌 TESTCASE:
 console.log(pattern(3)); // "1\n1*2\n1**3"
 console.log(pattern(7)); // "1\n1*2\n1**3\n1***4\n1****5\n1*****6\n1******7"
