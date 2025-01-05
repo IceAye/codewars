40e4dd68f634ad206d75c696b9716f3a33de25d3
Irene Volodko <ice7aye@gmail.com>
2025-01-05 03:57:43 +0300

    add description

--- 7kyu/arithmetic-progression.js
+++ 7kyu/arithmetic-progression.js
@@ -1,4 +1,11 @@
-function arithmeticSequenceElements(a, d, n) {
+// ❓ DESCRIPTION:
+// In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have
+// decided to write a function that will return the first n elements of the sequence with the given common difference d
+// and first element a. Note that the difference may be zero!
+// The result should be a string of numbers, separated by comma and space.
+//
+// ✅ SOLUTION 1:
+function arithmeticSequenceElements(a , d , n) {
     let array = [a];
     let number = a;
     while (array.length < n) {
@@ -8,11 +15,13 @@ function arithmeticSequenceElements(a, d, n) {
     return array.join(", ");
 }
 
-function arithmeticSequenceElements(a, d, n) {
-    return new Array(n).fill(0).map((i,k) => a + k * d).join(", ");
+// ✅ SOLUTION 2:
+function arithmeticSequenceElements(a , d , n) {
+    return new Array(n).fill(0).map((i , k) => a + k * d).join(", ");
 }
 
-console.log(arithmeticSequenceElements(1, 2, 5)); // "1, 3, 5, 7, 9"
-console.log(arithmeticSequenceElements(1, 0, 5)); // "1, 1, 1, 1, 1"
-console.log(arithmeticSequenceElements(1, -3, 10)); // "1, -2, -5, -8, -11, -14, -17, -20, -23, -26"
-console.log(arithmeticSequenceElements(100, -10, 10)); // "100, 90, 80, 70, 60, 50, 40, 30, 20, 10"
+// 📌 TESTCASE:
+console.log(arithmeticSequenceElements(1 , 2 , 5)); // "1, 3, 5, 7, 9"
+console.log(arithmeticSequenceElements(1 , 0 , 5)); // "1, 1, 1, 1, 1"
+console.log(arithmeticSequenceElements(1 , -3 , 10)); // "1, -2, -5, -8, -11, -14, -17, -20, -23, -26"
+console.log(arithmeticSequenceElements(100 , -10 , 10)); // "100, 90, 80, 70, 60, 50, 40, 30, 20, 10"
