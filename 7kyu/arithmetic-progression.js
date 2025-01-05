8276795c1c6e291e1838e775835dba23f4d6db28
Irene Volodko <ice7aye@gmail.com>
2025-01-05 03:55:35 +0300

    add solution

---
+++ 7kyu/arithmetic-progression.js
@@ -0,0 +1,14 @@
+function arithmeticSequenceElements(a, d, n) {
+    let array = [a];
+    let number = a;
+    while (array.length < n) {
+        number += d;
+        array.push(number);
+    }
+    return array.join(", ");
+}
+
+console.log(arithmeticSequenceElements(1, 2, 5)); // "1, 3, 5, 7, 9"
+console.log(arithmeticSequenceElements(1, 0, 5)); // "1, 1, 1, 1, 1"
+console.log(arithmeticSequenceElements(1, -3, 10)); // "1, -2, -5, -8, -11, -14, -17, -20, -23, -26"
+console.log(arithmeticSequenceElements(100, -10, 10)); // "100, 90, 80, 70, 60, 50, 40, 30, 20, 10"
