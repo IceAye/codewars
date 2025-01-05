1d024d6d896f5d82d744a48ce68f2234ab57b78c
Irene Volodko <ice7aye@gmail.com>
2025-01-05 04:19:32 +0300

    add description

--- 7kyu/from-a-to-z.js
+++ 7kyu/from-a-to-z.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter.
+// Note that if the range is given in capital letters, return the string in capitals also!
+//
+// Notes
+// A hyphen will separate the two letters in the string.
+// You don't need to worry about error handling in this kata (i.e. both letters will be the same case and the second
+// letter will not be before the first alphabetically).
+//
+// ✅ SOLUTION:
 function gimmeTheLetters(sp) {
     let start = sp.charCodeAt(0);
     let end = sp.charCodeAt(sp.length - 1);
@@ -11,6 +21,7 @@ function gimmeTheLetters(sp) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(gimmeTheLetters("J-J")); // 'J'
 console.log(gimmeTheLetters("Z-Z")); // 'Z'
 console.log(gimmeTheLetters("a-a")); // 'a'
