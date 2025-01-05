e6e0bf878b2d1525bc6ac01cf30c996997270db2
Irene Volodko <ice7aye@gmail.com>
2025-01-05 03:56:34 +0300

    add solution 2

--- 7kyu/arithmetic-progression.js
+++ 7kyu/arithmetic-progression.js
@@ -8,6 +8,10 @@ function arithmeticSequenceElements(a, d, n) {
     return array.join(", ");
 }
 
+function arithmeticSequenceElements(a, d, n) {
+    return new Array(n).fill(0).map((i,k) => a + k * d).join(", ");
+}
+
 console.log(arithmeticSequenceElements(1, 2, 5)); // "1, 3, 5, 7, 9"
 console.log(arithmeticSequenceElements(1, 0, 5)); // "1, 1, 1, 1, 1"
 console.log(arithmeticSequenceElements(1, -3, 10)); // "1, -2, -5, -8, -11, -14, -17, -20, -23, -26"
