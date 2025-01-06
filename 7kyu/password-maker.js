4b19261c9ec094a9a26a8c784064e94ab71a1aeb
Irene Volodko <ice7aye@gmail.com>
2025-01-06 23:07:58 +0300

    add description

--- 7kyu/password-maker.js
+++ 7kyu/password-maker.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// One suggestion to build a satisfactory password is to start with a memorable phrase or sentence and make a password by extracting the first letter of each word.
+//
+// Even better is to replace some of those letters with numbers (e.g., the letter O can be replaced with the number 0):
+//
+// instead of including i or I put the number 1 in the password;
+// instead of including o or O put the number 0 in the password;
+// instead of including s or S put the number 5 in the password.
+//
+// ✅ SOLUTION 1:
 function makePassword(phrase) {
     const cypher = {
         i: "1",
@@ -10,6 +20,7 @@ function makePassword(phrase) {
     }, "");
 }
 
+// ✅ SOLUTION 2:
 function makePassword(phrase) {
     return phrase
         .split(" ")
@@ -18,5 +29,6 @@ function makePassword(phrase) {
         .replace(/[iso]/gi, (x) => ({ i: 1, s: 5, o: 0 })[x.toLowerCase()]);
 }
 
+// 📌 TESTCASE:
 console.log(makePassword("Give me liberty or give me death")); // "Gml0gmd"
 console.log(makePassword("Keep Calm and Carry On")); // "KCaC0"
