9e2759026db9d26c0268da248f22d82e6d924d7a
Irene Volodko <ice7aye@gmail.com>
2025-01-06 23:36:56 +0300

    add description

--- 7kyu/interactive-dictionary.js
+++ 7kyu/interactive-dictionary.js
@@ -1,3 +1,18 @@
+// ❓ DESCRIPTION:
+// In this kata, your job is to create a class Dictionary which you can add words to and their entries. Example:
+//
+// >>> let d = new Dictionary();
+//
+// >>> d.newEntry("Apple", "A fruit that grows on trees");
+//
+// >>> console.log(d.look("Apple"));
+// A fruit that grows on trees
+//
+// >>> console.log(d.look("Banana"));
+// Can't find entry for Banana
+// Good luck and happy coding!
+//
+// ✅ SOLUTION:
 class Dictionary {
     constructor() {
         this.dict = {};
@@ -12,6 +27,7 @@ class Dictionary {
     }
 }
 
+// 📌 TESTCASE:
 let d = new Dictionary();
 d.newEntry("Apple", "A fruit");
 console.log(d.look("Apple")); // 'A fruit'
