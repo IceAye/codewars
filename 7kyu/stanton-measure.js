de5a7807deeaf15960c5d4836c47fe78b2a678b2
Irene Volodko <ice7aye@gmail.com>
2025-01-06 23:46:33 +0300

    add solution 2

--- 7kyu/stanton-measure.js
+++ 7kyu/stanton-measure.js
@@ -6,4 +6,12 @@ function stantonMeasure(a) {
     return map.get(map.get(1)) || 0;
 }
 
+function stantonMeasure(a) {
+    function sum(n) {
+        return a.filter((x) => x === n).length;
+    }
+
+    return sum(sum(1));
+}
+
 console.log(stantonMeasure([1, 4, 3, 2, 1, 2, 3, 2])); // 3
