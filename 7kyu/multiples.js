6b0db997edc8b2f68a7e08f5adc8c699bc05cb11
Irene Volodko <ice7aye@gmail.com>
2025-01-07 23:06:59 +0300

    add description

--- 7kyu/multiples.js
+++ 7kyu/multiples.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Make a program that takes a value (x) and returns "Bang" if the number is divisible by 3, "Boom" if it is divisible by 5, "BangBoom" if it divisible by 3 and 5, and "Miss" if it isn't divisible by any of them. Note: Your program should only return one value
+//
+// Ex: Input: 105 --> Output: "BangBoom" Ex: Input: 9 --> Output: "Bang" Ex:Input: 25 --> Output: "Boom"
+//
+// ✅ SOLUTION:
 function multiple(x) {
     return x % 5 === 0 && x % 3 === 0
         ? "BangBoom"
@@ -8,6 +14,7 @@ function multiple(x) {
                 : "Miss";
 }
 
+// 📌 TESTCASE:
 console.log(multiple(30)); // "BangBoom"
 console.log(multiple(3)); // "Bang"
 console.log(multiple(98)); //"Miss"
