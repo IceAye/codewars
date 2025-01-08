5b821e390092789de6f6189026166d20cb0dd4ee
Irene Volodko <ice7aye@gmail.com>
2025-01-08 21:59:46 +0300

    add description

--- 7kyu/find-all-occurrences-of-an-element-in-an-array.js
+++ 7kyu/find-all-occurrences-of-an-element-in-an-array.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given an array (a list in Python) of integers and an integer n, find all occurrences of n in the given array and
+// return another array containing all the index positions of n in the given array.
+// If n is not in the given array, return an empty array [].
+// Assume that n and all values in the given array will always be integers.
+//
+// ✅ SOLUTION:
 const findAll = (array, n) => {
     return array.reduce((result, current, index) => {
         if (current === n) {
@@ -7,6 +14,7 @@ const findAll = (array, n) => {
     }, []);
 };
 
+// 📌 TESTCASE:
 console.log(findAll([6, 9, 3, 4, 3, 82, 11], 3)) // [2, 4]);
 console.log(findAll([10, 16, 20, 6, 14, 11, 20, 2, 17, 16, 14], 16)) // [1, 9]);
 console.log(findAll([20, 20, 10, 13, 15, 2, 7, 2, 20, 3, 18, 2, 3, 2, 16, 10, 9, 9, 7, 5, 15, 5], 20)) // [0, 1, 8]);
