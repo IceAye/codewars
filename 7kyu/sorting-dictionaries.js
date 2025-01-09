1a3985e198d9d949d8bf7641582acacdd51494f2
Irene Volodko <ice7aye@gmail.com>
2025-01-09 20:58:47 +0300

    add description

--- 7kyu/sorting-dictionaries.js
+++ 7kyu/sorting-dictionaries.js
@@ -1,9 +1,16 @@
+// ❓ DESCRIPTION:
+// Python dictionaries are inherently unsorted. So what do you do if you need to sort the contents of a dictionary?
+// Create a function that returns a sorted list of (key, value) tuples (Javascript: arrays of 2 items).
+// The list must be sorted by the value and be sorted largest to smallest.
+//
+// ✅ SOLUTION:
 function sortDict(dict) {
     return Object.entries(dict)
                  .map((entry) => [+entry[0] || entry[0], entry[1]])
                  .sort((a, b) => b[1] - a[1]);
 }
 
+// 📌 TESTCASE:
 console.log(sortDict({ 1: 3, 2: 2, 3: 1 })); //,          [[1,3],[2,2],[3,1]]
 console.log(sortDict({ 3: 1, 2: 2, 1: 3 })); //,          [[1,3],[2,2],[3,1]]
 console.log(sortDict({ 1: 2, 2: 4, 3: 6 })); //,          [[3,6],[2,4],[1,2]]
