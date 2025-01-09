2e6c2e8dcaa321978d289f731a5847daf3435b73
Irene Volodko <ice7aye@gmail.com>
2025-01-09 21:18:23 +0300

    add description

--- 7kyu/return-a-sortes-list-of-objects.js
+++ 7kyu/return-a-sortes-list-of-objects.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// You'll be passed an array of objects (list) - you must sort them in descending order based on the value of the specified property (sortBy).
+//
+// ✅ SOLUTION:
 function sortList (sortBy, list) {
     return list.sort((a, b) => b[sortBy] - a[sortBy]);
 }
 
+// 📌 TESTCASE:
 console.log(sortList("a", [
     {"a": 1, "b": 3},
     {"a": 3, "b": 2},
