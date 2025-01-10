a0f8aaadac1b776a1a5178ceee1d2fad39af64ca
Irene Volodko <ice7aye@gmail.com>
2025-01-10 23:52:48 +0300

    add description

--- 8kyu/price-of-mangoes.js
+++ 8kyu/price-of-mangoes.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Accountant time! For a given quantity and price (per mango), calculate the total cost of the mangoes.
+// But! Every third mango is free!
+//
+// ✅ SOLUTION:
 function mango(quantity, price){
     return (quantity - Math.floor(quantity / 3)) * price
 }
