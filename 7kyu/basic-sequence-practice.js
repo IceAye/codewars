4a07d3f923a5c7886d233fff512dbb51a49d792c
Irene Volodko <ice7aye@gmail.com>
2025-01-11 21:10:09 +0300

    add description

--- 7kyu/basic-sequence-practice.js
+++ 7kyu/basic-sequence-practice.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// A sequence or a series, in mathematics, is a string of objects, like numbers, that follow a particular pattern. The individual elements in a sequence are called terms. A simple example is 3, 6, 9, 12, 15, 18, 21, ..., where the pattern is: "add 3 to the previous term".
+// In this kata, we will be using a more complicated sequence: 0, 1, 3, 6, 10, 15, 21, 28, ... This sequence is generated with the pattern: "the nth term is the sum of numbers from 0 to n, inclusive".
+// [ 0,  1,    3,      6,   ...]
+//   0  0+1  0+1+2  0+1+2+3
+// Your Task
+// Complete the function that takes an integer n and returns a list/array of length abs(n) + 1 of the arithmetic series explained above. Whenn < 0 return the sequence with negative terms.
+//
+// ✅ SOLUTION:
 function sumOfN(n) {
     let result = [0];
     for (let i = 0; i < Math.abs(n); i++) {
@@ -6,6 +15,7 @@ function sumOfN(n) {
     return n < 0 ? result.map(item => -item) : result;
 }
 
+// 📌 TESTCASE:
 console.log(sumOfN(3)) // [0, 1, 3, 6]
 console.log(sumOfN(-4)) // [0, -1, -3, -6, -10]
 console.log(sumOfN(1)) // [0, 1]
