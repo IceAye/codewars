7dc8276ca01890a6430f462fb1a6cb79ff7b0980
Irene Volodko <ice7aye@gmail.com>
2025-01-12 03:43:06 +0300

    add description

--- 7kyu/regex-failure-bug-fixing-2.js
+++ 7kyu/regex-failure-bug-fixing-2.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// Problem Description:
+// Oh no, Timmy's received some hate mail recently but he knows better. Help Timmy fix his regex filter so he can be awesome again!
+//
+// Task:
+// You are given a string phrase containing some potentially offensive words such as "bad," "mean," "ugly," "horrible," and "hideous." Timmy wants to replace these words with the word "awesome" to make the message more positive. Your task is to implement a function, that replaces all occurrences of these offensive words with "awesome" in the input string phrase.
+//
+// Input:
+// A string phrase containing words separated by spaces.
+// Output:
+// A string with the same words as phrase, but with any offensive words replaced by "awesome."
+//
+// ✅ SOLUTION:
 function filterWords(phrase){
     return phrase.replace(/(bad|mean|ugly|horrible|hideous)/gi,'awesome');
 }
