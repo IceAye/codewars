b1ec324d0e85234c0feea4551e94eaf28d6cc1cd
Irene Volodko <ice7aye@gmail.com>
2025-01-12 03:39:32 +0300

    add description

--- 7kyu/debug-the-functions-easy.js
+++ 7kyu/debug-the-functions-easy.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Should be easy, begin by looking at the code. Debug the code and the functions should work.
+// There are three functions: Multiplication (x) Addition (+) and Reverse (!esreveR)
+//
+// ✅ SOLUTION:
 function multi(arr) {
     return arr.reduce((acc, curr) => acc * curr);
 }
@@ -10,6 +15,7 @@ function reverse(str) {
     return [...str].reverse().join("");
 }
 
+// 📌 TESTCASE:
 console.log(multi([8, 2, 5])); // 80
 console.log(add([1, 15, 3])); // 19
 console.log(add([7, 8, 6, 5, 4, 9])); // 39
