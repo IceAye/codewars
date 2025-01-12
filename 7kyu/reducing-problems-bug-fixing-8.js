75163bf21e638cbbe633abb3a7d6df0ad00d28ab
Irene Volodko <ice7aye@gmail.com>
2025-01-12 04:07:52 +0300

    add description

--- 7kyu/reducing-problems-bug-fixing-8.js
+++ 7kyu/reducing-problems-bug-fixing-8.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Oh no! Timmy's reduce is causing problems, Timmy's goal is to calculate the two teams scores and return the winner but timmy has gotten confused and sometimes teams don't enter their scores, total the scores out of 3! Help timmy fix his program!
+// Return true if team 1 wins or false if team 2 wins!
+//
+// ✅ SOLUTION:
 function calculateTotal(team1, team2) {
     let t1s = team1.reduce((t, c) => t + c, 0);
     let t2s = team2.reduce((t, c, i) => t + c, 0);
