6e3a2193e0ba3a02d6b468f4e44d0124d6251f20
Irene Volodko <ice7aye@gmail.com>
2025-01-13 15:46:34 +0300

    add solution 2

--- 7kyu/class-conundrum-bug-fixing-7.js
+++ 7kyu/class-conundrum-bug-fixing-7.js
@@ -15,6 +15,25 @@ class List {
     }
 }
 
+class List {
+    constructor(type) {
+        this.type = type;
+        this.items = [];
+    }
+
+    add(item) {
+        if (typeof item !== this.type)
+            return `This item is not of type: ${this.type}`;
+
+        this.items.push(item);
+        return this;
+    }
+
+    get count() {
+        return this.items.length;
+    }
+}
+
 const myList = new List('string')
 console.log(myList.add('Hello').count) // 1
 console.log(myList.add(5)) // `This item is not of type: string`
