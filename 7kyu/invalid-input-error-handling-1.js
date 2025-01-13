6bbb05dd08eca1457bdfb8ab8abe0673e27c511b
Irene Volodko <ice7aye@gmail.com>
2025-01-13 17:59:36 +0300

    add description

--- 7kyu/invalid-input-error-handling-1.js
+++ 7kyu/invalid-input-error-handling-1.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Error Handling is very important in coding and seems to be overlooked or not implemented properly.
+//
+// #Task
+// Your task is to implement a function which takes a string as input and return an object containing the properties vowels and consonants. The vowels property must contain the total count of vowels {a,e,i,o,u}, and the total count of consonants {a,..,z} - {a,e,i,o,u}. Handle invalid input and don't forget to return valid ones.
+//
+// #Input
+// The input is any random string. You must then discern what are vowels and what are consonants and sum for each category their total occurrences in an object. However you could also receive inputs that are not strings. If this happens then you must return an object with a vowels and consonants total of 0 because the input was NOT a string. Refer to the Example section for a more visual representation of which inputs you could receive and the outputs expected. :)
+//
+// ✅ SOLUTION:
 function getCount(words) {
     let sum = 0;
     let vowels = 0;
@@ -15,6 +25,7 @@ function getCount(words) {
     return { vowels, consonants };
 }
 
+// 📌 TESTCASE:
 console.log(getCount('Test')) // {vowels:1,consonants:3}
 console.log(getCount('Here is some text')) // {vowels:6,consonants:8}
 console.log(getCount('To be a Codewarrior or not to be')) // {vowels:12,consonants:13}
