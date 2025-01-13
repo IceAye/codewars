3d044cb69953dcbd36095fa8b80f7f8b2d191449
Irene Volodko <ice7aye@gmail.com>
2025-01-13 17:40:25 +0300

    add description

--- 7kyu/check-your-arguments.js
+++ 7kyu/check-your-arguments.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Bob has written a function to detect the type of an object. It receives any primitive values or objects, and returns its object type as a string. But it seems to be working incorrectly. Can you figure out why, and fix the code for him?
+//
+// ✅ SOLUTION:
 function objectType(...obj) {
     return Object.prototype.toString.call(obj.length ? obj[0] : null);
 }
 
+// 📌 TESTCASE:
 console.log(objectType(42)) // '[object Number]'
 console.log(objectType(undefined)) //'[object Undefined]'
 console.log(objectType()) //'[object Null]'
