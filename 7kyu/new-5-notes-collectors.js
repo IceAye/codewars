683e814d52b2d27eca80bee346e60da3d10a381d
Irene Volodko <ice7aye@gmail.com>
2025-01-15 14:59:37 +0300

    add description

--- 7kyu/new-5-notes-collectors.js
+++ 7kyu/new-5-notes-collectors.js
@@ -1,8 +1,15 @@
+// ❓ DESCRIPTION:
+// The new £5 notes have been recently released in the UK and they've certainly became a sensation! Even those of us who haven't been carrying any cash around for a while, having given in to the convenience of cards, suddenly like to have some of these in their purses and pockets. But how many of them could you get with what's left from your salary after paying all bills? The programme that you're about to write will count this for you!
+// Given a salary and the array of bills, calculate your disposable income for a month and return it as a number of new £5 notes you can get with that amount. If the money you've got (or do not!) doesn't allow you to get any £5 notes return 0.
+// £££ GOOD LUCK! £££
+//
+// ✅ SOLUTION:
 function getNewNotes(salary,bills){
     let result = (salary - bills.reduce((acc, curr) => acc + curr, 0)) / 5;
     return result > 0 ? Math.floor(result) : 0;
 }
 
+// 📌 TESTCASE:
 console.log(getNewNotes(2000, [500, 160, 400])) // 188);
 console.log(getNewNotes(1260, [500, 50, 100])) // 122
 console.log(getNewNotes(3600, [1800, 350, 460, 500, 15])) // 95
