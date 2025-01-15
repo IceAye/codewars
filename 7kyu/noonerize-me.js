3d64b021cf1fa9afadcc38cb1aee71d38441e2f9
Irene Volodko <ice7aye@gmail.com>
2025-01-15 13:39:10 +0300

    add description

--- 7kyu/noonerize-me.js
+++ 7kyu/noonerize-me.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Spoonerize... with numbers... numberize?... numboonerize?... noonerize? ...anyway! If you don't yet know what a spoonerism is and haven't yet tried my spoonerism kata, please do check it out first.
+// You will create a function which takes an array of two positive integers, spoonerizes them, and returns the positive difference between them as a single number or 0 if the numbers are equal:
+// [123, 456] = 423 - 156 = 267
+// Your code must test that all array items are numbers and return "invalid array" if it finds that either item is not a number. The provided array will always contain 2 elements.
+// When the inputs are valid, they will always be integers, no floats will be passed. However, you must take into account that the numbers will be of varying magnitude, between and within test cases.
+//
+// ✅ SOLUTION:
 function noonerize(numbers) {
     if (typeof numbers[0] !== "number" || typeof numbers[1] !== "number") {
         return "invalid array";
@@ -11,6 +19,7 @@ function noonerize(numbers) {
     return Math.abs(num1 - num2);
 }
 
+// 📌 TESTCASE:
 console.log(noonerize([12, 34])); // 18
 console.log(noonerize([55, 63])); // 12
 console.log(noonerize([357, 579])); // 178
