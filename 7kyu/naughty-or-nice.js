72b533dee2d62de8f27e20eb37f5588f5b1224a7
Irene Volodko <ice7aye@gmail.com>
2025-01-15 16:35:23 +0300

    add description

--- 7kyu/naughty-or-nice.js
+++ 7kyu/naughty-or-nice.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// In this kata, you will write a function that receives an array of string and returns a string that is either 'naughty' or 'nice'. Strings that start with the letters b, f, or k are naughty. Strings that start with the letters g, s, or n are nice. Other strings are neither naughty nor nice.
+// If there is an equal amount of bad and good actions, return 'naughty'
+//
+// ✅ SOLUTION:
 function whatListAmIOn(actions) {
     let naughty = 0;
     let nice = 0;
@@ -20,6 +25,7 @@ function whatListAmIOn(actions) {
     return naughty >= nice ? "naughty" : "nice";
 }
 
+// 📌 TESTCASE:
 const bad_actions = ["broke someone's window", "fought over a toaster", "killed a bug"],
     good_actions = ["got someone a new car", "saved a man from drowning", "never got into a fight"],
     actions = ["broke a vending machine", "never got into a fight", "tied someone's shoes"];
