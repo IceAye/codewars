8e52fa69b698c50f5dc3694bc61b219300403155
Irene Volodko <ice7aye@gmail.com>
2025-01-17 08:10:53 +0300

    add solution 2

--- 7kyu/paperboy.js
+++ 7kyu/paperboy.js
@@ -23,6 +23,26 @@ function cheapestQuote(newspapers) {
     return +count.toFixed(2);
 }
 
+function cheapestQuote(newspapers) {
+    let count = 0;
+
+    count += Math.floor(newspapers/40) * 3.85;
+    newspapers %= 40;
+
+    count += Math.floor(newspapers/20) * 1.93;
+    newspapers %= 20;
+
+    count += Math.floor(newspapers/10) * 0.97;
+    newspapers %= 10;
+
+    count += Math.floor(newspapers/5) * 0.49;
+    newspapers %= 5;
+
+    count += newspapers * 0.1;
+
+    return Math.round(count * 100) / 100;
+};
+
 console.log(cheapestQuote(40)) //  3.85
 console.log(cheapestQuote(20)) //  1.93
 console.log(cheapestQuote(10)) //  0.97
