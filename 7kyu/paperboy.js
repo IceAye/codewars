645757c817d173bf225d3697e7853ecd13916f1f
Irene Volodko <ice7aye@gmail.com>
2025-01-17 08:12:22 +0300

    add description

--- 7kyu/paperboy.js
+++ 7kyu/paperboy.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// You and your best friend Stripes have just landed your first high school jobs! You'll be delivering newspapers to your neighbourhood on weekends. For your services you'll be charging a set price depending on the quantity of the newspaper bundles.
+//
+// The cost of deliveries is:
+// $3.85 for 40 newspapers
+// $1.93 for 20
+// $0.97 for 10
+// $0.49 for 5
+// $0.10 for 1
+// Stripes is taking care of the footwork doing door-to-door drops and your job is to take care of the finances. What you'll be doing is providing the cheapest possible quotes for your services.
+// Write a function that's passed an integer representing the amount of newspapers and returns the cheapest price. The returned number must be rounded to two decimal places.
+//
+// ✅ SOLUTION 1:
 function cheapestQuote(newspapers) {
     let count = 0;
     while (newspapers >= 40) {
@@ -23,6 +36,7 @@ function cheapestQuote(newspapers) {
     return +count.toFixed(2);
 }
 
+// ✅ SOLUTION 2:
 function cheapestQuote(newspapers) {
     let count = 0;
 
@@ -41,8 +55,9 @@ function cheapestQuote(newspapers) {
     count += newspapers * 0.1;
 
     return Math.round(count * 100) / 100;
-};
+}
 
+// 📌 TESTCASE:
 console.log(cheapestQuote(40)) //  3.85
 console.log(cheapestQuote(20)) //  1.93
 console.log(cheapestQuote(10)) //  0.97
