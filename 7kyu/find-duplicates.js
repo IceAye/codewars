dfd550d9760afb6fc1b3bf67cfe67549e9ffa75e
Irene Volodko <ice7aye@gmail.com>
2025-01-17 15:04:34 +0300

    add solution

---
+++ 7kyu/find-duplicates.js
@@ -0,0 +1,23 @@
+function duplicates(arr) {
+    let result = [];
+    let prefix = [];
+
+    for (const item of arr) {
+        if (prefix.includes(item) && !result.includes(item)) {
+            result.push(item);
+        }
+        prefix.push(item);
+    }
+
+    return result;
+}
+
+console.log(duplicates([1, 2, 3, 4, 5])); // []
+console.log(duplicates([])); // []
+console.log(duplicates(["no", "duplicates", "here"])); // []
+console.log(duplicates([1, 2, 3, 4, 3])); // [3]
+console.log(duplicates([1, 2, 3, 3, 2, 1])); // [3, 2, 1]
+console.log(duplicates([1, 2, 1, 2, 1, 2, 1])); // [1, 2]
+console.log(duplicates([1, 2, 3, 4, "3"])); // []
+console.log(duplicates(["1", 2, 3, 3, "2", 1])); // [3]
+console.log(duplicates(["zut", "alors", 1, 2, 4, 4, 3, 3, "1", 5, 3, "zut"])); // [4, 3, 'zut']
