4ff7823ffb81d4945ea7a89feeaa3b26e2429a3d
Irene Volodko <ice7aye@gmail.com>
2025-01-18 13:33:10 +0300

    add solution

---
+++ 7kyu/pair-zeroes.js
@@ -0,0 +1,21 @@
+function pairZeros(arr) {
+    let zeroes = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) zeroes.push(i);
+    }
+    for (let i = 0; i < zeroes.length; i++) {
+        if (i % 2 === 1) {
+            arr[zeroes[i]] = "";
+        }
+    }
+
+    return arr.reduce((acc, curr) => {
+        if (curr !== "") acc.push(curr);
+        return acc;
+    }, []);
+}
+
+console.log(pairZeros([1])); // [1]
+console.log(pairZeros([0, 0])); // [0]
+console.log(pairZeros([0, 0, 0])); // [0,0]
+console.log(pairZeros([1, 0, 1, 0, 2, 0, 0, 3, 0])); // [1,0,1,2,0,3,0]
