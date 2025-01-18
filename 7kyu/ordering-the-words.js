444267a9691cec39ef2374c1f3c37017d546f2d7
Irene Volodko <ice7aye@gmail.com>
2025-01-18 13:46:09 +0300

    add description

--- 7kyu/ordering-the-words.js
+++ 7kyu/ordering-the-words.js
@@ -1,7 +1,17 @@
+// ❓ DESCRIPTION:
+// Given a string, you need to write a method that order every letter in this string in ascending order.
+// Also, you should validate that the given string is not empty or null. If so, the method should return:
+// "Invalid String!"
+// Notes
+// • the given string can be lowercase and uppercase.
+// • the string could include spaces or other special characters like '# ! or ,'. Sort them based on their ASCII codes
+//
+// ✅ SOLUTION:
 function orderWord(s) {
     return s ? [...s].sort().join("") : "Invalid String!";
 }
 
+// 📌 TESTCASE:
 console.log(orderWord("Hello, World!")); //  " !,HWdellloor"
 console.log(orderWord("bobby")); //  "bbboy"
 console.log(orderWord("b")); //  "b"
