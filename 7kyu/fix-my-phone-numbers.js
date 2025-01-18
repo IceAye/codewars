471ee299216e30f21f309be88d30489f7c2eab93
Irene Volodko <ice7aye@gmail.com>
2025-01-18 15:44:45 +0300

    add solution 2

--- 7kyu/fix-my-phone-numbers.js
+++ 7kyu/fix-my-phone-numbers.js
@@ -3,6 +3,11 @@ function isItANum(str) {
     return phoneNumber.startsWith("0") && phoneNumber.length === 11 ? phoneNumber : "Not a phone number";
 }
 
+function isItANum(str) {
+    let phoneNumber = str.replace(/\D/gi, "");
+    return /^0\d{10}$/.test(phoneNumber) ? phoneNumber : "Not a phone number";
+}
+
 console.log(isItANum("S:)0207ERGQREG88349F82!efRF)")); // "02078834982"
 console.log(isItANum("sjfniebienvr12312312312ehfWh")); // "Not a phone number"
 console.log(isItANum("0192387415456")); // "Not a phone number"
