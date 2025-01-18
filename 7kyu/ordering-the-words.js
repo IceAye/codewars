7cf37d3ee9a7d3f3fa9d110f17d31c5c3c1ccb82
Irene Volodko <ice7aye@gmail.com>
2025-01-18 13:44:39 +0300

    add solution

---
+++ 7kyu/ordering-the-words.js
@@ -0,0 +1,14 @@
+function orderWord(s) {
+    return s ? [...s].sort().join("") : "Invalid String!";
+}
+
+console.log(orderWord("Hello, World!")); //  " !,HWdellloor"
+console.log(orderWord("bobby")); //  "bbboy"
+console.log(orderWord("b")); //  "b"
+console.log(orderWord("")); //  "Invalid String!"
+console.log(orderWord("completesolution")); //  "ceeillmnooopsttu"
+console.log(orderWord('"][@!$*(^&%')); //  "!\"$%&(*@[]^"
+console.log(orderWord('i"d][@z!$r(^a&world%')); // "!\"$%&(@[]^addilorrwz"
+console.log(orderWord(null)); // "Invalid String!"
+console.log(orderWord("cba")); // "abc"
+console.log(orderWord("abc")); // "abc"
