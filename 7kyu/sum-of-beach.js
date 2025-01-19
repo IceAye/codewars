5ee04fc0617df15cff6c4219cc338eca2066ca16
Irene Volodko <ice7aye@gmail.com>
2025-01-19 19:36:37 +0300

    add description

--- 7kyu/sum-of-beach.js
+++ 7kyu/sum-of-beach.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Beaches are filled with sand, water, fish, and sun. Given a string, calculate how many times the words "Sand", "Water", "Fish", and "Sun" appear without overlapping (regardless of the case).
+//
+// ✅ SOLUTION:
 function sumOfABeach(beach) {
     return beach.split(/sun|water|sand|fish/i).length - 1;
 }
 
+// 📌 TESTCASE:
 console.log(sumOfABeach("SanD"))  // 1
 console.log(sumOfABeach("sunsunsunsun"))  // 4
 console.log(sumOfABeach("123FISH321"))  // 1
