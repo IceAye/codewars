d111d2b821d35da1f309e4672c82e7ebf42da9f0
Irene Volodko <ice7aye@gmail.com>
2025-01-19 19:51:25 +0300

    add description

--- 7kyu/frequence-sequence.js
+++ 7kyu/frequence-sequence.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Your task is to return an output string that translates an input string s by replacing each character in s with a number representing the number of times that character occurs in s and separating each number with the sep character(s).
+//
+// ✅ SOLUTION:
 function freqSeq(str, sep) {
     let map = new Map();
     for (const char of str) {
@@ -6,6 +10,7 @@ function freqSeq(str, sep) {
     return [...str].map((item) => map.get(item)).join(sep);
 }
 
+// 📌 TESTCASE:
 console.log(freqSeq("hello world", "-")); // '1-1-3-3-2-1-1-2-1-3-1'
 console.log(freqSeq("19999999", ":")); // '1:7:7:7:7:7:7:7'
 console.log(freqSeq("^^^**$", "x")); // '3x3x3x2x2x1'
