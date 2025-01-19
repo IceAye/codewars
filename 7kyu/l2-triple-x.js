ede01874b8e057edaaace681996d42ba6f6e3bb2
Irene Volodko <ice7aye@gmail.com>
2025-01-19 20:40:59 +0300

    add description

--- 7kyu/l2-triple-x.js
+++ 7kyu/l2-triple-x.js
@@ -1,12 +1,27 @@
+// ❓ DESCRIPTION:
+// Given a string, return true if the first instance of "x" in the string is immediately followed by the string "xx".
+//
+// "abraxxxas" → true
+// "xoxotrololololololoxxx" → false
+// "softX kitty, warm kitty, xxxxx" → true
+// "softx kitty, warm kitty, xxxxx" → false
+// Note :
+//
+// capital X's do not count as an occurrence of "x".
+// if there are no "x"'s then return false
+//
+// ✅ SOLUTION 1:
 function tripleX(str) {
     let index = str.indexOf("x");
     return str.slice(index, index + 3) === "xxx";
 }
 
+// ✅ SOLUTION 2:
 function tripleX(str) {
     return /^[^x]*x{3}/.test(str);
 }
 
+// 📌 TESTCASE:
 console.log(tripleX("abraxxxas")); // true
 console.log(tripleX("xoxotrololololololoxxx")); // false
 console.log(tripleX("soft kitty, warm kitty, xxxxx")); // true
