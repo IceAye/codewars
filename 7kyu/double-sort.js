138450b041b861c5e47aa1d331171f3da8c9ee6c
Irene Volodko <ice7aye@gmail.com>
2025-01-19 19:24:37 +0300

    add description

--- 7kyu/double-sort.js
+++ 7kyu/double-sort.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Simple enough this one - you will be given an array. The values in the array will either be numbers or strings, or a mix of both. You will not get an empty array, nor a sparse one.
+// Your job is to return a single array that has first the numbers sorted in ascending order, followed by the strings sorted in alphabetic order. The values must maintain their original type.
+// Note that numbers written as strings are strings and must be sorted with the other strings.
+//
+// ✅ SOLUTION:
 function dbSort(a) {
     return [
         ...a.filter((el) => typeof el === "number").sort((a, b) => a - b),
@@ -5,6 +11,7 @@ function dbSort(a) {
     ];
 }
 
+// 📌 TESTCASE:
 console.log(dbSort([6, 2, 3, 4, 5])); // [2, 3, 4, 5, 6]
 console.log(dbSort([14, 32, 3, 5, 5])); // [3, 5, 5, 14, 32]
 console.log(dbSort([1, 2, 3, 4, 5])); // [1, 2, 3, 4, 5]
