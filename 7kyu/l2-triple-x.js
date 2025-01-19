1a97d2a0699437533a514971997636af1cd0edf9
Irene Volodko <ice7aye@gmail.com>
2025-01-19 20:39:45 +0300

    add solution 2

--- 7kyu/l2-triple-x.js
+++ 7kyu/l2-triple-x.js
@@ -3,6 +3,10 @@ function tripleX(str) {
     return str.slice(index, index + 3) === "xxx";
 }
 
+function tripleX(str) {
+    return /^[^x]*x{3}/.test(str);
+}
+
 console.log(tripleX("abraxxxas")); // true
 console.log(tripleX("xoxotrololololololoxxx")); // false
 console.log(tripleX("soft kitty, warm kitty, xxxxx")); // true
