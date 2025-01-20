422991184d2b1cbac9512611d7dcdddc501b73d4
Irene Volodko <ice7aye@gmail.com>
2025-01-20 21:56:21 +0300

    add description

--- 7kyu/likes-vs-dislikes.js
+++ 7kyu/likes-vs-dislikes.js
@@ -1,9 +1,25 @@
+// ❓ DESCRIPTION:
+// YouTube had a like and a dislike button, which allowed users to express their opinions about particular content. It was set up in such a way that you cannot like and dislike a video at the same time. There are two other interesting rules to be noted about the interface: Pressing a button, which is already active, will undo your press. If you press the like button after pressing the dislike button, the like button overwrites the previous "Dislike" state. The same is true for the other way round.
+//
+// Task
+// Create a function that takes in a list of button inputs and returns the final state.
+//
+// Examples
+// likeOrDislike([Dislike]) => Dislike
+// likeOrDislike([Like,Like]) => Nothing
+// likeOrDislike([Dislike,Like]) => Like
+// likeOrDislike([Like,Dislike,Dislike]) => Nothing
+// Notes
+// If no button is currently active, return Nothing.
+// If the list is empty, return Nothing.
 // Like, Dislike, Nothing come from Preloaded
-
+//
+// ✅ SOLUTION:
 function likeOrDislike(buttons) {
     return buttons.reduce((acc, curr) => (acc === curr ? Nothing : curr), Nothing)
 }
 
+// 📌 TESTCASE:
 console.log( likeOrDislike(['Dislike'])) // Dislike
 console.log( likeOrDislike(['Like','Like'])) // Nothing
 console.log( likeOrDislike(['Dislike','Like'])) // Like
