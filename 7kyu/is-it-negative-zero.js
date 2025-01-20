9af5ae262410d59825c5b011c5de29fc3ff0f808
Irene Volodko <ice7aye@gmail.com>
2025-01-20 22:25:21 +0300

    add description

--- 7kyu/is-it-negative-zero.js
+++ 7kyu/is-it-negative-zero.js
@@ -1,11 +1,20 @@
+// ❓ DESCRIPTION:
+// There exist two zeroes: +0 (or just 0) and -0.
+// Write a function that returns true if the input number is -0 and false otherwise (True and False for Python).
+// In JavaScript / TypeScript / Coffeescript the input will be a number.
+// In Python / Java / C / NASM / Haskell / the input will be a float.
+//
+// ✅ SOLUTION 1:
 function isNegativeZero(n) {
     return n === 0 && 1 / n === -Infinity;
 }
 
+// ✅ SOLUTION 2:
 function isNegativeZero(n) {
     return Object.is(n, -0);
 }
 
+// 📌 TESTCASE:
 console.log(isNegativeZero(-0)); // true
 console.log(isNegativeZero(-Infinity)); // false
 console.log(isNegativeZero(-5)); // false
