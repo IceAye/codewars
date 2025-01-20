960ced3eb941a3264019956602c439f9dd6c1a74
Irene Volodko <ice7aye@gmail.com>
2025-01-20 22:03:15 +0300

    add solution

---
+++ 7kyu/training-js-19-methods-of-string-object.js
@@ -0,0 +1,11 @@
+function alienLanguage(str) {
+    return str
+        .split(" ")
+        .map((word) => word.slice(0, -1).toUpperCase() + word.slice(-1).toLowerCase())
+        .join(" ");
+}
+
+console.log(alienLanguage("My name is John")); // "My NAMe Is JOHn"
+console.log(alienLanguage("this is an example")); // "THIs Is An EXAMPLe"
+console.log(alienLanguage("Hello World")); // "HELLo WORLd"
+console.log(alienLanguage("HELLO WORLD")); // "HELLo WORLd"
