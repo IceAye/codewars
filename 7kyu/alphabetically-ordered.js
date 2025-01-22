5588863595ddef0714cd8bf5655dce59eb8fefa9
Irene Volodko <ice7aye@gmail.com>
2025-01-22 11:17:24 +0300

    add description

--- 7kyu/alphabetically-ordered.js
+++ 7kyu/alphabetically-ordered.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Your task is very simple. Just write a function that takes an input string of lowercase letters and returns true/false depending on whether the string is in alphabetical order or not.
+//
+// Examples (input -> output)
+// "kata" -> false ('a' comes after 'k')
+// "ant" -> true (all characters are in alphabetical order)
+//
+// ✅ SOLUTION 1:
 function alphabetic(s) {
     for (let i = 0; i < s.length - 1; i++) {
         if (s.charCodeAt(i) > s.charCodeAt(i + 1)) return false;
@@ -5,10 +13,12 @@ function alphabetic(s) {
     return true;
 }
 
+// ✅ SOLUTION 2:
 function alphabetic(s) {
     return s === s.split("").sort().join("");
 }
 
+// 📌 TESTCASE:
 console.log(alphabetic("asd")) // false
 console.log(alphabetic("codewars")) // false
 console.log(alphabetic("door")) // true
