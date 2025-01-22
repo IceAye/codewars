241b87de7937213b62d910c29109b807f365e216
Irene Volodko <ice7aye@gmail.com>
2025-01-22 11:31:57 +0300

    add description

--- 7kyu/find-the-squares.js
+++ 7kyu/find-the-squares.js
@@ -1,9 +1,14 @@
+// ❓ DESCRIPTION:
+// Complete the function that takes an odd integer (0 < n < 1000000) which is the difference between two consecutive perfect squares, and return these squares as a string in the format "bigger-smaller"
+//
+// ✅ SOLUTION:
 const findSquares = (num) => {
     let min = ((num - 1) / 2) ** 2;
     let max = ((num - 1) / 2 + 1) ** 2;
     return `${max}-${min}`;
 };
 
+// 📌 TESTCASE:
 console.log(findSquares(9)); // '25-16'
 console.log(findSquares(5)); // '9-4'
 console.log(findSquares(81)); // '1681-1600'
