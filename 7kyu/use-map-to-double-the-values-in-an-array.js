4ff6da4bf44ed3769df08376cb35b8a1b9d3ebfc
Irene Volodko <ice7aye@gmail.com>
2025-01-22 11:06:39 +0300

    add solution

---
+++ 7kyu/use-map-to-double-the-values-in-an-array.js
@@ -0,0 +1,5 @@
+function double(array) {
+    return array.map((num) => num * 2);
+}
+console.log(double([1,2,3,4,5])) // [2,4,6,8,10]
+console.log(double([71,-548,12.3,31415])) // [142,-1096,24.6,62830]
