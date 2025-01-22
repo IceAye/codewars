36987c73dd0426da8b143d733aacc558642d9a3d
Irene Volodko <ice7aye@gmail.com>
2025-01-22 09:58:21 +0300

    add description

--- 4kyu/sum-strings-as-numbers.js
+++ 4kyu/sum-strings-as-numbers.js
@@ -1,6 +1,15 @@
+// ❓ DESCRIPTION:
+// Given the string representations of two integers, return the string representation of the sum of those integers.
+//
+// For example:
+// sumStrings('1','2') // => '3'
+// A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+//
+// ✅ SOLUTION:
 function sumStrings(a,b) {
     return (BigInt(a) + BigInt(b)).toString()
 }
 
+// 📌 TESTCASE:
 console.log(sumStrings('123','456')) //'579'
 console.log(sumStrings('712569312664357328695151392', '8100824045303269669937')) //'712577413488402631964821329'
