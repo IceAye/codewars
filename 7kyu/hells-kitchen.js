e48e823a7bf1be009c11865c4879d1775bd1c416
Irene Volodko <ice7aye@gmail.com>
2025-01-23 21:46:28 +0300

    add description

--- 7kyu/hells-kitchen.js
+++ 7kyu/hells-kitchen.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Gordon Ramsay shouts. He shouts and swears. There may be something wrong with him.
+// Anyway, you will be given a string of four words. Your job is to turn them in to Gordon language.
+// Rules:
+// Obviously the words should be Caps, Every word should end with '!!!!', Any letter 'a' or 'A' should become '@', Any other vowel should become '*'.
+//
+// ✅ SOLUTION:
 function gordon(a) {
     return a
         .split(" ")
@@ -9,6 +16,7 @@ function gordon(a) {
         .join(" ");
 }
 
+// 📌 TESTCASE:
 console.log(gordon("What feck damn cake")); // 'WH@T!!!! F*CK!!!! D@MN!!!! C@K*!!!!'
 console.log(gordon("are you stu pid")); // '@R*!!!! Y**!!!! ST*!!!! P*D!!!!'
 console.log(gordon("i am a chef")); // '*!!!! @M!!!! @!!!! CH*F!!!!'
