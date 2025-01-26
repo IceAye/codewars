b07663273850c029ca110c9af24536131d0a5a66
Irene Volodko <ice7aye@gmail.com>
2025-01-26 23:43:47 +0300

    add description

--- 7kyu/four-seven.js
+++ 7kyu/four-seven.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Simple kata, simple rules: your function should accept the inputs 4 and 7. If 4 is entered, the function should return 7. If 7 is entered, the function should return 4. Anything else entered as input should return 0. There's only one catch, your function cannot include if statements, switch statements, or the ternary operator (or the eval function due to the fact that you can get around the if requirement using it).
+// There are some very simple ways of answering this problem, but I encourage you to try and be as creative as possible.
+// Good Luck!
+//
+// ✅ SOLUTION:
 function fourSeven(n) {
     const obj = {
         7: 4,
@@ -6,6 +12,7 @@ function fourSeven(n) {
     return obj[n] || 0;
 }
 
+// 📌 TESTCASE:
 console.log(fourSeven(7)); // 4
 console.log(fourSeven(4)); // 7
 console.log(fourSeven(1)); // 0
