099a114643e22b842ac334c09fbedfd03b8d70cc
Irene Volodko <ice7aye@gmail.com>
2025-01-27 23:32:03 +0300

    add description

--- 7kyu/power-of-4.js
+++ 7kyu/power-of-4.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Write a method that returns true if a given parameter is a power of 4, and false if it's not. If parameter is not an Integer (eg String, Array) method should return false as well.
+//
+// ✅ SOLUTION:
 function powerOf4(n) {
     if (typeof n === "number") {
         return n >= 1 ? Number.isInteger(Math.log(n) / Math.log(4)) : false;
@@ -5,6 +9,7 @@ function powerOf4(n) {
     return false;
 }
 
+// 📌 TESTCASE:
 console.log(powerOf4(4)); // true
 console.log(powerOf4(16)); // true
 console.log(powerOf4(1)); // true
