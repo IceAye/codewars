51bd3651991fc1909aec79359b86332eb4bd14be
Irene Volodko <ice7aye@gmail.com>
2025-01-27 23:30:45 +0300

    add solution

---
+++ 7kyu/power-of-4.js
@@ -0,0 +1,20 @@
+function powerOf4(n) {
+    if (typeof n === "number") {
+        return n >= 1 ? Number.isInteger(Math.log(n) / Math.log(4)) : false;
+    }
+    return false;
+}
+
+console.log(powerOf4(4)); // true
+console.log(powerOf4(16)); // true
+console.log(powerOf4(1)); // true
+console.log(powerOf4(8)); // false
+console.log(powerOf4(20)); // false
+console.log(powerOf4(32)); // false
+console.log(powerOf4(44)); // false
+console.log(powerOf4(3.1415)); // false
+console.log(powerOf4("1")); // false
+console.log(powerOf4("4")); // false
+console.log(powerOf4(null)); // false
+console.log(powerOf4(undefined)); // false
+console.log(powerOf4(function () {})); // false
