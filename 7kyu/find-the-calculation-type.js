1a894d2b76326cdb0d6362ec09ecc5655a0fb91e
Irene Volodko <ice7aye@gmail.com>
2025-01-27 22:18:02 +0300

    add description

--- 7kyu/find-the-calculation-type.js
+++ 7kyu/find-the-calculation-type.js
@@ -1,3 +1,17 @@
+// ❓ DESCRIPTION:
+// You have to create a function calcType, which receives 3 arguments: 2 numbers, and the result of an unknown operation performed on them (also a number).
+// Based on those 3 values you have to return a string, that describes which operation was used to get the given result.
+// The possible return strings are: "addition", "subtraction", "multiplication", "division".
+//
+// Example:
+// calcType(1, 2, 3) -->   1 ? 2 = 3   --> "addition"
+// Notes
+// In case of division you should expect that the result of the operation is obtained by using / operator on the input values - no manual data type conversion or rounding should be performed.
+// Cases with just one possible answers are generated.
+// Only valid arguments will be passed to the function.
+// Only valid arguments will be passed to the function!
+//
+// ✅ SOLUTION:
 function calcType(a, b, res) {
     return a - b === res
         ? "subtraction"
@@ -8,6 +22,7 @@ function calcType(a, b, res) {
                 : "division";
 }
 
+// 📌 TESTCASE:
 console.log(calcType(1, 2, 3)); // 'addition'
 console.log(calcType(10, 4, 40)); // 'multiplication'
 console.log(calcType(10, 5, 5)); // 'subtraction'
