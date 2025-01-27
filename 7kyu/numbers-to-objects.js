85a79c6ef0b426ac6a72bd9ebf381bc53b6b8069
Irene Volodko <ice7aye@gmail.com>
2025-01-27 23:10:47 +0300

    add solution

---
+++ 7kyu/numbers-to-objects.js
@@ -0,0 +1,6 @@
+function numObj(s) {
+    return s.map((key) => ({ [key]: String.fromCharCode(key) }));
+}
+
+console.log(numObj([118,117,120])) // [{'118':'v'}, {'117':'u'}, {'120':'x'}]
+console.log(numObj([101,121,110,113,113,103])) // [{'101':'e'}, {'121':'y'}, {'110':'n'}, {'113':'q'}, {'113':'q'}, {'103':'g'}]
