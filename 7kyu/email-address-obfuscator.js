c6682ff5e7e4eebb78dfb9a6e93ed58f5ab4b6ac
Irene Volodko <ice7aye@gmail.com>
2025-01-28 23:53:40 +0300

    add description

--- 7kyu/email-address-obfuscator.js
+++ 7kyu/email-address-obfuscator.js
@@ -1,6 +1,11 @@
+// ❓ DESCRIPTION:
+// Many people choose to obfuscate their email address when displaying it on the Web. One common way of doing this is by substituting the @ and . characters for their literal equivalents in brackets.
+//
+// ✅ SOLUTION:
 obfuscate = function(email) {
     return email.replace(/@/, " [at] ").replace(/\./g, " [dot] ")
 }
 
+// 📌 TESTCASE:
 console.log(obfuscate('test@123.com')) // 'test [at] 123 [dot] com'
 console.log(obfuscate('Code_warrior@foo.ac.uk')) // 'Code_warrior [at] foo [dot] ac [dot] uk'
