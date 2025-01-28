14c8669579c86897165e7b35cd405f267513251a
Irene Volodko <ice7aye@gmail.com>
2025-01-28 20:45:32 +0300

    add description

--- 7kyu/c-wars.js
+++ 7kyu/c-wars.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// A person's full name is usually composed of a first name, middle name and last name; however in some cultures (for example in South India) there may be more than one middle name.
+// Write a function that takes the full name of a person, and returns the initials, separated by dots ('.'). The initials must be uppercase. The last name of the person must appear in full, with its first letter in uppercase as well.
+// See the pattern below:
+// "code wars"            ---> "C.Wars"
+// "Barack hussein obama" ---> "B.H.Obama"
+// Names in the full name are separated by exactly one space (' ' ) ; without leading or trailing spaces. Names will always be lowercase, except optionally their first letter.
+//
+// ✅ SOLUTION:
 function initials(n) {
     const result = [];
     let array = n.split(" ");
@@ -10,6 +19,7 @@ function initials(n) {
     return result.join(".");
 }
 
+// 📌 TESTCASE:
 console.log(initials("code wars")); // 'C.Wars'
 console.log(initials("Barack hussain obama")); // 'B.H.Obama'
 console.log(initials("barack hussain Obama")); // 'B.H.Obama'
