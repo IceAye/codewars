755eb7b296d0ffdd25107b275b39c7912714ee50
Irene Volodko <ice7aye@gmail.com>
2025-01-28 23:52:56 +0300

    add solution

---
+++ 7kyu/email-address-obfuscator.js
@@ -0,0 +1,6 @@
+obfuscate = function(email) {
+    return email.replace(/@/, " [at] ").replace(/\./g, " [dot] ")
+}
+
+console.log(obfuscate('test@123.com')) // 'test [at] 123 [dot] com'
+console.log(obfuscate('Code_warrior@foo.ac.uk')) // 'Code_warrior [at] foo [dot] ac [dot] uk'
