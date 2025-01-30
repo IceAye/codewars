685a18b92b67e5b682965571a0e56c33f33fcca9
Irene Volodko <ice7aye@gmail.com>
2025-01-30 22:58:51 +0300

    add solution

---
+++ 7kyu/calculate-mean-and-concatenate-string.js
@@ -0,0 +1,20 @@
+function mean(lst) {
+    let sum = 0;
+    let string = "";
+    for (const item of lst) {
+        if (/\d/.test(item)) {
+            sum += +item;
+        } else {
+            string += item;
+        }
+    }
+    return [sum / 10, string];
+}
+
+console.log(mean(["u", "6", "d", "1", "i", "w", "6", "s", "t", "4", "a", "6", "g", "1", "2", "w", "8", "o", "2", "0"])) // [3.6, "udiwstagwo"]
+console.log(mean(["0", "c", "7", "x", "6", "2", "3", "5", "w", "7", "0", "y", "v", "u", "h", "i", "n", "u", "0", "0"])) // [3.0, "cxwyvuhinu"]
+console.log(mean(["0", "u", "a", "y", "0", "a", "9", "q", "3", "v", "g", "7", "6", "4", "y", "d", "8", "6", "0", "d"])) // [4.3, "uayaqvgydd"]
+console.log(mean(["s", "n", "9", "l", "0", "m", "i", "z", "9", "7", "y", "4", "z", "3", "3", "k", "4", "1", "0", "k"])) // [4.0, "snlmizyzkk"]
+console.log(mean(["5", "v", "u", "k", "8", "4", "9", "b", "9", "g", "5", "z", "3", "f", "6", "u", "i", "6", "6", "t"])) // [6.1, "vukbgzfuit"]
+console.log(mean(["1", "1", "1", "1", "1", "1", "1", "1", "1", "0", "a", "a", "d", "d", "g", "q", "u", "v", "y", "y"])) // [0.9, "aaddgquvyy"]
+console.log(mean(["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "a", "a", "d", "d", "g", "q", "u", "v", "y", "y"])) // [1.0, "aaddgquvyy"]
