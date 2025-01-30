3adc021095310db3dd7a65673a8065f486d981df
Irene Volodko <ice7aye@gmail.com>
2025-01-30 23:13:19 +0300

    add description

--- 7kyu/the-reject-function.js
+++ 7kyu/the-reject-function.js
@@ -1,5 +1,10 @@
+// ❓ DESCRIPTION:
+// Implement a function which filters out array values which satisfy the given predicate.
+//
+// ✅ SOLUTION:
 function reject(array, predicate) {
     return array.filter((item) => !predicate(item));
 }
 
+// 📌 TESTCASE:
 console.log(reject([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; })) // [1, 3, 5]
