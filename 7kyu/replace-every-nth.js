67c5c489940edef1208a26b317b31b366fa9a9da
Irene Volodko <ice7aye@gmail.com>
2025-01-30 22:08:14 +0300

    add solution

---
+++ 7kyu/replace-every-nth.js
@@ -0,0 +1,26 @@
+function replaceNth(text, n, oldValue, newValue) {
+    let map = new Map();
+    let j = 1;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === oldValue) {
+            map.set(i, j);
+            j++;
+        }
+    }
+    if (n <= 0 || n > map.size) return text;
+    let array = text.split("");
+    for (let [key, value] of map.entries()) {
+        if (value % n === 0) {
+            array[+key] = newValue;
+        }
+    }
+    return array.join("");
+}
+
+console.log(replaceNth("Vader said: No, I am your father!", 2, "a", "o")); // "Vader soid: No, I am your fother!"
+console.log(replaceNth("Vader said: No, I am your father!", 4, "a", "o")); // "Vader said: No, I am your fother!"
+console.log(replaceNth("Vader said: No, I am your father!", 6, "a", "o")); // "Vader said: No, I am your father!"
+console.log(replaceNth("Vader said: No, I am your father!", 0, "a", "o")); // "Vader said: No, I am your father!"
+console.log(replaceNth("Vader said: No, I am your father!", -2, "a", "o")); // "Vader said: No, I am your father!"
+console.log(replaceNth("Vader said: No, I am your father!", 1, "i", "y")); // "Vader sayd: No, I am your father!"
+console.log(replaceNth("Luke cries: Noooooooooooooooo!", 6, "o", "i")); // "Luke cries: Noooooioooooioooo!"
