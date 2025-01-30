cb76fbc12613189f152c59161797955a2e04d531
Irene Volodko <ice7aye@gmail.com>
2025-01-30 22:10:57 +0300

    add description

--- 7kyu/replace-every-nth.js
+++ 7kyu/replace-every-nth.js
@@ -1,3 +1,19 @@
+// ❓ DESCRIPTION:
+// Task
+// Write a method, that replaces every nth char oldValue with char newValue.
+//
+// Inputs
+// text: the string to modify
+// n: change the target letter every nth occurrencies
+// old_value (or similar): the targetted character
+// new_value (or similar): the character to use as replacement
+// Note for untyped languages: all inputs are always valid and of their expected type.
+//
+// Rules
+// Your method has to be case sensitive!
+// If n is 0 or negative or if it is larger than the count of the oldValue, return the original text without a change.
+//
+// ✅ SOLUTION:
 function replaceNth(text, n, oldValue, newValue) {
     let map = new Map();
     let j = 1;
@@ -17,6 +33,7 @@ function replaceNth(text, n, oldValue, newValue) {
     return array.join("");
 }
 
+// 📌 TESTCASE:
 console.log(replaceNth("Vader said: No, I am your father!", 2, "a", "o")); // "Vader soid: No, I am your fother!"
 console.log(replaceNth("Vader said: No, I am your father!", 4, "a", "o")); // "Vader said: No, I am your fother!"
 console.log(replaceNth("Vader said: No, I am your father!", 6, "a", "o")); // "Vader said: No, I am your father!"
