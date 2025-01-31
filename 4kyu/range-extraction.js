758f1a224528a6a6f55931de02a03c5ba132858b
Irene Volodko <ice7aye@gmail.com>
2025-01-31 19:38:29 +0300

    add description

--- 4kyu/range-extraction.js
+++ 4kyu/range-extraction.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// A format for expressing an ordered list of integers is to use a comma separated list of either individual integers
+// or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers. For example "12,13,15-17"
+// Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+//
+// ✅ SOLUTION:
+
 function solution(list) {
     let result = [];
     let inner = [];
@@ -15,6 +22,6 @@ function solution(list) {
     return result.join().replace(/,-,/g, "-");
 }
 
-
+// 📌 TESTCASE:
 console.log(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
 // "-6,-3-1,3-5,7-11,14,15,17-20")
