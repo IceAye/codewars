1d7ba71f6221f0a3b53481f8a297f06be449db19
Irene Volodko <ice7aye@gmail.com>
2025-02-02 22:04:56 +0300

    add description

--- 7kyu/homogenous-arrays.js
+++ 7kyu/homogenous-arrays.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Given a two-dimensional array, return a new array which carries over only those arrays from the original, which were not empty and whose items are all of the same type (i.e. homogenous). For simplicity, the arrays inside the array will only contain characters and integers.
+//
+// Example:
+// Given [[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]], your function should return [[1, 5, 4], ['b']].
+//
+// Addendum:
+// Please keep in mind that for this kata, we assume that empty arrays are not homogenous.
+// The resultant arrays should be in the order they were originally in and should not have its values changed.
+// No implicit type casting is allowed. A subarray [1, '2'] would be considered illegal and should be filtered out.
+//
+// ✅ SOLUTION 1:
 function filterHomogenous(arrays) {
     let result = [];
     for (const array of arrays) {
@@ -9,9 +21,11 @@ function filterHomogenous(arrays) {
     return result;
 }
 
+// ✅ SOLUTION 2:
 function filterHomogenous(arrays) {
     return arrays.filter(array => array.length && array.every(item => typeof item === typeof array[0]));
 }
 
+// 📌 TESTCASE:
 console.log(filterHomogenous([[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]])) // [[1, 5, 4], ['b']]
 console.log(filterHomogenous([[123, 234, 432], ['', 'abc'], [''], ['', 1], ['', '1'], []])) // [[123, 234, 432], ['', 'abc'], [''], ['', '1']]
