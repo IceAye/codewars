425bc5c0036f04aac1dcf41a08cd9c2a5209c5f6
Irene Volodko <ice7aye@gmail.com>
2025-02-02 22:08:53 +0300

    add solution

---
+++ 7kyu/unscrambled-eggs.js
@@ -0,0 +1,6 @@
+function unscrambleEggs(word) {
+    return word.replace(/egg/g, "");
+}
+
+console.log(unscrambleEggs("ceggodegge heggeregge")); //  "code here"
+console.log(unscrambleEggs("FeggUNegg KeggATeggA")); // "FUN KATA"
