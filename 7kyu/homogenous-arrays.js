23188216ad3a6345b8de367eb1e70d25848ea4c0
Irene Volodko <ice7aye@gmail.com>
2025-02-02 22:03:10 +0300

    add solution 2

--- 7kyu/homogenous-arrays.js
+++ 7kyu/homogenous-arrays.js
@@ -9,5 +9,9 @@ function filterHomogenous(arrays) {
     return result;
 }
 
+function filterHomogenous(arrays) {
+    return arrays.filter(array => array.length && array.every(item => typeof item === typeof array[0]));
+}
+
 console.log(filterHomogenous([[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]])) // [[1, 5, 4], ['b']]
 console.log(filterHomogenous([[123, 234, 432], ['', 'abc'], [''], ['', 1], ['', '1'], []])) // [[123, 234, 432], ['', 'abc'], [''], ['', '1']]
