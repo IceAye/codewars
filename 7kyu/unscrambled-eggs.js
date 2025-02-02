90ab49d88815d82f7713d87d024095dae390ae5c
Irene Volodko <ice7aye@gmail.com>
2025-02-02 22:38:17 +0300

    add solution 2

--- 7kyu/unscrambled-eggs.js
+++ 7kyu/unscrambled-eggs.js
@@ -2,5 +2,9 @@ function unscrambleEggs(word) {
     return word.replace(/egg/g, "");
 }
 
+function unscrambleEggs(word) {
+    return word.split('egg').join('');
+}
+
 console.log(unscrambleEggs("ceggodegge heggeregge")); //  "code here"
 console.log(unscrambleEggs("FeggUNegg KeggATeggA")); // "FUN KATA"
