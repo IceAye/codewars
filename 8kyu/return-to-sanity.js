7254a16e7c7f3a18d0354ee599f0a9b36f9aadb3
Irene Volodko <ice7aye@gmail.com>
2025-02-03 22:57:24 +0300

    add description

--- 8kyu/return-to-sanity.js
+++ 8kyu/return-to-sanity.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// This function should return an object, but it's not doing what's intended. What's wrong?
+//
+// ✅ SOLUTION:
 function mystery() {
     return {
         sanity: 'Hello'
