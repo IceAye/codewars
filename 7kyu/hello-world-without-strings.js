2c82b5837b690186b1c27e19937a11d90319d438
Irene Volodko <ice7aye@gmail.com>
2025-02-04 17:57:40 +0300

    add solution 2

--- 7kyu/hello-world-without-strings.js
+++ 7kyu/hello-world-without-strings.js
@@ -4,4 +4,6 @@ const helloWorld = () => {
     return result;
 };
 
+const helloWorld = () => String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33);
+
 console.log(helloWorld()); // "Hello, World!"
