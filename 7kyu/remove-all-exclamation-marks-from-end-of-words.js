a483cb686fc838c3eb2f2551d213dc58558c64c5
Irene Volodko <ice7aye@gmail.com>
2025-02-04 17:32:20 +0300

    add description

--- 7kyu/remove-all-exclamation-marks-from-end-of-words.js
+++ 7kyu/remove-all-exclamation-marks-from-end-of-words.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Remove all exclamation marks from the end of words. Words are separated by a single space. There are no exclamation marks within a word.
+//
+// ✅ SOLUTION:
 function remove (string) {
     return string.split(' ').map(str => str.replace(/!+$/g, '')).join(' ');
 }
 
+// 📌 TESTCASE:
 console.log(remove("Hi!")) // "Hi"
 console.log(remove("Hi!!!")) // "Hi"
 console.log(remove("!Hi")) // "!Hi"
