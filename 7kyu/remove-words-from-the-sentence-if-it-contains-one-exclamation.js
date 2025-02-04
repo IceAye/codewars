2d9f1fb143609257751c9bbc70a9b63f29d58ddb
Irene Volodko <ice7aye@gmail.com>
2025-02-04 18:31:58 +0300

    add description

--- 7kyu/remove-words-from-the-sentence-if-it-contains-one-exclamation.js
+++ 7kyu/remove-words-from-the-sentence-if-it-contains-one-exclamation.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single space, without leading/trailing spaces.
+//
+// ✅ SOLUTION:
 function remove(string) {
     return string
         .split(" ")
