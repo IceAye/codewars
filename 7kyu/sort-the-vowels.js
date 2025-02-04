30f945cb46707e59fe5701e424d867d33f221f55
Irene Volodko <ice7aye@gmail.com>
2025-02-04 18:56:39 +0300

    add description

--- 7kyu/sort-the-vowels.js
+++ 7kyu/sort-the-vowels.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Write a function which takes a input string s and return a string in the following way:
+// List all the Vowels on the right side of |
+// List every character except Vowels on the left side of |
+// for the purpose of this kata, the vowels are : a e i o u
+// Return every character in its original case
+// Each line is seperated with \n
+// Invalid input ( undefined / null / integer ) should return an empty string
+//
+// ✅ SOLUTION 1:
 function sortVowels(s) {
     return typeof s === "string"
         ? [...s]
@@ -12,10 +22,12 @@ function sortVowels(s) {
         : "";
 }
 
+// ✅ SOLUTION 2:
 function sortVowels(s){
     return s === s + [] ? [...s].map(el => el.match(/[aeiou]/i) ? `|${el}` : `${el}|`).join('\n') : ''
 }
 
+// 📌 TESTCASE:
 console.log(sortVowels("Codewars")); // 'C|\n|o\nd|\n|e\nw|\n|a\nr|\ns|'
 console.log(sortVowels("Rnd Te5T")); // 'R|\nn|\nd|\n |\nT|\n|e\n5|\nT|'
 console.log(sortVowels(1337)); // ''
