0d0a2a289b8f649c599a210473175fa52b1e388a
Irene Volodko <ice7aye@gmail.com>
2025-02-05 19:44:55 +0300

    add solution

---
+++ 7kyu/sum-of-array-averages.js
@@ -0,0 +1,6 @@
+const sumAverage = (arr) => {
+    return Math.round(arr.reduce((acc, curr) => curr.reduce((acc, curr) => acc + curr, 0) / curr.length + acc, 0));
+}
+
+console.log(sumAverage([[3, 4, 1, 3, 5, 1, 4], [21, 54, 33, 21, 77]])) // 44
+console.log(sumAverage([[-4, 3, -8, -2], [2, 9, 1, -5], [-7, -2, -6, -4]])) // -6
