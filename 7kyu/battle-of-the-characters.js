a03b279278d4bce4ff9bc06a060737081b6ff9b8
Irene Volodko <ice7aye@gmail.com>
2025-02-05 20:33:52 +0300

    add description

--- 7kyu/battle-of-the-characters.js
+++ 7kyu/battle-of-the-characters.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Groups of characters decided to make a battle. Help them to figure out which group is more powerful. Create a function that will accept 2 strings and return the one who's stronger.
+// Each character have its own power: A = 1, B = 2, ... Y = 25, Z = 26
+// Strings will consist of uppercase letters only
+// Only two groups to a fight.
+// Group whose total power (A + B + C + ...) is bigger wins.
+// If the powers are equal, it's a tie.
+//
+// ✅ SOLUTION:
+
 function sumOfChars(string) {
     return [...string].reduce((acc, curr) => acc + (curr.charCodeAt(0) - 64), 0);
 }
@@ -10,6 +20,7 @@ function battle(x, y) {
             : "Tie!";
 }
 
+// 📌 TESTCASE:
 console.log(battle("AAA", "Z")); // "Z"
 console.log(battle("ONE", "TWO")); // "TWO"
 console.log(battle("ONE", "NEO")); // "Tie!"
