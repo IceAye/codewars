b5144303a44329583afd9b0fa975b98c0c449c46
Irene Volodko <ice7aye@gmail.com>
2025-02-05 20:13:23 +0300

    add description

--- 7kyu/possibilities-array.js
+++ 7kyu/possibilities-array.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// A non-empty array a of length n is called an array of all possibilities if it contains all numbers between 0 and a.length - 1 (both inclusive).
+// Write a function that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+//
+// ✅ SOLUTION 1:
 function isAllPossibilities(x) {
     let set = new Set(x);
     for (let i = 0; i <= x.length - 1; i++) {
@@ -7,9 +12,11 @@ function isAllPossibilities(x) {
     return set.size <= 0;
 }
 
+// ✅ SOLUTION 2:
 function isAllPossibilities(x){
     return x.length > 0 ? x.every((a,i) => x.includes(i)) : false;
 }
 
+// 📌 TESTCASE:
 console.log(isAllPossibilities([0, 1, 2, 3])); // true
 console.log(isAllPossibilities([1, 2, 3, 4])); // false
