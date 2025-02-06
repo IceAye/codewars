32ad936a771674d928a679d4484b133c8b5bf852
Irene Volodko <ice7aye@gmail.com>
2025-02-06 16:17:32 +0300

    add description

--- 7kyu/reverser.js
+++ 7kyu/reverser.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Impliment the reverse function, which takes in input n and reverses it. For instance, reverse(123) should return 321. You should do this without converting the inputted number into a string.
+//
+//✅ SOLUTION:
 function reverse(n) {
     let result = 0;
     for (let i = n; i >= 1; i = Math.floor(i / 10)) {
@@ -6,6 +10,7 @@ function reverse(n) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(reverse(1234)); // 4321
 console.log(reverse(10987)); // 78901
 console.log(reverse(1020)); // 201
