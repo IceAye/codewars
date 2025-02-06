ddc7f9e444e1e4444d208de28e6951650c95ac5b
Irene Volodko <ice7aye@gmail.com>
2025-02-06 20:13:33 +0300

    add description

--- 7kyu/converting-integer-to-currency-format.js
+++ 7kyu/converting-integer-to-currency-format.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Write a function that takes an integer in input and outputs a string with currency format.
+// Integer in currency format is expressed by a string of number where every three characters are separated by comma.
+//
+// For example:
+// 123456 --> "123,456"
+// Input will always be an positive integer, so don't worry about type checking or negative/float values.
+//
+// ✅ SOLUTION 1:
 function toCurrency(price) {
     let result = [];
     let count = 0;
@@ -14,10 +23,12 @@ function toCurrency(price) {
     return result.reverse().join("");
 }
 
+// ✅ SOLUTION 2:
 function toCurrency(price) {
     return price.toLocaleString();
 }
 
+// 📌 TESTCASE:
 console.log(toCurrency(123456)); // "123,456"
 console.log(toCurrency(1234)); // "1,234"
 console.log(toCurrency(123)); // "123"
