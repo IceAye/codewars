6542857e37c7edef7cc01f1a6c9c8e9f7ba97f5e
Irene Volodko <ice7aye@gmail.com>
2025-02-07 18:40:14 +0300

    add description

--- 6kyu/encrypt-this.js
+++ 6kyu/encrypt-this.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Encrypt this!
+// You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+// Your message is a string containing space separated words.
+// You need to encrypt each word in the message using the following rules:
+// The first letter must be converted to its ASCII code.
+// The second letter must be switched with the last letter
+// Keepin' it simple: There are no special characters in the input.
+//
+// ✅ SOLUTION:
 const encryptThis = function (text) {
     let result = [];
     let array = text.split(" ");
@@ -19,6 +29,7 @@ const encryptThis = function (text) {
     return result.join(" ");
 };
 
+// 📌 TESTCASE:
 console.log(encryptThis("A")); // "65"
 console.log(encryptThis("A wise old owl lived in an oak")); // "65 119esi 111dl 111lw 108dvei 105n 97n 111ka"
 console.log(encryptThis("The more he saw the less he spoke")); // "84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp"
