71d28cb408a1d747dc0e30970e44321aaba4fa7a
Irene Volodko <ice7aye@gmail.com>
2025-02-07 19:46:01 +0300

    add description

--- 7kyu/keypad-horror.js
+++ 7kyu/keypad-horror.js
@@ -1,3 +1,26 @@
+// ❓ DESCRIPTION:
+// Having two standards for a keypad layout is inconvenient!
+// Computer keypad's layout:
+// 7 8 9  \n
+// 4 5 6  \n
+// 1 2 3  \n
+//   0 \n
+//
+// Cell phone keypad's layout:
+// 1 2 3\n
+// 4 5 6\n
+// 7 8 9\n
+//   0\n
+//
+// Solve the horror of unstandardized keypads by providing a function that converts computer input to a number as if it was typed on a phone.
+//
+// Example:
+// "789" -> "123"
+//
+// Notes:
+// You get a string with numbers only
+//
+// ✅ SOLUTION:
 function computerToPhone(numbers) {
     const obj = {
         1: 7,
@@ -10,6 +33,7 @@ function computerToPhone(numbers) {
     return numbers.replace(/./g, (c) => obj[c] || c);
 }
 
+// 📌 TESTCASE:
 console.log(computerToPhone("0789456123")); // "0123456789"
 console.log(computerToPhone("000")); // "000"
 console.log(computerToPhone("94561")); // "34567"
