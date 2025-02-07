459dd779a76e39ac1afe7c6a201fdc2d6a6a31fb
Irene Volodko <ice7aye@gmail.com>
2025-02-07 19:11:52 +0300

    add description

--- 6kyu/valid-phone-number.js
+++ 6kyu/valid-phone-number.js
@@ -1,7 +1,15 @@
+// ❓ DESCRIPTION:
+// Write a function that accepts a string, and returns true if it is in the form of a phone number.
+// Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+// Only worry about the following format:
+// (123) 456-7890 (don't forget the space after the close parentheses)
+//
+// ✅ SOLUTION:
 function validPhoneNumber(phoneNumber) {
     return /^\(\d{3}\)\s\d{3}-\d{4}$/g.test(phoneNumber);
 }
 
+// 📌 TESTCASE:
 console.log(validPhoneNumber("(123) 456-7890")); // true
 console.log(validPhoneNumber("(1111)555 2345")); // false
 console.log(validPhoneNumber("(098) 123 4567")); // false
