94bb92ed4e1ce3f0990cf96ac0a2dad4f033687b
Irene Volodko <ice7aye@gmail.com>
2025-02-08 07:28:21 +0300

    add description

--- 7kyu/even-odd-disparity.js
+++ 7kyu/even-odd-disparity.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given an array, return the difference between the count of even numbers and the count of odd numbers. 0 will be considered an even number.
+// The input will be an array of lowercase letters and numbers only.
+//
+// ✅ SOLUTION:
 function solve(a) {
     let even = 0;
     let odd = 0;
@@ -7,8 +12,9 @@ function solve(a) {
         if (el % 2 === 1) odd++
     }
     return even - odd;
-};
+}
 
+// 📌 TESTCASE:
 console.log(solve([0,1,2,3])) // 0
 console.log(solve([0,1,2,3,'a','b'])) // 0
 console.log(solve([0, 15,'z',16,'m', 13, 14,'c', 9, 10, 13,'u', 4, 3])) // 0
