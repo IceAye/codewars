f8aa4c1b1ff4ab8448574460044b7105de584243
Irene Volodko <ice7aye@gmail.com>
2025-02-09 21:40:13 +0300

    add description

--- 7kyu/next-palindromic-number.js
+++ 7kyu/next-palindromic-number.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// There were and still are many problem in CW about palindrome numbers and palindrome strings. We suposse that you know which kind of numbers they are. If not, you may search about them using your favourite search engine.
+// In this kata you will be given a positive integer, val and you have to create the function next_pal()(nextPal Javascript) that will output the smallest palindrome number higher than val.
+// You will be receiving values higher than 10, all valid.
+// Enjoy it!!
+//
+// ✅ SOLUTION:
 function nextPal(val) {
     let i = val + 1;
     while (i !== +Array.from(i.toString()).reverse().join("")) {
@@ -6,6 +13,7 @@ function nextPal(val) {
     return i;
 }
 
+// 📌 TESTCASE:
 console.log(nextPal(11)); // 22
 console.log(nextPal(188)); // 191
 console.log(nextPal(191)); // 202
