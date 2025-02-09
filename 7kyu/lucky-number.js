4c7ed2b2b7893ab36abf949654147d6486037044
Irene Volodko <ice7aye@gmail.com>
2025-02-09 21:27:29 +0300

    add solution

---
+++ 7kyu/lucky-number.js
@@ -0,0 +1,9 @@
+function isLucky(n) {
+    return n % 9 === 0;
+}
+
+console.log(isLucky(1892376)); // true
+console.log(isLucky(189237)); // false
+console.log(isLucky(1098)); // true
+console.log(isLucky(22869)); // true
+console.log(isLucky(0)); // true
