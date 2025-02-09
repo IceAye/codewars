8e77fa6152e65361cd71d2bd01dc404b9ae95a2a
Irene Volodko <ice7aye@gmail.com>
2025-02-09 21:10:50 +0300

    add description

--- 7kyu/array-array-array.js
+++ 7kyu/array-array-array.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given an initial 2-value array (x). You will use this to calculate a score.
+// If both values in (x) are numbers, the score is the sum of the two. If only one is a number, the score is that number. If neither is a number, return 'Void!'.
+// Once you have your score, you must return an array of arrays. Each sub array will be the same as (x) and the number of sub arrays should be equal to the score.
+//
+// ✅ SOLUTION:
 function explode(x) {
     if (typeof x[0] === typeof x[1]) {
         return typeof x[0] === "string" ? "Void!" : new Array(x[0] + x[1]).fill(x);
@@ -5,6 +11,7 @@ function explode(x) {
     return new Array(typeof x[0] === "number" ? x[0] : x[1]).fill(x);
 }
 
+// 📌 TESTCASE:
 console.log(explode([9, 3])); // [[9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9,
 // 3], [9, 3]]
 console.log(explode(["a", 3])); // [['a', 3], ['a', 3], ['a', 3]]
