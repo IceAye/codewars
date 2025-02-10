2aa38d374740a91ab728632e976b2e28bdf8227c
Irene Volodko <ice7aye@gmail.com>
2025-02-10 19:22:27 +0300

    add description

--- 7kyu/move-10.js
+++ 7kyu/move-10.js
@@ -1,8 +1,15 @@
+// ❓ DESCRIPTION:
+// Move every letter in the provided string forward 10 letters through the alphabet.
+// If it goes past 'z', start again at 'a'.
+// Input will be a string with length > 0.
+//
+// ✅ SOLUTION:
 function moveTen(s) {
     const abc = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
     return s.replace(/./g, (c) => abc[abc.indexOf(c) + 10]);
 }
 
+// 📌 TESTCASE:
 console.log(moveTen("testcase")); // "docdmkco"
 console.log(moveTen("codewars")); // "mynogkbc"
 console.log(moveTen("exampletesthere")); // "ohkwzvodocdrobo"
