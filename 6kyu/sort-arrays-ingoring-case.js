d0c89c883c7676d3435e97f3bb151b95092f966a
Irene Volodko <ice7aye@gmail.com>
2025-02-11 20:01:37 +0300

    add description

--- 6kyu/sort-arrays-ingoring-case.js
+++ 6kyu/sort-arrays-ingoring-case.js
@@ -1,8 +1,12 @@
+// ❓ DESCRIPTION:
+// Sort the given array of strings in alphabetical order, case insensitive.
+//
+// ✅ SOLUTION:
 function sortme(names){
     return names.sort((a, b) => (a.toUpperCase()).localeCompare(b.toUpperCase()));
 }
 
-
+// 📌 TESTCASE:
 console.log(sortme(["Hello","there","I'm","fine"])) // ["fine", "Hello", "I'm", "there"]
 console.log(sortme(["C", "d", "a", "B"])) // ["a", "B", "C", "d"]
 console.log(sortme(["CodeWars"])) // ["CodeWars"]
