0bf11bd8d5121c610abf7db6530cd58e2745374c
Irene Volodko <ice7aye@gmail.com>
2025-02-11 21:19:49 +0300

    add description

--- 7kyu/mirror-mirror-on-the-wall.js
+++ 7kyu/mirror-mirror-on-the-wall.js
@@ -1,8 +1,17 @@
+// ❓ DESCRIPTION:
+// Too long, didn't read
+// You get a list of integers, and you have to write a function mirror that returns the "mirror" (or symmetric) version of this list: i.e. the middle element is the greatest, then the next greatest on both sides, then the next greatest, and so on...
+//
+// More info
+// The list will always consist of integers in range -1000..1000 and will vary in size between 0 and 10000. Your function should not mutate the input array, and this will be tested (where applicable). Notice that the returned list will always be of odd size, since there will always be a definitive middle element.
+//
+// ✅ SOLUTION:
 function mirror(data) {
     let sorted = [...data].sort((a, b) => a - b);
     return [ ...sorted.slice(0, -1), ...sorted.reverse() ];
 }
 
+// 📌 TESTCASE:
 console.log(mirror( [])) // []
 console.log(mirror( [1])) // [1]
 console.log(mirror([2, 1])) // [1, 2, 1]
