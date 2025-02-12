f2887fd371534ab892e7dfb83a4a670da20da9dd
Irene Volodko <ice7aye@gmail.com>
2025-02-12 22:46:26 +0300

    add solution 2

--- 7kyu/santas-naughty-list.js
+++ 7kyu/santas-naughty-list.js
@@ -8,6 +8,11 @@ function findChildren(santasList, children) {
     return result.sort();
 }
 
+function findChildren(santasList, children) {
+    return [...new Set(children)].filter(item => santasList.includes(item)).sort()
+}
+
+
 console.log(findChildren(["Jason", "Jackson", "Jordan", "Johnny"], ["Jason", "Jordan", "Jennifer"])) // ["Jason", "Jordan"]
 console.log(findChildren(["jASon", "JAsoN", "JaSON", "jasON"], ["JasoN", "jASOn", "JAsoN", "jASon", "JASON"])) // ["JAsoN", "jASon"]
 console.log(findChildren(["Jason", "James", "Johnson"], ["Jason", "James", "JJ"])) // ["James", "Jason"]
