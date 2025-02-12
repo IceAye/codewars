72d6d9bc4fbf7f7a4d91430ace69e4be1c2e46e9
Irene Volodko <ice7aye@gmail.com>
2025-02-12 22:36:54 +0300

    add description

--- 7kyu/words-to-sentence.js
+++ 7kyu/words-to-sentence.js
@@ -1,5 +1,10 @@
+// ❓ DESCRIPTION:
+// Write function which will create a string from a list of strings, separated by space.
+//
+// ✅ SOLUTION:
 function wordsToSentence(words) {
     return words.join(' ');
 }
 
+// 📌 TESTCASE:
 console.log(wordsToSentence(["hello", "world"])) // "hello world"
