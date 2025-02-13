8a12f832bf8c2c23f4281cb50247609d7f741903
Irene Volodko <ice7aye@gmail.com>
2025-02-13 21:55:25 +0300

    add description

--- 7kyu/digits-average.js
+++ 7kyu/digits-average.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given a non-negative integer, take the (mean) average of each pair of consecutive digits. Repeat this process until you have a single integer, then return that integer. e.g.
+// Note: if the average of two digits is not an integer, round the result up (e.g. the average of 8 and 9 will be 9)
+//
+// ✅ SOLUTION:
 function digitsAverage(input) {
     if (input < 10) return input;
     let digits = Array.from(input.toString(), Number);
@@ -10,6 +15,7 @@ function digitsAverage(input) {
     return digitsAverage(newNumber);
 }
 
+// 📌 TESTCASE:
 console.log(digitsAverage(246)); // 4
 console.log(digitsAverage(89)); // 9
 console.log(digitsAverage(2)); // 2
