fe7d34419937f3abfed5e75620583656a7637130
Irene Volodko <ice7aye@gmail.com>
2025-02-13 23:10:45 +0300

    add description

--- 7kyu/number-manipulation-1-easy.js
+++ 7kyu/number-manipulation-1-easy.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// For a given large number (num), write a function which returns the number with the second half of digits changed to 0.
+// In cases where the number has an odd number of digits, the middle digit onwards should be changed to 0.
+// Example:
+// 192827764920 --> 192827000000
+// 938473 --> 938000
+// 2837743 --> 2830000
+//
+// ✅ SOLUTION:
 function manipulate(num) {
     let digits = Array.from(num.toString(), Number);
     let middle = Math.floor(digits.length / 2);
@@ -8,6 +17,7 @@ function manipulate(num) {
     return +digits.join("");
 }
 
+// 📌 TESTCASE:
 console.log(manipulate(192827764920)); // 192827000000
 console.log(manipulate(838473)); // 838000
 console.log(manipulate(8173648710293847)); // 8173648700000000
