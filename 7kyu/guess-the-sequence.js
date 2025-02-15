0cd821f2fb4b7b2bfbfa8c89298bbb532ae94948
Irene Volodko <ice7aye@gmail.com>
2025-02-15 20:48:19 +0300

    add description

--- 7kyu/guess-the-sequence.js
+++ 7kyu/guess-the-sequence.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// You have read the title: you must guess a sequence. It will have something to do with the number given.
+//
+// Example
+// x = 16
+// result = [1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9]
+// Good luck!
+//
+// ✅ SOLUTION:
 function sequence(x) {
     let result = [];
     let i = 1;
@@ -8,5 +17,6 @@ function sequence(x) {
     return result.sort();
 }
 
+// 📌 TESTCASE:
 console.log(sequence(15)) // [1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9]
 console.log(sequence(9)) // [1, 2, 3, 4, 5, 6, 7, 8, 9]
