285b41ddbcc6ed580e0f237eada30ca6c6b7af89
Irene Volodko <ice7aye@gmail.com>
2025-02-15 21:05:51 +0300

    add solution 2

--- 7kyu/geometric-progression-sequence.js
+++ 7kyu/geometric-progression-sequence.js
@@ -9,6 +9,10 @@ function geometricSequenceElements(a, r, n) {
     return result.join(", ");
 }
 
+function geometricSequenceElements(a , r , n) {
+    return [...Array(n)].map((_ , i) => a * r ** i).join(', ')
+}
+
 console.log(geometricSequenceElements(2, 3, 5)) // '2, 6, 18, 54, 162'
 console.log(geometricSequenceElements(2, 2, 10)) // '2, 4, 8, 16, 32, 64, 128, 256, 512, 1024'
 console.log(geometricSequenceElements(1, -2, 10)) // '1, -2, 4, -8, 16, -32, 64, -128, 256, -512'
