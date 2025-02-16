4bb0d955a265026d19ad03653408099145011ac3
Irene Volodko <ice7aye@gmail.com>
2025-02-16 19:21:27 +0300

    add description

--- 7kyu/sliding-puzzle-verification.js
+++ 7kyu/sliding-puzzle-verification.js
@@ -1,3 +1,17 @@
+// ❓ DESCRIPTION:
+// Given a board of NxN, distributed with tiles labeled 0 to N² - 1(inclusive):
+// A solved grid will have the tiles in order of label, left to right, top to bottom.
+// Return true if the board state is currently solved, and false if the board state is unsolved.
+// Input will always be a square 2d array.
+//
+// For example, a 2x2 solved grid:
+// [ [0, 1],
+//   [2, 3] ]
+// A 2x2 unsolved grid:
+// [ [2, 1],
+//   [0, 3] ]
+//
+// ✅ SOLUTION:
 function isSolved(board) {
     let arr = board.flat();
     for (let i = 0; i < arr.length - 1; i++) {
@@ -6,5 +20,6 @@ function isSolved(board) {
     return true;
 }
 
+// 📌 TESTCASE:
 console.log(isSolved([[0, 1],[2, 3]])) // true
 console.log(isSolved([[1, 0],[3, 3]])) // false
