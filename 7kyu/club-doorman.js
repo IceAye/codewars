7ca98a4e740bc256f8256d31d3f68168ff95b7d4
Irene Volodko <ice7aye@gmail.com>
2025-02-16 23:51:13 +0300

    add solution 2

--- 7kyu/club-doorman.js
+++ 7kyu/club-doorman.js
@@ -1,5 +1,16 @@
-function passTheDoorMan(word){
-    return (word.charCodeAt([...word].findIndex((char, ind) => char === word[ind + 1])) - 96) * 3;
+function passTheDoorMan(word) {
+    return (word.charCodeAt([...word].findIndex((char , ind) => char === word[ind + 1])) - 96) * 3;
+}
+
+function passTheDoorMan(word) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let letter;
+    for (let i = 0; i < word.length; i += 1) {
+        if (word[i] === word[i + 1]) {
+            letter = word[i];
+        }
+    }
+    return (alphabet.indexOf(letter) + 1) * 3
 }
 
 console.log(passTheDoorMan("lettuce")) // 60
