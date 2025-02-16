f484b694c446caff5e1e759fc60e14beb3797dfb
Irene Volodko <ice7aye@gmail.com>
2025-02-16 23:40:01 +0300

    add description

--- 7kyu/heggeleggleggo.js
+++ 7kyu/heggeleggleggo.js
@@ -1,6 +1,12 @@
+// ❓ DESCRIPTION:
+// Egg Talk.
+// Insert an egg after each consonant. If there are no consonants, there will be no eggs. Argument will consist of a string with only alphabetic characters and possibly some spaces.
+//
+// ✅ SOLUTION:
 function heggeleggleggo(word){
     return word.replace(/([^aeiou ])/gi, '$1egg');
 }
 
+// 📌 TESTCASE:
 console.log(heggeleggleggo("hello")) // "heggeleggleggo"
 console.log(heggeleggleggo("code here")) // "ceggodegge heggeregge"
