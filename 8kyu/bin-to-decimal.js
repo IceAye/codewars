69166730661fc16f7fae17054337fb0e73113062
Irene Volodko <ice7aye@gmail.com>
2025-02-18 23:55:21 +0300

    add description

--- 8kyu/bin-to-decimal.js
+++ 8kyu/bin-to-decimal.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+//
+// ✅ SOLUTION:
 function binToDec(bin) {
     return parseInt(bin, 2)
 }
