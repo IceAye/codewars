729354a0a84565112b6e1f7b789d3ac544b70896
Irene Volodko <ice7aye@gmail.com>
2025-02-18 23:48:22 +0300

    add solution

---
+++ 8kyu/basic-mathematical-operations.js
@@ -0,0 +1,5 @@
+const basicOp = (operation, value1, value2) =>
+    operation == "+" ? value1 + value2 :
+        operation == "-" ? value1 - value2 :
+            operation == "*" ? value1 * value2 :
+                value1 / value2
