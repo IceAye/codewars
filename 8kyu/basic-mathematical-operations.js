eff71faa43aa70c530fdfaf768e77d0d0f964f89
Irene Volodko <ice7aye@gmail.com>
2025-02-18 23:49:00 +0300

    add description

--- 8kyu/basic-mathematical-operations.js
+++ 8kyu/basic-mathematical-operations.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Your task is to create a function that does four basic mathematical operations.
+//
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+//
+// ✅ SOLUTION:
 const basicOp = (operation, value1, value2) =>
     operation == "+" ? value1 + value2 :
         operation == "-" ? value1 - value2 :
