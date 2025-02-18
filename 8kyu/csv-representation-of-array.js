0fd955c5db5d004d9bcf3c6514e3c6532829ec0a
Irene Volodko <ice7aye@gmail.com>
2025-02-18 23:57:02 +0300

    add description

--- 8kyu/csv-representation-of-array.js
+++ 8kyu/csv-representation-of-array.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+//
+// ✅ SOLUTION:
 function toCsvText(array) {
     return array.join('\n');
 }
