3948a9a043ccf3fef4a32e844591cd33d7a4bc94
Irene Volodko <ice7aye@gmail.com>
2025-02-19 23:52:09 +0300

    add description

--- 8kyu/convert-a-string-to-an-array.js
+++ 8kyu/convert-a-string-to-an-array.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Write a function to split a string and convert it into an array of words.
+//
+// ✅ SOLUTION:
 function stringToArray(string){
     return string.split(' ');
 }
