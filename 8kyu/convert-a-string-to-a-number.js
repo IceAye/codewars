f94f0f5fad2d3b388f9c3513615fcc7136fdbe24
Irene Volodko <ice7aye@gmail.com>
2025-02-19 23:49:09 +0300

    add solution 3

--- 8kyu/convert-a-string-to-a-number.js
+++ 8kyu/convert-a-string-to-a-number.js
@@ -3,6 +3,9 @@ const stringToNumber = function(str){
 }
 
 const stringToNumber = function(str){
-    // put your code here
     return Number(str);
+}
+
+const stringToNumber = function(str){
+    return +str;
 }
