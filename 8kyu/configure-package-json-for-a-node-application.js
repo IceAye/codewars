aefda5c7d39373f8503147edfbfff67034bee995
Irene Volodko <ice7aye@gmail.com>
2025-02-19 23:43:48 +0300

    add solution

---
+++ 8kyu/configure-package-json-for-a-node-application.js
@@ -0,0 +1,13 @@
+const configuration = {
+    "name": "great-pack",
+    "version": "5.3.2",
+    "author": "Kata Master email@example.com",
+    "description": "",
+    "main": "index.js",
+    "scripts": {
+        "precompress": "before.js",
+        "compress": "init.js",
+        "postcompress": "after.js"
+    },
+    "keywords": ["program", "best", "world"],
+};
