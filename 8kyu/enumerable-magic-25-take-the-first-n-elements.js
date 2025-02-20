131c5778f5d08b2d3e81ef5ed491ae4c15dff2ce
Irene Volodko <ice7aye@gmail.com>
2025-02-20 23:59:44 +0300

    add description

--- 8kyu/enumerable-magic-25-take-the-first-n-elements.js
+++ 8kyu/enumerable-magic-25-take-the-first-n-elements.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+//
+// ✅ SOLUTION 1:
 function take(arr, n) {
     return arr.filter((item, index) => index < n)
 }
 
+// ✅ SOLUTION 2:
 function take(arr, n) {
     return arr.slice(0, n);
 }
