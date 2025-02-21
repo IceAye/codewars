f76b4b5ddf004dc9c99e7dadb20a61e6f336a803
Irene Volodko <ice7aye@gmail.com>
2025-02-21 23:52:30 +0300

    add description

--- 8kyu/exclamation-marks-series-11.js
+++ 8kyu/exclamation-marks-series-11.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+//
+// ✅ SOLUTION:
 function replace(s){
     return s.replace(/[aeiou]/gi, '!')
 }
