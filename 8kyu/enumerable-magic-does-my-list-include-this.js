caadd8d715a90b6f16cef5705d13fdc399dee59b
Irene Volodko <ice7aye@gmail.com>
2025-02-21 23:48:30 +0300

    add description

--- 8kyu/enumerable-magic-does-my-list-include-this.js
+++ 8kyu/enumerable-magic-does-my-list-include-this.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+//
+// ✅ SOLUTION 1:
 function include(arr, item){
     return arr.includes(item);
 }
 
+// ✅ SOLUTION 2:
 function include(arr, item) {
     return arr.indexOf(item) !== -1;
 }
