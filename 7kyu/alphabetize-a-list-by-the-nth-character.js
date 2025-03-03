7bcc3b8d2dcadc3ce8c1fb5adcf525d6f1f53b51
Irene Volodko <ice7aye@gmail.com>
2025-03-03 19:06:33 +0300

    add description

--- 7kyu/alphabetize-a-list-by-the-nth-character.js
+++ 7kyu/alphabetize-a-list-by-the-nth-character.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Write a function that accepts two parameters, i) a string (containing a list of words) and ii) an integer (n). The function should alphabetize the list based on the nth letter of each word.
+// The letters should be compared case-insensitive. If both letters are the same, order them normally (lexicographically), again, case-insensitive.
+//
+// ✅ SOLUTION:
 function sortIt(list, n) {
     return list
         .split(', ')
@@ -5,5 +10,6 @@ function sortIt(list, n) {
         .join(', ');
 }
 
+// 📌 TESTCASE:
 console.log(sortIt("bill, bell, ball, bull", 2)); // 'ball, bell, bill, bull'
 console.log(sortIt("cat, dog, eel, bee", 3)); // 'bee, dog, eel, cat'
