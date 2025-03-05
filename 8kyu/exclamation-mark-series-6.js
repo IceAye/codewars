20511bac34d45591bf729d4ede088f5d56a44029
Irene Volodko <ice7aye@gmail.com>
2025-03-05 23:46:56 +0300

    add description

--- 8kyu/exclamation-mark-series-6.js
+++ 8kyu/exclamation-mark-series-6.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Remove n exclamation marks in the sentence from left to right. n is positive integer.
+//
+// ✅ SOLUTION:
 function remove(s, n) {
     let i = 0;
     while (i < n) {
