047aedaac7135513d8c67cf0b1d71c164f70b852
Irene Volodko <ice7aye@gmail.com>
2025-03-14 21:31:10 +0300

    description: find twins

--- 7kyu/find-twins.js
+++ 7kyu/find-twins.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Agent 47, you have a new task!
+// Among citizens of the city X are hidden 2 dangerous criminal twins.
+// Your task is to identify them and eliminate!
+//
+// Everyone except the twins are single-born (i.e., unique individuals).
+//
+// Given an array of integers, your task is to find two same numbers and return one of them. For example, in the array ```` the answer is 2.
+//
+// If there are no twins in the city - return None or the equivalent in the language that you are using.
+//
+// ✅ SOLUTION:
 function elimination(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) return arr[i];
@@ -5,6 +17,7 @@ function elimination(arr) {
     return null;
 }
 
+// 📌 TESTCASE:
 console.log(elimination([2,5,34,1,22,1])) // 1
 console.log(elimination([2,2,34,1,22])) // 2
 console.log(elimination([2,5,34,1,22])) // null
