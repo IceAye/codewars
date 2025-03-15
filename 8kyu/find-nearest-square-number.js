ea9cae08e14da31598f51a7737f70227cdd91a0d
Irene Volodko <ice7aye@gmail.com>
2025-03-15 23:50:57 +0300

    add solution 2: find nearest square number

--- 8kyu/find-nearest-square-number.js
+++ 8kyu/find-nearest-square-number.js
@@ -1,10 +1,14 @@
-function nearestSq( n ) {
+function nearestSq(n) {
     let plainDigit = Math.floor(Math.sqrt(n))
-    let nextNumber = Math.pow(plainDigit + 1, 2)
-    let prevNumber = Math.pow(plainDigit, 2)
+    let nextNumber = Math.pow(plainDigit + 1 , 2)
+    let prevNumber = Math.pow(plainDigit , 2)
 
     return Number.isInteger(Math.sqrt(n)) ? n
         : nextNumber - n < n - prevNumber ? nextNumber
             : prevNumber
 
+}
+
+function nearestSq(n) {
+    return Math.pow(Math.round(Math.sqrt(n)), 2);
 }
