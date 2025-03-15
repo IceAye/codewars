0a31a5658e52eec15d8d1e5137a3dda73b785db2
Irene Volodko <ice7aye@gmail.com>
2025-03-15 23:51:54 +0300

    description: find nearest square number

--- 8kyu/find-nearest-square-number.js
+++ 8kyu/find-nearest-square-number.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Your task is to find the nearest square number of a positive integer n. In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+//
+// For example, if n = 111, then the nearest square number equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
+//
+// If n is already a perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
+//
+// ✅ SOLUTION 1:
 function nearestSq(n) {
     let plainDigit = Math.floor(Math.sqrt(n))
     let nextNumber = Math.pow(plainDigit + 1 , 2)
@@ -6,9 +14,9 @@ function nearestSq(n) {
     return Number.isInteger(Math.sqrt(n)) ? n
         : nextNumber - n < n - prevNumber ? nextNumber
             : prevNumber
-
 }
 
+// ✅ SOLUTION 2:
 function nearestSq(n) {
     return Math.pow(Math.round(Math.sqrt(n)), 2);
 }
