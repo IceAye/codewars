e4b1b9d6b560ce46c88c6f367d9dc8529550073e
Irene Volodko <ice7aye@gmail.com>
2025-03-18 23:11:08 +0300

    description: interlocking binary pairs

--- 7kyu/interlocking-binary-pairs.js
+++ 7kyu/interlocking-binary-pairs.js
@@ -1,7 +1,17 @@
+// ❓ DESCRIPTION:
+// Write a function that checks if two non-negative integers make an "interlocking binary pair".
+//
+// Interlock ?
+// numbers can be interlocked if their binary representations have no 1's in the same place
+// comparisons are made by bit position, starting from right to left (see the examples below)
+// when representations are of different lengths, the unmatched left-most bits are ignored
+//
+// ✅ SOLUTION:
 function interlockable(a, b) {
     return (a & b) == 0;
 }
 
+// 📌 TESTCASE:
 console.log(interlockable(9n, 4n)) // true
 console.log(interlockable(3n, 6n)) // false
 console.log(interlockable(2n, 5n)) // true
