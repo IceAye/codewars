6ca41ade61424b0478ef4f86fc4c9a33cf21fc10
Irene Volodko <ice7aye@gmail.com>
2025-03-19 19:44:57 +0300

    description: character concatenation

--- 7kyu/character-concatenation.js
+++ 7kyu/character-concatenation.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given a string, you progressively need to concatenate the first character from the left and the first character from the right and "1", then the second character from the left and the second character from the right and "2", and so on.
+// If the string's length is odd drop the central element.
+//
+// ✅ SOLUTION:
 function charConcat(string){
     let middle = Math.floor(string.length / 2);
     let result = '';
@@ -8,5 +13,6 @@ function charConcat(string){
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(charConcat("abc def")) // 'af1be2cd3'
 console.log(charConcat("CodeWars")) // 'Cs1or2da3eW4'
