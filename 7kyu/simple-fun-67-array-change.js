d6c80f391ddf08214742790effbda006787bd094
Irene Volodko <ice7aye@gmail.com>
2025-03-19 23:56:30 +0300

    description: array change

--- 7kyu/simple-fun-67-array-change.js
+++ 7kyu/simple-fun-67-array-change.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+//
+// ✅ SOLUTION:
 function arrayChange(arr) {
     let count = 0;
 
