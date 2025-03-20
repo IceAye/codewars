05623acd3a5b629b8e967722d975e4f656c04b33
Irene Volodko <ice7aye@gmail.com>
2025-03-20 23:27:33 +0300

    description: find the position

--- 8kyu/find-the-position.js
+++ 8kyu/find-the-position.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// When provided with a letter, return its position in the alphabet.
+// Input :: "a"
+// Output :: "Position of alphabet: 1"
+// Note: Only lowercased English letters are tested
+//
+// ✅ SOLUTION:
 function position(letter){
     return `Position of alphabet: ${letter.charCodeAt() - 96}`;
 }
