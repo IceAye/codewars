23a2cfd280beb187dc746f5ebd7bc45326ae318e
Irene Volodko <ice7aye@gmail.com>
2025-03-20 20:21:36 +0300

    description: is infinite process

--- 7kyu/simple-fun-6-is-infinite-process.js
+++ 7kyu/simple-fun-6-is-infinite-process.js
@@ -1,7 +1,18 @@
+// ❓ DESCRIPTION:
+// Given integers a and b, determine whether the following pseudocode results in an infinite loop
+//
+//  while (a !== b){
+//      a++
+//      b--
+//  }
+// Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
+//
+// ✅ SOLUTION:
 function isInfiniteProcess(a, b) {
     return a > b || (b - a) % 2 !== 0;
 }
 
+// 📌 TESTCASE:
 console.log(isInfiniteProcess(2,6)) // false
 console.log(isInfiniteProcess(2,3)) // true
 console.log(isInfiniteProcess(2,10)) // false
