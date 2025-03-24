1bc3802bb3195b16b4d5e1613303d074eaad7c44
Irene Volodko <ice7aye@gmail.com>
2025-03-24 22:36:48 +0300

    description: no ifs no buts

--- 7kyu/no-ifs-no-buts.js
+++ 7kyu/no-ifs-no-buts.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// Write a function that accepts two numbers a and b and returns whether a is smaller than, bigger than, or equal to b, as a string.
+//
+// (5, 4)   ---> "5 is greater than 4"
+// (-4, -7) ---> "-4 is greater than -7"
+// (2, 2)   ---> "2 is equal to 2"
+// There is only one problem...
+// You cannot use if statements, and you cannot use the ternary operator ? :.
+// In fact the word if and the character ? are not allowed in your code.
+//
+// ✅ SOLUTION 1:
 function noIfsNoButs(a, b) {
     switch (a > b) {
         case true:
@@ -12,6 +23,7 @@ function noIfsNoButs(a, b) {
     }
 }
 
+// ✅ SOLUTION 2:
 function noIfsNoButs(a, b) {
     switch (Math.sign(a - b)) {
         case 1:
@@ -23,6 +35,7 @@ function noIfsNoButs(a, b) {
     }
 }
 
+// 📌 TESTCASE:
 console.log(noIfsNoButs(45, 51)) // "45 is smaller than 51"
 console.log(noIfsNoButs(1, 2)) // "1 is smaller than 2"
 console.log(noIfsNoButs(-3, 2)) // "-3 is smaller than 2"
