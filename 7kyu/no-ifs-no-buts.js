c9dcf7e22e359917288efe209840cd2bb85c4683
Irene Volodko <ice7aye@gmail.com>
2025-03-24 22:31:32 +0300

    add solution: no ifs no buts

---
+++ 7kyu/no-ifs-no-buts.js
@@ -0,0 +1,21 @@
+function noIfsNoButs(a, b) {
+    switch (a > b) {
+        case true:
+            return `${a} is greater than ${b}`;
+        case false:
+            switch (a - b) {
+                case 0:
+                    return `${a} is equal to ${b}`;
+                default:
+                    return `${a} is smaller than ${b}`;
+            }
+    }
+}
+
+console.log(noIfsNoButs(45, 51)) // "45 is smaller than 51"
+console.log(noIfsNoButs(1, 2)) // "1 is smaller than 2"
+console.log(noIfsNoButs(-3, 2)) // "-3 is smaller than 2"
+console.log(noIfsNoButs(1, 1)) // "1 is equal to 1"
+console.log(noIfsNoButs(100, 100)) // "100 is equal to 100"
+console.log(noIfsNoButs(100, 80)) // "100 is greater than 80"
+console.log(noIfsNoButs(20, 19)) // "20 is greater than 19"
