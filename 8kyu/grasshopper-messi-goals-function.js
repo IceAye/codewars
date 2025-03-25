3bbae83896dda3bc587f3a85f00f07280ae9a9b2
Irene Volodko <ice7aye@gmail.com>
2025-03-25 23:57:51 +0300

    add solution 2: Messi goals function

--- 8kyu/grasshopper-messi-goals-function.js
+++ 8kyu/grasshopper-messi-goals-function.js
@@ -1,3 +1,7 @@
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+function goals (...goalsArray) {
+    return goalsArray.reduce((acc, curr) => acc + curr , 0);
+};
