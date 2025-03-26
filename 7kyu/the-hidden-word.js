acee140c4837afa25e352fa4c9ff19fce6e412db
Irene Volodko <ice7aye@gmail.com>
2025-03-26 19:37:24 +0300

    add solution: the hidden word

---
+++ 7kyu/the-hidden-word.js
@@ -0,0 +1,26 @@
+const words = {
+    6: "a",
+    1: "b",
+    7: "d",
+    4: "e",
+    3: "i",
+    2: "l",
+    9: "m",
+    8: "n",
+    0: "o",
+    5: "t",
+};
+
+function hiddenWord(num) {
+    let result = "";
+    let digits = num.toString().split("");
+    for (const digit of digits) {
+        result += words[digit];
+    }
+    return result;
+}
+
+console.log(hiddenWord(637)) // "aid"
+console.log(hiddenWord(7415)) // "debt"
+console.log(hiddenWord(49632)) // "email"
+console.log(hiddenWord(942547)) // "melted"
