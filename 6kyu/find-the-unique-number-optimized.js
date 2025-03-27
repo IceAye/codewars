1b10429214f2dae8bbed8e6e5acf3ac6c3e11f4a
Irene Volodko <ice7aye@gmail.com>
2025-03-27 21:54:35 +0300

    description: find the unique number

--- 6kyu/find-the-unique-number-optimized.js
+++ 6kyu/find-the-unique-number-optimized.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Write a function called findUnique which returns the only unique number from an array.
+// All numbers in the unsorted array are present twice, except the one you have to find. The numbers are always valid integer values between 1 and 2147483647, so no need for type and error checking. The array contains at least one number and may contain millions of numbers. So make sure your solution is optimized for speed.
+//
+// ✅ SOLUTION 1:
 function findUnique(numbers) {
     const count = {};
 
@@ -10,10 +15,12 @@ function findUnique(numbers) {
     }
 }
 
+// ✅ SOLUTION 2:
 function findUnique(numbers) {
     return numbers.reduce((a, b) => a ^ b);
 }
 
+// 📌 TESTCASE:
 console.log(findUnique([1, 8, 4, 4, 6, 1, 8])); // 6
 console.log(findUnique([1234567])); // 1234567
 console.log(findUnique([1, 4, 4, 5, 5, 3, 3, 2, 2])); // 1
