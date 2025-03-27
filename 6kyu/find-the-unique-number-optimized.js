9d99f3644a2c11afdebc750cfaad0cb9279240cd
Irene Volodko <ice7aye@gmail.com>
2025-03-27 21:53:34 +0300

    add solution 2: find the unique number

--- 6kyu/find-the-unique-number-optimized.js
+++ 6kyu/find-the-unique-number-optimized.js
@@ -10,6 +10,10 @@ function findUnique(numbers) {
     }
 }
 
+function findUnique(numbers) {
+    return numbers.reduce((a, b) => a ^ b);
+}
+
 console.log(findUnique([1, 8, 4, 4, 6, 1, 8])); // 6
 console.log(findUnique([1234567])); // 1234567
 console.log(findUnique([1, 4, 4, 5, 5, 3, 3, 2, 2])); // 1
