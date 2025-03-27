0c1305c9a3517eec4f0f99adb180a2037fa7a9bf
Irene Volodko <ice7aye@gmail.com>
2025-03-27 21:11:02 +0300

    description: find SuperMan

--- 6kyu/crazy-programmer-01-find-superman.js
+++ 6kyu/crazy-programmer-01-find-superman.js
@@ -1,3 +1,24 @@
+// ❓ DESCRIPTION:
+// Hey, young man, don't look around, I'm talking to you!
+//
+// I am a programmer, work in this madhouse. my job is research code, and... at the same time I am also the other researcher's study object. There are a lot of the same people and work with me.
+//
+// My recent research is how to find Superman, because I found that a SuperMan is hidden inside us, we must find him, then let him go to save the planet.
+//
+// The conclusion of my research:
+// 1. If a person is talking to you, the word 'Superman' appears in
+//    the sentence, so he is not a superman.
+// 2. Because Superman usually careful to hide the word 'Superman' in
+//    a sentence, and each of the two letters are not continuous.
+// 3. Sometimes Superman will be especially careful, reverse the word,
+//    or transform uppercase and lowercase, let others more difficult
+//    to find out.
+// Now, I invite you to help me write the code (my hand was injured, there is no way to knock on the keyboard), a function which can correctly find out who is Superman, and let him wear the red pants and cloak, save our earth!
+//
+// output is a string, if find out SuperMan,
+// say "Hi, SuperMan!" to him,  otherwise, say "Are you crazy?"
+//
+// ✅ SOLUTION 1:
 function findSuperMan(s) {
     let string = s.replace("Hello, I am ", "").toLowerCase();
     let indices = [];
@@ -20,10 +41,12 @@ function findSuperMan(s) {
     return "Hi, SuperMan!";
 }
 
+// ✅ SOLUTION 2:
 function findSuperMan(s){
     return /s.+u.+p.+e.+r.+m.+a.+n|n.+a.+m.+r.+e.+p.+u.+s/i.test(s) ? "Hi, SuperMan!" : "Are you crazy?";
 }
 
+// 📌 TESTCASE:
 console.log(findSuperMan("Hello, I am SuxpxexrxMxaxn")) // "Are you crazy?"
 console.log(findSuperMan("Hello, I am SxuxpxexrxMxaxn")) // "Hi, SuperMan!"
 console.log(findSuperMan("Hello, I am nxaxmxrxexpxuxs")) // "Hi, SuperMan!"
