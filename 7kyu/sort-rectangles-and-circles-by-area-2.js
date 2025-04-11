442a0dac1a9bc7d79d1447f1d08ccfbbcce4e512
Irene Volodko <ice7aye@gmail.com>
2025-04-11 17:30:47 +0300

    add solution 1: sort rectangles and circles by area

---
+++ 7kyu/sort-rectangles-and-circles-by-area-2.js
@@ -0,0 +1,16 @@
+function sortByArea(array) {
+    let result = new Map();
+
+    for (const item of array) {
+        typeof item === "object"
+            ? result.set(item, item[0] * item[1])
+            : result.set(item, Math.PI * item ** 2);
+    }
+
+    return [...result.keys()].sort((a, b) => result.get(a) - result.get(b));
+}
+
+console.log(sortByArea([[4.23, 6.43], 1.23, 3.444, [1.342, 3.212]])); // [ [1.342, 3.212], 1.23, [4.23, 6.43],
+// 3.444 ]
+console.log(sortByArea([[2, 5], 6])); // [ [2, 5], 6 ]
+console.log(sortByArea([])); // []
