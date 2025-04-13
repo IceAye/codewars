80d1bf8ecc17582aad917d23308596b76d013048
Irene Volodko <ice7aye@gmail.com>
2025-04-13 19:26:22 +0300

    description: dominant array elements

--- 7kyu/dominant-array-elements.js
+++ 7kyu/dominant-array-elements.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// An element in an array is dominant if it is greater than all elements to its right. You will be given an array and your task will be to return a list of all dominant elements. For example:
+//
+// solve([1,21,4,7,5]) = [21,7,5] because 21, 7 and 5 are greater than elments to their right.
+// solve([5,4,3,2,1]) = [5,4,3,2,1]
+//
+// Notice that the last element is always included. All numbers will be greater than 0.
+// More examples in the test cases.
+//
+// Good luck!
+//
+// ✅ SOLUTION:
 function solve(arr) {
     let result = [];
     for (let i = 0; i < arr.length - 1; i++) {
@@ -6,6 +18,7 @@ function solve(arr) {
     return [...result, arr.at(-1)];
 }
 
+// 📌 TESTCASE:
 console.log(solve([16,17,14,3,14,5,2])) // [17,14,5,2]
 console.log(solve([92,52,93,31,89,87,77,105])) // [105]
 console.log(solve([75,47,42,56,13,55])) // [75,56,55]
