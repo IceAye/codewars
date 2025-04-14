44e0877eea9e1a9f2b01bbc6e1af311e85fb0c0c
Irene Volodko <ice7aye@gmail.com>
2025-04-14 17:34:52 +0300

    description: extra perfect numbers

--- 7kyu/extra-perfect-numbers-special-numbers-series-7.js
+++ 7kyu/extra-perfect-numbers-special-numbers-series-7.js
@@ -1,3 +1,17 @@
+// ❓ DESCRIPTION:
+// Definition
+// Extra perfect number is the number that first and last bits are set bits.
+//
+// Task
+// Given a positive integer N , Return the extra perfect numbers in range from 1 to N .
+//
+// Notes
+// Number passed is always Positive .
+//
+// Returned array/list should contain the extra perfect numbers in ascending order from lowest to highest
+//
+// ✅ SOLUTION:
+
 function extraPerfect(n){
     let result = [];
     let i = 1;
@@ -8,6 +22,7 @@ function extraPerfect(n){
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(extraPerfect(3), [1,3]);
 console.log(extraPerfect(5), [1,3,5]);
 console.log(extraPerfect(7), [1,3,5,7]);
