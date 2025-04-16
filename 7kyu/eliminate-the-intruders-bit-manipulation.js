7c1953fa5a3e1bb818ca91778ec3d6c64786e8f2
Irene Volodko <ice7aye@gmail.com>
2025-04-16 16:49:44 +0300

    description: eliminate the intruders, bit manipulation

--- 7kyu/eliminate-the-intruders-bit-manipulation.js
+++ 7kyu/eliminate-the-intruders-bit-manipulation.js
@@ -1,7 +1,21 @@
+// ❓ DESCRIPTION:
+// You are given a string representing a number in binary. Your task is to delete all the unset bits in this string and return the corresponding number (after keeping only the '1's).
+// In practice, you should implement this function:
+// function eliminateUnsetBits(number);
+//
+// Examples
+// eliminateUnsetBits("11010101010101") ->  255 (= 11111111)
+// eliminateUnsetBits("111") ->  7
+// eliminateUnsetBits("1000000") -> 1
+// eliminateUnsetBits("000") -> 0
+//
+// ✅ SOLUTION:
+
 function eliminateUnsetBits(number) {
     return parseInt(number.replace(/0/g, ''), 2) || 0;
 }
 
+// 📌 TESTCASE:
 console.log(eliminateUnsetBits("11010101010101"), 255)
 console.log(eliminateUnsetBits("111"), 7)
 console.log(eliminateUnsetBits("1000000"), 1)
