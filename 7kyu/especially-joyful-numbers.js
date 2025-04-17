4edda62b8aeebf9f7d8501a7dd0e13d8ef76c804
Irene Volodko <ice7aye@gmail.com>
2025-04-17 15:38:13 +0300

    description: especially joyful numbers

--- 7kyu/especially-joyful-numbers.js
+++ 7kyu/especially-joyful-numbers.js
@@ -1,9 +1,20 @@
+// ❓ DESCRIPTION:
+// Positive integers that are divisible exactly by the sum of their digits are called Harshad numbers. The first few Harshad numbers are: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 18, ...
+//
+// We are interested in Harshad numbers where the product of its digit sum s and s with the digits reversed, gives the original number n. For example consider number 1729:
+// its digit sum, s = 1 + 7 + 2 + 9 = 19
+// reversing s = 91
+// and 19 * 91 = 1729 --> the number that we started with.
+// Complete the function which tests if a positive integer n is Harshad number, and returns True if the product of its digit sum and its digit sum reversed equals n; otherwise return False.
+//
+// ✅ SOLUTION:
 function numberJoy(n) {
     let sum = Array.from(n.toString(), Number).reduce((acc, curr) => acc + curr, 0);
     let reverse = +Array.from(sum.toString()).reverse().join('');
     return sum * reverse === n;
 }
 
+// 📌 TESTCASE:
 console.log(numberJoy(1997), false)
 console.log(numberJoy(1998), false)
 console.log(numberJoy(1729), true)
