1e32958c8742ed65e8d4ffd3428c4db3926a354c
Irene Volodko <ice7aye@gmail.com>
2025-04-18 17:41:38 +0300

    description: regex validation of 24 hours time

--- 7kyu/regex-validation-of-24-hours-time.js
+++ 7kyu/regex-validation-of-24-hours-time.js
@@ -1,8 +1,15 @@
+// ❓ DESCRIPTION:
+// Write a regex to validate a 24 hours time string. See examples to figure out what you should check for:
+// Accepted: 01:00, 1:00, 00:00
+// Not accepted: 24:00, 13:1, 12:60
+// You should check for correct length and no spaces.
+//
+// ✅ SOLUTION:
 function validateTime(time) {
     return /^(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/gi.test(time);
 }
 
-
+// 📌 TESTCASE:
 console.log(validateTime('01:00'), true);
 console.log(validateTime('1:00'), true);
 console.log(validateTime('1:00'), true);
