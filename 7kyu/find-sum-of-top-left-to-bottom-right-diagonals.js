68d85fd2175e5f46130a7f23e65167a052ea9367
Irene Volodko <ice7aye@gmail.com>
2025-04-22 20:31:30 +0300

    add solution: find sum of top left to bottom right diagonals

---
+++ 7kyu/find-sum-of-top-left-to-bottom-right-diagonals.js
@@ -0,0 +1,8 @@
+function diagonalSum(matrix){
+    return matrix.reduce((acc, curr, ind) => acc + curr[ind], 0);
+}
+
+console.log(diagonalSum([[12]]), 12);
+console.log(diagonalSum([[1, 2], [3, 4]]), 5);
+console.log(diagonalSum([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), 15);
+console.log(diagonalSum([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]), 34);
