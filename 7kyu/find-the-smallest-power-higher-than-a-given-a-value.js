37e0b93948f6736bb8a55ff18417b5dd914e3611
Irene Volodko <ice7aye@gmail.com>
2025-04-23 16:53:32 +0300

    description: find the smallest power higher than a given a value

--- 7kyu/find-the-smallest-power-higher-than-a-given-a-value.js
+++ 7kyu/find-the-smallest-power-higher-than-a-given-a-value.js
@@ -1,8 +1,27 @@
+// ❓ DESCRIPTION:
+// We have the number 12385 . We want to know the value of the closest cube but higher than 12385. The answer will be 13824 .
+//
+// Now, another case. We have the number 1245678 . We want to know the 5th power, closest and higher than that number. The value will be 1419857.
+//
+// We need a function that receives two arguments, a value val , and the exponent of the power,  pow_ , and outputs the value that we want to find.
+//
+// Let'see some cases:
+//
+// (12385, 3) ==> 13824
+// (1245678, 5) ==> 1419857
+// The value, val will be always a positive integer.
+//
+// The power, pow_ , always higher than 1 .
+//
+// Happy coding!!
+//
+// ✅ SOLUTION:
 function findNextPower(val, pow_) {
     let base = Math.ceil(Math.pow(val + 1, 1 / pow_));
     return Math.pow(base, pow_);
 }
 
+// 📌 TESTCASE:
 console.log(findNextPower(8, 3), 27)
 console.log(findNextPower(12385, 3), 13824)
 console.log(findNextPower(1245678, 5), 1419857)
