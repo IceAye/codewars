739c2d7aa1afa383b04bea0deff27c3a95d1e535
Irene Volodko <ice7aye@gmail.com>
2025-04-23 16:52:46 +0300

    add solution: find the smallest power higher than a given a value

---
+++ 7kyu/find-the-smallest-power-higher-than-a-given-a-value.js
@@ -0,0 +1,10 @@
+function findNextPower(val, pow_) {
+    let base = Math.ceil(Math.pow(val + 1, 1 / pow_));
+    return Math.pow(base, pow_);
+}
+
+console.log(findNextPower(8, 3), 27)
+console.log(findNextPower(12385, 3), 13824)
+console.log(findNextPower(1245678, 5), 1419857)
+console.log(findNextPower(1245678, 6), 1771561)
+console.log(findNextPower(4782969, 7), 10000000)
