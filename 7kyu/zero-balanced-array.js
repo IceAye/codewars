cf8b8130de48c329ea78248d44175a344b18721e
Irene Volodko <ice7aye@gmail.com>
2025-04-24 18:46:05 +0300

    add solution: zero-balanced array

--- 7kyu/zero-balanced-array.js
+++ 7kyu/zero-balanced-array.js
@@ -1,4 +1,4 @@
-function ìsZeroBalanced(n){
+function isZeroBalanced(n) {
     const map = new Map();
     let sum = 0;
 
@@ -6,21 +6,21 @@ function ìsZeroBalanced(n){
         sum += number;
         const current = Math.abs(number);
 
-        if (number !== 0) map.set(current, map.get(current) + 1 || 1);
+        if (number !== 0) map.set(current , map.get(current) + 1 || 1);
     }
 
     return n.length > 0 && sum === 0 && [...map.values()].every(item => item % 2 === 0);
 }
 
-console.log(ìsZeroBalanced([0, 0, 0, 0, 0, 0], true));
-console.log(ìsZeroBalanced([0, 1, -1], true));
-console.log(ìsZeroBalanced([0], true));
-console.log(ìsZeroBalanced([0, 0, 0, 0, 0], true));
-console.log(ìsZeroBalanced([1, -1, 2, -2], true));
-console.log(ìsZeroBalanced([3, -3, 5, -5, 7, -7], true));
-console.log(ìsZeroBalanced([3], false));
-console.log(ìsZeroBalanced([-3], false));
-console.log(ìsZeroBalanced([], false));
-console.log(ìsZeroBalanced([3, -2, -1], false));
-console.log(ìsZeroBalanced([1, 1, -2], false));
-console.log(ìsZeroBalanced([-1, 1, -2, 2, -2, -2, -4, 4], false));
+console.log(isZeroBalanced([0, 0, 0, 0, 0, 0], true));
+console.log(isZeroBalanced([0, 1, -1], true));
+console.log(isZeroBalanced([0], true));
+console.log(isZeroBalanced([0, 0, 0, 0, 0], true));
+console.log(isZeroBalanced([1, -1, 2, -2], true));
+console.log(isZeroBalanced([3, -3, 5, -5, 7, -7], true));
+console.log(isZeroBalanced([3], false));
+console.log(isZeroBalanced([-3], false));
+console.log(isZeroBalanced([], false));
+console.log(isZeroBalanced([3, -2, -1], false));
+console.log(isZeroBalanced([1, 1, -2], false));
+console.log(isZeroBalanced([-1, 1, -2, 2, -2, -2, -4, 4], false));
