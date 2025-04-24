293c5564a4071a473ba32e0e6225dc1c04b7c274
Irene Volodko <ice7aye@gmail.com>
2025-04-24 19:14:29 +0300

    add solution 2: inverse slicer

--- 7kyu/thinkful-list-and-loop-drills-inverse-slicer.js
+++ 7kyu/thinkful-list-and-loop-drills-inverse-slicer.js
@@ -2,6 +2,11 @@ function inverseSlice(items, a, b) {
     return [...items.slice(0, a), ...items.slice(b)];
 }
 
+function inverseSlice(items, a, b) {
+    items.splice(a, b - a);
+    return items;
+}
+
 console.log(inverseSlice([12, 14, 63, 72, 55, 24], 2, 4), [12, 14, 55, 24]);
 console.log(inverseSlice([12, 14, 63, 72, 55, 24], 0, 3), [72, 55, 24]);
 console.log(inverseSlice(['Intuition', 'is', 'a', 'poor', 'guide', 'when', 'facing', 'probabilistic', 'evidence'], 5, 13), ['Intuition', 'is', 'a', 'poor', 'guide']);
