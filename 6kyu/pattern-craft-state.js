8a56f720988e73f310ade784f9a7964a4f76541a
Irene Volodko <ice7aye@gmail.com>
2025-04-25 20:37:45 +0300

    description: patternCraft - state

--- 6kyu/pattern-craft-state.js
+++ 6kyu/pattern-craft-state.js
@@ -1,3 +1,19 @@
+// ❓ DESCRIPTION:
+// The State Design Pattern can be used, for example, to manage the state of a tank in the StarCraft game.
+//
+// The pattern consists in isolating the state logic in different classes rather than having multiple ifs to determine what should happen.
+//
+// Your Task
+// Complete the code so that when a Tank goes into SiegeMode it cannot move and its damage goes to 20. When it goes to TankMode it should be able to move and the damage should be set to 5.
+//
+// You have 3 classes:
+//
+// Tank: has a state, canMove and damage properties
+// SiegeState and TankState: has canMove and damage properties
+// Note: The tank initial state should be TankState.
+//
+// ✅ SOLUTION:
+
 class SiegeState {
     constructor() {
         this.canMove = false
@@ -26,6 +42,7 @@ class Tank {
     }
 }
 
+// 📌 TESTCASE:
 let tank = new Tank();
 console.log(tank.canMove, true);
 console.log(tank.damage, 5);
