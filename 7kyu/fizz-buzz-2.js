41f51f6c63d5c2537ce145caf6530147e286902d
Irene Volodko <ice7aye@gmail.com>
2025-04-25 21:08:31 +0300

    description: fizz buzz

--- 7kyu/fizz-buzz-2.js
+++ 7kyu/fizz-buzz-2.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Write a function that takes an integer and returns an array [A, B, C], where A is the number of multiples of 3 (but not 5) below the given integer, B is the number of multiples of 5 (but not 3) below the given integer and C is the number of multiples of 3 and 5 below the given integer.
+// For example, solution(20) should return [5, 2, 1]
+//
+// ✅ SOLUTION:
 function solution(number){
     let result = [0, 0, 0];
 
@@ -10,6 +15,7 @@ function solution(number){
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(solution(20), [5, 2, 1]);
 console.log(solution(2), [0, 0, 0]);
 console.log(solution(14), [4,2,0]);
