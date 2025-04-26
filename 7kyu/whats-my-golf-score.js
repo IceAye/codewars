76d497c72cede4ee51e2c582c4f582d384859f84
Irene Volodko <ice7aye@gmail.com>
2025-04-26 15:21:22 +0300

    add solution 2: what's my golf score

--- 7kyu/whats-my-golf-score.js
+++ 7kyu/whats-my-golf-score.js
@@ -8,5 +8,9 @@ function golfScoreCalculator(parList, scoreList){
     return result;
 }
 
+function golfScoreCalculator(parList, scoreList){
+    return [...scoreList].reduce((acc, curr, ind) => acc + (curr - parList[ind]), 0);
+}
+
 console.log(golfScoreCalculator('443454444344544443', '353445334534445344'), -1);
 console.log(golfScoreCalculator('123456123456123456', '123456123456123456'), 0);
