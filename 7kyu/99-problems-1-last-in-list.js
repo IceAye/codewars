cdde132f439042898a698f319baa9f29cd3cb92e
Irene Volodko <ice7aye@gmail.com>
2025-05-01 19:51:25 +0300

    description: last in list

--- 7kyu/99-problems-1-last-in-list.js
+++ 7kyu/99-problems-1-last-in-list.js
@@ -1,6 +1,18 @@
+// ❓ DESCRIPTION:
+// Write a function last that accepts a list and returns the last element in the list.
+//
+// If the list is empty:
+//
+// In languages that have a built-in option or result type (like OCaml or Haskell), return an empty option
+//
+// In languages that do not have an empty option, just return null
+//
+// ✅ SOLUTION 1:
 const last = xs => xs.length ? xs[xs.length - 1] : null;
 
+// ✅ SOLUTION 2:
 const last = xs => xs.length ? xs.pop() : null;
 
+// 📌 TESTCASE:
 console.log( last([1,2,3]), 3);
 console.log( last([]), null);
