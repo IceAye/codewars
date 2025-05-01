1ce2995383a8996bca8dbe90ae2281fb24fda95b
Irene Volodko <ice7aye@gmail.com>
2025-05-01 19:20:50 +0300

    description: object drills - quarks

--- 7kyu/thinkful-object-drills-quarks.js
+++ 7kyu/thinkful-object-drills-quarks.js
@@ -1,3 +1,18 @@
+// ❓ DESCRIPTION:
+// Background
+// You're modelling the interaction between a large number of quarks and have decided to create a Quark class so you can generate your own quark objects.
+//
+// Quarks are fundamental particles and the only fundamental particle to experience all four fundamental forces.
+//
+// Your task
+// Your Quark class should allow you to create quarks of any valid color ("red", "blue", and "green") and any valid flavor ('up', 'down', 'strange', 'charm', 'top', and 'bottom').
+//
+// Every quark has the same baryon_number (BaryonNumber in C#): 1/3.
+//
+// Every quark should have an .interact() (.Interact() in C#) method that allows any quark to interact with another quark via the strong force. When two quarks interact they exchange colors.
+//
+// ✅ SOLUTION:
+
 class Quark {
     constructor(color, flavor) {
         this.color = color
@@ -11,6 +26,7 @@ class Quark {
     }
 }
 
+// 📌 TESTCASE:
 let q1 = new Quark("red", "up")
 let q2 = new Quark("blue", "strange")
 
