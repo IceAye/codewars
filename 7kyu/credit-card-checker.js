ff3199a23ea088785da7acf91a41c19679b79673
Irene Volodko <ice7aye@gmail.com>
2025-05-05 18:20:29 +0300

    description: credit card checker

--- 7kyu/credit-card-checker.js
+++ 7kyu/credit-card-checker.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Write a function that checks whether a credit card number is correct or not, using the Luhn algorithm.
+//
+// The algorithm is as follows:
+// From the rightmost digit, which is the check digit, moving left, double the value of every second digit; if the product of this doubling operation is greater than 9 (e.g., 8 × 2 = 16), then sum the digits of the products (e.g., 16: 1 + 6 = 7, 18: 1 + 8 = 9) or alternatively subtract 9 from the product (e.g., 16: 16 - 9 = 7, 18: 18 - 9 = 9).
+// Take the sum of all the digits.
+// If the total modulo 10 is equal to 0 (if the total ends in zero) then the number is valid according to the Luhn formula; else it is not valid.
+// The input is a string with the full credit card number, in groups of 4 digits separated by spaces, i.e. "1234 5678 9012 3456"
+// Don´t worry about wrong inputs, they will always be a string with 4 groups of 4 digits each separated by space.
+//
+// ✅ SOLUTION:
+
 function validCard(card){
     function check(digit) {
         let double = 2 * digit;
@@ -13,6 +25,7 @@ function validCard(card){
     return (sum + lastDigit) % 10 === 0;
 }
 
+// 📌 TESTCASE:
 console.log(validCard("3333 3333 3333 3331"), true);
 console.log(validCard("5457 1111 9323 4311"), false);
 console.log(validCard("5457 6238 9323 4311"), false);
