4dec421044a84c01cb0803fcbcc9f667c6c09da8
Irene Volodko <ice7aye@gmail.com>
2025-05-05 22:06:55 +0300

    description: dropcaps

--- 7kyu/dropcaps.js
+++ 7kyu/dropcaps.js
@@ -1,12 +1,20 @@
+// ❓ DESCRIPTION:
+// DropCaps means that the first letter of the starting word of the paragraph should be in caps and the remaining lowercase, just like you see in the newspaper.
+// But for a change, let"s do that for each and every word of the given String. Your task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+// *should work also on Leading and Trailing Spaces and caps.
+// Note: you will be provided at least one word and should take string as input and return string as output.
+//
+// ✅ SOLUTION 1:
 function dropCap(n) {
     return n.split(' ').map(word => word.length > 2 ? word[0].toUpperCase() + word.slice(1).toLowerCase() : word).join(' ');
 }
 
+// ✅ SOLUTION 2:
 function dropCap(n) {
     return n.replace(/\S{3,}/g, w => w[0].toUpperCase() + w.slice(1).toLowerCase());
 }
 
-
+// 📌 TESTCASE:
 console.log(dropCap('Apple Banana'),"Apple Banana");
 console.log(dropCap('Apple'),"Apple");
 console.log(dropCap(''),"");
