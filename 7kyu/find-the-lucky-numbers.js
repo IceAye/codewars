e5579ba4fb4a2743cb363d071e3d6b76c90ef3d0
Irene Volodko <ice7aye@gmail.com>
2025-05-05 22:11:18 +0300

    add solution: find the lucky numbers

---
+++ 7kyu/find-the-lucky-numbers.js
@@ -0,0 +1,7 @@
+const filterLucky = x =>{
+    return x.filter(number => number.toString().includes('7'));
+}
+
+
+console.log( filterLucky([1,2,3,4,5,6,7,68,69,70,15,17]), [7,70,17] );
+console.log( filterLucky([71,9907,69]), [71,9907] );
