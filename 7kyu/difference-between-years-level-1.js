65ac6641ed702ada9913eb26f32deea8e3dbf94b
Irene Volodko <ice7aye@gmail.com>
2025-05-05 22:34:27 +0300

    description: difference between years

--- 7kyu/difference-between-years-level-1.js
+++ 7kyu/difference-between-years-level-1.js
@@ -1,7 +1,17 @@
+// ❓ DESCRIPTION:
+// Write a function that receives two strings as parameter. This strings are in the following format of date: YYYY/MM/DD. Your job is: Take the years and calculate the difference between them.
+//
+// Examples:
+// '1997/10/10' and '2015/10/10' -> 2015 - 1997 = returns 18
+// '2015/10/10' and '1997/10/10' -> 2015 - 1997 = returns 18
+// At this level, you don't need validate months and days to calculate the difference.
+//
+// ✅ SOLUTION:
 const howManyYears = function(date1, date2){
     return Math.abs(+date1.slice(0, 4) - +date2.slice(0, 4));
 }
 
+// 📌 TESTCASE:
 console.log(howManyYears('1997/10/10', '2015/10/10'), 18);
 console.log(howManyYears('1990/10/10', '2015/10/10'), 25);
 console.log(howManyYears('2015/10/10', '1990/10/10'), 25);
