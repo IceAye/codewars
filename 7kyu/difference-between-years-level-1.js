409d837c9580b7a643fc08501f0b0f760d533263
Irene Volodko <ice7aye@gmail.com>
2025-05-05 22:33:46 +0300

    add solution: difference between years

---
+++ 7kyu/difference-between-years-level-1.js
@@ -0,0 +1,9 @@
+const howManyYears = function(date1, date2){
+    return Math.abs(+date1.slice(0, 4) - +date2.slice(0, 4));
+}
+
+console.log(howManyYears('1997/10/10', '2015/10/10'), 18);
+console.log(howManyYears('1990/10/10', '2015/10/10'), 25);
+console.log(howManyYears('2015/10/10', '1990/10/10'), 25);
+console.log(howManyYears('1992/10/24', '2015/10/24'), 23);
+console.log(howManyYears('2018/10/10', '2000/10/10'), 18);
