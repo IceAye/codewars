fb0518e73ccbb8d8132ee199c501e5937172e5e1
Irene Volodko <ice7aye@gmail.com>
2025-05-06 17:40:36 +0300

    description: alphabetized

--- 6kyu/alphabetized.js
+++ 6kyu/alphabetized.js
@@ -1,5 +1,11 @@
+// ❓ DESCRIPTION:
+// Re-order the characters of a string, so that they are concatenated into a new string in "case-insensitively-alphabetical-order-of-appearance" order. Whitespace and punctuation shall simply be removed!
+// The input is restricted to contain no numerals and only words containing the english alphabet letters.
+//
+// ✅ SOLUTION:
 function alphabetized(s) {
     return [...s.replace(/[^a-z]/ig, '')].sort((a, b) => a.toLowerCase().charCodeAt(0) - b.toLowerCase().charCodeAt(0)).join('');
 }
 
+// 📌 TESTCASE:
 console.log(alphabetized('The Holy Bible'), 'BbeehHilloTy')
