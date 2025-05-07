0da47e1ea432d3718fc95ff3e6ca264e64a9fb7d
Irene Volodko <ice7aye@gmail.com>
2025-05-07 17:31:17 +0300

    description: replace all items

--- 7kyu/replace-all-items.js
+++ 7kyu/replace-all-items.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Write function replaceAll (Python: replace_all) that will replace all occurrences of an item with another.
+// Python / JavaScript: The function has to work for strings and lists.
+// Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]
+//
+// ✅ SOLUTION:
 function replaceAll(seq, find, replace) {
     if (seq.length === 0) return seq;
     if (Array.isArray(seq)) {
@@ -8,6 +14,7 @@ function replaceAll(seq, find, replace) {
     return seq;
 }
 
+// 📌 TESTCASE:
 console.log(replaceAll([], 1, 2), []);
 console.log(replaceAll([1, 2, 2], 1, 2), [2, 2, 2]);
 console.log(replaceAll("1,2,2", 1, 2), "2,2,2");
