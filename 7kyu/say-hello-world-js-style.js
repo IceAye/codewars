ee7d264c527960bb9103b551ff841b22dc3635d5
Irene Volodko <ice7aye@gmail.com>
2025-05-08 10:01:21 +0300

    description: say Hello world

--- 7kyu/say-hello-world-js-style.js
+++ 7kyu/say-hello-world-js-style.js
@@ -1,7 +1,16 @@
+// ❓ DESCRIPTION:
+// Write the definition of the function "say" such that calling this:
+//
+// say("Hello")("World")
+// returns "Hello World"
+//
+// ✅ SOLUTION 1:
 const say = function(string1) {
     return (string2) => string1 + " " + string2;
 }
 
+// ✅ SOLUTION 2:
 const say = string1 => string2 => `${string1} ${string2}`;
 
+// 📌 TESTCASE:
 console.log(say('Hello')('World'), 'Hello World');
