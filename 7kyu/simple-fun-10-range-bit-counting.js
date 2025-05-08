3a9cbaef2a1b38d0423ffc2fac1aef62f5257c77
Irene Volodko <ice7aye@gmail.com>
2025-05-08 08:53:05 +0300

    description: range bit counting

--- 7kyu/simple-fun-10-range-bit-counting.js
+++ 7kyu/simple-fun-10-range-bit-counting.js
@@ -1,3 +1,21 @@
+// ❓ DESCRIPTION:
+// You are given two numbers a and b where 0 ≤ a ≤ b. Imagine you construct an array of all the integers from a to b inclusive. You need to count the number of 1s in the binary representations of all the numbers in the array.
+//
+// Example
+// For a = 2 and b = 7, the output should be 11
+//
+// Given a = 2 and b = 7 the array is: [2, 3, 4, 5, 6, 7]. Converting the numbers to binary, we get [10, 11, 100, 101, 110, 111], which contains 1 + 2 + 1 + 2 + 2 + 3 = 11 1s.
+//
+// Input/Output
+// [input] integer a
+// Constraints: 0 ≤ a ≤ b.
+//
+// [input] integer b
+// Constraints: a ≤ b ≤ 100.
+//
+// [output] an integer
+//
+// ✅ SOLUTION:
 function rangeBitCount(a, b) {
     let result = 0;
 
@@ -12,6 +30,7 @@ function rangeBitCount(a, b) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(rangeBitCount(2,7) , 11)
 console.log(rangeBitCount(0,1) , 1)
 console.log(rangeBitCount(4,4) , 1)
