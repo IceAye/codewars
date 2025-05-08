d9f3b6e54cc19e4b49053c1d5a89ed6688d14df2
Irene Volodko <ice7aye@gmail.com>
2025-05-08 09:14:45 +0300

    description: array comparator

--- 7kyu/array-comparator.js
+++ 7kyu/array-comparator.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// You have two arrays in this kata, every array contains unique elements only. Your task is to calculate number of elements in the first array which are also present in the second array.
+//
+// ✅ SOLUTION:
 function matchArrays(v,r){
     let result = 0;
     let set = new Set(v);
@@ -9,6 +13,7 @@ function matchArrays(v,r){
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(matchArrays(['Perl','Closure','JavaScript'],['Go', 'C++','Erlang']),0);
 console.log(matchArrays(['incapsulation','OOP','array'],['time', 'propert','paralelism','OOP']),1);
 console.log(matchArrays([1,2,3,4,5],[2,3,4,5,6]),4);
