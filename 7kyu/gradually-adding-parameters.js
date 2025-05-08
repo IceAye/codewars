f632fb4c29bffe14c5203536ad335a43dc7c4d12
Irene Volodko <ice7aye@gmail.com>
2025-05-08 09:20:02 +0300

    description: gradually adding parameters

--- 7kyu/gradually-adding-parameters.js
+++ 7kyu/gradually-adding-parameters.js
@@ -1,7 +1,15 @@
+// ❓ DESCRIPTION:
+// This kata is all about adding numbers.
+// You will create a function named add. This function will return the sum of all the arguments. Sounds easy, doesn't it??
+// Well here's the twist. The inputs will gradually increase with their index as parameter to the function.
+// Remember the function will return 0 if no arguments are passed.
+//
+// ✅ SOLUTION:
 function add(...args) {
     return args.reduce((acc, curr, ind) => acc + curr * (ind + 1), 0);
 }
 
+// 📌 TESTCASE:
 console.log(add(100,200,300),1400)
 console.log(add(), 0);
 console.log(add(1,2,3), 14);
