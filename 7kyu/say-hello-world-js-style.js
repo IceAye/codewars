622a7e703e52a79d34b64a7c1cd96a96bc5e1906
Irene Volodko <ice7aye@gmail.com>
2025-05-08 10:00:20 +0300

    add solution 2: say Hello world

--- 7kyu/say-hello-world-js-style.js
+++ 7kyu/say-hello-world-js-style.js
@@ -1,3 +1,7 @@
 const say = function(string1) {
     return (string2) => string1 + " " + string2;
 }
+
+const say = string1 => string2 => `${string1} ${string2}`;
+
+console.log(say('Hello')('World'), 'Hello World');
