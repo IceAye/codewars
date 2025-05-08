67060af1c154a8e0eb2319a9179e841aa58902c6
Irene Volodko <ice7aye@gmail.com>
2025-05-08 10:12:50 +0300

    add solution: function composition

---
+++ 6kyu/function-composition.js
@@ -0,0 +1,10 @@
+function compose(f,g) {
+    return (...args) => f(g(...args));
+}
+
+const add1 = function(a){return a + 1}
+const id = function(a){return a}
+console.log( compose(add1,id)(0), 1 )
+
+const addAll3 = function(a,b,c){return a + b + c}
+console.log( compose(add1,addAll3)(1,2,3), 7 )
