6489b1288c0f232362831500762f3a17af364fb4
Irene Volodko <ice7aye@gmail.com>
2025-05-09 15:20:24 +0300

    description: double trouble

--- 7kyu/double-trouble.js
+++ 7kyu/double-trouble.js
@@ -1,3 +1,20 @@
+// ❓ DESCRIPTION:
+// Given an array of integers (x), and a target (t), you must find out if any two consecutive numbers in the array sum to t. If so, remove the second number.
+//
+// Example:
+//
+// x = [1, 2, 3, 4, 5]
+// t = 3
+//
+// 1+2 = t, so remove 2. No other pairs = t, so rest of array remains:
+//
+// [1, 3, 4, 5]
+//
+// Work through the array from left to right.
+//
+// Return the resulting array.
+//
+// ✅ SOLUTION:
 function trouble(x, t){
     let i = 0;
     while (i < x.length - 1) {
@@ -8,6 +25,7 @@ function trouble(x, t){
     return x;
 }
 
+// 📌 TESTCASE:
 console.log(trouble([1, 3, 5, 6, 7, 4, 3],7), [1, 3, 5, 6, 7, 4]);
 console.log(trouble([4, 1, 1, 1, 4],2), [4, 1, 4]);
 console.log(trouble([2, 2, 2, 2, 2, 2], 4), [2]);
