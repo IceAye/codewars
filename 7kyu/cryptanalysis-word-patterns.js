ccfa4bf6fce9ee09a1b33bf89efdd5d768124568
Irene Volodko <ice7aye@gmail.com>
2025-05-09 15:50:29 +0300

    add solution: cryptanalysis word patterns

---
+++ 7kyu/cryptanalysis-word-patterns.js
@@ -0,0 +1,10 @@
+function wordPattern(word) {
+    word = word.toLowerCase();
+    let set = [...new Set(word)];
+    return [...word].map(char => set.indexOf(char)).join('.');
+}
+
+console.log(wordPattern("hello"), "0.1.2.2.3");
+console.log(wordPattern("heLlo"), "0.1.2.2.3");
+console.log(wordPattern("helLo"), "0.1.2.2.3");
+console.log(wordPattern("Hippopotomonstrosesquippedaliophobia"), "0.1.2.2.3.2.3.4.3.5.3.6.7.4.8.3.7.9.7.10.11.1.2.2.9.12.13.14.1.3.2.0.3.15.1.13");
