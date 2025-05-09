94f2d8c1a1cc096660022d29e5d1ca3fc7875c56
Irene Volodko <ice7aye@gmail.com>
2025-05-09 15:35:11 +0300

    description: stones on the table

--- 7kyu/stones-on-the-table.js
+++ 7kyu/stones-on-the-table.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// There are some stones on Bob's table in a row, and each of them can be red, green or blue, indicated by the characters R, G, and B.
+// Help Bob find the minimum number of stones he needs to remove from the table so that the stones in each pair of adjacent stones have different colors.
+//
+// ✅ SOLUTION:
 function solve(stones) {
     let result = 0;
 
@@ -7,6 +12,7 @@ function solve(stones) {
     return stones.length ? result : 0;
 }
 
+// 📌 TESTCASE:
 console.log( solve(""), 0 );
 console.log( solve("RRGGBB"), 3 );
 console.log( solve("RGBRGB"), 0 );
