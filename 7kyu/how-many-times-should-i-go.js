d04a56104c4967e02ea2009084cf223172620263
Irene Volodko <ice7aye@gmail.com>
2025-05-09 16:13:15 +0300

    add solution: how many times should I go

---
+++ 7kyu/how-many-times-should-i-go.js
@@ -0,0 +1,13 @@
+function howManyTimes(annualPrice, individualPrice) {
+    let result = 0;
+    let sum = 0;
+    while (sum < annualPrice) {
+        result++;
+        sum += individualPrice;
+    }
+    return individualPrice ? result : 0;
+}
+
+console.log(howManyTimes(40,15), 3)
+console.log(howManyTimes(30,10), 3)
+console.log(howManyTimes(80,15), 6)
