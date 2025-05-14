dbbb9933f5783efb07eb173a9c07abb251f6df8e
Irene Volodko <ice7aye@gmail.com>
2025-05-14 16:27:53 +0300

    description: product of the main diagonal of a square matrix

--- 7kyu/product-of-the-main-diagonal-of-a-square-matrix.js
+++ 7kyu/product-of-the-main-diagonal-of-a-square-matrix.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given a list of rows of a square matrix, find the product of the main diagonal.
+//
+// ✅ SOLUTION:
 function mainDiagonalProduct(mat) {
     let result = 1;
     for (let i = 0; i < mat.length; i++) {
@@ -6,5 +10,6 @@ function mainDiagonalProduct(mat) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(mainDiagonalProduct([[1,0],[0,1]]), 1);
 console.log(mainDiagonalProduct([[1,2,3],[4,5,6],[7,8,9]]), 45);
