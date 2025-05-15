04673ed4f5ab8aa9c2607582c0e55c8c665614c4
Irene Volodko <ice7aye@gmail.com>
2025-05-15 16:49:31 +0300

    add solution 2: the office 6 sabbatical

--- 7kyu/the-office-6-sabbatical.js
+++ 7kyu/the-office-6-sabbatical.js
@@ -7,6 +7,10 @@ function sabb(s, val, happiness){
     return result > 22 ? 'Sabbatical! Boom!' : 'Back to your desk, boy.';
 }
 
+function sabb(s, val, happiness){
+    return (s.match(/[sabticl]/gi) || []).length + val + happiness > 22 ? 'Sabbatical! Boom!' : 'Back to your desk, boy.';
+}
+
 console.log(sabb('Can I have a sabbatical?', 5, 5), 'Sabbatical! Boom!');
 console.log(sabb('Why are you shouting?', 7, 2), 'Back to your desk, boy.');
 console.log(sabb('What do you mean I cant learn to code??', 8, 9), 'Sabbatical! Boom!');
