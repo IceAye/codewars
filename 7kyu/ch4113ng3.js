8303d70f480f9d43a807521e8b876b444851e47e
Irene Volodko <ice7aye@gmail.com>
2025-05-15 17:57:51 +0300

    description: ch4113ng3

--- 7kyu/ch4113ng3.js
+++ 7kyu/ch4113ng3.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
+//
+// ✅ SOLUTION 1:
 function nerdify(txt) {
     let string = "";
     for (const char of txt) {
@@ -12,10 +16,12 @@ function nerdify(txt) {
     return string;
 }
 
+// ✅ SOLUTION 2:
 function nerdify(txt) {
     return txt.replace(/[aelAE]/g, char => ({'a': 4, 'e': 3, 'l':1 }[char.toLowerCase()]));
 }
 
+// 📌 TESTCASE:
 console.log(nerdify("Fund4m3nt41s"), "Fund4m3nt41s");
 console.log(nerdify("Seven"), "S3v3n");
 console.log(nerdify("Los Angeles"), "Los 4ng313s");
