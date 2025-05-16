a7b42700c9f8da524d9f5b3a13a16dca0b07577c
Irene Volodko <ice7aye@gmail.com>
2025-05-16 17:39:27 +0300

    add solution: kebabize

---
+++ 6kyu/kebabize.js
@@ -0,0 +1,13 @@
+function kebabize(str) {
+    return str[0].toLowerCase() + str.slice(1).replace(/[A-Z]/g, c =>  `-${c.toLowerCase()}`).replace(/[0-9]/g, "")
+}
+
+
+console.log(kebabize('MyCamelCasedString')) // 'my-camel-cased-string'
+console.log(kebabize('myCamelCasedString')) // 'my-camel-cased-string'
+console.log(kebabize('MyCamelHas3Humps')) // 'my-camel-has-humps'
+console.log(kebabize('myCamelHas3Humps')) // 'my-camel-has-humps'
+console.log(kebabize('CAMEL')) // 'c-a-m-e-l'
+console.log(kebabize('cAMEL')) // 'c-a-m-e-l'
+console.log(kebabize('AbstractSingletonProxyFactoryBean')) // 'abstract-singleton-proxy-factory-bean'
+console.log(kebabize('abstractSingletonProxyFactoryBean')) // 'abstract-singleton-proxy-factory-bean'
