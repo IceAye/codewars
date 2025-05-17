9ee8f1f8312d3c9b3a6971f484f6032a5cbfc185
Irene Volodko <ice7aye@gmail.com>
2025-05-17 23:02:10 +0300

    description: coprime validator

--- 7kyu/coprime-validator.js
+++ 7kyu/coprime-validator.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Write a program to determine if the two given numbers are coprime. A pair of numbers are coprime if their greatest shared factor is 1.
+// The inputs will always be two positive integers between 2 and 99.
+//
+// ✅ SOLUTION:
 function isCoprime(x, y) {
     let max = Math.max(x, y);
     for (let i = 2; i <= max; i++) {
@@ -6,5 +11,6 @@ function isCoprime(x, y) {
     return true;
 }
 
+// 📌 TESTCASE:
 console.log(isCoprime(20, 27), true)
 console.log(isCoprime(12, 39), false)
