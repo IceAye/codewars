8dbaa5abcd98418d23069544f5a4a64036cf039a
Irene Volodko <ice7aye@gmail.com>
2025-05-17 23:28:50 +0300

    description: the first non repeated character in a string

--- 7kyu/the-first-non-repeated-character-in-a-string.js
+++ 7kyu/the-first-non-repeated-character-in-a-string.js
@@ -1,7 +1,15 @@
+// ❓ DESCRIPTION:
+// You need to write a function, that returns the first non-repeated character in the given string.
+// If all the characters are unique, return the first character of the string.
+// If there is no unique character, return null in JS or Java, None in Python, '\0' in C.
+// You can assume, that the input string has always non-zero length.
+//
+// ✅ SOLUTION:
 function firstNonRepeated(s) {
     return [...s].find((item) => s.indexOf(item) === s.lastIndexOf(item)) || null;
 }
 
+// 📌 TESTCASE:
 console.log(firstNonRepeated("test"), 'e')
 console.log(firstNonRepeated("teeter"), 'r')
 console.log(firstNonRepeated("1122321235121222"), '5')
