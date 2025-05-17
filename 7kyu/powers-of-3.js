0d1cfe16cec1f691bef41ac1add7f834cc219b3f
Irene Volodko <ice7aye@gmail.com>
2025-05-17 23:19:50 +0300

    description: powers of 3

--- 7kyu/powers-of-3.js
+++ 7kyu/powers-of-3.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Given a positive integer N, return the largest integer k such that 3^k < N.
+//
+// For example,
+// largest_power(3) == 0
+// largest_power(4) == 1
+// You may assume that the input to your function is always a positive integer.
+//
+// ✅ SOLUTION:
 function largestPower(n){
     let i = 0;
     while (Math.pow(3, i) < n) {
@@ -6,6 +15,7 @@ function largestPower(n){
     return i - 1;
 }
 
+// 📌 TESTCASE:
 console.log(largestPower(3), 0)
 console.log(largestPower(5), 1)
 console.log(largestPower(7), 1)
