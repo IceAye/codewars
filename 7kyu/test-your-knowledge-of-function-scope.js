a876f7840b09b900c33c458e643a900c9b52faa1
Irene Volodko <ice7aye@gmail.com>
2025-05-18 17:09:00 +0300

    description: test your knowledge of function scope

--- 7kyu/test-your-knowledge-of-function-scope.js
+++ 7kyu/test-your-knowledge-of-function-scope.js
@@ -1,6 +1,15 @@
+// ❓ DESCRIPTION:
+// Write a function that adds from two invocations.
+//
+// All inputs will be integers.
+// add(3)(4)  // 7
+// add(12)(20) // 32
+//
+// ✅ SOLUTION:
 function add(number) {
     return (sum) => sum + number;
 }
 
+// 📌 TESTCASE:
 console.log(add(2)(5), 7)
 console.log(add(14)(25), 39)
