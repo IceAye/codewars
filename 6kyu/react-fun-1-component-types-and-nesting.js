433f37b67d07be0aaa7b85e97d148d1af47e7773
Irene Volodko <ice7aye@gmail.com>
2025-05-18 18:07:40 +0300

    description: react fun 1 component types and nesting

--- 6kyu/react-fun-1-component-types-and-nesting.js
+++ 6kyu/react-fun-1-component-types-and-nesting.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Create three components. Following the provided rules
+// One component should be named Hello and it should return 'Hello' wrapped inside of h1.
+// The second component should be named World and it should return 'World!' wrapped inside of h2.
+// Create a parent component using class syntax called Greet and nest previous two components inside of it, wrapping them with a div.
+// If you are stuck, refer to examples provided here. Good luck! :)
+//
+// ✅ SOLUTION:
 const React = require('react');
 
 const Hello = () => <h1>Hello</h1>;
