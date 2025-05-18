344d380dd02041e36781c222404789a60788e571
Irene Volodko <ice7aye@gmail.com>
2025-05-18 17:22:37 +0300

    description: lazily executing a function

--- 7kyu/lazily-executing-a-function.js
+++ 7kyu/lazily-executing-a-function.js
@@ -1,3 +1,22 @@
+// ❓ DESCRIPTION:
+// Deferring a function execution can sometimes save a lot of execution time in our programs by postponing the execution to the latest possible instant of time, when we're sure that the time spent while executing it is worth it.
+//
+// Write a method make_lazy that takes in a function (symbol for Ruby) and the arguments to the function and returns another function (lambda for Ruby) which when invoked, returns the result of the original function invoked with the supplied arguments.
+//
+// For example:
+// Given a function add
+// function add (a, b) {
+//   return a + b;
+// }
+// One could make it lazy as:
+// var lazy_value = make_lazy(add, 2, 3);
+// The expression does not get evaluated at the moment, but only when you invoke lazy_value as:
+// lazy_value() => 5
+// The above invokation then performs the sum.
+//
+// Please note: The functions that are passed to make_lazy may take one or more arguments and the number of arguments is not fixed.
+//
+// ✅ SOLUTION:
 function makeLazy (...args) {
     return () => args[0](...args.slice(1));
 }
@@ -6,7 +25,7 @@ function makeLazy (func, ...restArgs) {
     return () => func(...restArgs);
 }
 
-
+// 📌 TESTCASE:
 function add(a, b) { return a + b; }
 function double(n) { return n * 2; }
 function superAdd(...args) { return args.reduce((sum, x) => sum + x, 0); }
