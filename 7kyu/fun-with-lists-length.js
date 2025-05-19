96da3aa145627be40b4470469e1c1018f253a6a0
Irene Volodko <ice7aye@gmail.com>
2025-05-19 18:59:55 +0300

    add solution: fun with lists - length

---
+++ 7kyu/fun-with-lists-length.js
@@ -0,0 +1,11 @@
+function length(head) {
+    let count = 0;
+    let current = head;
+
+    while (current !== null) {
+        count++;
+        current = current.next;
+    }
+
+    return count;
+}
