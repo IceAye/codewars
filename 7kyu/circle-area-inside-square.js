567c6f9a01c28cb6b767ba7070414e2e1b97e4e4
Irene Volodko <ice7aye@gmail.com>
2025-05-19 18:39:02 +0300

    add solution: circle area inside square

---
+++ 7kyu/circle-area-inside-square.js
@@ -0,0 +1,6 @@
+function squareAreaToCircle(size){
+    return size * Math.PI / 4;
+}
+
+console.log(squareAreaToCircle(9)); // 7.0685834705770345
+console.log(squareAreaToCircle(20)); // 15.70796326794897
