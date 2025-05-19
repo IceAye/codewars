619b3fa078c58a77150f3675aaaab4892560772d
Irene Volodko <ice7aye@gmail.com>
2025-05-19 19:06:26 +0300

    description: all star code challenge 1

--- 7kyu/all-star-code-challenge-1.js
+++ 7kyu/all-star-code-challenge-1.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// This Kata is intended as a small challenge for my students
+// All Star Code Challenge #1
+// Write a function, called sumPPG, that takes two NBA player objects/struct/Hash/Dict/Class and sums their PPG
+//
+// ✅ SOLUTION:
 function sumPPG(playerOne, playerTwo){
     return playerOne.ppg + playerTwo.ppg;
 }
