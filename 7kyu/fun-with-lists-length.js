8ddb69ec42d51b0f1781fcf173d8e271a6069835
Irene Volodko <ice7aye@gmail.com>
2025-05-19 19:02:27 +0300

    description: fun with lists - length

--- 7kyu/fun-with-lists-length.js
+++ 7kyu/fun-with-lists-length.js
@@ -1,3 +1,17 @@
+// ❓ DESCRIPTION:
+// Implement the method length, which accepts a linked list (head), and returns the length of the list.
+//
+// For example: Given the list: 1 -> 2 -> 3 -> 4, length should return 4.
+//
+// The linked list is defined as follows:
+//
+// function Node(data, next = null) {
+//   this.data = data;
+//   this.next = next;
+// }
+// Note: the list may be null and can hold any type of value.
+//
+// ✅ SOLUTION 1:
 function length(head) {
     let count = 0;
     let current = head;
@@ -10,6 +24,7 @@ function length(head) {
     return count;
 }
 
+// ✅ SOLUTION 2:
 function length(head) {
     if (head === null) return 0;
     return 1 + length(head.next);
