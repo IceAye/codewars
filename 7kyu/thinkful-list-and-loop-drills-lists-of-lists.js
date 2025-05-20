5e22826ad918e79913d61627513ac774bfbe29ae
Irene Volodko <ice7aye@gmail.com>
2025-05-20 16:58:33 +0300

    description: list and loop drills

--- 7kyu/thinkful-list-and-loop-drills-lists-of-lists.js
+++ 7kyu/thinkful-list-and-loop-drills-lists-of-lists.js
@@ -1,6 +1,23 @@
+// ❓ DESCRIPTION:
+// You have a two-dimensional list in the following format:
+//
+// data = [[2, 5], [3, 4], [8, 7]]
+// Each sub-list contains two items, and each item in the sub-lists is an integer.
+//
+// Write a function process_data()/processData() that processes each sub-list like so:
+//
+// [2, 5] --> 2 - 5 --> -3
+// [3, 4] --> 3 - 4 --> -1
+// [8, 7] --> 8 - 7 --> 1
+// and then returns the product of all the processed sub-lists: -3 * -1 * 1 --> 3.
+//
+// For input, you can trust that neither the main list nor the sublists will be empty.
+//
+// ✅ SOLUTION:
 function processData(data){
     return data.reduce((acc, curr) => acc * (curr[0] - curr[1]), 1)
 }
 
+// 📌 TESTCASE:
 console.log(processData([[2, 5], [3, 4], [8, 7]])) // 3
 console.log(processData([[2, 9], [2, 4], [7, 5]])) // 28
