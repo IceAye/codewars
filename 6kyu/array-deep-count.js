1bf7ff959597e7614138d65a50cc26e5987ad340
Irene Volodko <ice7aye@gmail.com>
2025-05-20 18:00:06 +0300

    description: array deep count

--- 6kyu/array-deep-count.js
+++ 6kyu/array-deep-count.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+//
+// ✅ SOLUTION:
 function deepCount(a) {
     let count = a.length;
     for (const item of a) {
@@ -8,6 +12,7 @@ function deepCount(a) {
     return count;
 }
 
+// 📌 TESTCASE:
 console.log(deepCount([])) // 0
 console.log(deepCount([1, 2, 3])) // 3
 console.log(deepCount(["x", "y", ["z"]])) // 4
