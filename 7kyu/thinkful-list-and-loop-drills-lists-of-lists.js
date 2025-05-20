c5be46915a0b16bbed3c5985a533ad97f9eba0a1
Irene Volodko <ice7aye@gmail.com>
2025-05-20 16:57:46 +0300

    add solution: list and loop drills

---
+++ 7kyu/thinkful-list-and-loop-drills-lists-of-lists.js
@@ -0,0 +1,6 @@
+function processData(data){
+    return data.reduce((acc, curr) => acc * (curr[0] - curr[1]), 1)
+}
+
+console.log(processData([[2, 5], [3, 4], [8, 7]])) // 3
+console.log(processData([[2, 9], [2, 4], [7, 5]])) // 28
