e2c34321389d86b87da608b732d8049d305bdd99
Irene Volodko <ice7aye@gmail.com>
2025-05-21 16:35:13 +0300

    description: complete the pattern

--- 7kyu/complete-the-pattern-4.js
+++ 7kyu/complete-the-pattern-4.js
@@ -1,3 +1,26 @@
+// ❓ DESCRIPTION:
+// You have to write a function pattern which creates the following pattern upto n number of rows. If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+//
+// Examples:
+// pattern(4):
+//
+// 1234
+// 234
+// 34
+// 4
+// pattern(6):
+//
+// 123456
+// 23456
+// 3456
+// 456
+// 56
+// 6
+// Note: There are no blank spaces
+//
+// Hint: Use \n in string to jump to next line
+//
+// ✅ SOLUTION:
 function pattern(n) {
     let output = [];
     let i = 0;
@@ -12,6 +35,7 @@ function pattern(n) {
     return output.join("\n");
 }
 
+// 📌 TESTCASE:
 console.log(pattern(1)) // "1"
 console.log(pattern(2)) // "12\n2"
 console.log(pattern(5)) // "12345\n2345\n345\n45\n5"
