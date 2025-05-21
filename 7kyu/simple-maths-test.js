7aa8922f97334d28d24d84ff9efd18af610a19f9
Irene Volodko <ice7aye@gmail.com>
2025-05-21 16:12:08 +0300

    description: simple maths test

--- 7kyu/simple-maths-test.js
+++ 7kyu/simple-maths-test.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Create a function which checks a number for three different properties.
+//
+// is the number prime?
+// is the number even?
+// is the number a multiple of 10?
+// Each should return either true or false, which should be given as an array. Remark: The Haskell variant uses data Property.
+// The number will always be an integer, either positive or negative. Note that negative numbers cannot be primes, but they can be multiples of 10:
+//
+// ✅ SOLUTION:
 function numberProperty(n) {
     if (n === 2) return [ true,  true, false]
     if (n % 10 === 0) {
@@ -20,6 +30,7 @@ function numberProperty(n) {
     return [checkPrime(n), false, false];
 }
 
+// 📌 TESTCASE:
 console.log(numberProperty(  0), [false,  true,  true])
 console.log(numberProperty(  1), [false, false, false])
 console.log(numberProperty(  2), [ true,  true, false])
