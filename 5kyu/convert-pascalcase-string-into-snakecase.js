ce064ec3065371613e12ff91b3a65ce3d30e7556
Irene Volodko <ice7aye@gmail.com>
2025-05-21 17:13:35 +0300

    add solution: convert PascalCase string into snake_case

---
+++ 5kyu/convert-pascalcase-string-into-snakecase.js
@@ -0,0 +1,11 @@
+function toUnderscore(string) {
+    return string.toString().split(/(?=[A-Z])/).join('_').toLowerCase();
+}
+
+console.log(toUnderscore('A123aaa123AAA123')) // 'a123aaa123_a_a_a123'
+console.log(toUnderscore('AAA')) // 'a_a_a'
+console.log(toUnderscore('TestController')) // 'test_controller'
+console.log(toUnderscore('ThisIsBeautifulDay')) // 'this_is_beautiful_day'
+console.log(toUnderscore('Am7Days', 'am7_days')) // 'Am7Days should equals am7_days'
+console.log(toUnderscore('My3CodeIs4TimesBetter')) // 'my3_code_is4_times_better'
+console.log(toUnderscore(5)) // '5'
