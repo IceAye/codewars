c0ac6f51f6df4d0aed9db862fc696ab60448ecf7
Irene Volodko <ice7aye@gmail.com>
2025-05-23 16:54:13 +0300

    add solution 1: exclamation mark series 8

---
+++ 7kyu/exclamation-marks-series-8.js
@@ -0,0 +1,18 @@
+function remove(string) {
+    let count = 0
+    let array = string.split(" ");
+    for (let i = 0; i < array.length; i++) {
+        count += array[i].match(/!/g) ? array[i].match(/!/g).length : 0;
+        array[i] = array[i].replace(/!/g, "");
+    }
+    array[array.length - 1] += "!".repeat(count);
+    return array.join(" ");
+}
+
+
+console.log(remove("Hi!")) // "Hi!"
+console.log(remove("Hi! Hi!")) // "Hi Hi!!"
+console.log(remove("Hi! Hi! Hi!")) // "Hi Hi Hi!!!"
+console.log(remove("Hi! !Hi Hi!")) // "Hi Hi Hi!!!"
+console.log(remove("Hi! Hi!! Hi!")) // "Hi Hi Hi!!!!"
+
