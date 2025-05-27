2414d8e667b41b89f29617c01665170ef44761e5
Irene Volodko <ice7aye@gmail.com>
2025-05-27 17:24:37 +0300

    description: building a calculator

--- 7kyu/basic-js-building-a-calculator.js
+++ 7kyu/basic-js-building-a-calculator.js
@@ -1,3 +1,21 @@
+// ❓ DESCRIPTION:
+// Let's build a calculator.
+//
+// This is very basic Javascript kata.
+//
+// The test expects you to provide a Calculator object with the following methods:
+//
+// Calculator.add()
+// Calculator.subtract()
+// Calculator.multiply()
+// Calculator.divide()
+// Each method expects two arguments, so for instance:
+//
+// Calculator.add(1,4) should return 5.
+//
+// Only integers are expected to be passed in as arguments, and the divide method should return 'false' when trying the divide by zero.
+//
+// ✅ SOLUTION:
 const Calculator = {
     add(num1, num2) {
         return num1 + num2;
