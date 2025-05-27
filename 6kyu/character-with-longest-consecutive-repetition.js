5c06e12d3efc5dc511780dd28e94d6016119b0e3
Irene Volodko <ice7aye@gmail.com>
2025-05-27 16:00:25 +0300

    description: character with longest consecutive repetition

--- 6kyu/character-with-longest-consecutive-repetition.js
+++ 6kyu/character-with-longest-consecutive-repetition.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// For a given string s find the character c (or C) with longest consecutive repetition and return:
+//
+// [c, l]
+// where l (or L) is the length of the repetition. If there are two or more characters with the same l return the first in order of appearance.
+//
+// For empty string return:
+// ["", 0]
+// Happy coding! :)
+//
+// ✅ SOLUTION 1:
 function longestRepetition(s) {
     const max = ['', 0];
     let temp = 1;
@@ -18,6 +29,7 @@ function longestRepetition(s) {
     return max;
 }
 
+// ✅ SOLUTION 2:
 function longestRepetition(s) {
     const max = ['', 0];
     if (s.length) {
@@ -32,6 +44,7 @@ function longestRepetition(s) {
     return max;
 }
 
+// 📌 TESTCASE:
 console.log( longestRepetition("aaaabb"),      ["a",4] );
 console.log( longestRepetition("bbbaaabaaaa"), ["a",4] );
 console.log( longestRepetition("cbdeuuu900"),  ["u",3] );
@@ -39,5 +52,3 @@ console.log( longestRepetition("abbbbb"),      ["b",5] );
 console.log( longestRepetition("aabb"),        ["a",2] );
 console.log( longestRepetition(""),            ["",0] );
 console.log( longestRepetition("ba"),          ["b",1] );
-
-// https://www.codewars.com/kata/586d6cefbcc21eed7a001155/train/javascript
