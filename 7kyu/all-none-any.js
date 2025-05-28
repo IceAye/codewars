26efcf81e0eecb5b90d5627e54311d9173977008
Irene Volodko <ice7aye@gmail.com>
2025-05-28 17:29:16 +0300

    description: all, none and any

--- 7kyu/all-none-any.js
+++ 7kyu/all-none-any.js
@@ -1,3 +1,20 @@
+// ❓ DESCRIPTION:
+// As a part of this Kata, you need to create three functions that one needs to be able to call upon an array:
+// all
+//
+// This function returns true only if the predicate supplied returns true for all the items in the array [1, 2, 3].all(isGreaterThanZero) => true [-1, 0, 2].all(isGreaterThanZero) => false
+//
+// none
+//
+// This function returns true only if the predicate supplied returns false for all the items in the array [-1, 2, 3].none(isLessThanZero) => false [-1, -2, -3].none(isGreaterThanZero) => true
+//
+// any
+//
+// This function returns true if at least one of the items in the array returns true for the predicate supplied [-1, 2, 3].any(isGreaterThanZero) => true [-1, -2, -3].any(isGreaterThanZero) => false
+//
+// You do not need to worry about the data supplied, it will be an array at all times.
+//
+// ✅ SOLUTION:
 Array.prototype.all = function (p) {
     for (const item of this) {
         if (p(item) === false) return false;
@@ -19,6 +36,7 @@ Array.prototype.any = function (p) {
     return false;
 };
 
+// 📌 TESTCASE:
 function isGreaterThanZero(num) {
     return num > 0;
 }
