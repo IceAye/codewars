396e8e3c323bc5f3bacf537d5f42b2f894a4c0bd
Irene Volodko <ice7aye@gmail.com>
2025-05-29 17:46:24 +0300

    description: sum oof a nested list

--- 7kyu/sum-of-a-nested-list.js
+++ 7kyu/sum-of-a-nested-list.js
@@ -1,6 +1,13 @@
+// ❓ DESCRIPTION:
+// Implement a function to calculate the sum of the numerical values in a nested list. For example :
+//
+// sumNested([1, [2, [3, [4]]]]) => 10
+//
+// ✅ SOLUTION:
 const sumNested = arr => {
     return arr.flat(Infinity).reduce((acc, curr) => acc + curr, 0);
 };
 
+// 📌 TESTCASE:
 console.log(sumNested([1, [1], [[1]], [[[1]]]]), 4);
 console.log(sumNested([1, [1], [1, [1]], [1, [1], [1, [1]]]]), 8);
