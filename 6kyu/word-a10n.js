57988fd8e595a93856b2249c9e3cd6f7a720ca78
Irene Volodko <ice7aye@gmail.com>
2025-05-29 16:17:14 +0300

    add solution 2: word a10n

--- 6kyu/word-a10n.js
+++ 6kyu/word-a10n.js
@@ -22,3 +22,14 @@ function abbreviate(string) {
 
     return result;
 }
+
+function abbreviate(string) {
+    return string.replace(/[a-z]{4,}/gi, word => word[0] + (word.length - 2) + word[word.length - 1]);
+}
+
+console.log(abbreviate("internationalization"), "i18n");
+console.log(abbreviate('Go'), 'Go');
+console.log(abbreviate("elephant-rides are really fun!"), "e6t-r3s are r4y fun!");
+console.log(abbreviate(""), "");
+console.log(abbreviate(":;=;:"), ":;=;:");
+console.log(abbreviate("You need, need not want, to complete this code-wars mission"));
