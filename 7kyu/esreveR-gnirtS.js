8a2a7984f6b32e3f0934c246a66cdd3df167220b
Irene Volodko <ice7aye@gmail.com>
2025-05-29 16:36:46 +0300

    description: esreveR gnirtS

--- 7kyu/esreveR-gnirtS.js
+++ 7kyu/esreveR-gnirtS.js
@@ -1,7 +1,18 @@
+// ❓ DESCRIPTION:
+// Create a function called reverse for the String prototype that will allow the following functionality:
+// "String".reverse();// => returns "gnirtS"
+// "Super awesome string".reverse();// => returns "gnirts emosewa repuS"
+//
+// ✅ SOLUTION 1:
 String.prototype.reverse = function ()  {
     return [...this].reduceRight((acc, curr) => acc + curr, '');
 }
 
+// ✅ SOLUTION 2:
 String.prototype.reverse = function ()  {
     return [...this].reverse().join('');
 }
+
+// 📌 TESTCASE:
+console.log("String".reverse(), "gnirtS");
+console.log("Another string".reverse(), "gnirts rehtonA");
