3a8bad953254217ed4c1e001f2c19899f53bccf5
Irene Volodko <ice7aye@gmail.com>
2025-05-29 16:35:37 +0300

    add solution 2: esreveR gnirtS

--- 7kyu/esreveR-gnirtS.js
+++ 7kyu/esreveR-gnirtS.js
@@ -1,3 +1,7 @@
 String.prototype.reverse = function ()  {
     return [...this].reduceRight((acc, curr) => acc + curr, '');
 }
+
+String.prototype.reverse = function ()  {
+    return [...this].reverse().join('');
+}
