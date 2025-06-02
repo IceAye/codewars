235a6acc36be91a2b1cb39fd4499ce9ea195eff5
Irene Volodko <ice7aye@gmail.com>
2025-06-02 17:13:15 +0300

    add solution: recursion Fibonacci

---
+++ 7kyu/recursion-2-fibonacci.js
@@ -0,0 +1,9 @@
+const fibonacci = n => {
+    if (n <= 2) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+console.log(fibonacci(1), 1);
+console.log(fibonacci(2), 1);
+console.log(fibonacci(3), 2);
+console.log(fibonacci(4), 3);
