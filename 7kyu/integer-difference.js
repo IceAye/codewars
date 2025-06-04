ba3a7f7ab47fac6307901af72e87a77c30bae1f9
Irene Volodko <ice7aye@gmail.com>
2025-06-04 16:16:03 +0300

    description: integer difference

--- 7kyu/integer-difference.js
+++ 7kyu/integer-difference.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Write a function that accepts two arguments: an array/list of integers and another integer (n).
+//
+// Determine the number of times where two integers in the array have a difference of n.
+//
+// For example:
+// [1, 1, 5, 6, 9, 16, 27], n=4  -->  3  # (1,5), (1,5), (5,9)
+// [1, 1, 3, 3], n=2             -->  4  # (1,3), (1,3), (1,3), (1,3)
+//
+// ✅ SOLUTION:
 const intDiff = (arr, n) => {
     let count = 0;
 
@@ -12,6 +22,7 @@ const intDiff = (arr, n) => {
     return count;
 };
 
+// 📌 TESTCASE:
 console.log(intDiff([1, 1, 5, 6, 9, 16, 27], 4), 3);
 console.log(intDiff([1, 1, 3, 3], 2), 4);
 console.log(intDiff([4, 8, 12, 12, 3, 6, 2], 6), 3);
