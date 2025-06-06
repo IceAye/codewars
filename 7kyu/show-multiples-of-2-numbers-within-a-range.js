6860f08a2054a5994f5a336f13243b981a5fd719
Irene Volodko <ice7aye@gmail.com>
2025-06-06 21:51:50 +0300

    description: show multiples of 2 numbers within a range

--- 7kyu/show-multiples-of-2-numbers-within-a-range.js
+++ 7kyu/show-multiples-of-2-numbers-within-a-range.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// Print all numbers up to 3rd parameter which are multiple of both 1st and 2nd parameter.
+//
+// Python, Javascript, Java, Ruby versions: return results in a list/array
+//
+// NOTICE:
+//
+// Do NOT worry about checking zeros or negative values.
+// To find out if 3rd parameter (the upper limit) is inclusive or not, check the tests, it differs in each translation
+//
+// ✅ SOLUTION:
 function multiples(s1,s2,s3){
     let result = [];
     for (let i = 1; i < s3; i++) {
@@ -6,7 +17,7 @@ function multiples(s1,s2,s3){
     return result;
 }
 
-
+// 📌 TESTCASE:
 console.log(multiples(2,4, 40), [4, 8, 12, 16, 20, 24, 28, 32, 36]);
 console.log(multiples(13, 5, 800), [65, 130, 195, 260, 325, 390, 455, 520, 585, 650, 715, 780]);
 console.log(multiples( 13, 15, 800), [195, 390, 585, 780]);
