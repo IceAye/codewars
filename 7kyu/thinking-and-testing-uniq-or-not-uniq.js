aed8989a5b0a88ac03c66cfe6d5bfb44ae11fddf
Irene Volodko <ice7aye@gmail.com>
2025-06-06 21:24:29 +0300

    description: thinking and testing

--- 7kyu/thinking-and-testing-uniq-or-not-uniq.js
+++ 7kyu/thinking-and-testing-uniq-or-not-uniq.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// No Story
+// No Description
+// Only by Thinking and Testing
+// Look at result of testcase, guess the code!
+//
+// ✅ SOLUTION:
 function testit(a,b){
     return [...new Set(a), ...new Set(b)].sort((a, b) => a - b);
 }
