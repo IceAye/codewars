bfb192f7e0b8953cd4ee14fd9420d8b02af88848
Irene Volodko <ice7aye@gmail.com>
2025-06-07 17:39:59 +0300

    description: simple equation reversal

--- 7kyu/simple-equation-reversal.js
+++ 7kyu/simple-equation-reversal.js
@@ -1,7 +1,15 @@
+// ❓ DESCRIPTION:
+// Given a mathematical equation that has *,+,-,/, reverse it as follows:
+//
+// solve("100*b/y") = "y/b*100"
+// solve("a+b-c/d*30") = "30*d/c-b+a"
+//
+// ✅ SOLUTION:
 function solve(eq){
     return eq.split(/([*+\-/])/).reverse().join('');
 }
 
+// 📌 TESTCASE:
 console.log(solve("100*b/y"),"y/b*100");
 console.log(solve("a+b-c/d*30"),"30*d/c-b+a");
 console.log(solve("a*b/c+50"),"50+c/b*a");
