ed18daadb99653defc20f4a1ea956a5b2aabc6fe
Irene Volodko <ice7aye@gmail.com>
2025-06-08 20:16:26 +0300

    description: excel sheet column number

--- 7kyu/excel-sheet-column-numbers.js
+++ 7kyu/excel-sheet-column-numbers.js
@@ -1,7 +1,18 @@
+// ❓ DESCRIPTION:
+// Write a function that, given a column title as it appears in an Excel sheet, returns its corresponding column number.
+// All column titles will be uppercase.
+//
+// Examples:
+// Column title: "A" --> return 1
+// Column title: "Z" --> return 26
+// Column title: "AA" --> return 27
+//
+// ✅ SOLUTION:
 function titleToNumber(title) {
     return [...title].reduce((acc, curr, ind) => acc + (curr.charCodeAt() - 64) * Math.pow(26, title.length - 1 - ind), 0);
 }
 
+// 📌 TESTCASE:
 console.log(titleToNumber('A'),1);
 console.log(titleToNumber('Z'),26);
 console.log(titleToNumber('AA'),27);
