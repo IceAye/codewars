6f5c3e323a87a3f2db2e473795507ab3846704b1
Irene Volodko <ice7aye@gmail.com>
2025-06-09 14:01:49 +0300

    description: make me slow

--- 7kyu/make-me-slow.js
+++ 7kyu/make-me-slow.js
@@ -1,9 +1,14 @@
+// ❓ DESCRIPTION:
+// Make me slow! Calling makeMeSlow() should take at least 7 seconds.
+//
+// ✅ SOLUTION 1:
 function makeMeSlow() {
     const start = Date.now();
     while (Date.now() - start < 7000) {
     }
 }
 
+// ✅ SOLUTION 2:
 function makeMeSlow() {
     // this function is too fast!
     for (let x = 0; x < 5500000000; x++) {
