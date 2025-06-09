be115e9cfdd3536e055bb03937b23fdb84f8eec9
Irene Volodko <ice7aye@gmail.com>
2025-06-09 12:18:26 +0300

    description: ga-de-ty-po-lu-ki cypher

--- 7kyu/ga-de-ry-po-lu-ki-cypher.js
+++ 7kyu/ga-de-ry-po-lu-ki-cypher.js
@@ -1,3 +1,23 @@
+// ❓ DESCRIPTION:
+// The GADERYPOLUKI is a simple substitution cypher used in scouting to encrypt messages. The encryption is based on short, easy to remember key. The key is written as paired letters, which are in the cipher simple replacement.
+//
+// The most frequently used key is "GA-DE-RY-PO-LU-KI".
+//
+//  G => A
+//  g => a
+//  a => g
+//  A => G
+//  D => E
+//   etc.
+// The letters, which are not on the list of substitutes, stays in the encrypted text without changes.
+//
+// Task
+// Your task is to help scouts to encrypt and decrypt thier messages. Write the Encode and Decode functions.
+//
+// Input/Output
+// The input string consists of lowercase and uperrcase characters and white . The substitution has to be case-sensitive.
+//
+// ✅ SOLUTION:
 const encryptionString = 'gGaAdDeErRyYpPoOlLuUkKiI';
 const decryptionString = 'aAgGeEdDyYrRoOpPuUlLiIkK';
 
@@ -6,10 +26,10 @@ function encode(str) {
 }
 
 function decode(str) {
-    return str.replace(/\w/g, char => decryptionString[encryptionString.indexOf(char)] || char);;
+    return str.replace(/\w/g, char => decryptionString[encryptionString.indexOf(char)] || char);
 }
 
-
+// 📌 TESTCASE:
 console.log( encode("Ala has a cat") , "Gug hgs g cgt" );
 console.log( decode("Gug hgs g cgt") , "Ala has a cat" );
 console.log( encode("ABCD") , "GBCE" );
