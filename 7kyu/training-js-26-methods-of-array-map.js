ca0dfc7d463c3f5585d12fb6da5bd5d9e02246c0
Irene Volodko <ice7aye@gmail.com>
2025-06-09 13:28:26 +0300

    description: methods of array map

--- 7kyu/training-js-26-methods-of-array-map.js
+++ 7kyu/training-js-26-methods-of-array-map.js
@@ -1,7 +1,18 @@
+// ❓ DESCRIPTION:
+// Coding in function isolateIt. function accept 1 parameters arr, it's a string array. Your task is to put a character "|" into the middle of each element.
+//
+// If the string length is an even number, use the insert method. for example: "abcd" should became "ab|cd". "|" should be inserted between ab and cd.
+//
+// If the string length is an odd number, use the replacement method. for example: "abcde" should became "ab|de". Character c will be replaced by |.
+//
+// The original array should not be changed, you need to return a new array(if you use the map method, you do not need to worry about this).
+//
+// ✅ SOLUTION:
 function isolateIt(arr){
     return arr.map(item => item.slice(0, item.length / 2) + '|' + item.slice(-item.length / 2));
 }
 
+// 📌 TESTCASE:
 console.log(isolateIt(["abcd","efgh"]) , ["ab|cd","ef|gh"]);
 console.log(isolateIt(["abcde","fghij"]) , ["ab|de","fg|ij"]);
 console.log(isolateIt(["1234","56789"]) , ["12|34","56|89"]);
