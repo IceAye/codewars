9c4ab060501d0fb2eaaf16d304c3fd39bec0db99
Irene Volodko <ice7aye@gmail.com>
2025-06-10 07:27:09 +0300

    description: no loops 1 small enough

--- 7kyu/no-loops-1-small-enough.js
+++ 7kyu/no-loops-1-small-enough.js
@@ -1,7 +1,18 @@
+// ❓ DESCRIPTION:
+// *** No Loops Allowed ***
+//
+// You will be given an array (a) and a limit value (limit). You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+//
+// You can assume all values in the array are numbers.
+//
+// Do not use loops. Do not modify input array.
+//
+// ✅ SOLUTION:
 function smallEnough(a, limit){
     return a.every(i => i <= limit)
 }
 
+// 📌 TESTCASE:
 console.log(smallEnough([66, 101], 200), true);
 console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100), false);
 console.log(smallEnough([101, 45, 75, 105, 99, 107], 107), true);
