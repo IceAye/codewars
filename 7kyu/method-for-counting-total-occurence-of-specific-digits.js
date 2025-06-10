8afa2f7c5c95d01d27c24816e897f74e650512e2
Irene Volodko <ice7aye@gmail.com>
2025-06-10 07:50:19 +0300

    description: method for counting total occurrence of specific digits

--- 7kyu/method-for-counting-total-occurence-of-specific-digits.js
+++ 7kyu/method-for-counting-total-occurence-of-specific-digits.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// We need a method in the List Class that may count specific digits from a given list of integers. This marked digits will be given in a second list. The method .count_spec_digits()/.countSpecDigits() will accept two arguments, a list of an uncertain amount of integers integers_lists/integersLists (and of an uncertain amount of digits, too) and a second list, digits_list/digitsList that has the specific digits to count which length cannot be be longer than 10 (It's obvious, we've got ten digits). The method will output a list of tuples, each tuple having two elements, the first one will be a digit to count, and second one, its corresponding total frequency in all the integers of the first list. This list of tuples should be ordered with the same order that the digits have in digitsList
+//
+// ✅ SOLUTION 1:
 function List(){
     this.countSpecDigits = function(integersList, digitsList) {
         const freqMap = new Map();
@@ -17,12 +21,14 @@ function List(){
     }
 }
 
+// ✅ SOLUTION 2:
 function List(){
     this.countSpecDigits = function(integersList, digitsList) {
         return digitsList.map(digit => [digit, integersList.toString().split(digit).length - 1]);
     }
 }
 
+// 📌 TESTCASE:
 let l = new List();
 
 console.log(l.countSpecDigits([1, 1, 2, 3, 1, 2, 3, 4], [1, 3]),[[1, 3], [3, 2]]);
