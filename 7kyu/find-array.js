255146e7d093a88479e2642506a98dffa239f1e2
Irene Volodko <ice7aye@gmail.com>
2025-06-10 08:03:30 +0300

    add solution: find array

---
+++ 7kyu/find-array.js
@@ -0,0 +1,13 @@
+function findArray(arr1, arr2){
+    return arr1.length ? arr2.map((ind) => arr1[ind]) : [];
+}
+
+console.log(findArray(['a', 'a', 'a', 'a', 'a'], [2, 4]), ['a', 'a'])
+console.log(findArray([0, 1, 5, 2, 1, 8, 9, 1, 5], [1, 4, 7]), [1, 1, 1])
+console.log(findArray([1, 2, 3, 4, 5], [0]), [1])
+console.log(findArray([1, 2, 3, 4, 5], [4,2,0]), [5,3,1])
+console.log(findArray([1, 2, 3, 4, 5], [2,2,2]), [3,3,3])
+console.log(findArray(['this', 'is', 'test'], [0, 1, 2]), ['this', 'is', 'test'])
+console.log(findArray([1,2,3], []), [])
+console.log(findArray([], [2,1,3]), [])
+console.log(findArray([], []), [])
