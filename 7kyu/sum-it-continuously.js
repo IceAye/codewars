8824457300aaf08f0b0dbc53345285aebe191963
Irene Volodko <ice7aye@gmail.com>
2025-06-11 07:19:33 +0300

    description: sum it continuously

--- 7kyu/sum-it-continuously.js
+++ 7kyu/sum-it-continuously.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Make a function "add" that will be able to sum elements of list continuously and return a new list of sums.
+//
+// For example:
+// add [1,2,3,4,5] == [1, 3, 6, 10, 15], because it's calculated like
+// this : [1, 1 + 2, 1 + 2 + 3, 1 + 2 + 3 + 4, 1 + 2 + 3 + 4 + 5]
+//
+// ✅ SOLUTION:
 function add(arr) {
     let result = [];
 
@@ -8,6 +16,7 @@ function add(arr) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(add([0])); // [0]))
 console.log(add([-33])); // [-33]))
 console.log(add([32, -9, 2, 0])); // [32, 23, 25, 25]))
