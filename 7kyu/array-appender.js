63268f5527272e27e8b9e77251cea9684b5a0f15
Irene Volodko <ice7aye@gmail.com>
2025-06-12 17:00:18 +0300

    add solution: array appender

---
+++ 7kyu/array-appender.js
@@ -0,0 +1,10 @@
+function appendArrays (arr1, arr2) {
+    return [...arr1, ...arr2];
+}
+
+console.log(appendArrays([1,2], [2,4]), [1,2,2,4])
+console.log(appendArrays([1,2],[3,4]), [1,2,3,4])
+console.log(appendArrays(['this'],['that']), ['this','that'])
+console.log(appendArrays(['a','B'],['c','D']), ['a','B','c','D'])
+console.log(appendArrays([1,2], [1]), [1,2,1])
+console.log(appendArrays([['x','x'],'B'], ['c','D']), [['x','x'],'B','c','D'])
