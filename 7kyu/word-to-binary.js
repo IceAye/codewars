ec4058cf85e34683ab628a199ca669f308b9b2fd
Irene Volodko <ice7aye@gmail.com>
2025-06-12 16:32:43 +0300

    add solution: word to binary

--- 7kyu/word-to-binary.js
+++ 7kyu/word-to-binary.js
@@ -1,5 +1,6 @@
-function wordToBin(str){
-    return [...str].map(char => char.charCodeAt().toString(2).padStart(8, '0'));
+function wordToBin(str) {
+    return [...str].map(char => char.charCodeAt().toString(2).padStart(8 , '0'));
+}
 
 console.log(wordToBin('man')) //  [ '01101101', '01100001', '01101110' ]
 console.log(wordToBin('AB')) //  ['01000001', '01000010']
