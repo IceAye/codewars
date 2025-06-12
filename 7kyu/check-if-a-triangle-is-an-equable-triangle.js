2cc7e111bea0dccdf2cea9b4e40d3d904df609e8
Irene Volodko <ice7aye@gmail.com>
2025-06-12 16:44:17 +0300

    description: check if a triangle is an equable triangle

--- 7kyu/check-if-a-triangle-is-an-equable-triangle.js
+++ 7kyu/check-if-a-triangle-is-an-equable-triangle.js
@@ -1,10 +1,14 @@
+// ❓ DESCRIPTION:
+// A triangle is called an equable triangle if its area equals its perimeter. Return true, if it is an equable triangle, else return false. You will be provided with the length of sides of the triangle. Happy Coding!
+//
+// ✅ SOLUTION:
 function equableTriangle(a,b,c) {
     const perimeter = a + b + c;
     const halfPer = perimeter / 2;
     return perimeter === Math.sqrt(halfPer * (halfPer - a) * (halfPer - b) * (halfPer - c));
 }
 
-
+// 📌 TESTCASE:
 console.log(equableTriangle(5,12,13),true);
 console.log(equableTriangle(2,3,4),false);
 console.log(equableTriangle(6,8,10),true);
