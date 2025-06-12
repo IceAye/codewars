876bf41636285de4a93c5f687b62a97f5884f4ad
Irene Volodko <ice7aye@gmail.com>
2025-06-12 16:33:35 +0300

    description: word to binary

--- 7kyu/word-to-binary.js
+++ 7kyu/word-to-binary.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Write a function that takes a string and returns an array containing binary numbers equivalent to the ASCII codes of the characters of the string. The binary strings should be eight digits long.
+//
+// ✅ SOLUTION:
 function wordToBin(str) {
     return [...str].map(char => char.charCodeAt().toString(2).padStart(8 , '0'));
 }
 
+// 📌 TESTCASE:
 console.log(wordToBin('man')) //  [ '01101101', '01100001', '01101110' ]
 console.log(wordToBin('AB')) //  ['01000001', '01000010']
 console.log(wordToBin('wecking')) // [ '01110111', '01100101', '01100011', '01101011', '01101001', '01101110', '01100111' ]
