ded8091bcad1093f87a9befa9cd002f7424faf02
Irene Volodko <ice7aye@gmail.com>
2025-06-13 13:22:59 +0300

    add solution 2: identical elements

--- 7kyu/identical-elements.js
+++ 7kyu/identical-elements.js
@@ -2,6 +2,9 @@ function duplicateElements(m, n) {
     return m.filter(num => n.includes(num)).length > 0;
 }
 
+function duplicateElements(m, n) {
+    return m.some(num => n.includes(num));
+}
 
 console.log(duplicateElements([1, 2, 3, 4, 5], [1, 6, 7, 8, 9]), true);
 console.log(duplicateElements([9, 8, 7], [8, 1, 3]), true);
