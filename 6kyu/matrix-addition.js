cb86e7c795d1a919c1e7fcd1252302da7a913acc
Irene Volodko <ice7aye@gmail.com>
2025-06-16 10:22:39 +0300

    description: matrix addition

--- 6kyu/matrix-addition.js
+++ 6kyu/matrix-addition.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Write a function that accepts two square matrices (N x N two dimensional arrays), and return the sum of the two. Both matrices being passed into the function will be of size N x N (square), containing only integers.
+//
+// How to sum two matrices:
+//
+// Take each cell [n][m] from the first matrix, and add it with the same [n][m] cell from the second matrix. This will be cell [n][m] of the solution matrix. (Except for C where solution matrix will be a 1d pseudo-multidimensional array).
+//
+// ✅ SOLUTION:
 function matrixAddition(a, b){
     const result = [];
     for (let n = 0; n < a.length; n++) {
@@ -9,6 +17,7 @@ function matrixAddition(a, b){
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(matrixAddition(
         [ [1, 2],
             [1, 2] ],
