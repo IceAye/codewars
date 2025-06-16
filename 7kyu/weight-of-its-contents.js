9f12a32de611078e422d946da95e439c2276425b
Irene Volodko <ice7aye@gmail.com>
2025-06-16 10:02:21 +0300

    description: weight of its contents

--- 7kyu/weight-of-its-contents.js
+++ 7kyu/weight-of-its-contents.js
@@ -1,7 +1,18 @@
+// ❓ DESCRIPTION:
+// Welcome to the Mathematics gameshow. I'm your host, Apex Rhombus, and it's time for the lightning round!
+// Today we'll talk about a hypothetical bottle. This entire bottle weighs 120 grams. Its contents weigh twice as much as the bottle itself. What, may I ask, do the contents weigh?
+// ...Did you guess 80 grams? Correct! Now that you've got that idea, I'm gonna ask you that question in many different ways so you'd better get ready!
+// Let's complete the function that takes in two parameters: bottle weight and scale. This function will return the weight of the contents inside the bottle.
+// bottle weight will be an integer representing the weight of the entire bottle (contents included).
+// scale will be a string that you will need to parse. It will tell you how the content weight compares to the weight of the bottle by itself. 2 times larger, 6 times larger, and 15 times smaller would all be valid strings (smaller and larger are the only comparison words).
+// It is guaranteed that the result will always be an integer. Good luck!
+//
+// ✅ SOLUTION:
 function contentWeight(bottleWeight, scale) {
     const params = scale.split(" ");
     const waterScale = parseInt(params[0]);
     return params[2] === "larger" ? waterScale * bottleWeight / (waterScale + 1) : bottleWeight / (waterScale + 1);
 }
 
+// 📌 TESTCASE:
 console.log(contentWeight(120, "2 times larger"), 80);
