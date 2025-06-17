e2b95724703308d9739126bd86a5d4ccf7e188de
Irene Volodko <ice7aye@gmail.com>
2025-06-17 16:14:11 +0300

    description: handshake problem

--- 6kyu/handshake-problem.js
+++ 6kyu/handshake-problem.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Johnny is a farmer and he annually holds a beet farmers convention "Drop the beet".
+// Every year he takes photos of farmers handshaking. Johnny knows that no two farmers handshake more than once. He also knows that some of the possible handshake combinations may not happen.
+// However, Johnny would like to know the minimal amount of people that participated this year just by counting all the handshakes.
+// Help Johnny by writing a function, that takes the amount of handshakes and returns the minimal amount of people needed to perform these handshakes (a pair of farmers handshake only once).
+//
+// ✅ SOLUTION 1:
 function getParticipants(handshakes) {
     if (handshakes === 0) return 0;
     if (handshakes === 1) return 2;
@@ -16,6 +23,7 @@ function getParticipants(handshakes) {
     return n;
 }
 
+// ✅ SOLUTION 2:
 function getParticipants(handshakes) {
     let n = 0;
 
@@ -26,6 +34,7 @@ function getParticipants(handshakes) {
     return n;
 }
 
+// 📌 TESTCASE:
 console.log(getParticipants(0), 0);
 console.log(getParticipants(1), 2);
 console.log(getParticipants(3), 3);
