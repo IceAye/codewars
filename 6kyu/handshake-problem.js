00e14a360022c5b833bd6f9cc88ace788050d758
Irene Volodko <ice7aye@gmail.com>
2025-06-17 16:11:48 +0300

    add solution: handshake problem

---
+++ 6kyu/handshake-problem.js
@@ -0,0 +1,23 @@
+function getParticipants(handshakes) {
+    if (handshakes === 0) return 0;
+    if (handshakes === 1) return 2;
+
+    let n = 3;
+
+    function factorial(number) {
+        if (number === 1) return 1;
+        return number * factorial(number - 1);
+    }
+
+    while (handshakes > factorial(n) / (factorial(n - 2) * 2)) {
+        n++;
+    }
+
+    return n;
+}
+
+console.log(getParticipants(0), 0);
+console.log(getParticipants(1), 2);
+console.log(getParticipants(3), 3);
+console.log(getParticipants(6), 4);
+console.log(getParticipants(7), 5);
