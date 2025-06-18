1dbea01af39ddc7dd53f4bd381204100f7d403d3
Irene Volodko <ice7aye@gmail.com>
2025-06-18 13:06:03 +0300

    description: cyclops numbers

--- 7kyu/cyclops-numbers.js
+++ 7kyu/cyclops-numbers.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// A cyclops number is a number in binary that is made up of all 1's, with one 0 in the exact middle. That means all cyclops numbers must have an odd number of digits for there to be an exact middle.
+// A couple examples:
+// 101
+// 11111111011111111
+// You must take an input, n, that will be in decimal format (base 10), then return True if that number wil be a cyclops number when converted to binary, or False if it won't.
+// Assume n will be a positive integer.
+//
+// ✅ SOLUTION 1:
 function cyclops(n) {
     const binary = n.toString(2);
     const isOddLength = binary.length % 2 === 1;
@@ -5,11 +14,13 @@ function cyclops(n) {
     return isOddLength  && isZeroCenter && binary.match(/0/g).length === 1;
 }
 
+// ✅ SOLUTION 2:
 function cyclops(n) {
     const binary = n.toString(2).split('0');
     return binary.length === 2 && binary[0] === binary[1];
 }
 
+// 📌 TESTCASE:
 console.log(cyclops(1), false);
 console.log(cyclops(5), true);
 console.log(cyclops(3), false);
