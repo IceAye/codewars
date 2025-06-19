648a55d78873e8253cf4e3a215c7e8c1c47391ac
Irene Volodko <ice7aye@gmail.com>
2025-06-19 15:13:00 +0300

    description: compound array

--- 7kyu/compound-array.js
+++ 7kyu/compound-array.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You have to create a method "compoundArray" which should take as input two int arrays of different length and return one int array with numbers of both arrays shuffled one by one.
+//
+// Input - {1,2,3,4,5,6} and {9,8,7,6}
+// Output - {1,9,2,8,3,7,4,6,5,6}
+//
+// ✅ SOLUTION:
 function compoundArray(a, b) {
     const result = [];
     let i = 0;
@@ -16,6 +23,7 @@ function compoundArray(a, b) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(compoundArray([11, 12], [21, 22, 23, 24]), [11, 21, 12, 22, 23, 24]);
 console.log(compoundArray([2147483647,2147483646,2147483645,2147483644,2147483643], [9]), [2147483647,9,2147483646,2147483645,2147483644,2147483643]);
 console.log(compoundArray([214,215,216,217,218], []), [214,215,216,217,218]);
