4e75ae080bc15068e59049351f8fcac76839cc60
Irene Volodko <ice7aye@gmail.com>
2025-06-19 14:48:03 +0300

    description: something capitalized

--- 7kyu/thinking-and-testing-something-capitalized.js
+++ 7kyu/thinking-and-testing-something-capitalized.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// No Story
+// No Description
+// Only by Thinking and Testing
+// Look at result of testcase, guess the code!
+//
+// ✅ SOLUTION:
 function testit(s){
     return s.replace(/\w\b/g, c => c.toUpperCase());
 }
