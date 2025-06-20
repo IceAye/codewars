e60fcb69a85f0804b3ffec3e58476902451e5f71
Irene Volodko <ice7aye@gmail.com>
2025-06-20 08:27:38 +0300

    description: sum all the arrays

--- 7kyu/sum-all-the-arrays.js
+++ 7kyu/sum-all-the-arrays.js
@@ -1,7 +1,14 @@
+// ❓ DESCRIPTION:
+// You are given an array of values.
+// Sum every number value in the array, and any nested arrays (to any depth).
+// Ignore all other types of values.
+//
+// ✅ SOLUTION:
 function arraySum(arr) {
     return arr.flat(Infinity).reduce((acc, curr) => acc + (typeof curr === 'number' ? curr : 0), 0);
 }
 
+// 📌 TESTCASE:
 console.log(arraySum([1, 2]), 3);
 console.log(arraySum([1, 2, 3]), 6);
 console.log(arraySum([1, 2, [1, 2]]), 6);
