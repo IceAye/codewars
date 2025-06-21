1cc2408c58af9be64fa0f657a4610d9d44999627
Irene Volodko <ice7aye@gmail.com>
2025-06-21 13:13:18 +0300

    description: extract nested object references

--- 6kyu/extract-nested-object-reference.js
+++ 6kyu/extract-nested-object-reference.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// You are given a complex object that has many deeply nested variables. You don't want to go the usual if obj.property == null route. Create a prototype method that given a nested path, either return the value or undefined.
+//
+// ✅ SOLUTION:
 Object.prototype.hash = function(string) {
     return string.split('.').reduce((obj, key) => obj ? obj[key] : obj, this)
 }
 
+// 📌 TESTCASE:
 const obj = {
     person: {
         name: 'joe',
