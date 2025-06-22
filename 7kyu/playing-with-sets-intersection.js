e78fe1047b47ea4ae2deb93b98d999a167f81307
Irene Volodko <ice7aye@gmail.com>
2025-06-22 15:14:04 +0300

    description: playing with sets - intersection

--- 7kyu/playing-with-sets-intersection.js
+++ 7kyu/playing-with-sets-intersection.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Create function inter getting 2 sets as arguments and returning a new Set as result of intersection of these 2 sets.
+//
+// ✅ SOLUTION 1:
 function inter(s1, s2) {
     let set = new Set();
     for (const key of s1) {
@@ -6,8 +10,10 @@ function inter(s1, s2) {
     return set;
 }
 
+// ✅ SOLUTION 2:
 function inter(s1, s2) {
     return new Set([...s1].filter(el => s2.has(el)));
 }
 
+// 📌 TESTCASE:
 console.log(inter(new Set([1, 2]), new Set([2, 3]))); // [2]
