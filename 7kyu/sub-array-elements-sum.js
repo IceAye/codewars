c4d7e47095f33a8af4276c047b19290b0451aa03
Irene Volodko <ice7aye@gmail.com>
2025-06-22 16:17:35 +0300

    add solution 2: subarray elements sum

--- 7kyu/sub-array-elements-sum.js
+++ 7kyu/sub-array-elements-sum.js
@@ -10,6 +10,14 @@ function elementsSum(arr, d = 0) {
     return result;
 }
 
+function elementsSum(arr, d = 0) {
+    return arr.reduce((acc, curr, ind) => {
+        const num = curr[arr.length - 1 - ind];
+        acc += num === undefined ? d : num;
+        return acc;
+    }, 0);
+}
+
 console.log(elementsSum([[3, 2, 1, 0], [4, 6, 5, 3, 2], [9, 8, 7, 4]]), 16);
 console.log(elementsSum([[3], [4, 6, 5, 3, 2], [9, 8, 7, 4]]), 15);
 console.log(elementsSum([[3, 2, 1, 0], [4, 6, 5, 3, 2], []]), 7);
