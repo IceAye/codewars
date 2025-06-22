40831dfe6b40c59cab4a0d92c79c69021e61fe9a
Irene Volodko <ice7aye@gmail.com>
2025-06-22 14:18:19 +0300

    description: survive the attack

--- 7kyu/survive-the-attack.js
+++ 7kyu/survive-the-attack.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Given two Arrays in which values are the power of each soldier, return true if you survive the attack or false if you perish.
+//
+// CONDITIONS
+// Each soldier attacks the opposing soldier in the same index of the array. The survivor is the number with the highest value.
+// If the value is the same they both perish
+// If one of the values is empty(different array lengths) the non-empty value soldier survives.
+// To survive the defending side must have more survivors than the attacking side.
+// In case there are the same number of survivors in both sides, the winner is the team with the highest initial attack power. If the total attack power of both sides is the same return true.
+// The initial attack power is the sum of all the values in each array.
+//
+// ✅ SOLUTION:
 function hasSurvived(attackers, defenders) {
     let attackPower = 0;
     let defenderPower = 0;
@@ -23,7 +35,7 @@ function hasSurvived(attackers, defenders) {
     return attackSurvivors < defenderSurvivors;
 }
 
-
+// 📌 TESTCASE:
 console.log(hasSurvived([2, 9, 9, 7], [1, 1, 3, 8]), false);
 console.log(hasSurvived([1, 3, 5, 7], [2, 4, 6, 8]), true);
 console.log(hasSurvived([10, 10, 1, 1], [4, 4, 7, 7]), true);
