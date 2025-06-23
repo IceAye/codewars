b7ad76f3dadd31d99f80650fd999454ae2a0a3bb
Irene Volodko <ice7aye@gmail.com>
2025-06-23 12:46:21 +0300

    description: Congo warehouses

--- 7kyu/thinkful-number-drills-congo-warehouses.js
+++ 7kyu/thinkful-number-drills-congo-warehouses.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Your company, Congo Pizza, is the second-largest online frozen pizza retailer. You own a number of international warehouses that you use to store your frozen pizzas, and you need to figure out how many crates of pizzas you can store at each location.
+// Congo recently standardized its storage containers: all pizzas fit inside a cubic crate, 16-inches on a side. The crates are super tough so you can stack them as high as you want.
+// Write a function box_capacity() that figures out how many crates you can store in a given warehouse. The function should take three arguments: the length, width, and height of your warehouse (in feet) and should return an integer representing the number of boxes you can store in that space.
+//
+// For example: a warehouse 32 feet long, 64 feet wide, and 16 feet high can hold 13,824 boxes because you can fit 24 boxes across, 48 boxes deep, and 12 boxes high, so box_capacity(32, 64, 16) should return 13824.
+//
+// ✅ SOLUTION:
 function boxCapacity(length, width, height) {
     const long = Math.floor(length * 12 / 16);
     const wide = Math.floor(width * 12 / 16)
@@ -5,6 +13,7 @@ function boxCapacity(length, width, height) {
     return long * wide * high;
 }
 
+// 📌 TESTCASE:
 console.log(boxCapacity(32, 64, 16), 13824);
 console.log(boxCapacity(20, 20, 20), 3375);
 console.log(boxCapacity(80, 40, 20), 27000);
