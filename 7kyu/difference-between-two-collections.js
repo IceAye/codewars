af15fcd0d13b92c34a96b9545557390da5aaa4fe
Irene Volodko <ice7aye@gmail.com>
2025-06-25 15:57:19 +0300

    description: difference between two

--- 7kyu/difference-between-two-collections.js
+++ 7kyu/difference-between-two-collections.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Find the difference between two collections. The difference means that either the character is present in one collection or it is present in other, but not in both. Return a sorted list with the difference.
+//
+// The collections can contain any character and can contain duplicates.
+//
+// ✅ SOLUTION 1:
 function diff(a, b) {
     let setA = new Set(a);
     let setB = new Set(b);
@@ -12,10 +18,12 @@ function diff(a, b) {
     return result.sort();
 }
 
+// ✅ SOLUTION 2:
 function diff(a, b) {
     return [...new Set(a), ...new Set(b)].filter(item => a.includes(item) ^ b.includes(item)).sort();
 }
 
+// 📌 TESTCASE:
 console.log(diff(["a","b"], ["a","b"]), []);
 console.log(diff(["a","b"], []), ["a","b"]);
 console.log(diff([], ["a","b"]), ["a","b"]);
