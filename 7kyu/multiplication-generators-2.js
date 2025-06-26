5cd377a4c866118bc30a3cccdb6885bb4a71d3cc
Irene Volodko <ice7aye@gmail.com>
2025-06-26 15:56:01 +0300

    description: multiplication generators

--- 7kyu/multiplication-generators-2.js
+++ 7kyu/multiplication-generators-2.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Generators can be used to wonderful things. You can use them for numerous things, but here is one specific example. You are studying for a test so you must practice your multiplication, but you don't have a multiplication table showing the different examples. You have decided to create a generator that prints out a limitless list of time tables.
+// Task
+// Your generator must take one parameter `a` then everytime the generator is called you must return a string in the format of: `'a x b = c'` where c is the answer. Also, the value of `b`, which starts at 1, must increment by 1 each time!
+//
+// ✅ SOLUTION:
 function* generator(a) {
     let i = 1;
     while (true) {
@@ -5,6 +11,7 @@ function* generator(a) {
     }
 }
 
+// 📌 TESTCASE:
 let gen = generator(1);
 
 console.log(gen.next().value, '1 x 1 = 1', '1 x 1 = 1');
