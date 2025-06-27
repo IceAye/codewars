d22b81fea61c3cd76ba8fc3b6f18470be8123391
Irene Volodko <ice7aye@gmail.com>
2025-06-27 17:31:12 +0300

    description: sum of n elements

--- 7kyu/arithmetic-sequence-sum-of-n-elements.js
+++ 7kyu/arithmetic-sequence-sum-of-n-elements.js
@@ -1,3 +1,17 @@
+// ❓ DESCRIPTION:
+// In your class, you have started lessons about "arithmetic progression". Because you are also a programmer, you have decided to write a function.
+//
+// This function, arithmetic_sequence_sum(a, r, n), should return the sum of the first (n) elements of a sequence in which each element is the sum of the given integer (a), and a number of occurences of the given integer (r), based on the element's position within the sequence.
+//
+// For example:
+//
+// arithmetic_sequence_sum(2, 3, 5) should return 40:
+//
+// 1     2        3          4            5
+// a + (a+r) + (a+r+r) + (a+r+r+r) + (a+r+r+r+r)
+// 2 + (2+3) + (2+3+3) + (2+3+3+3) + (2+3+3+3+3) = 40
+//
+// ✅ SOLUTION:
 function ArithmeticSequenceSum(a, r, n) {
     let result = a;
     let step = a;
@@ -11,6 +25,7 @@ function ArithmeticSequenceSum(a, r, n) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(ArithmeticSequenceSum(3, 2, 20), 440);
 console.log(ArithmeticSequenceSum(2, 2, 10), 110);
 console.log(ArithmeticSequenceSum(1, -2, 10), -80);
