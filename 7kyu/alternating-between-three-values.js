731723aa8edbb3a5c0fa579be893d2ef6c9c2f8a
Irene Volodko <ice7aye@gmail.com>
2025-06-28 15:04:16 +0300

    description: alternating between three values

--- 7kyu/alternating-between-three-values.js
+++ 7kyu/alternating-between-three-values.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Suppose a variable x can have only three possible different values a, b and c, and you wish to assign to x the value other than its current one, and you wish your code to be independent of the values of a, b and c.
+//
+//     What is the most efficient way to cycle among three values? Write a function f so that it satisfies
+//
+// f(a) = b
+// f(b) = c
+// f(c) = a
+// EXAMPLE
+// f( 3, { a:3, b:4, c:5 } ) => 4
+//
+// ✅ SOLUTION:
 function f(x, cc) {
     switch (x) {
         case cc.a: return cc.b;
@@ -6,6 +18,7 @@ function f(x, cc) {
     }
 }
 
+// 📌 TESTCASE:
 console.log(f(3, { a: 3, b: 4, c: 5 }), 4);
 console.log(f(4, { a: 3, b: 4, c: 5 }), 5);
 console.log(f(5, { a: 3, b: 4, c: 5 }), 3);
