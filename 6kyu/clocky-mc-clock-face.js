d070db4ec1f06744b16681f2b6fe96ff81a16a4d
Irene Volodko <ice7aye@gmail.com>
2025-06-28 15:32:12 +0300

    description: clocky mc clock face

--- 6kyu/clocky-mc-clock-face.js
+++ 6kyu/clocky-mc-clock-face.js
@@ -1,3 +1,28 @@
+// ❓ DESCRIPTION:
+// Due to lack of maintenance the minute-hand has fallen off Town Hall clock face.
+// And because the local council has lost most of our tax money to an elaborate email scam there are no funds to fix the clock properly.
+// Instead, they are asking for volunteer programmers to write some code that tell the time by only looking at the remaining hour-hand!
+// What a bunch of cheapskates!
+// Can you do it?
+//
+// Kata
+// Given the angle (in degrees) of the hour-hand, return the time in 12 hour HH:MM format. Round down to the nearest minute.
+//
+// Examples
+// 12:00 = 0 degrees
+// 03:00 = 90 degrees
+// 06:00 = 180 degrees
+// 09:00 = 270 degrees
+// 12:00 = 360 degrees
+//
+// Notes
+// 0 <= angle <= 360
+// Do not make any AM or PM assumptions for the HH:MM result. They are indistinguishable for this Kata.
+// 3 o'clock is 03:00, not 15:00
+// 7 minutes past midnight is 12:07
+// 7 minutes past noon is also 12:07
+//
+// ✅ SOLUTION:
 const whatTimeIsIt = function (angle) {
     let hours = Math.floor(angle / 30);
     if (hours === 0) hours = 12;
@@ -5,6 +30,7 @@ const whatTimeIsIt = function (angle) {
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
 };
 
+// 📌 TESTCASE:
 console.log(whatTimeIsIt(0), "12:00");
 console.log(whatTimeIsIt(90), "03:00");
 console.log(whatTimeIsIt(180), "06:00");
