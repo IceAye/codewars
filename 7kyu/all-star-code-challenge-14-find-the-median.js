053a543239b2f81fde6a1e4bf8185d3f2735ad7c
Irene Volodko <ice7aye@gmail.com>
2025-06-28 14:50:34 +0300

    description: find the median

--- 7kyu/all-star-code-challenge-14-find-the-median.js
+++ 7kyu/all-star-code-challenge-14-find-the-median.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// Your non-profit company has assigned you the task of calculating some simple statistics on donations. You have an array of integers, representing various amounts of donations your company has been given. In particular, you're interested in the median value for donations.
+//
+// The median is the middle number of a sorted list of numbers. If the list is of even length, the 2 middle values are averaged.
+//
+// Write a function that takes an array of integers as an argument and returns the median of those integers.
+//
+// Notes:
+//
+// The sorting step is vital.
+// Input arrays are non-empty.
+//
+// ✅ SOLUTION:
 function median(array) {
     array.sort((a, b) => a - b);
 
@@ -10,5 +23,6 @@ function median(array) {
     return array[medianIndex];
 }
 
+// 📌 TESTCASE:
 console.log(median([3,2,1]),2);
 console.log(median([33,99,100,30,29,50]), 41.5);
