15e05e0d2d1619527b96cb2f4583926f38ec025a
Irene Volodko <ice7aye@gmail.com>
2025-06-30 18:21:14 +0300

    add solution: areacode extractor

---
+++ 7kyu/string-drills-areacode-extractor.js
@@ -0,0 +1,7 @@
+function areaCode(text) {
+    return text.match(/\(\d+\)/g)[0].slice(1, -1);
+}
+
+console.log(areaCode("The supplier's phone number is (555) 867-5309"), '555');
+console.log(areaCode("Grae's cell number used to be (123) 456-7890"), '123');
+console.log(areaCode("The 102nd district court's fax line is (124) 816-3264"), '124');
