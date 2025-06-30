56e391f23d1d5e861eb43294603c3e8e4702a5d7
Irene Volodko <ice7aye@gmail.com>
2025-06-30 18:37:12 +0300

    description: find missing numbers

--- 7kyu/find-missing-numbers.js
+++ 7kyu/find-missing-numbers.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// You will get an array of numbers.
+//
+// Every preceding number is smaller than the one following it.
+//
+// Some numbers will be missing, for instance:
+//
+// [-3,-2,1,5] //missing numbers are: -1,0,2,3,4
+// Your task is to return an array of those missing numbers:
+//
+// [-1,0,2,3,4]
+//
+// ✅ SOLUTION 1:
 function findMissingNumbers(arr) {
     const result = [];
     let num = arr[0];
@@ -11,6 +24,7 @@ function findMissingNumbers(arr) {
     return result;
 }
 
+// ✅ SOLUTION 2:
 function findMissingNumbers(arr) {
     const result = [];
     for (let i = arr[0]; i < arr[arr.length - 1]; i++) {
@@ -21,6 +35,7 @@ function findMissingNumbers(arr) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(findMissingNumbers([-3,-2,1,4]), [-1,0,2,3]);
 console.log(findMissingNumbers([-1,0,1,2,3,4]), []);
 console.log(findMissingNumbers([]), []);
