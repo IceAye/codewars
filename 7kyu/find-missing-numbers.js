affbeed3af615e73de2e35e0a76286e993de7bab
Irene Volodko <ice7aye@gmail.com>
2025-06-30 18:36:12 +0300

    add solution 2: find missing numbers

--- 7kyu/find-missing-numbers.js
+++ 7kyu/find-missing-numbers.js
@@ -11,6 +11,16 @@ function findMissingNumbers(arr) {
     return result;
 }
 
+function findMissingNumbers(arr) {
+    const result = [];
+    for (let i = arr[0]; i < arr[arr.length - 1]; i++) {
+        if (arr.indexOf(i) === -1) {
+            result.push(i);
+        }
+    }
+    return result;
+}
+
 console.log(findMissingNumbers([-3,-2,1,4]), [-1,0,2,3]);
 console.log(findMissingNumbers([-1,0,1,2,3,4]), []);
 console.log(findMissingNumbers([]), []);
