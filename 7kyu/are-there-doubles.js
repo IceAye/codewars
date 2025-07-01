f26a877d773e445b586be4cfd4f023549721b3cd
Irene Volodko <ice7aye@gmail.com>
2025-07-01 15:02:44 +0300

    description: are there doubles

--- 7kyu/are-there-doubles.js
+++ 7kyu/are-there-doubles.js
@@ -1,7 +1,14 @@
+// ❓ DESCRIPTION:
+// space characters). For example aa, !! or   .
+//
+// You want the function to return true if the string contains double characters and false if not. The test should not be case sensitive; for example both aa & aA return true.
+//
+// ✅ SOLUTION:
 function doubleCheck(str){
     return /(.)\1/ig.test(str);
 }
 
+// 📌 TESTCASE:
 console.log(doubleCheck("abca"), false);
 console.log(doubleCheck("aabc"), true);
 console.log(doubleCheck("a 11 c d"), true);
