ff716e702030935c2cb4f2b5d8e3b33e95bbc381
Irene Volodko <ice7aye@gmail.com>
2025-07-01 15:00:43 +0300

    add solution: are there doubles

---
+++ 7kyu/are-there-doubles.js
@@ -0,0 +1,10 @@
+function doubleCheck(str){
+    return /(.)\1/ig.test(str);
+}
+
+console.log(doubleCheck("abca"), false);
+console.log(doubleCheck("aabc"), true);
+console.log(doubleCheck("a 11 c d"), true);
+console.log(doubleCheck("AabBcC"), true);
+console.log(doubleCheck("a b  c"), true);
+console.log(doubleCheck("a b c d e f g h i h k"), false);
