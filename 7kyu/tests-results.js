e20dfec7a23402cd16e91c73aeb8526f333b004c
Irene Volodko <ice7aye@gmail.com>
2025-07-03 12:38:20 +0300

    description: tests results

--- 7kyu/tests-results.js
+++ 7kyu/tests-results.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// It's important day today: the class has just had a math test. You will be given a list of marks. Complete the function that will:
+//
+// Calculate the average mark of the whole class and round it to 3 decimal places.
+// Make a dictionary/hash with keys "h", "a", "l" to make clear how many high, average and low marks they got. High marks are 9 & 10, average marks are 7 & 8, and low marks are 1 to 6.
+// Return list [class_average, dictionary] if there are different type of marks, or [class_average, dictionary, "They did well"] if there are only high marks.
+//
+// ✅ SOLUTION:
 function testResult(array) {
     const marksCount = {"h": 0, "a": 0, "l": 0};
 
@@ -15,6 +23,7 @@ function testResult(array) {
     return marksCount["h"] === array.length ? [...result, "They did well"] : result ;
 }
 
+// 📌 TESTCASE:
 console.log(testResult([10,9,9,10,9,10,9]),[9.429, {'h': 7, 'a': 0, 'l': 0}, 'They did well']);
 console.log(testResult([5,6,4,8,9,8,9,10,10,10]),[7.9, {'h': 5, 'a': 2, 'l': 3}]);
 console.log(testResult([5,6,5,7,4,5,6,6,5]),[5.444, {'h': 0, 'a': 1, 'l': 8}]);
