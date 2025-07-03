1954283653d414f763c944cd845c695140527d92
Irene Volodko <ice7aye@gmail.com>
2025-07-03 13:05:59 +0300

    description: help Alf find his spaceship

--- 7kyu/80-s-kids-2-help-alf-find-his-spaceship.js
+++ 7kyu/80-s-kids-2-help-alf-find-his-spaceship.js
@@ -1,3 +1,27 @@
+// ❓ DESCRIPTION:
+// Late last night in the Tanner household, ALF was repairing his spaceship so he might get back to Melmac. Unfortunately for him, he forgot to put on the parking brake, and the spaceship took off during repair. Now it's hovering in space.
+//
+// ALF has the technology to bring the spaceship home if he can lock on to its location.
+//
+// Given a map:
+//
+// ..........
+// ..........
+// ..........
+// .......X..
+// ..........
+// ..........
+// The map will be given in the form of a string with \n separating new lines. The bottom left of the map is [0, 0]. X is ALF's spaceship.
+//
+// In this example:
+//
+// findSpaceship(map) => [7, 2]
+// If you cannot find the spaceship, the result should be
+//
+// "Spaceship lost forever."
+// Can you help ALF?
+//
+// ✅ SOLUTION:
 function findSpaceship(map) {
     if (map) {
         let parking = map.split("\n");
@@ -9,6 +33,7 @@ function findSpaceship(map) {
     return "Spaceship lost forever.";
 }
 
+// 📌 TESTCASE:
 console.log(findSpaceship('..........\n..........\n.......X..\n..........\n..........'), [7, 2])
 console.log(findSpaceship('.......\nX.......'), [0, 0])
 console.log(findSpaceship('........................'), 'Spaceship lost forever.')
