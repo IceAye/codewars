e42531d56b455a5a3c54835ff54155ca06ba4266
Irene Volodko <ice7aye@gmail.com>
2025-07-03 12:47:47 +0300

    description: even and odd

--- 7kyu/even-and-odd.js
+++ 7kyu/even-and-odd.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Given a number N, can you fabricate the two numbers NE and NO such that NE is formed by even digits of N and NO is formed by odd digits of N? Examples:
+//
+// input	NE	NO
+// 126453	264	153
+// 3012	2	31
+// 4628	4628	0
+// 0 is considered as an even number.
+//
+// In C and JavaScript you should return an array of two elements such as the first is NE and the second is NO.
+
+// ✅ SOLUTION:
 function evenAndOdd(num) {
     let NE = "";
     let NO = "";
@@ -9,7 +21,7 @@ function evenAndOdd(num) {
 
     return [+NE, +NO];
 }
-
+// 📌 TESTCASE:
 console.log(evenAndOdd(126453), [264, 153], `For input 126453`);
 console.log(evenAndOdd(3012), [2, 31], `For input 3012`);
 console.log(evenAndOdd(4628), [4628, 0], `For input 4628`);
