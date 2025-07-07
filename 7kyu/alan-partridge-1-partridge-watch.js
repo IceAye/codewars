f6b5b5a16fb37db9417c877192be5816ca7871a3
Irene Volodko <ice7aye@gmail.com>
2025-07-07 21:51:03 +0300

    add solution 2: Partridge watch

--- 7kyu/alan-partridge-1-partridge-watch.js
+++ 7kyu/alan-partridge-1-partridge-watch.js
@@ -21,6 +21,25 @@ function part(x) {
         : "Lynn, I've pierced my foot on a spike!!";
 }
 
+function part(x) {
+    const alan = new Set([
+        "Partridge",
+        "PearTree",
+        "Chat",
+        "Dan",
+        "Toblerone",
+        "Lynn",
+        "AlphaPapa",
+        "Nomad",
+    ]);
+
+    let exclamationCount = x.filter(word => alan.has(word)).length;
+
+    return exclamationCount
+        ? "Mine's a Pint" + "!".repeat(exclamationCount)
+        : "Lynn, I've pierced my foot on a spike!!";
+}
+
 console.log(part(['Grouse', 'Partridge', 'Pheasant']), 'Mine\'s a Pint!');
 console.log(part(['Pheasant', 'Goose', 'Starling', 'Robin']), 'Lynn, I\'ve pierced my foot on a spike!!');
 console.log(part(['Grouse', 'Partridge', 'Pheasant', 'Goose', 'Starling', 'Robin', 'Thrush', 'Emu', 'PearTree', 'Chat', 'Dan', 'Square', 'Toblerone', 'Lynn', 'AlphaPapa', 'BMW', 'Graham', 'Tool', 'Nomad', 'Finger', 'Hamster']), 'Mine\'s a Pint!!!!!!!!');
