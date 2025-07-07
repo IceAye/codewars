04411865c9c4a4483112d7a3f3258fd7acd69352
Irene Volodko <ice7aye@gmail.com>
2025-07-07 15:36:22 +0300

    description: capitals first

--- 7kyu/capitals-first.js
+++ 7kyu/capitals-first.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Create a function that takes an input String and returns a String, where all the uppercase words of the input String are in front and all the lowercase words at the end. The order of the uppercase and lowercase words should be the order in which they occur.
+//
+// If a word starts with a number or special character, skip the word and leave it out of the result.
+//
+// Input String will not be empty.
+//
+// For an input String: "hey You, Sort me Already!" the function should return: "You, Sort Already! hey me"
+
+// ✅ SOLUTION:
 function capitalsFirst(str){
     const array = str.split(' ');
     const capitals =  array.filter(word => /[A-Z]/.test(word[0]));
@@ -5,4 +15,5 @@ function capitalsFirst(str){
     return [...capitals, ...others].join(' ');
 }
 
+// 📌 TESTCASE:
 console.log(capitalsFirst("hey You, Sort me Already!"), "You, Sort Already! hey me")
