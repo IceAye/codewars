b36b15b84e9d2bccd701a8e31b30aa816222181f
Irene Volodko <ice7aye@gmail.com>
2025-07-08 21:57:37 +0300

    add solution: extract portion of file name

---
+++ 6kyu/extract-portion-of-file-name.js
@@ -0,0 +1,8 @@
+class FileNameExtractor {
+    static extractFileName (dirtyFileName) {
+        return dirtyFileName.replace(/\d+_([^.]+\.\w+).*$/g, "$1");
+    }
+}
+
+console.log(FileNameExtractor.extractFileName("1_FILE_NAME.EXTENSION.OTHEREXTENSIONadasdassdassds34"), "FILE_NAME.EXTENSION");
+console.log(FileNameExtractor.extractFileName("1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION"), "FILE_NAME.EXTENSION");
