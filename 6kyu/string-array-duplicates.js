cef4002043d42f42c932c715265ed99958466058
Irene Volodko <ice7aye@gmail.com>
2025-07-08 21:47:52 +0300

    description: string array duplicates

--- 6kyu/string-array-duplicates.js
+++ 6kyu/string-array-duplicates.js
@@ -1,7 +1,20 @@
+// ❓ DESCRIPTION:
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+//
+// For example:
+//
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+//
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+//
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+
+// ✅ SOLUTION:
 function dup(s) {
     return s.map(word => word.replace(/(.)\1+/g, "$1"));
 }
 
+// 📌 TESTCASE:
 console.log(dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo']);
 console.log(dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese']);
 console.log(dup(["kelless","keenness"]), ['keles','kenes']);
