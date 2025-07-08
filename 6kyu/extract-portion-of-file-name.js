2e21e7fcc2f02426d350e5a41ae8443ab6870d8a
Irene Volodko <ice7aye@gmail.com>
2025-07-08 21:58:54 +0300

    add solution 2: extract portion of file name

--- 6kyu/extract-portion-of-file-name.js
+++ 6kyu/extract-portion-of-file-name.js
@@ -4,5 +4,11 @@ class FileNameExtractor {
     }
 }
 
+class FileNameExtractor {
+    static extractFileName (dirtyFileName) {
+        return dirtyFileName.slice(dirtyFileName.indexOf("_") + 1, dirtyFileName.lastIndexOf("."));
+    }
+}
+
 console.log(FileNameExtractor.extractFileName("1_FILE_NAME.EXTENSION.OTHEREXTENSIONadasdassdassds34"), "FILE_NAME.EXTENSION");
 console.log(FileNameExtractor.extractFileName("1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION"), "FILE_NAME.EXTENSION");
