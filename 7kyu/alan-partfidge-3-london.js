5342fe481dcb0f6a4bb4a4b3518c513f15e21e2b
Irene Volodko <ice7aye@gmail.com>
2025-07-08 15:26:43 +0300

    add solution: Alan Partridge London

---
+++ 7kyu/alan-partfidge-3-london.js
@@ -0,0 +1,8 @@
+function alan(x){
+    const listOfStops = ['Rejection', 'Disappointment', 'Backstabbing Central', 'Shattered Dreams Parkway'];
+    return listOfStops.every(stop => x.includes(stop)) ? 'Smell my cheese you mother!' : 'No, seriously, run. You will miss it.';
+}
+
+console.log(alan(['Norwich', 'Rejection', 'Disappointment', 'Backstabbing Central', 'Shattered Dreams Parkway', 'London']), 'Smell my cheese you mother!');
+console.log(alan(['London', 'Norwich']), 'No, seriously, run. You will miss it.');
+console.log(alan(['Norwich', 'Tooting', 'Bank', 'Rejection', 'Disappointment', 'Backstabbing Central', 'Exeter', 'Shattered Dreams Parkway', 'Belgium','London']), 'Smell my cheese you mother!');
