f2c41bef5fa0660b34a31e048e8c7066d6fca531
Irene Volodko <ice7aye@gmail.com>
2025-07-08 15:42:55 +0300

    description: x marks the spot

--- 7kyu/x-marks-the-spot.js
+++ 7kyu/x-marks-the-spot.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Given a two dimensional array, return the co-ordinates of x.
+//
+// If x is not inside the array, or if x appears multiple times, return [].
+//
+// The co-ordinates should be zero indexed in row-major order.
+// You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
+
+// ✅ SOLUTION:
 function xMarksTheSpot(matrix) {
     const coords = [];
     for (let row = 0; row < matrix.length; row++) {
@@ -10,6 +19,7 @@ function xMarksTheSpot(matrix) {
     return coords.length > 2 ? [] : coords;
 }
 
+// 📌 TESTCASE:
 console.log(xMarksTheSpot([['o', 'o'], ['o', 'o']], []));
 console.log(xMarksTheSpot([['x', 'o'], ['o', 'x']], []));
 console.log(xMarksTheSpot([['x', 'o'], ['o', 'o']], [0, 0]));
