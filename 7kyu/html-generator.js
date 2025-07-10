a2899eb252d4b16b7dfae40850da6cdae5a72812
Irene Volodko <ice7aye@gmail.com>
2025-07-10 21:52:15 +0300

    description: html generator

--- 7kyu/html-generator.js
+++ 7kyu/html-generator.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Another rewarding day in the fast-paced world of WebDev. Man, you love your job! But as with any job, somtimes things can get a little tedious. Part of the website you're working on has a very repetitive structure, and writing all the HTML by hand is a bore. Time to automate! You want to write some functions that will generate the HTML for you.
+//
+// To organize your code, make of all your functions methods of a class called HTMLGen. Tag functions should be named after the tag of the element they create. Each function will take one argument, a string, which is the inner HTML of the element to be created. The functions will return the string for the appropriate HTML element.
+
+// ✅ SOLUTION:
 function HTMLGen() {
     this.div = function (value) {
         return `<div>${value}</div>`;
@@ -25,6 +31,7 @@ function HTMLGen() {
     }
 }
 
+// 📌 TESTCASE:
 let g = new HTMLGen();
 
 console.log(g.div("test") === "<div>test</div>");
