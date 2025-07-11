81ad0f9998425bd86e3713ad8a6e98eb3b1f9a46
Irene Volodko <ice7aye@gmail.com>
2025-07-11 15:22:25 +0300

    add solution: complete the pattern 3

---
+++ 7kyu/complete-the-pattern-3-horizontal-image-of-2.js
@@ -0,0 +1,23 @@
+function pattern(n) {
+    const output = [];
+
+    for (let i = 1; i <= n; i++) {
+        let current = n;
+        let string = "";
+        let j = 0;
+
+        while (j < i) {
+            string += current;
+            current--;
+            j++;
+        }
+
+        output.push(string);
+    }
+
+    return output.join("\n");
+}
+
+console.log(pattern(1)); // "1");
+console.log(pattern(2)); // "2\n21");
+console.log(pattern(5)); // "5\n54\n543\n5432\n54321");
