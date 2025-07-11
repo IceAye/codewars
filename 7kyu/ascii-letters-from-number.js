275b13fe53d40db5570ebd4c1623b09bafc3e1c3
Irene Volodko <ice7aye@gmail.com>
2025-07-11 23:30:00 +0300

    add solution: ascii letters from numbers

---
+++ 7kyu/ascii-letters-from-number.js
@@ -0,0 +1,8 @@
+function convert(number){
+    return number.replace(/(..)/g, c => String.fromCharCode(+c));
+}
+
+console.log(convert("65"),"A")
+console.log(convert("656667"),"ABC")
+console.log(convert("676584"),"CAT")
+console.log(convert("73327673756932858080698267658369"),"I LIKE UPPERCASE")
