bea00719e93c0b8f1fab532fba112c60b59abf5f
Irene Volodko <ice7aye@gmail.com>
2025-07-11 15:25:21 +0300

    description: complete the pattern 3

--- 7kyu/complete-the-pattern-3-horizontal-image-of-2.js
+++ 7kyu/complete-the-pattern-3-horizontal-image-of-2.js
@@ -1,3 +1,18 @@
+// ❓ DESCRIPTION:
+// You have to write a function pattern which creates the following pattern upto n number of rows. If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+//
+// Pattern:
+// (n)
+// (n)(n-1)
+// (n)(n-1)(n-2)
+// ................
+// .................
+// (n)(n-1)(n-2)....4
+// (n)(n-1)(n-2)....43
+// (n)(n-1)(n-2)....432
+// (n)(n-1)(n-2)....4321
+
+// ✅ SOLUTION 1:
 function pattern(n) {
     const output = [];
 
@@ -18,6 +33,7 @@ function pattern(n) {
     return output.join("\n");
 }
 
+// ✅ SOLUTION 2:
 function pattern(n) {
     const output = [];
 
@@ -30,6 +46,7 @@ function pattern(n) {
     return output.join("\n");
 }
 
+// 📌 TESTCASE:
 console.log(pattern(1)); // "1");
 console.log(pattern(2)); // "2\n21");
 console.log(pattern(5)); // "5\n54\n543\n5432\n54321");
