b62a997aca5f68abb3d2a1059cdf104ef1593c5a
Irene Volodko <ice7aye@gmail.com>
2025-07-11 23:31:08 +0300

    add solution 2: ascii letters from numbers

--- 7kyu/ascii-letters-from-number.js
+++ 7kyu/ascii-letters-from-number.js
@@ -2,6 +2,10 @@ function convert(number){
     return number.replace(/(..)/g, c => String.fromCharCode(+c));
 }
 
+function convert(number){
+    return String.fromCharCode(...number.match(/\d{2}/g));
+}
+
 console.log(convert("65"),"A")
 console.log(convert("656667"),"ABC")
 console.log(convert("676584"),"CAT")
