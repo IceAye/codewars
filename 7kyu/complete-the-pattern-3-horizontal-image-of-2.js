16c63fe0659523d5006b1cdaf7388be2e73b0692
Irene Volodko <ice7aye@gmail.com>
2025-07-11 15:24:23 +0300

    add solution 2: complete the pattern 3

--- 7kyu/complete-the-pattern-3-horizontal-image-of-2.js
+++ 7kyu/complete-the-pattern-3-horizontal-image-of-2.js
@@ -18,6 +18,18 @@ function pattern(n) {
     return output.join("\n");
 }
 
+function pattern(n) {
+    const output = [];
+
+    let string = '';
+    for (let i = n; i > 0; i--) {
+        string += i;
+        output.push(string);
+    }
+
+    return output.join("\n");
+}
+
 console.log(pattern(1)); // "1");
 console.log(pattern(2)); // "2\n21");
 console.log(pattern(5)); // "5\n54\n543\n5432\n54321");
