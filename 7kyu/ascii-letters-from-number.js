76c290da1a5097579e01d2c00079ee47baad9a05
Irene Volodko <ice7aye@gmail.com>
2025-07-11 23:32:22 +0300

    description: ascii letters from numbers

--- 7kyu/ascii-letters-from-number.js
+++ 7kyu/ascii-letters-from-number.js
@@ -1,11 +1,23 @@
+// ❓ DESCRIPTION:
+// You have to create a function that converts integer given as string into ASCII uppercase letters or spaces.
+//
+// All ASCII characters have their numerical order in table.
+//
+// For example,
+// from ASCII table, character of number 65 is "A".
+// Numbers will be next to each other, So you have to split given number to two digit long integers.
+
+// ✅ SOLUTION 1:
 function convert(number){
     return number.replace(/(..)/g, c => String.fromCharCode(+c));
 }
 
+// ✅ SOLUTION 2:
 function convert(number){
     return String.fromCharCode(...number.match(/\d{2}/g));
 }
 
+// 📌 TESTCASE:
 console.log(convert("65"),"A")
 console.log(convert("656667"),"ABC")
 console.log(convert("676584"),"CAT")
