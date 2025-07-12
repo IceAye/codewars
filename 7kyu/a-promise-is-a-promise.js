b562a12d4e55918b84560d595d291c12e19dbb7c
Irene Volodko <ice7aye@gmail.com>
2025-07-12 20:43:07 +0300

    description: a promise is a promise

--- 7kyu/a-promise-is-a-promise.js
+++ 7kyu/a-promise-is-a-promise.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Create a resolved javascript Promise that will return 'Hello World!'.
+
+// ✅ SOLUTION:
 function promiseHelloWorld() {
     return Promise.resolve('Hello World!');
 }
