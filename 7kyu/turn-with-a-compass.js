1c187ce0a336e32c8255dad08439868ad502944f
Irene Volodko <ice7aye@gmail.com>
2025-07-13 19:23:51 +0300

    description: turn with a compass

--- 7kyu/turn-with-a-compass.js
+++ 7kyu/turn-with-a-compass.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You receive the direction you are facing (one of the 8 directions: N, NE, E, SE, S, SW, W, NW) and a certain degree to turn (a multiple of 45, between -1080 and 1080); positive means clockwise, and negative means counter-clockwise.
+//
+// Return the direction you will face after the turn.
+
+// ✅ SOLUTION:
 function direction(facing, turn) {
     const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
     const targetDirectionIndex = (directions.indexOf(facing) + turn / 45 ) % directions.length;
@@ -5,6 +11,7 @@ function direction(facing, turn) {
     return targetDirectionIndex < 0 ? directions[directions.length + targetDirectionIndex] : directions[targetDirectionIndex];
 }
 
+// 📌 TESTCASE:
 console.log(direction("S", 180), "N");
 console.log(direction("SE", -45), "E");
 console.log(direction("W", 495), "NE");
