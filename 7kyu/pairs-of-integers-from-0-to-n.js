9c7224a68011660a331d43c7cbaaf7fe41ebbcaa
Irene Volodko <ice7aye@gmail.com>
2025-07-14 18:29:01 +0300

    description: pairs of integers from zero to n

--- 7kyu/pairs-of-integers-from-0-to-n.js
+++ 7kyu/pairs-of-integers-from-0-to-n.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Write a function that accepts an integer argument n and generates an array containing the pairs of integers [a, b] that satisfy the condition
+//
+// 0 <= a <= b <= n
+// The pairs should be sorted by increasing values of a, then by increasing values of b.
+//
+// For example,
+//
+// for input: 2
+// it should return: [  [0, 0], [0, 1], [0, 2],  [1, 1], [1, 2],  [2, 2]  ]
+
+// ✅ SOLUTION:
 function generatePairs(n) {
     const array = [];
     for (let i = 0; i <= n; i++) {
@@ -8,4 +20,5 @@ function generatePairs(n) {
     return array;
 }
 
+// 📌 TESTCASE:
 console.log(generatePairs(2), [ [0, 0], [0, 1], [0, 2], [1, 1], [1, 2], [2, 2] ]);
