f70431fef0dd22cd3813262f47bf2d984ac8640d
Irene Volodko <ice7aye@gmail.com>
2025-07-15 20:46:28 +0300

    description: the lazy startup office

--- 7kyu/the-lazy-startup-office.js
+++ 7kyu/the-lazy-startup-office.js
@@ -1,3 +1,23 @@
+// ❓ DESCRIPTION:
+// A startup office has an ongoing problem with its bin. Due to low budgets, they don't hire cleaners. As a result, the staff are left to voluntarily empty the bin. It has emerged that a voluntary system is not working and the bin is often overflowing. One staff member has suggested creating a rota system based upon the staff seating plan.
+// Create a function that accepts a 2D array of names. The function will return a single array containing staff names in the order that they should empty the bin.
+// Adding to the problem, the office has some temporary staff. This means that the seating plan changes every month. Both staff members' names and the number of rows of seats may change. Ensure that the function works when tested with these changes.
+//
+// Notes:
+// All the rows will always be the same length as each other.
+// There will be no empty spaces in the seating plan.
+// There will be no empty arrays.
+// Each row will be at least one seat long.
+//
+// [ ["Stefan", "Raj",    "Marie"],
+//   ["Alexa",  "Amy",    "Edward"],
+//   ["Liz",    "Claire", "Juan"],
+//   ["Dee",    "Luke",   "Katie"] ]
+//
+// As an output you would expect in this case:
+// ["Stefan", "Raj", "Marie", "Edward", "Amy", "Alexa", "Liz", "Claire", "Juan", "Katie", "Luke", "Dee"])
+
+// ✅ SOLUTION:
 function binRota(arr) {
     const result = [];
 
@@ -13,6 +33,7 @@ function binRota(arr) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(binRota([["Bob","Nora"],["Ruby","Carl"]])) // ["Bob","Nora","Carl","Ruby"]
 console.log(binRota([["Billy"]])) // ["Billy"]
 console.log(binRota([["Billy", "Nancy"]])) // ["Billy","Nancy"]
