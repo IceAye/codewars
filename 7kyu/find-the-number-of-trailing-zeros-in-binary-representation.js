e738bb710de2f4a5b03e111ad7fed79484b2ec6d
Irene Volodko <ice7aye@gmail.com>
2025-07-16 17:41:51 +0300

    description: find the number of trailing zeros in its binary representation of a number

--- 7kyu/find-the-number-of-trailing-zeros-in-binary-representation.js
+++ 7kyu/find-the-number-of-trailing-zeros-in-binary-representation.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Given a number n, find the number of trailing zeros in its binary representation.
+//
+// Examples:
+// 4  ->  2, because 4 is represented as 100
+// 5  ->  0, because 5 is represented as 101
+
+// ✅ SOLUTION:
 function trailingZeros(n) {
     const binaryString = n.toString(2);
     if (!binaryString.endsWith("0")) return 0;
@@ -11,6 +19,7 @@ function trailingZeros(n) {
     return sum;
 }
 
+// 📌 TESTCASE:
 console.log(trailingZeros(4), 2);
 console.log(trailingZeros(5), 0);
 console.log(trailingZeros(32), 5);
