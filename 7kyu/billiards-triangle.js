63728727ecbc4ec6914d3f4f52572041e753a98d
Irene Volodko <ice7aye@gmail.com>
2025-07-23 20:04:30 +0300

    description: billiards triangle

--- 7kyu/billiards-triangle.js
+++ 7kyu/billiards-triangle.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Remember the triangle of balls in billiards? To build a classic triangle (5 levels) you need 15 balls. With 3 balls you can build a 2-level triangle, etc.
+//
+// For more examples,
+// pyramid(1) == 1
+// pyramid(3) == 2
+// pyramid(6) == 3
+// pyramid(10) == 4
+// pyramid(15) == 5
+// Write a function that takes number of balls (≥ 1) and calculates how many levels you can build a triangle.
+
+// ✅ SOLUTION:
 function pyramid(balls) {
     let line = 1;
 
@@ -11,6 +23,7 @@ function pyramid(balls) {
     return line - 1;
 }
 
+// 📌 TESTCASE:
 console.log(pyramid(1), 1);
 console.log(pyramid(4), 2);
 console.log(pyramid(20), 5);
