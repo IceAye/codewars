313c6da6ac54fb8f9be5998e2def01423faea573
Irene Volodko <ice7aye@gmail.com>
2025-07-24 18:33:30 +0300

    description: histogram h1

--- 7kyu/histogram-h1.js
+++ 7kyu/histogram-h1.js
@@ -1,3 +1,18 @@
+// ❓ DESCRIPTION:
+// A 6-sided die is rolled a number of times and the results are plotted as a character-based histogram.
+//
+// Example:
+//
+// 6|##### 5
+// 5|
+// 4|# 1
+// 3|########## 10
+// 2|### 3
+// 1|####### 7
+// Task
+// You will be passed the dice value frequencies, and your task is to write the code to return a string representing a histogram, so that when it is printed it has the same format as the example.
+
+// ✅ SOLUTION:
 function histogram(results) {
     let histogram = '';
     let i = results.length - 1;
@@ -10,6 +25,7 @@ function histogram(results) {
     return histogram;
 }
 
+// 📌 TESTCASE:
 const expected = `6|##### 5
 5|
 4|# 1
