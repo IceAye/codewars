c08791a735c47dd7a05cce41be123b71684ddb98
Irene Volodko <ice7aye@gmail.com>
2025-07-24 18:45:29 +0300

    add solution: unique pairs

---
+++ 7kyu/unique-pairs.js
@@ -0,0 +1,8 @@
+function projectPartners(n) {
+    return n * (n - 1) / 2;
+}
+
+console.log(projectPartners(2), 1);
+console.log(projectPartners(3), 3);
+console.log(projectPartners(4), 6);
+console.log(projectPartners(5), 10);
