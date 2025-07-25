ca4e644e0380c1f677db354b91e011654af9e137
Irene Volodko <ice7aye@gmail.com>
2025-07-25 19:02:46 +0300

    add solution 2: which section did you scroll to

--- 7kyu/which-section-did-you-scroll-to.js
+++ 7kyu/which-section-did-you-scroll-to.js
@@ -9,6 +9,11 @@ function getSectionIdFromScroll(scrollY, sizes) {
     return scrollY >= sum ? -1 : sum;
 }
 
+function getSectionIdFromScroll(scrollY, sizes) {
+    let sum = 0;
+    return sizes.findIndex(size => scrollY < (sum += size));
+}
+
 console.log(getSectionIdFromScroll(299, [300, 200, 400, 600, 100]), 0);
 console.log(getSectionIdFromScroll(300, [300, 200, 400, 600, 100]), 1);
 console.log(getSectionIdFromScroll(1600, [300, 200, 400, 600, 100]), -1);
