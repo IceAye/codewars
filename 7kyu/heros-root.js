26975071cecac004f0ebf6f3b3c2c720daaa6cca
Irene Volodko <ice7aye@gmail.com>
2025-07-26 23:42:22 +0300

    description: Hero's root

--- 7kyu/heros-root.js
+++ 7kyu/heros-root.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// One of the first algorithm used for approximating the integer square root of a positive integer n is known as "Hero's method", named after the first-century Greek mathematician Hero of Alexandria who gave the first description of the method. Hero's method can be obtained from Newton's method which came 16 centuries after.
+// We approximate the square root of a number n by taking an initial guess x, an error e and repeatedly calculating a new approximate integer value x using: (x + n / x) / 2; we are finished when the previous x and the new x have an absolute difference less than e.
+// We supply to a function (int_rac) a number n (positive integer) and a parameter guess (positive integer) which will be our initial x. For this kata the parameter 'e' is set to 1.
+// Hero's algorithm is not always going to come to an exactly correct result! For instance: if n = 25 we get 5 but for n = 26 we also get 5. Nevertheless 5 is the integer square root of 26.
+// The kata is to return the count of the progression of integer approximations that the algorithm makes.
+
+// ✅ SOLUTION:
 function intRac(n, guess) {
     let count = 0;
     let prev = 0;
@@ -12,6 +20,7 @@ function intRac(n, guess) {
     return count;
 }
 
+// 📌 TESTCASE:
 console.log(intRac(25, 1, 4));
 console.log(intRac(125348, 300, 3));
 console.log(intRac(236, 12, 2));
