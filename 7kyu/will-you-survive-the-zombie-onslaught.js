a04be0e251324ace574c45d99e86f560a89ed535
Irene Volodko <ice7aye@gmail.com>
2025-07-27 23:22:16 +0300

    add solution 2: will you survive the zombie onslaught

--- 7kyu/will-you-survive-the-zombie-onslaught.js
+++ 7kyu/will-you-survive-the-zombie-onslaught.js
@@ -26,6 +26,13 @@ function zombieShootout(zombies, range, ammo) {
     }
 }
 
+function zombieShootout(zombies, range, ammo, shotsFired = 0) {
+    if (!zombies) return `You shot all ${shotsFired} zombies.`
+    if (!range) return `You shot ${shotsFired} zombies before being eaten: overwhelmed.`
+    if (!ammo) return `You shot ${shotsFired} zombies before being eaten: ran out of ammo.`
+    return zombieShootout(zombies - 1, range - 0.5, ammo - 1, shotsFired + 1);
+}
+
 console.log(zombieShootout(3, 10, 10), "You shot all 3 zombies.");
 console.log(zombieShootout(100, 8, 200), "You shot 16 zombies before being eaten: overwhelmed.");
 console.log(zombieShootout(50, 10, 8), "You shot 8 zombies before being eaten: ran out of ammo.");
