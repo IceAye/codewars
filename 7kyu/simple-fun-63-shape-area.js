f7d91ff462b3051a1570eabaf1292911c13265d9
Irene Volodko <ice7aye@gmail.com>
2025-07-28 20:28:39 +0300

    description: shape area

--- 7kyu/simple-fun-63-shape-area.js
+++ 7kyu/simple-fun-63-shape-area.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// Below we will define what and n-interesting polygon is and your task is to find its area for a given n.
+// A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim side by side. You can see the 1-, 2- and 3-interesting polygons in the picture below.
+//
+// Input/Output
+// [input] integer n
+// Constraints: 1 ≤ n < 10000.
+// [output] an integer
+// The area of the n-interesting polygon.
+
+// ✅ SOLUTION:
 function shapeArea(n) {
     let sum = 1;
     while (n > 0) {
@@ -7,6 +18,7 @@ function shapeArea(n) {
     return sum;
 }
 
+// 📌 TESTCASE:
 console.log(shapeArea(2), 5)
 console.log(shapeArea(3), 13)
 console.log(shapeArea(1), 1)
