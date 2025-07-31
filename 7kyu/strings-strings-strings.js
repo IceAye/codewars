92ee8c6ab3d670e357526c3293ca9c2c8edc05a9
Irene Volodko <ice7aye@gmail.com>
2025-07-31 19:34:39 +0300

    add solution 1: strings strings strings

---
+++ 7kyu/strings-strings-strings.js
@@ -0,0 +1,20 @@
+Boolean.prototype.toString = function() {
+    return '' + this;
+}
+
+Number.prototype.toString = function() {
+    return '' + this;
+}
+
+Array.prototype.toString = function() {
+    return `[${this.join(',')}]`;
+}
+
+console.log((123).toString(), "123");
+console.log(Math.PI.toString(), "3.141592653589793");
+console.log(Math.E.toString(), "2.718281828459045");
+console.log(true.toString(), "true");
+console.log(false.toString(), "false");
+console.log([].toString())
+console.log([1].toString())
+console.log([1,2,3,4,5].toString().replace(/\s+/g, ""), "[1,2,3,4,5]");
