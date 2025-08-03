0a1a4b58c41df41e63118d60c6efc9100914a869
Irene Volodko <ice7aye@gmail.com>
2025-08-03 16:24:44 +0300

    description: count salutes

--- 7kyu/count-salutes.js
+++ 7kyu/count-salutes.js
@@ -1,3 +1,23 @@
+// ❓ DESCRIPTION:
+// There is a narrow hallway in which people can go right and left only. When two people meet in the hallway, by tradition they must salute each other. People move at the same speed left and right.
+//
+// Your task is to write a function that, given a string representation of people moving in the hallway, will count the number of salutes that will occur.
+// Note: 2 salutes occur when people meet, one to the other and vice versa.
+//
+// Input
+// People moving right will be represented by >; people moving left will be represented by <. An example input would be >--<--->->. The - character represents empty space, which you need not worry about.
+//
+// Examples
+// Input: >----->-----<--<
+// Output: 8
+// Explanation: Both guys moving right will meet both guys moving left.
+// Hence a total of 4 meetings will occur and 8 salutes will occur.
+//
+// Input: <---<--->----<
+// Output: 2
+// Explanation: Only one meeting occurs.
+
+// ✅ SOLUTION:
 function countSalutes(hallway) {
     let result = 0;
 
@@ -23,6 +43,7 @@ function countSalutes(hallway) {
     return result;
 }
 
+// 📌 TESTCASE:
 console.log(countSalutes('<---->---<---<-->', 4))
 console.log(countSalutes('------', 0))
 console.log(countSalutes('>>>>>>>>>>>>>>>>>>>>>----<->', 42))
