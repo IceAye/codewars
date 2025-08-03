27088f15cbf365131617c80338f5f076958e6f8f
Irene Volodko <ice7aye@gmail.com>
2025-08-03 16:23:21 +0300

    add solution 2: count salutes

--- 7kyu/count-salutes.js
+++ 7kyu/count-salutes.js
@@ -9,6 +9,20 @@ function countSalutes(hallway) {
     return result;
 }
 
+function countSalutes(hallway) {
+    let result = 0;
+    let right = 0;
+
+    for (const char of hallway) {
+        if (char === '>') {
+            right++;
+        } else if (char === '<') {
+            result += 2 * right;
+        }
+    }
+    return result;
+}
+
 console.log(countSalutes('<---->---<---<-->', 4))
 console.log(countSalutes('------', 0))
 console.log(countSalutes('>>>>>>>>>>>>>>>>>>>>>----<->', 42))
