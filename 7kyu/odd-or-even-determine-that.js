6896cf25b3cc2fcf61387f77c219b995a12629d7
Irene Volodko <ice7aye@gmail.com>
2025-08-04 21:32:07 +0300

    description: odd or even determine that

--- 7kyu/odd-or-even-determine-that.js
+++ 7kyu/odd-or-even-determine-that.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Given a number N, determine if the sum of N consecutive numbers is odd or even.
+//
+// If the sum is definitely an odd number, return Odd.
+// If the sum is definitely an even number, return Even.
+// If the sum can be either odd or even ( depending on which first number you choose ), return Either.
+// Examples
+// Odd_or_Even(1) should return Either, because the sum can be odd or even.
+// Odd_or_Even(6) should return Odd, because 6 consecutive numbers contain 3 odd and 3 even numbers, so their sum is always odd.
+// Odd_or_Even(8) should return Even, because 8 consecutive numbers contain 4 odd and 4 even numbers, so their sum is always even.
+
+// ✅ SOLUTION:
 const ODD = "Odd";
 const EVEN = "Even";
 const EITHER = "Either";
@@ -8,6 +20,7 @@ function oddOrEven(n) {
     return half % 2 === 0 ? EVEN : ODD;
 }
 
+// 📌 TESTCASE:
 console.log( oddOrEven(1), EITHER );
 console.log( oddOrEven(6), ODD );
 console.log( oddOrEven(8), EVEN );
