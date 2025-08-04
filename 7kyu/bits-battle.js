f791053c52c3e365caae4b689b7b27b73f6bc023
Irene Volodko <ice7aye@gmail.com>
2025-08-04 21:06:39 +0300

    description: bits battle

--- 7kyu/bits-battle.js
+++ 7kyu/bits-battle.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// The odd and even numbers are fighting against each other!
+// You are given a list of positive integers. The odd numbers from the list will fight using their 1 bits from their binary representation, while the even numbers will fight using their 0 bits. If present in the list, number 0 will be neutral, hence not fight for either side.
+// You should return:
+// odds win if number of 1s from odd numbers is larger than 0s from even numbers
+// evens win if number of 1s from odd numbers is smaller than 0s from even numbers
+// tie if equal, including if list is empty
+// Please note that any prefix that might appear in the binary representation, e.g. 0b, should not be counted towards the battle.
+
+// ✅ SOLUTION:
 function bitsBattle(numbers) {
     let oddOnesSum = 0;
     let evenZeroesSum = 0;
@@ -19,6 +29,7 @@ function bitsBattle(numbers) {
              : 'evens win';
 }
 
+// 📌 TESTCASE:
 console.log(bitsBattle([5, 3, 14], 'odds win'));
 console.log(bitsBattle([3, 8, 22, 15, 78], 'evens win'));
 console.log(bitsBattle([], 'tie'));
