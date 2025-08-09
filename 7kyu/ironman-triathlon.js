46eaf983de02819dda194c68b36ea9cf87923d36
Irene Volodko <ice7aye@gmail.com>
2025-08-09 12:59:53 +0300

    add solution 2: ironman triathlon

--- 7kyu/ironman-triathlon.js
+++ 7kyu/ironman-triathlon.js
@@ -25,6 +25,16 @@ function iTri(s) {
     }
 }
 
+function iTri(dist){
+    let totalDist = 2.4 + 112 + 26.2;
+    if (dist === 0) return 'Starting Line... Good Luck!';
+    if (dist <= 2.4) return {'Swim': `${(totalDist - dist).toFixed(2)} to go!`};
+    if (dist <= 114.4) return {'Bike': `${(totalDist - dist).toFixed(2)} to go!`};
+    if (dist <= 127) return {'Run': `${(totalDist - dist).toFixed(2)} to go!`};
+    if (dist < totalDist) return {'Run': `Nearly there!`};
+    return "You're done! Stop running!";
+}
+
 console.log(iTri(36), { Bike: '104.60 to go!' });
 console.log(iTri(103.5), { Bike: '37.10 to go!' });
 console.log(iTri(0), 'Starting Line... Good Luck!');
