c137d92463f68ec48d067cab2dd2ddcb2a84d371
Irene Volodko <ice7aye@gmail.com>
2025-08-13 19:28:49 +0300

    description: hex hash sum

--- 7kyu/hex-hash-sum.js
+++ 7kyu/hex-hash-sum.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Complete the function that accepts a valid string and returns an integer.
+// Wait, that would be too easy! Every character of the string should be converted to the hex value of its ascii code, then the result should be the sum of the numbers in the hex strings (ignore letters).
+
+// ✅ SOLUTION:
 function hexHash(code) {
     // return [...code].reduce((acc, el) => acc + parseInt(el.charCodeAt(0).toString(16)), '')
     return [...code]
@@ -9,6 +14,7 @@ function hexHash(code) {
         .reduce((acc, curr) => acc + +curr, 0);
 }
 
+// 📌 TESTCASE:
 console.log(hexHash('Yo'), 20);
 console.log(hexHash('Hello, World!'), 91);
 console.log(hexHash('Forty4Three'), 113);
