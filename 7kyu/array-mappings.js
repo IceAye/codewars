8c863333d56fbfc4afe0f242453d208f2e19842e
Irene Volodko <ice7aye@gmail.com>
2025-08-14 15:56:24 +0300

    description: array mappings

--- 7kyu/array-mappings.js
+++ 7kyu/array-mappings.js
@@ -1,3 +1,17 @@
+// ❓ DESCRIPTION:
+// Oh no, the map method for arrays has been disabled. Can you fix it?
+//
+// In case you haven't come across the map method for arrays, here is how it works:
+//
+// [1,2,3].map(x => x ** 2) === [1,4,9]
+// [1,2,3].map(x => 2 * x) === [2,4,6]
+// [1,2,3].map(x => 2 ** x) === [2,4,8]
+// [1,2,3].map(x => x.toString()) === ["1","2","3"]
+// ["1","2","3"].map(x => parseInt(x)) === [1,2,3]
+// ["random","array","to","be","mapped"].map(x => mapping(x)) === [mapping("random"),mapping("array"),mapping("to"),mapping("be"),mapping("mapped")]
+// The map method does not mutate the original array.
+
+// ✅ SOLUTION:
 Array.prototype.map = function(clb) {
     const copy = [];
     for (const item of this) {
@@ -6,6 +20,7 @@ Array.prototype.map = function(clb) {
     return copy;
 }
 
+// 📌 TESTCASE:
 console.log([1,2,3].map(x => x ** 2), [1,4,9]);
 console.log([1,2,3].map(x => 2 * x), [2,4,6]);
 console.log([1,2,3].map(x => 2 ** x), [2,4,8]);
