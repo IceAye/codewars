4e9b88335a717e26022c826ef36fe662985ca7fc
Irene Volodko <ice7aye@gmail.com>
2025-08-14 15:18:43 +0300

    add solution 2: grid blast

--- 7kyu/grid-blast.js
+++ 7kyu/grid-blast.js
@@ -4,6 +4,10 @@ function fire(x,y) {
     return x === 1 && y === 1 ? 'center' : `${axisY} ${axisX}`;
 }
 
+function fire(x,y) {
+    return grid[x + (3 * y)];
+}
+
 console.log(fire(0,0),'top left')
 console.log(fire(1,2),'bottom middle')
 console.log(fire(1,1),'center')
