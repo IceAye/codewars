d42878628beb1c7084b1332a55d970566e403934
Irene Volodko <ice7aye@gmail.com>
2025-08-18 15:31:49 +0300

    description: you can't code under pressure 2

--- 7kyu/you-cant-code-under-pressure-2.js
+++ 7kyu/you-cant-code-under-pressure-2.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Code as fast as you can!
+// You need to make a constructor function with two methods (and only these two methods) to return and increment a counter, but the counter should not be directly accessible from outside the function.
+
+// ✅ SOLUTION:
 function Counter(initialValue = 0) {
     let _value = initialValue;
     this.check = function() {
@@ -8,6 +13,7 @@ function Counter(initialValue = 0) {
     }
 }
 
+// 📌 TESTCASE:
 let myCounter = new Counter();
 
 myCounter.increment();
