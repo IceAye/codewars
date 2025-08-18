fb8383076cc8fab88a053ad99b5f9d25c0327776
Irene Volodko <ice7aye@gmail.com>
2025-08-18 15:42:34 +0300

    description: find factors of a number

--- 7kyu/find-factors-of-a-number.js
+++ 7kyu/find-factors-of-a-number.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Create a function that takes a number and finds the factors of it, listing them in descending order in an array.
+// If the parameter is not an integer or less than 1, return -1. In C# return an empty array.
+// For Example: factors(54) should return [54, 27, 18, 9, 6, 3, 2, 1]
+
+// ✅ SOLUTION:
 function factors(x){
     if (!Number.isInteger(x)|| x < 1) return -1;
     const numbers = [];
@@ -7,5 +13,6 @@ function factors(x){
     return numbers;
 }
 
+// 📌 TESTCASE:
 console.log(factors(54),[54,27,18,9,6,3,2,1])
 console.log(factors(9),[9,3,1])
