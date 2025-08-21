4f579a3fbe5b4aa2a9b6661ada20cb36aa629ba7
Irene Volodko <ice7aye@gmail.com>
2025-08-21 16:17:13 +0300

    add solution 1: operator overload

---
+++ 7kyu/operator-overload.js
@@ -0,0 +1,12 @@
+const Foo = function(value) {
+    this.val = value;
+}
+
+Foo.prototype.valueOf = function() {
+    return this.val;
+}
+
+console.log(new Foo(2) + new Foo(3), 5);
+console.log(new Foo(-2) + new Foo(3), 1);
+console.log(new Foo(-21) + new Foo(-7), -28);
+console.log(new Foo(1.5) + new Foo(0.2), 1.7);
