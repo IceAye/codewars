d61b2f48ce14d4141d8a31e5c140f0bee85ffcd7
Irene Volodko <ice7aye@gmail.com>
2025-08-21 16:20:10 +0300

    description: operator overload

--- 7kyu/operator-overload.js
+++ 7kyu/operator-overload.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// We have a class named 'Foo' with a 'val' attribute.
+// var foo = new Foo(2);     // foo.val = 2;
+// var boo = new Foo(3);    // boo.val = 3;
+// We want to be able to sum two Foo object using the '+' operator.
+// foo + boo // => must return 5
+
+// ✅ SOLUTION 1:
 const Foo = function(value) {
     this.val = value;
 }
@@ -6,11 +14,12 @@ Foo.prototype.valueOf = function() {
     return this.val;
 }
 
-
+// ✅ SOLUTION 2:
 const Foo = function(value) {
     return new Number(value);
 }
 
+// 📌 TESTCASE:
 console.log(new Foo(2) + new Foo(3), 5);
 console.log(new Foo(-2) + new Foo(3), 1);
 console.log(new Foo(-21) + new Foo(-7), -28);
