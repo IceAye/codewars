d6a34a38f238abdcaba8762dd8cb03dc2436486f
Irene Volodko <ice7aye@gmail.com>
2025-08-22 16:34:30 +0300

    description: move all vowels

--- 7kyu/move-all-vowels.js
+++ 7kyu/move-all-vowels.js
@@ -1,8 +1,15 @@
+// ❓ DESCRIPTION:
+// Given a string as input, move all of its vowels to the end of the string, in the same order as they were before.
+// Vowels are (in this kata): a, e, i, o, u
+// Note: all provided input strings are lowercase.
+
+// ✅ SOLUTION:
 function moveVowel(input) {
     const vowels = input.match(/[aeiou]/g || []).join('');
     return input.replace(/[aeiou]/g, '') + vowels;
 }
 
+// 📌 TESTCASE:
 console.log(moveVowel("day"),"dya");
 console.log(moveVowel("apple"),"pplae");
 console.log(moveVowel("peace"),"pceae");
