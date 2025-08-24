f1b7ce03f4f251bade7ccb3c983b40c89080f555
Irene Volodko <ice7aye@gmail.com>
2025-08-24 23:21:35 +0300

    description: filter even length words

--- 7kyu/filter-even-length-words.js
+++ 7kyu/filter-even-length-words.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given an array of strings, write a function that returns an array containing only the elements of the given array whose length is an even number.
+
+// ✅ EXAMPLE
+// ["One", "Two", "Three", "Four"] --> ["Four"]
+
+// 📌 SOLUTION:
 function filterEvenLengthWords(words) {
     return words.filter(word => word.length % 2 === 0);
 }
