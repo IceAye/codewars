3075e70f1886976141319e935d4da96b14f029b2
Irene Volodko <ice7aye@gmail.com>
2025-08-27 15:58:52 +0300

    description: is it a vowel on this position

--- 7kyu/is-it-a-vowel-on-this-position.js
+++ 7kyu/is-it-a-vowel-on-this-position.js
@@ -1,7 +1,21 @@
+// ❓ DESCRIPTION:
+// Check if it is a vowel(a, e, i, o, u,) on the n position in a string (the first argument). Don't forget about uppercase.
+//
+// A few cases:
+//
+// {
+// checkVowel('cat', 1)  ->   true // 'a' is a vowel
+// checkVowel('cat', 0)  ->   false // 'c' is not a vowel
+// checkVowel('cat', 4)  ->   false // this position doesn't exist
+// }
+// P.S. If n < 0, return false
+
+// ✅ SOLUTION:
 function checkVowel(string, position) {
     return string[position] ? /[aeiou]/i.test(string.at(position)) : false;
 }
 
+// 📌 TESTCASE:
 console.log(checkVowel('cat', 1), true);
 console.log(checkVowel('cat', 0), false);
 console.log(checkVowel('cat', 4), false);
