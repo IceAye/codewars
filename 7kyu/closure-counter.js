20b71bb80a5cf2ec14c352157bbb8ee7d54dc6bd
Irene Volodko <ice7aye@gmail.com>
2025-08-28 17:12:51 +0300

    description: closure counter

--- 7kyu/closure-counter.js
+++ 7kyu/closure-counter.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// Define the function counter that returns a function that returns an increasing value.
+// The first value should be 1.
+// You're going to have to use closures.
+
+// 📌 EXAMPLE:
+// const newCounter = counter();
+// newCounter() // 1
+// newCounter() // 2
+
+// ✅ SOLUTION:
 function counter(){
     let value = 0;
     return () => {
