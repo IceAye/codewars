55582f3e2fab2f5e1491187fc354b226f0506b31
Irene Volodko <ice7aye@gmail.com>
2025-09-02 18:33:19 +0300

    add solution: every archer has its arrows

---
+++ 7kyu/every-archer-has-its-arrows.js
@@ -0,0 +1,8 @@
+function archersReady(archers){
+    return archers.length > 0 && archers.every(archer => archer >= 5 );
+}
+
+console.log(archersReady([]), false);
+console.log(archersReady([1, 2, 3, 4]), false);
+console.log(archersReady([5, 6, 7, 8]), true);
+console.log(archersReady([1, 2, 3, 4, 5, 6, 7, 8]), false);
