51468b1e83a48f1b1e42d9e87c47aec4c47e0b7d
Irene Volodko <ice7aye@gmail.com>
2025-09-04 16:10:31 +0300

    add solution: binary tree search (not BST)

---
+++ 7kyu/binary-tree-search.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor(value, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+function search(n, root) {
+    if (!root) return false;
+    if (n === root.value) return true;
+    return search(n, root.left) || search(n, root.right);
+}
+
+
+
+console.log(search(666, null), false);
+
+const root1 = new Node(666, new Node(555), new Node(444));
+console.log(search(444, root1), true);
+console.log(search(555, root1), true);
+console.log(search(666, root1), true);
+console.log(search(777, root1), false);
+
+const root2 = new Node(42);
+console.log(search(42, root2), true);
+console.log(search(43, root2), false);
+
+const SIZE = 20;
+const nodes = Array.from({ length: SIZE }, (_, i) => new Node(i));
+
+for (let i = 0; i < SIZE; i++) {
+    const idx_left = 2 * i + 1;
+    const idx_right = 2 * i + 2;
+    nodes[i].left = nodes[idx_left] || null;
+    nodes[i].right = nodes[idx_right] || null;
+}
+
+for (let i = 0; i < SIZE; i++) {
+    console.log(search(i, nodes[0]), true);
+}
+
+console.log(search(SIZE, nodes[0]), false);
