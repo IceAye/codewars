8d4fb86307b06b5079878041e6f08105b751c11f
Irene Volodko <ice7aye@gmail.com>
2025-09-12 18:14:41 +0300

    description: number from identity matrix

--- 7kyu/number-from-identity-matrix-1.js
+++ 7kyu/number-from-identity-matrix-1.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given the size n of an identity matrix (I), find the number represented by the identity matrix values as its bits.
+
+// 📌 SOLUTION:
 function solve(n) {
     const number = [];
     for (let i = 0; i < n; i++) {
@@ -8,5 +12,6 @@ function solve(n) {
     return BigInt('0b' + number.join(''));
 }
 
+// ✅ TESTCASE:
 console.log(solve(2), 9n);
 console.log(solve(4), 33825n);
