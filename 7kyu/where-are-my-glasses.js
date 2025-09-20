2b2c6206940e5bb0a8f2bfd7ec92e03f841fe780
Irene Volodko <ice7aye@gmail.com>
2025-09-20 15:20:37 +0300

    add solution: where are my glasses

---
+++ 7kyu/where-are-my-glasses.js
@@ -0,0 +1,14 @@
+function findGlasses(arr){
+    for (let i = 0; i < arr.length; i++) {
+        if (/O-+O/g.test(arr[i])) {
+            return i;
+        }
+    }
+}
+
+console.log(findGlasses(['phone', 'O-O', 'coins', 'keys']), 1)
+console.log(findGlasses(['OO', 'wallet', 'O##O', 'O----O']), 3)
+console.log(findGlasses(['O_O', 'O-O', 'OwO']), 1)
+console.log(findGlasses(['O--#--O', 'dustO---Odust', 'more dust']), 1)
+console.log(findGlasses(['floor', 'the floor again', 'pockets', 'bed', 'cabinet', 'the top of my head O-O']), 5)
+console.log(findGlasses(['OOOO----~OOO', '-------', 'OOOOOOO', 'OO-OO-OO-O']), 3)
