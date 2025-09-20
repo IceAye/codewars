0686634af313028e1e3d30b372a00d4145ae4380
Irene Volodko <ice7aye@gmail.com>
2025-09-20 15:23:09 +0300

    description: where are my glasses

--- 7kyu/where-are-my-glasses.js
+++ 7kyu/where-are-my-glasses.js
@@ -1,3 +1,21 @@
+// ❓ DESCRIPTION:
+// Oh no! I've lost my glasses, but paradoxically, I need glasses to find my glasses!
+//
+// Please help me out by showing me the index in the list which contains my glasses. They look like two capital Os, with at least one dash in between!
+//
+// This means that both O--O and O------------O are valid glasses, but not O----#--O for example!
+// Search thoroughly, you might find my glasses in places such as 'dustO-Odust'
+// Examples
+// ["phone", "O-O", "coins", "keys"] ➞ 1
+//
+// ["OO", "wallet", "O##O", "O----O"] ➞ 3
+//
+// ["O--#--O", "dustO---Odust", "more dust"] ➞ 1
+// Notes
+// All lists will include one valid pair of glasses because I swear I dropped them around here somewhere ...
+// All elements in the list are strings.
+
+// ✅ SOLUTION 1:
 function findGlasses(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (/O-+O/g.test(arr[i])) {
@@ -6,10 +24,12 @@ function findGlasses(arr) {
     }
 }
 
+// ✅ SOLUTION 2:
 function findGlasses(arr) {
     return arr.findIndex(item => /O-+O/.test(item));
 }
 
+// 📌 TESTCASE:
 console.log(findGlasses(['phone' , 'O-O' , 'coins' , 'keys']) , 1)
 console.log(findGlasses(['OO' , 'wallet' , 'O##O' , 'O----O']) , 3)
 console.log(findGlasses(['O_O' , 'O-O' , 'OwO']) , 1)
