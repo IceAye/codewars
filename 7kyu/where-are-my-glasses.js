73ce9a61b2ced5666b6d5b8379f4a2beecfb1508
Irene Volodko <ice7aye@gmail.com>
2025-09-20 15:21:35 +0300

    add solution 2: where are my glasses

--- 7kyu/where-are-my-glasses.js
+++ 7kyu/where-are-my-glasses.js
@@ -1,4 +1,4 @@
-function findGlasses(arr){
+function findGlasses(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (/O-+O/g.test(arr[i])) {
             return i;
@@ -6,9 +6,13 @@ function findGlasses(arr){
     }
 }
 
-console.log(findGlasses(['phone', 'O-O', 'coins', 'keys']), 1)
-console.log(findGlasses(['OO', 'wallet', 'O##O', 'O----O']), 3)
-console.log(findGlasses(['O_O', 'O-O', 'OwO']), 1)
-console.log(findGlasses(['O--#--O', 'dustO---Odust', 'more dust']), 1)
-console.log(findGlasses(['floor', 'the floor again', 'pockets', 'bed', 'cabinet', 'the top of my head O-O']), 5)
-console.log(findGlasses(['OOOO----~OOO', '-------', 'OOOOOOO', 'OO-OO-OO-O']), 3)
+function findGlasses(arr) {
+    return arr.findIndex(item => /O-+O/.test(item));
+}
+
+console.log(findGlasses(['phone' , 'O-O' , 'coins' , 'keys']) , 1)
+console.log(findGlasses(['OO' , 'wallet' , 'O##O' , 'O----O']) , 3)
+console.log(findGlasses(['O_O' , 'O-O' , 'OwO']) , 1)
+console.log(findGlasses(['O--#--O' , 'dustO---Odust' , 'more dust']) , 1)
+console.log(findGlasses(['floor' , 'the floor again' , 'pockets' , 'bed' , 'cabinet' , 'the top of my head O-O']) , 5)
+console.log(findGlasses(['OOOO----~OOO' , '-------' , 'OOOOOOO' , 'OO-OO-OO-O']) , 3)
