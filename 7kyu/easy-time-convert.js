06b57e478dfd435f7fc418ced1654602b544711f
Irene Volodko <ice7aye@gmail.com>
2025-09-26 15:54:25 +0300

    add solution: easy time convert

---
+++ 7kyu/easy-time-convert.js
@@ -0,0 +1,17 @@
+function timeConvert(num) {
+    if (num <= 0) return '00:00';
+    const hours = Math.floor(num / 60).toString().padStart(2, '0');
+    const min = (num % 60).toString().padStart(2, '0');
+    return `${hours}:${min}`;
+}
+
+console.log(timeConvert(0), "00:00");
+console.log(timeConvert(-6), "00:00");
+console.log(timeConvert(8), '0'+0+':'+'0'+8);
+console.log(timeConvert(32), '0'+0+':'+32);
+console.log(timeConvert(75), '0'+1+':'+15);
+console.log(timeConvert(63), '0'+1+':'+'0'+3);
+console.log(timeConvert(134), '0'+2+':'+14);
+console.log(timeConvert(180), '0'+3+':'+'0'+0);
+console.log(timeConvert(970), 16+':'+10);
+console.log(timeConvert(565757), 9429+':'+17);
