05b7a5c4f94efad4a6fb1536e70c5979b01186c5
Irene Volodko <ice7aye@gmail.com>
2025-10-07 18:38:03 +0300

    add solution 2: most sales

--- 7kyu/most-sales.js
+++ 7kyu/most-sales.js
@@ -6,6 +6,13 @@ function top3(products, amounts, prices) {
     return [...revenue.keys()].sort((a, b) => revenue.get(b) - revenue.get(a)).slice(0, 3);
 }
 
+const top3 = (products, amounts, prices) =>
+    products
+        .map((item, index) => ({ item, index, revenue: amounts[index] * prices[index] }))
+        .sort((a, b) => b.revenue - a.revenue || a.index - b.index)
+        .slice(0, 3)
+        .map(product => product.item)
+
 console.log(top3(["Computer", "Cell Phones", "Vacuum Cleaner"], [3, 24, 8], [199, 299, 399]), ["Cell Phones", "Vacuum Cleaner", "Computer"]);
 console.log(top3(["Cell Phones", "Vacuum Cleaner", "Computer", "Autos", "Gold", "Fishing Rods", "Lego", " Speakers"], [5, 25, 2, 7, 10, 3, 2, 24], [51, 225, 22, 47, 510, 83, 82, 124]), ['Vacuum Cleaner', 'Gold', ' Speakers']);
 console.log(top3(["Cell Phones", "Vacuum Cleaner", "Computer", "Autos", "Gold", "Fishing Rods", "Lego", " Speakers"], [0, 12, 24, 17, 19, 23, 120, 8], [9, 24, 29, 31, 51, 8, 120, 14]), ['Lego', 'Gold', 'Computer']);
