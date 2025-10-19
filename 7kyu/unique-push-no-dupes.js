e31978b1b61247969478080576a91f9e5a62f8ce
Irene Volodko <ice7aye@gmail.com>
2025-10-19 23:12:37 +0300

    description: unique push - no dupes

--- 7kyu/unique-push-no-dupes.js
+++ 7kyu/unique-push-no-dupes.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Write a function that will take an array and a person object as parameters. The function will only push a "person" object onto the end of an array if someone with that phone number doesn't already exist in that array.
+// -A "person" is a javascript object with a name and a phoneNumber : {name:'SomeName', phoneNumber:1234567890}
+// -A duplicate person object is an object with the same phoneNumber as someone else
+// If the person object is unique, push them onto the end of the array, and return true.
+// If the person object is NOT unique, don't push them to the array and return false;
+// If the person doesn't have a phoneNumber, don't add them to the array and return false.
+
+// ✅ SOLUTION:
 function uniquePush(arr, obj) {
     if (!obj.phoneNumber) return false;
 
@@ -11,6 +20,7 @@ function uniquePush(arr, obj) {
     return true;
 }
 
+// 📌 TESTCASE:
 const arr = [];
 console.log(uniquePush(arr, { name: 'bob', phoneNumber: 1234 }), true);
 console.log(uniquePush(arr, { name: 'joe', phoneNumber: 12345 }), true);
