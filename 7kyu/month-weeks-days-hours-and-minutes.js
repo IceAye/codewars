2515e8f51d6c17d969f2749ed6aca4664d546695
Irene Volodko <ice7aye@gmail.com>
2025-10-22 23:23:21 +0300

    description: months, weeks, days and minutes

--- 7kyu/month-weeks-days-hours-and-minutes.js
+++ 7kyu/month-weeks-days-hours-and-minutes.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given a number of minutes, translate it into a readible human timestamp.
+// For example: 100 minutes equals "1 hour 40 minutes" And: 52874 minutes equals "1 month 1 week 1 day 17 hours 14 minutes"
+// Given that each month has 28 days.
+// The largest amount of minutes you'll have to test for is under a year so you'll have to translate it to Months, Weeks, Days, Hours and Minutes.
+
+// ✅ SOLUTION:
 function displayValue(value) {
     const MINUTES_IN_HOUR = 60;
     const MINUTES_IN_DAY = 24 * MINUTES_IN_HOUR;
