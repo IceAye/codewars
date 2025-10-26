8627e7664b0fb6caa4e02b0d041c696c823e6e37
Irene Volodko <ice7aye@gmail.com>
2025-10-26 23:41:10 +0300

    description: rock paper scissors oracle

--- 7kyu/rock-paper-scissors-oracle.js
+++ 7kyu/rock-paper-scissors-oracle.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// You are the rock paper scissors oracle.
+// Famed throughout the land, you have the incredible ability to predict which hand gestures your opponent will choose out of rock, paper and scissors.
+// Unfortunately you're no longer a youngster, and have trouble moving your hands between rounds. For this reason, you can only pick a single gesture for each opponent. If it's possible for you to win, you will, but you're also happy to tie.
+// Given an array of gestures — for example ["paper", "scissors", "scissors"] — return the winning gesture/s in the order in which they appear in the title, separated by a forward slash. For example, if rock and scissors could both be used to win you would return:
+// "rock/scissors"
+// If it's not possible for you to win then return:
+// "tie"
+
+// ✅ SOLUTION:
 function oracle(gestures) {
     const beats = {
         rock: 'scissors',
@@ -20,6 +30,7 @@ function oracle(gestures) {
     return result.length ? result.join('/') : 'tie';
 }
 
+// 📌 TESTCASE:
 console.log(oracle(["rock","paper","scissors","rock"]), "paper");
 console.log(oracle(["rock","paper","scissors"]), "tie");
 console.log(oracle(["rock","paper","paper","scissors","scissors"]), "scissors");
