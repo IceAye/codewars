411701d8f21af00f1414fd32a303fe7dc09c25e2
Irene Volodko <ice7aye@gmail.com>
2025-11-04 21:52:51 +0300

    description: airport itinerary

--- 7kyu/airport-itinerary.js
+++ 7kyu/airport-itinerary.js
@@ -1,3 +1,37 @@
+// ❓ DESCRIPTION:
+// Travel itinerary
+// When you travel around the world you pass though different airports.
+//
+// TRN -> FCO -> JFK
+// And then return back to home
+//
+// JFK - TRN
+// In order to propose the unique list of airports that your trip uses we have to create an itinerary feature that can compress the list of airports including only the list of unique in/out combination.
+//
+// For example, a trip with:
+//
+// [TRN-FCO] [FCO-JFK] [JFK-TRN]
+// Should be represented as:
+//
+// TRN-FCO-JFK-TRN
+// That is the unique list of airport steps.
+//
+// Now in our database we save the travel as a list of objects with in/out properties and you will receive that list always sorted in the right way.
+//
+// [
+//   {in: "TRN", out: "FCO"},
+//   {in: "FCO", out: "JFK"},
+//   {in: "JFK", out: "FCO"}
+// ]
+// Now we have to create a helper function itinerary for JS that extract the unique airport list:
+//
+// travel = itinerary([
+//   {in: "TRN", out: "FCO"},
+//   {in: "FCO", out: "JFK"},
+//   {in: "JFK", out: "FCO"}
+// ]); // TRN-FCO-JFK-FCO
+
+// ✅ SOLUTION:
 function itinerary(travel) {
     const result = [travel[0].in, travel[0].out];
     for (let i = 1; i < travel.length; i++) {
@@ -7,6 +41,7 @@ function itinerary(travel) {
     return result.join('-');
 }
 
+// 📌 TESTCASE:
 console.log(itinerary([{in: "TRN", out: "FCO"}]), "TRN-FCO")
 console.log(itinerary([{in: "TRN", out: "FCO"}, {in: "CIA", out: "JFK"}]), "TRN-FCO-CIA-JFK")
 console.log(itinerary([{in: "TRN", out: "FCO"}, {in: "FCO", out: "JFK"}]), "TRN-FCO-JFK")
