0ff69cc067ebcef118fe5ec9e1f214ec2cecf927
Irene Volodko <ice7aye@gmail.com>
2025-11-14 12:17:46 +0300

    description: is it bugged

--- 7kyu/oh-dear-god-is-it-bugged.js
+++ 7kyu/oh-dear-god-is-it-bugged.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Chingel was creating a function which would return true if the input followed this time format 14-10-2016 01:12 (it doesn't need to reflect an actual time) and false otherwise. But looks like he has messed it up. Could you help him out? Please!
+
+// ✅ SOLUTION:
 function isItBugged(code){
     return /^(\d{2})-(\d{2})-(\d{4})\s(\d{2}):(\d{2})$/g.test(code);
 }
 
+// 📌 TESTCASE:
 console.log(isItBugged("01_09_2016 01:20"), false);
 console.log(isItBugged("14-10-1066 12:00"), true);
 console.log(isItBugged("Tenth of January"), false);
