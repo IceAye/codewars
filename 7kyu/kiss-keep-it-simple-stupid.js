553ed49e0f2ea8ce9695fe5cd4f6e36d365218ae
Irene Volodko <ice7aye@gmail.com>
2025-11-14 13:02:25 +0300

    add solution 1: keep it simple stupid

---
+++ 7kyu/kiss-keep-it-simple-stupid.js
@@ -0,0 +1,12 @@
+function isKiss(words) {
+    const arr = words.split(' ');
+    for (const el of arr) {
+        if (el.length > arr.length) return 'Keep It Simple Stupid';
+    }
+    return 'Good work Joe!';
+}
+
+console.log(isKiss('Joe had a bad day'), 'Good work Joe!');
+console.log(isKiss('Joe had some bad days'), 'Good work Joe!');
+console.log(isKiss('Joe is having no fun'), 'Keep It Simple Stupid');
+console.log(isKiss('Sometimes joe cries for hours'), 'Keep It Simple Stupid');
