5c6dfa71a9a5555f78e51dbd9a1310869646cfa5
Irene Volodko <ice7aye@gmail.com>
2025-11-26 16:15:15 +0300

    description: only one

--- 7kyu/only-one.js
+++ 7kyu/only-one.js
@@ -1,6 +1,11 @@
+// ❓ DESCRIPTION:
+// Write function only_one which return True if ONLY ONE of the boolean flag is True, otherwise return False. If there are no boolean flag, return False
+
+// ✅ SOLUTION:
 function onlyOne(...args) {
     return (args.filter(el => el === true)).length === 1;
 }
 
+// 📌 TESTCASE:
 console.log(onlyOne(true, false) == true, 'one true and one false')
 console.log(onlyOne())
