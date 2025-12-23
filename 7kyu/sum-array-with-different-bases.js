8f1e1ade8758404dedeecc348dfcc93ec552c8fe
Irene Volodko <ice7aye@gmail.com>
2025-12-23 16:49:21 +0300

    description: sum array with different bases

--- 7kyu/sum-array-with-different-bases.js
+++ 7kyu/sum-array-with-different-bases.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// You get an array of different numbers to sum up. But there is one problem, those numbers all have different bases. For example:
+// You get an array of numbers with their base as an input:
+// [["101",16],["7640",8],["1",9]]
+// The output should be the sum as an integer value with a base of 10, so according to the example this would be:
+// 4258
+// A few more examples:
+// [["101",2], ["10",8]] --> 13
+// [["ABC",16], ["11",2]] --> 2751
+// Bases can be between 2 and 36 (2<=base<=36)
+
+// ✅ SOLUTION:
 function sumItUp(numbersWithBases){
     let sum = 0;
     for (const [number, base] of numbersWithBases) {
@@ -6,6 +18,7 @@ function sumItUp(numbersWithBases){
     return sum;
 }
 
+// 📌 TESTCASE:
 console.log(sumItUp([["101",2], ["10",8]]),13);
 console.log(sumItUp([["ABC",16], ["11",2]]),2751);
 console.log(sumItUp([["101",16],["7640",8],["1",9]]),4258);
