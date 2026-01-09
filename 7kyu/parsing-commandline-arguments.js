2848aa0488469cff50978b8e1115801648a318ee
Irene Volodko <ice7aye@gmail.com>
2026-01-09 13:35:42 +0300

    description: parsing commandline arguments

--- 7kyu/parsing-commandline-arguments.js
+++ 7kyu/parsing-commandline-arguments.js
@@ -1,3 +1,8 @@
+/* ❓ DESCRIPTION:
+We're given some typical *nix commands and want to parse them into their arguments. We'll ignore quoted arguments and other special characters, although if any of the characters &, ;, >, | show up, those signify that a new command has started (so we can ignore any arguments after it).
+*/
+
+// ✅ SOLUTION:
 function args(cmd) {
     const regExp = new RegExp(/[&;>|]/);
     const markedIndex = cmd.search(regExp);
@@ -5,6 +10,7 @@ function args(cmd) {
     return finalString.split(' ');
 }
 
+// 📌 TESTCASE:
 console.log(args('ls -R /'), ['ls', '-R', '/'], 'failed parsing "ls -R /"');
 console.log(args('cat /tmp/data.txt | less'), ['cat', '/tmp/data.txt'], 'failed parsing command with pipe (|) operator');
 console.log(args('ls -R /&echo not done;echo ok'));
