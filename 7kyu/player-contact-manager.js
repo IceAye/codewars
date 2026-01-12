31e5ad46c5a41c7a1bbdbf3da684ec1418b1a999
Irene Volodko <ice7aye@gmail.com>
2026-01-12 15:52:38 +0300

    description: player contact manager

--- 7kyu/player-contact-manager.js
+++ 7kyu/player-contact-manager.js
@@ -1,3 +1,10 @@
+/* ❓ DESCRIPTION:
+You are the Dungeon Master for a public DnD game at your local comic shop and recently you've had some trouble keeping your players' info neat and organized so you've decided to write a bit of code to help keep them sorted!
+The goal of this code is to create an array of objects that stores a player's name and contact number from a given string.
+The method should return an empty array if the argument passed is an empty string or nil/None/null.
+*/
+
+// ✅ SOLUTION:
 function playerManager(players) {
     const result = [];
     if (players) {
@@ -11,16 +18,8 @@ function playerManager(players) {
     return [];
 }
 
-console.log(
-    playerManager('John Doe, 8167238327, Jane Doe, 8163723827'),
-    [{player: 'John Doe', contact: 8167238327}, {player: 'Jane Doe', contact: 8163723827}]
-);
-console.log(
-    playerManager('John Doe, 8167238327, Jane Doe, 8163723827, Harry P., 9131071991'),
-    [{player: 'John Doe', contact: 8167238327}, {player: 'Jane Doe', contact: 8163723827}, {
-        player: 'Harry P.',
-        contact: 9131071991
-    }]
-);
+// 📌 TESTCASE:
+console.log(playerManager('John Doe, 8167238327, Jane Doe, 8163723827'),[{player: 'John Doe', contact: 8167238327}, {player: 'Jane Doe', contact: 8163723827}]);
+console.log(playerManager('John Doe, 8167238327, Jane Doe, 8163723827, Harry P., 9131071991'), {player: 'John Doe', contact: 8167238327}, {player: 'Jane Doe', contact: 8163723827}, {player: 'Harry P.', contact: 9131071991}]);
 console.log(playerManager(''), []);
 console.log(playerManager(null), []);
